6e475c99b360c4b63fb99f07857e28f6e70325a4
Paul <gafgafgarion05@gmail.com>
2021-08-18 14:56:22 +0300

    init

---
+++ 8 kyu altERnaTIng cAsE = ALTerNAtiNG CaSe.py
@@ -0,0 +1,25 @@
+def to_alternating_case(string):
+    s = ''
+
+    for i in string:
+        if i == i.isupper():
+            s += i.islower()
+        elif i == i.islower():
+            s += i.isupper()
+        else:
+            s += i
+
+    return s
+
+
+
+
+print(to_alternating_case("hello world"))
+print(to_alternating_case("HELLO WORLD"))
+print(to_alternating_case("hello WORLD"))
+print(to_alternating_case("HeLLo WoRLD"))
+print(to_alternating_case("12345"))
+print(to_alternating_case("1a2b3c4d5e"))
+print(to_alternating_case("String.prototype.toAlternatingCase"))
+print(to_alternating_case(to_alternating_case("Hello World")))
+

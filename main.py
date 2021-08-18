a15665b4aa2270d47f7e554152223c3b91877dbc
Paul <gafgafgarion05@gmail.com>
2021-08-18 14:13:04 +0300

    add 1 function(def in Python): 1)multiply and it's call in print

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ def arithmetic(a, b, operator):
     return {
         'plus': a + b,
         'minus': a - b,
+        'multiply': a * b,
     }[operator]
 
 
 print(arithmetic(5, 6, 'plus'))
 print(arithmetic(50, 12, 'minus'))
+print(arithmetic(5, 5, 'multiply'))
 

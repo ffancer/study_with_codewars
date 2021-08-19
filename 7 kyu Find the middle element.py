c62e46ef92774364a0842172a6cc1b3afd651f6e
Paul <gafgafgarion05@gmail.com>
2021-08-19 10:33:39 +0300

    find the index of middle element

--- 7 kyu Find the middle element.py
+++ 7 kyu Find the middle element.py
@@ -2,6 +2,12 @@ def gimme(input_array):
     min_elem = min(input_array)
     max_elem = max(input_array)
 
+    for i in input_array:
+        if i == min_elem or i == max_elem:
+            continue
+        else:
+            return input_array.index(i)
+
 
 
 

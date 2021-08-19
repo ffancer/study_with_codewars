a759ab886715f6e868c689379c6649c54fd20233
Paul <gafgafgarion05@gmail.com>
2021-08-19 10:52:31 +0300

    add third and fourth solutions forgot about isalpha() method

--- 7 kyu Simple Fun #176 Reverse Letter.py
+++ 7 kyu Simple Fun #176 Reverse Letter.py
@@ -1,4 +1,5 @@
 def reverse_letter(string):
+    # first
     # s = ''
     #
     # for i in string:
@@ -7,8 +8,15 @@ def reverse_letter(string):
     #
     # return s[::-1]
 
-    s = [i for i in string if 'z' >= i >= 'a']
-    return ''.join(s)[::-1]
+    # second
+    # s = [i for i in string if 'z' >= i >= 'a']
+    # return ''.join(s)[::-1]
+
+    # third
+    #     return ''.join([i for i in string if 'z' >= i >= 'a'])[::-1]
+
+    # fourth:
+    return ''.join([i for i in string if i.isalpha()])[::-1]
 
 
 print(reverse_letter("krishan"))

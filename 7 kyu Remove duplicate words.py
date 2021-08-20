1f27e5b64e118e65cb02b625cbe674f15de675aa
Paul <gafgafgarion05@gmail.com>
2021-08-20 11:34:56 +0300

    add second solution

--- 7 kyu Remove duplicate words.py
+++ 7 kyu Remove duplicate words.py
@@ -1,13 +1,18 @@
 def remove_duplicate_words(s):
+    # first:
+    # lst = []
+    # s = s.split()
+    #
+    # for i in s:
+    #     if i not in lst:
+    #         lst.append(i)
+    #
+    # return ' '.join(lst)
+
+    # second:
     lst = []
-    s = s.split()
-
-    for i in s:
-        if i not in lst:
-            lst.append(i)
-
+    [lst.append(i) for i in s.split() if i not in lst]
     return ' '.join(lst)
-
 
 print(remove_duplicate_words("alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"))  # "alpha beta gamma delta"
 print(remove_duplicate_words("my cat is my cat fat"))  # "my cat is fat"

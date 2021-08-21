5bb74ebb3299d3d8b33b327035f15509ed761653
Paul <gafgafgarion05@gmail.com>
2021-08-21 10:45:34 +0300

    init

---
+++ 7 kyu Are the numbers in order.py
@@ -0,0 +1,27 @@
+def in_asc_order(arr):
+    # random_ is not allowed
+    return bool #(True or false)
+
+
+
+
+# Array of 2 integers
+arr = [1, 2]
+print(in_asc_order(arr))
+
+arr = [2, 1]
+print(in_asc_order(arr))
+
+# Array of 3 integers
+arr = [1, 2, 3]
+print(in_asc_order(arr))
+
+arr = [1, 3, 2]
+print(in_asc_order(arr))
+
+# More complex cases
+arr = [1,4,13,97,508,1047,20058]
+print(in_asc_order(arr))
+
+arr = [56,98,123,67,742,1024,32,90969]
+print(in_asc_order(arr))

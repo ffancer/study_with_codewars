f65efc7f848160b4efe668bd714b84a7c12d4d47
Paul <gafgafgarion05@gmail.com>
2021-08-21 12:28:26 +0300

    init

---
+++ 7 kyu Sorted yes no how.py
@@ -0,0 +1,11 @@
+def is_sorted_and_how(arr):
+
+    pass
+
+
+
+
+
+print(is_sorted_and_how([1, 2]), 'yes, ascending')
+print(is_sorted_and_how([15, 7, 3, -8]), 'yes, descending')
+print(is_sorted_and_how([4, 2, 30]), 'no')

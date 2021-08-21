97991fcc2d7210279aa7bdf92e05c8482cb1e0b1
Paul <gafgafgarion05@gmail.com>
2021-08-21 09:49:35 +0300

    second solution, more compact

--- 7 kyu Find the capitals.py
+++ 7 kyu Find the capitals.py
@@ -1,13 +1,16 @@
 def capitals(word):
     # first:
-    def capitals(word):
-        lst = []
+    # def capitals(word):
+    #     lst = []
+    #
+    #     for i in range(len(word)):
+    #         if word[i].istitle():
+    #             lst.append(i)
+    #
+    #     return lst
 
-        for i in range(len(word)):
-            if word[i].istitle():
-                lst.append(i)
-
-        return lst
+    # second:
+    return [i for i in range(len(word)) if word[i].istitle()]
 
 
 print(capitals('CodEWaRs'))

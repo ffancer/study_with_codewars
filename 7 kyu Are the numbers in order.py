a95280d468156b286b79d33d2295f57490e04b27
Paul <gafgafgarion05@gmail.com>
2021-08-21 10:46:53 +0300

    first solution

--- 7 kyu Are the numbers in order.py
+++ 7 kyu Are the numbers in order.py
@@ -1,8 +1,7 @@
 def in_asc_order(arr):
-    # random_ is not allowed
-    return bool #(True or false)
+    lst_sort = sorted(arr)
 
-
+    return lst_sort == arr
 
 
 # Array of 2 integers
@@ -20,8 +19,8 @@ arr = [1, 3, 2]
 print(in_asc_order(arr))
 
 # More complex cases
-arr = [1,4,13,97,508,1047,20058]
+arr = [1, 4, 13, 97, 508, 1047, 20058]
 print(in_asc_order(arr))
 
-arr = [56,98,123,67,742,1024,32,90969]
+arr = [56, 98, 123, 67, 742, 1024, 32, 90969]
 print(in_asc_order(arr))

642b6e4f690daaaee63fb03e2dc54af9e696ce59
Paul <gafgafgarion05@gmail.com>
2021-08-21 12:32:20 +0300

    first solution, all tests are done

--- 7 kyu Sorted yes no how.py
+++ 7 kyu Sorted yes no how.py
@@ -1,11 +1,14 @@
 def is_sorted_and_how(arr):
+    answer = 'no'
 
-    pass
+    if arr == sorted(arr):
+        answer = 'yes, ascending'
+    elif arr == sorted(arr, reverse=True):
+        answer = 'yes, descending'
+
+    return answer
 
 
-
-
-
-print(is_sorted_and_how([1, 2]), 'yes, ascending')
-print(is_sorted_and_how([15, 7, 3, -8]), 'yes, descending')
-print(is_sorted_and_how([4, 2, 30]), 'no')
+print(is_sorted_and_how([1, 2]))  # 'yes, ascending'
+print(is_sorted_and_how([15, 7, 3, -8]))  # 'yes, descending'
+print(is_sorted_and_how([4, 2, 30]))  # no

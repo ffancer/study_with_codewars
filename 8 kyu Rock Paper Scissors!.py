e58711ec11155a89c10ad5d68b3ee336d3c1417c
Paul <gafgafgarion05@gmail.com>
2021-08-24 18:32:56 +0300

    first version

--- 8 kyu Rock Paper Scissors!.py
+++ 8 kyu Rock Paper Scissors!.py
@@ -1,5 +1,14 @@
 def rps(p1, p2):
+    answer = ""
 
+    if p1 == 'rock' and p2 == 'scissors' or p1 == 'paper' and p2 == 'rock' or p1 == 'scissors' and p2 == 'paper':
+        answer = 'Player 1 won!'
+    elif p1 == 'scissors' and p2 == 'rock' or p1 == 'rock' and p2 == 'paper' or p1 == 'paper' and p2 == 'scissors':
+        answer = "Player 2 won!"
+    else:
+        answer = 'Draw!'
+
+    return answer
 
 
 

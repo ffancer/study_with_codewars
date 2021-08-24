d5b9021afd470b2f627003318a8e26a1fce355f2
Paul <gafgafgarion05@gmail.com>
2021-08-24 17:58:07 +0300

    new var 'j' and solution is ready

--- 8 kyu Total amount of points.py
+++ 8 kyu Total amount of points.py
@@ -1,10 +1,10 @@
 def points(games):
 	total = 0
 	for i in games:
-		i.split(':')
-		if i[0] > i[1]:
+		j = i.split(':')
+		if j[0] > j[1]:
 			total += 3
-		elif i[0] == i[1]:
+		elif j[0] == j[1]:
 			total += 1
 	return total
 

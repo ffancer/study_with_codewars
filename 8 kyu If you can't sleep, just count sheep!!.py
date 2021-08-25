2b54443533d84ed4acd619a66ff01de7ae9c3619
Paul <gafgafgarion05@gmail.com>
2021-08-25 16:24:00 +0300

    init

---
+++ 8 kyu If you can't sleep, just count sheep!!.py
@@ -0,0 +1,10 @@
+def count_sheep(n):
+    # your code
+
+
+
+
+
+print(count_sheep(1), "1 sheep...");
+print(count_sheep(2), "1 sheep...2 sheep...")
+print(count_sheep(3), "1 sheep...2 sheep...3 sheep...")

a05fe9ddb690fa606269e68c1bfe3e4f199257c3
Paul <gafgafgarion05@gmail.com>
2021-08-26 11:26:32 +0300

    init

---
+++ 8 kyu Simple multiplication.py
@@ -0,0 +1,9 @@
+def simple_multiplication(number):
+# Your code goes here
+
+
+print(simple_multiplication(2), 16)
+print(simple_multiplication(1), 9)
+print(simple_multiplication(8), 64)
+print(simple_multiplication(4), 32)
+print(simple_multiplication(5), 45)

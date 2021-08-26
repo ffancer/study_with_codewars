80fa61caf2cb5a8d5e337166c086b131941b84e1
Paul <gafgafgarion05@gmail.com>
2021-08-26 11:11:16 +0300

    init

---
+++ 8 kyu Welcome!.py
@@ -0,0 +1,12 @@
+def greet(language):
+    #your code here
+
+
+
+
+
+print(greet('english'), 'Welcome')
+print(greet('dutch'), 'Welkom')
+print(greet('IP_ADDRESS_INVALID'), 'Welcome')
+print(greet(''), 'Welcome')
+print(greet(2), 'Welcome')

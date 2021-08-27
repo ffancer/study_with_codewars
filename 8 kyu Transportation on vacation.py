e919d5393439e52ab2b76e4a9d3a5ac8eea9f361
Paul <gafgafgarion05@gmail.com>
2021-08-27 17:07:04 +0300

    1 line solution

--- 8 kyu Transportation on vacation.py
+++ 8 kyu Transportation on vacation.py
@@ -1,12 +1,14 @@
 def rental_car_cost(d):
-    cnt = d * 40
+    # first
+    # cnt = d * 40
+    # if d >= 7:
+    #     cnt -= 50
+    # elif 7 > d > 3:
+    #     cnt -= 20
+    # return cnt
 
-    if d >= 7:
-        cnt -= 50
-    elif 7 > d > 3:
-        cnt -= 20
-
-    return cnt
+    # second
+    return d * 40 - 50 if d >= 7 else (d * 40 - 20 if 7 > d > 3 else d * 40)
 
 
 print(rental_car_cost(1), 40)

038907f930024fd7e2b2fd5fae774edd88c9e0df
Paul <gafgafgarion05@gmail.com>
2021-08-27 17:26:11 +0300

    second solution

--- 8 kyu Removing Elements.py
+++ 8 kyu Removing Elements.py
@@ -1,12 +1,15 @@
 def remove_every_other(my_list):
     # first
-    lst = []
+    # lst = []
+    #
+    # for i in range(len(my_list)):
+    #     if i % 2 == 0:
+    #         lst.append(my_list[i])
+    #
+    # return lst
 
-    for i in range(len(my_list)):
-        if i % 2 == 0:
-            lst.append(my_list[i])
-
-    return lst
+    # second
+    return my_list[::2]
 
 
 

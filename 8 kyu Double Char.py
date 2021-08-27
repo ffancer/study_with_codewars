d1e5cc04b82c8b5381f91e31297d49960e88d1f3
Paul <gafgafgarion05@gmail.com>
2021-08-27 16:23:54 +0300

    init

---
+++ 8 kyu Double Char.py
@@ -0,0 +1,7 @@
+def double_char(s):
+    pass
+
+
+print(double_char("String"),"SSttrriinngg")
+print(double_char("Hello World"),"HHeelllloo  WWoorrlldd")
+print(double_char("1234!_ "),"11223344!!__  ")

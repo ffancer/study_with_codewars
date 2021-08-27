69da8329113a7105931be1a16954cfe3ac104c30
Paul <gafgafgarion05@gmail.com>
2021-08-27 17:25:53 +0300

    first solution

--- 8 kyu Removing Elements.py
+++ 8 kyu Removing Elements.py
@@ -1,12 +1,17 @@
 def remove_every_other(my_list):
-    # Your code here!
-    pass
+    # first
+    lst = []
+
+    for i in range(len(my_list)):
+        if i % 2 == 0:
+            lst.append(my_list[i])
+
+    return lst
 
 
-print(remove_every_other(['Hello', 'Goodbye', 'Hello Again']),
-                   ['Hello', 'Hello Again'])
-print(remove_every_other([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
-                   [1, 3, 5, 7, 9])
-print(remove_every_other([[1, 2]]), [[1, 2]])
-print(remove_every_other([['Goodbye'], {'Great': 'Job'}]),
-                   [['Goodbye']])
+
+
+print(remove_every_other(['Hello', 'Goodbye', 'Hello Again']))
+print(remove_every_other([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+print(remove_every_other([[1, 2]]))
+print(remove_every_other([['Goodbye'], {'Great': 'Job'}]))

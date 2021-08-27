1e482b71b88333681c6a1cc0b7fc3b320dc0f2a0
Paul <gafgafgarion05@gmail.com>
2021-08-27 16:12:50 +0300

    third solution with import math

--- 8 kyu Count Odd Numbers below n.py
+++ 8 kyu Count Odd Numbers below n.py
@@ -1,3 +1,5 @@
+from math import floor
+
 def odd_count(n):
     # first solution, but it's slow with huge num-s:
     # cnt = 0
@@ -9,7 +11,11 @@ def odd_count(n):
     # return cnt
 
     # second solution
-    return len([i for i in range(1, n+1) if i > 0 and i % 2 == 0])
+    # return len([i for i in range(1, n+1) if i > 0 and i % 2 == 0])
+
+    # third solution
+    return floor(n / 2)
+
 
 
 print(odd_count(15), 7)

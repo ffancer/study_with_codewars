77a4d320a48141831523eabe79d96b5e998a2898
Paul <gafgafgarion05@gmail.com>
2021-08-27 16:26:57 +0300

    second solution

--- 8 kyu Double Char.py
+++ 8 kyu Double Char.py
@@ -1,10 +1,14 @@
 def double_char(s):
-    # first
-    s2 = ''
-    for i in s:
-        s2 += i * 2
+    # first:
+    # s2 = ''
+    #
+    # for i in s:
+    #     s2 += i * 2
+    #
+    # return s2
 
-    return s2
+    # second:
+    return ''.join([i*2 for i in s])
 
 
 print(double_char("String"))

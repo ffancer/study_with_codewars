ef1e26163fdb8914d7aae2c40ffcf502269a4e3d
Paul <gafgafgarion05@gmail.com>
2021-08-27 16:41:06 +0300

    first solution

--- 8 kyu Transportation on vacation.py
+++ 8 kyu Transportation on vacation.py
@@ -1,7 +1,12 @@
 def rental_car_cost(d):
-    pass
+    cnt = d * 40
 
+    if d >= 7:
+        cnt -= 50
+    elif 7 > d > 3:
+        cnt -= 20
 
+    return cnt
 
 
 print(rental_car_cost(1), 40)

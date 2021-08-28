438549ad569731d08fc9b6afd76347dd5e4d5c1e
Paul <gafgafgarion05@gmail.com>
2021-08-28 13:11:20 +0300

    second solution

--- 8 kyu Correct the mistakes of the character recognition software.py
+++ 8 kyu Correct the mistakes of the character recognition software.py
@@ -1,6 +1,21 @@
 def correct(s):
-    return s.replace('0', 'O').replace('1', 'I').replace('5', 'S')
+    # first
+    # return s.replace('0', 'O').replace('1', 'I').replace('5', 'S')
 
+    # second
+    answer = ''
+
+    for i in s:
+        if i == '0':
+            answer += 'O'
+        elif i == '1':
+            answer += 'I'
+        elif i == '5':
+            answer += 'S'
+        else:
+            answer += i
+
+    return answer
 
 print(correct("L0ND0N"), "LONDON")
 print(correct("DUBL1N"), "DUBLIN")

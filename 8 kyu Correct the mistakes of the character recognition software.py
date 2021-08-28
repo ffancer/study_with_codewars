5aadb3805f1eb5c3fa170757600154d5e171242f
Paul <gafgafgarion05@gmail.com>
2021-08-28 12:44:38 +0300

    init

---
+++ 8 kyu Correct the mistakes of the character recognition software.py
@@ -0,0 +1,9 @@
+def correct(s):
+    pass
+
+
+print(correct("L0ND0N"), "LONDON")
+print(correct("DUBL1N"), "DUBLIN")
+print(correct("51NGAP0RE"), "SINGAPORE")
+print(correct("BUDAPE5T"), "BUDAPEST")
+print(correct("PAR15"), "PARIS")

bc6ef5e4c107a2156d9766f5d699852457b28789
Paul <gafgafgarion05@gmail.com>
2021-08-28 13:42:01 +0300

    init

---
+++ 8 kyu All Star Code Challenge #18.py
@@ -0,0 +1,11 @@
+def str_count(strng, letter):
+
+
+# Your code here ;)
+
+
+print(str_count('hello', 'l'), 2)
+print(str_count('hello', 'e'), 1)
+print(str_count('codewars', 'c'), 1)
+print(str_count('ggggg', 'g'), 5)
+print(str_count('hello world', 'o'), 2)

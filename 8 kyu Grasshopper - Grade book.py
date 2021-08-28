cf52d1dd9feddb7d17d423a7909d62fa77a5f7d6
Paul <gafgafgarion05@gmail.com>
2021-08-28 12:40:50 +0300

    first solution

--- 8 kyu Grasshopper - Grade book.py
+++ 8 kyu Grasshopper - Grade book.py
@@ -1,6 +1,16 @@
 def get_grade(s1, s2, s3):
-    # Code here
-    return "F"
+    s = (s1 + s2 + s3) / 3
+
+    if 90 <= s <= 100:
+        return 'A'
+    elif 80 <= s < 90:
+        return 'B'
+    elif 70 <= s < 80:
+        return 'C'
+    elif 60 <= s < 70:
+        return 'D'
+    elif 0 <= s < 60:
+        return 'F'
 
 
 

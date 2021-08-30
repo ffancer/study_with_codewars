65c342ebf01ad38743b71ad12141fc9f17d3a6db
Paul <gafgafgarion05@gmail.com>
2021-08-30 19:32:52 +0300

    init

---
+++ 8 kyu Well of Ideas - Easy Version.py
@@ -0,0 +1,8 @@
+def well(x):
+    #your code here
+    return ''
+
+
+print(well(['bad', 'bad', 'bad']), 'Fail!')
+print(well(['good', 'bad', 'bad', 'bad', 'bad']), 'Publish!')
+print(well(['good', 'bad', 'bad', 'bad', 'bad', 'good', 'bad', 'bad', 'good']), 'I smell a series!')

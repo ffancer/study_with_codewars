347f87249c60e6cf40a5730ff1af91c9994ad3bb
Paul <gafgafgarion05@gmail.com>
2021-08-30 17:33:15 +0300

    1st example is done

--- 8 kyu To square(root) or not to square(root).py
+++ 8 kyu To square(root) or not to square(root).py
@@ -3,8 +3,7 @@ from math import sqrt
 lst = [4, 3, 9, 7, 2, 1]
 lst_answer = []
 for i in lst:
-    print(sqrt(i))
-    if str(sqrt(i))[2] == '0':
+    if sqrt(i) ** 2 == i:
         lst_answer.append(int(sqrt(i)))
     else:
         lst_answer.append(i ** 2)

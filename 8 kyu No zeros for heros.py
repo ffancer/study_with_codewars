9897fa55977bf1cf62c6726495aa105fa020d1a4
Paul <gafgafgarion05@gmail.com>
2021-08-30 19:12:53 +0300

    init

---
+++ 8 kyu No zeros for heros.py
@@ -0,0 +1,10 @@
+def no_boring_zeros(n):
+    pass
+
+
+print(no_boring_zeros(1450), 145)
+print(no_boring_zeros(960000), 96)
+print(no_boring_zeros(1050), 105)
+print(no_boring_zeros(-1050), -105)
+print(no_boring_zeros(0), 0)
+print(no_boring_zeros(2016), 2016)

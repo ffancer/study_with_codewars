0b2e4b78c9d379e2029a9bbe56eddb8c0c2bfeaa
Paul <gafgafgarion05@gmail.com>
2021-08-31 09:52:54 +0300

    first solution

--- 8 kyu Exclamation marks series #11 Replace all vowel to exclamation mark in the sentence.py
+++ 8 kyu Exclamation marks series #11 Replace all vowel to exclamation mark in the sentence.py
@@ -1,8 +1,16 @@
 def replace_exclamation(s):
-    return s
+    s2 = ''
+
+    for i in s:
+        if i in 'aeiouAEIOU':
+            s2 += '!'
+        else:
+            s2 += i
+
+    return s2
 
 
-print(replace_exclamation("Hi!") , "H!!")
-print(replace_exclamation("!Hi! Hi!") , "!H!! H!!")
-print(replace_exclamation("aeiou") , "!!!!!")
-print(replace_exclamation("ABCDE") , "!BCD!")
+print(replace_exclamation("Hi!"), "H!!")
+print(replace_exclamation("!Hi! Hi!"), "!H!! H!!")
+print(replace_exclamation("aeiou"), "!!!!!")
+print(replace_exclamation("ABCDE"), "!BCD!")

5322ee4bbad4e6613f0b96724838b2bc4e7904e3
Paul <gafgafgarion05@gmail.com>
2021-08-31 09:34:44 +0300

    init

---
+++ 8 kyu Watermelon.py
@@ -0,0 +1,14 @@
+def divide(weight):
+    pass
+
+
+print(divide(4), True)
+print(divide(2), False)
+print(divide(5), False)
+print(divide(88), True)
+print(divide(100), True)
+print(divide(67), False)
+print(divide(90), True)
+print(divide(10), True)
+print(divide(99), False)
+print(divide(32), True)

1bf5c4c1cc3681dc0b081a2c1d7103ca23ebedb8
Paul <gafgafgarion05@gmail.com>
2021-08-31 09:16:03 +0300

    init

---
+++ 8 kyu Training JS #7if..else and ternary operator.py
@@ -0,0 +1,14 @@
+def sale_hotdogs(n):
+    pass
+
+
+print(sale_hotdogs(0),0)
+print(sale_hotdogs(1),100)
+print(sale_hotdogs(2),200)
+print(sale_hotdogs(3),300)
+print(sale_hotdogs(4),400)
+print(sale_hotdogs(5),475)
+print(sale_hotdogs(9),855)
+print(sale_hotdogs(10),900)
+print(sale_hotdogs(11),990)
+print(sale_hotdogs(100),9000)

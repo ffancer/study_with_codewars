ef1337d2407bfaaf5e5888bf0e43728fbc8473f5
Paul <gafgafgarion05@gmail.com>
2021-08-31 09:51:11 +0300

    init

---
+++ 8 kyu Exclamation marks series #11 Replace all vowel to exclamation mark in the sentence.py
@@ -0,0 +1,8 @@
+def replace_exclamation(s):
+    return s
+
+
+print(replace_exclamation("Hi!") , "H!!")
+print(replace_exclamation("!Hi! Hi!") , "!H!! H!!")
+print(replace_exclamation("aeiou") , "!!!!!")
+print(replace_exclamation("ABCDE") , "!BCD!")

4a4e6bf6fc950d24a04d7404ccb1939664a9f9dc
Paul <gafgafgarion05@gmail.com>
2021-09-01 11:44:24 +0300

    solution

--- 8 kyu Student's Final Grade.py
+++ 8 kyu Student's Final Grade.py
@@ -1,5 +1,14 @@
 def final_grade(exam, projects):
-    pass
+    answer = 0
+
+    if exam > 90 or projects > 10:
+        answer = 100
+    elif exam > 75 and projects >= 5:
+        answer = 90
+    elif exam > 50 and projects >= 2:
+        answer = 75
+
+    return answer
 
 
 print(final_grade(100, 12), 100)

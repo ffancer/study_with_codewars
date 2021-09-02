45bdabda07caa7709239214a1291c7c2e9ae794e
Paul <gafgafgarion05@gmail.com>
2021-09-02 10:01:50 +0300

    init

---
+++ 8 kyu noobCode 01 SUPERSIZE ME.... or rather, this integer!.py
@@ -0,0 +1,14 @@
+def super_size(n):
+    pass
+
+
+print(super_size(69), 96)
+print(super_size(513), 531)
+print(super_size(2017), 7210)
+print(super_size(414), 441)
+print(super_size(608719), 987610)
+print(super_size(123456789), 987654321)
+print(super_size(700000000001), 710000000000)
+print(super_size(666666), 666666)
+print(super_size(2), 2)
+print(super_size(0), 0)

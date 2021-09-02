cff96da4a7314380ccb84ba03ffd96b4aa8e2e3d
Paul <gafgafgarion05@gmail.com>
2021-09-02 17:25:07 +0300

    init

---
+++ 8 kyu Triple Trouble.py
@@ -0,0 +1,9 @@
+def triple_trouble(one, two, three):
+    pass
+
+
+print(triple_trouble("aaa", "bbb", "ccc"), "abcabcabc")
+print(triple_trouble("aaaaaa", "bbbbbb", "cccccc"), "abcabcabcabcabcabc")
+print(triple_trouble("burn", "reds", "roll"), "brrueordlnsl")
+print(triple_trouble("Bm", "aa", "tn"), "Batman")
+print(triple_trouble("LLh", "euo", "xtr"), "LexLuthor")

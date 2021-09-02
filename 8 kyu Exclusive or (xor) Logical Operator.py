c7689c711008643d92519b865b2cd4560261778d
Paul <gafgafgarion05@gmail.com>
2021-09-02 13:16:14 +0300

    init

---
+++ 8 kyu Exclusive or (xor) Logical Operator.py
@@ -0,0 +1,18 @@
+def xor(a,b):
+    pass
+
+
+print(xor(False, False), False, "False xor False == False")
+print(xor(True, False), True, "True xor False == True")
+print(xor(False, True), True, "False xor True == True")
+print(xor(True, True), False, "True xor True == False")
+print(xor(False, xor(False, False)), False)
+print(xor(xor(True, False), False), True)
+print(xor(xor(True, True), False), False)
+print(xor(True, xor(True, True)), True)
+print(xor(xor(False, False), xor(False, False)), False)
+print(xor(xor(False, False), xor(False, True)), True)
+print(xor(xor(True, False), xor(False, False)), True)
+print(xor(xor(True, False), xor(True, False)), False)
+print(xor(xor(True, True), xor(True, False)), True)
+print(xor(xor(True, xor(True, True)), xor(xor(True, True), False)), True)

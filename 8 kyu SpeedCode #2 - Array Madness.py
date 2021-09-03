d1aa31ef5f7fbacb29722e093f7ac7b9ff5014e0
Paul <gafgafgarion05@gmail.com>
2021-09-03 16:50:23 +0300

    solution with 100%

--- 8 kyu SpeedCode #2 - Array Madness.py
+++ 8 kyu SpeedCode #2 - Array Madness.py
@@ -1,5 +1,5 @@
-def array_madness(a,b):
-    return sum(a) > sum(b)
+def array_madness(a, b):
+    return sum([i ** 2 for i in a]) > sum([j ** 3 for j in b])
 
 
 print(array_madness([4, 5, 6], [1, 2, 3]), True)

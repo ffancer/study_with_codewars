e0422d075c83c88bc1b680006f190332ed88bd99
Paul <gafgafgarion05@gmail.com>
2021-09-04 16:14:00 +0300

    init

---
+++ 6 kyu Dubstep.py
@@ -0,0 +1,7 @@
+def song_decoder(song):
+    pass
+
+
+print(song_decoder("AWUBBWUBC"), "A B C", "WUB should be replaced by 1 space")
+print(song_decoder("AWUBWUBWUBBWUBWUBWUBC"), "A B C", "multiples WUB should be replaced by only 1 space")
+print(song_decoder("WUBAWUBBWUBCWUB"), "A B C", "heading or trailing spaces should be removed")

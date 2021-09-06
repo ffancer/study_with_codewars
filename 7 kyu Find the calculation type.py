2cba9b1d801f43ae558d3429990e6e501a7624f4
Paul <gafgafgarion05@gmail.com>
2021-09-06 17:21:31 +0300

    init

---
+++ 7 kyu Find the calculation type.py
@@ -0,0 +1,8 @@
+def calc_type(a, b, res):
+    return # operation used
+
+
+print(calc_type(1, 2, 3), "addition")
+print(calc_type(10, 5, 5), "subtraction")
+print(calc_type(10, 4, 40), "multiplication")
+print(calc_type(9, 5, 1.8), "division")

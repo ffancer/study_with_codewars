ea3887ad7da3a1075aa7280ff482531342dbfc2b
Paul <gafgafgarion05@gmail.com>
2021-09-06 19:17:31 +0300

    savepoint

--- 4 kyu Next bigger number with the same digits.py
+++ 4 kyu Next bigger number with the same digits.py
@@ -1,10 +1,18 @@
+import itertools
+
+
 def next_bigger(n):
+    # n = list(str(n))
+    # n = [int(i) for i in n]
+    # first_digit = str(n[0])
+    # n = n[1::]
+    # n.sort(reverse=True)
+    # return int(first_digit + ''.join([str(i) for i in n]))
     n = list(str(n))
     n = [int(i) for i in n]
-    first_digit = str(n[0])
-    n = n[1::]
-    n.sort(reverse=True)
-    return int(first_digit + ''.join([str(i) for i in n]))
+    combinations = list(itertools.permutations(n))
+    print(combinations)
+    # return n
 
 print(next_bigger(12),21)
 print(next_bigger(513),531)

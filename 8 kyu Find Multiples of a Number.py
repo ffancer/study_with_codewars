4e05c389a02f35f7194ba881c954c253320bf2ee
Paul <gafgafgarion05@gmail.com>
2021-09-07 17:56:50 +0300

    1 line solution

--- 8 kyu Find Multiples of a Number.py
+++ 8 kyu Find Multiples of a Number.py
@@ -1,10 +1,6 @@
 def find_multiples(integer, limit):
-    lst = []
-    for i in range(1, limit+1):
-        if i % integer == 0:
-            lst.append(i)
-
-    return lst
+    #second
+    return [i for i in range(1, limit+1) if i % integer == 0]
 
 
 print(find_multiples(5, 25), [5, 10, 15, 20, 25])

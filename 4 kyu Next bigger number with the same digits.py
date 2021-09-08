a90cd52914cc3341c9b209b103fde852a9bbabf9
Paul <gafgafgarion05@gmail.com>
2021-09-08 10:36:45 +0300

    use map and other to see all digits of n in list

--- 4 kyu Next bigger number with the same digits.py
+++ 4 kyu Next bigger number with the same digits.py
@@ -1,17 +1,14 @@
 def next_bigger(n):
     answer = -1
+    i, j = 0, 1
 
-    if len(str(n)) == 2:
-        if int(str(n)[0]+str(n)[1]) > int(str(n)[1]+str(n)[0]):
-            answer = int(str(n)[0]+str(n)[1])
-        else:
-            answer = int(str(n)[1]+str(n)[0])
+    lst = list(map(int, str(n)))
 
-    return answer
+    return lst
 
 
-
-print(next_bigger(12),21)
+print(next_bigger(12), 21)
+print(next_bigger(1228881758))
 # print(next_bigger(513),531)
 # print(next_bigger(2017),2071)
 # print(next_bigger(414),441)

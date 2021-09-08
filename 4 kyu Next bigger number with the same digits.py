08a66ec024c93d6221b7d8de83791a1ec83b5018
Paul <gafgafgarion05@gmail.com>
2021-09-08 14:57:54 +0300

    4 try, again 'bubble sort'

--- 4 kyu Next bigger number with the same digits.py
+++ 4 kyu Next bigger number with the same digits.py
@@ -1,21 +1,35 @@
-def next_bigger(n):
-    n = list(str(n))
-    index_to_swap = len(n) - 1
-    for i in range(len(n) - 2, -1, -1):
-        if n[i] < n[index_to_swap]:
-            n[index_to_swap], n[i] = n[i], n[index_to_swap]
-            break
-        else:
-            index_to_swap = i
-    else:
-        print('its already largest')
+import math
 
-    return int(''.join(n))
 
+def next_bigger(number):
+    lst = []
+    j = 0
+
+    while number != 0:
+        lst.append(number % 10)
+        number = math.trunc(number/10)
+        j += 1
+
+    lst = lst[::-1]
+
+    for i in range(j - 1, -1, -1):
+
+        for k in range(i - 1, -1, -1):
+            if lst[i] > lst[k]:
+                lst[i], lst[k] = lst[k], lst[i]
+                lst = lst[:k + 1] + sorted(lst[k+1:])
+                cnt = 1
+                result = 0
+
+                for item in range(j):
+                    result += lst[item] * (10 ** (j - cnt))
+                    cnt += 1
+
+                return result
 
 print(next_bigger(12), 21)
 print(next_bigger(1228881758))
-# print(next_bigger(513),531)
-# print(next_bigger(2017),2071)
-# print(next_bigger(414),441)
-# print(next_bigger(144),414)
+print(next_bigger(513),531)
+print(next_bigger(2017),2071)
+print(next_bigger(414),441)
+print(next_bigger(144),414)

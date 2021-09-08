1f148aa4e15a9dec9fc90c6aaeec6ec3ffb3a12a
Paul <gafgafgarion05@gmail.com>
2021-09-08 17:33:02 +0300

    final solution

--- 4 kyu Next bigger number with the same digits.py
+++ 4 kyu Next bigger number with the same digits.py
@@ -1,32 +1,18 @@
-import math
+def next_bigger(n):
+    n = list(str(n))
+    i = len(n) - 2
 
+    while i >= 0:
+        if n[i] < n[i + 1]:
+            swap_group = n[i:]
+            next_big_dig = min(j for j in swap_group if j > n[i])
+            swap_group.remove(next_big_dig)
+            n[i:] = [next_big_dig] + sorted(swap_group)
+            return int("".join(n))
+        i -= 1
 
-def next_bigger(number):
-    lst = []
-    j = 0
-
-    while number != 0:
-        lst.append(number % 10)
-        number = math.trunc(number/10)
-        j += 1
-
-    lst = lst[::-1]
-
-    for i in range(j - 1, -1, -1):
-
-        for k in range(i - 1, -1, -1):
-            if lst[i] > lst[k]:
-                lst[i], lst[k] = lst[k], lst[i]
-                lst = lst[:k + 1] + sorted(lst[k+1:])
-                cnt = 1
-                result = 0
-
-                for item in range(j):
-                    result += lst[item] * (10 ** (j - cnt))
-                    cnt += 1
-
-                return result
     return -1
+
 
 print(next_bigger(12), 21)
 print(next_bigger(1228881758))

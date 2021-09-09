138e66984a4f2c38667de8686d4c6980bf3ee68f
Paul <gafgafgarion05@gmail.com>
2021-09-09 11:05:03 +0300

    init

---
+++ 8 kyu Grasshopper - Debug sayHello.py
@@ -0,0 +1,8 @@
+def say_hello(name):
+"Hello"
+
+print(say_hello('Mr. Spock'), 'Hello, Mr. Spock')
+print(say_hello('Captain Kirk'), 'Hello, Captain Kirk')
+print(say_hello('Liutenant Uhura'), 'Hello, Liutenant Uhura')
+print(say_hello('Dr. McCoy'), 'Hello, Dr. McCoy')
+print(say_hello('Mr. Scott'), 'Hello, Mr. Scott')

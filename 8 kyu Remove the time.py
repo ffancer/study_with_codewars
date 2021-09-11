e839be528e5388581a09f46b1272ed58e148eaea
Paul <gafgafgarion05@gmail.com>
2021-09-11 11:32:14 +0300

    solution

--- 8 kyu Remove the time.py
+++ 8 kyu Remove the time.py
@@ -1,5 +1,13 @@
 def shorten_to_date(long_date):
-    #your code here
+    s = ''
+
+    for i in range(len(long_date)):
+        if long_date[i] == ',':
+            s += long_date[:i]
+
+    return s
+
+
 
 print(shorten_to_date("Monday February 2, 8pm"), "Monday February 2")
 print(shorten_to_date("Tuesday May 29, 8pm"), "Tuesday May 29")

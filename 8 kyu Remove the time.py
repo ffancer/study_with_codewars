e20c3075d9828f63f92f693b941ef02d237cf68f
Paul <gafgafgarion05@gmail.com>
2021-09-11 11:23:41 +0300

    init

---
+++ 8 kyu Remove the time.py
@@ -0,0 +1,8 @@
+def shorten_to_date(long_date):
+    #your code here
+
+print(shorten_to_date("Monday February 2, 8pm"), "Monday February 2")
+print(shorten_to_date("Tuesday May 29, 8pm"), "Tuesday May 29")
+print(shorten_to_date("Wed September 1, 3am"), "Wed September 1")
+print(shorten_to_date("Friday May 2, 9am"), "Friday May 2")
+print(shorten_to_date("Tuesday January 29, 10pm"), "Tuesday January 29")

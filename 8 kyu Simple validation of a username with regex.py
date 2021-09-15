ceba4ea707b2224b132c5e6f60ae2e0c469c2dca
Paul <gafgafgarion05@gmail.com>
2021-09-15 18:21:20 +0300

    just 1 mistake from 50 tests

--- 8 kyu Simple validation of a username with regex.py
+++ 8 kyu Simple validation of a username with regex.py
@@ -2,22 +2,22 @@ def validate_usr(username):
 
     if len(username) >= 17 or len(username) <= 3:
         return False
-    if username.isupper():
-        return False
-    if ' ' in username:
-        return False
 
+    for i in username:
+        if i.isupper() or i == ' ':
+            return False
     return True
 
 
 
-print(validate_usr('asddsa'), True)
-print(validate_usr('a'), False)
-print(validate_usr('Hass'), False)
-print(validate_usr('Hasd_12assssssasasasasasaasasasasas'), False)
-print(validate_usr(''), False)
-print(validate_usr('____'), True)
-print(validate_usr('012'), False)
-print(validate_usr('p1pp1'), True)
-print(validate_usr('asd43 34'), False)
-print(validate_usr('asd43_34'), True)
+# print(validate_usr('asddsa'), True)
+# print(validate_usr('a'), False)
+# print(validate_usr('Hass'), False)
+# print(validate_usr('Hasd_12assssssasasasasasaasasasasas'), False)
+# print(validate_usr(''), False)
+# print(validate_usr('____'), True)
+# print(validate_usr('012'), False)
+# print(validate_usr('p1pp1'), True)
+# print(validate_usr('asd43 34'), False)
+# print(validate_usr('asd43_34'), True)
+print(validate_usr('pRt_286ji6u9Il_M'), False)

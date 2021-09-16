a34ae835f05e11fd9a41e3cd52773804530f267e
Paul <gafgafgarion05@gmail.com>
2021-09-16 13:01:30 +0300

    init

---
+++ 8 kyu Is it a palindrome.py
@@ -0,0 +1,12 @@
+# 8 kyu Is it a palindrome?
+def is_palindrome(s):
+    return
+
+
+print(is_palindrome('a'), True)
+print(is_palindrome('aba'), True)
+print(is_palindrome('Abba'), True)
+print(is_palindrome('malam'), True)
+print(is_palindrome('walter'), False)
+print(is_palindrome('kodok'), True)
+print(is_palindrome('Kasue'), False)

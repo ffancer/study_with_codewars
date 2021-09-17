ffc39c7f54b65f60f69cfa3866e17a2d295d2f35
Paul <gafgafgarion05@gmail.com>
2021-09-17 17:11:20 +0300

    init

---
+++ 8 kyu Exclamation marks series #2 Remove all exclamation marks from the end of sentence.py
@@ -0,0 +1,13 @@
+# 8 kyu
+# Exclamation marks series #2: Remove all exclamation marks from the end of sentence
+
+def remove(s):
+    pass
+
+
+print("Hi!"  "Hi")
+print("Hi!!!", "Hi")
+print("!Hi", "!Hi")
+print("!Hi!", "!Hi")
+print("Hi! Hi!", "Hi! Hi")
+print("Hi", "Hi")

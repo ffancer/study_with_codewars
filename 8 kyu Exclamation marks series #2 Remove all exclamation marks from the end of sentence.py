4f638848ceab8f817eca01cdbd377da63c884d6a
Paul <gafgafgarion05@gmail.com>
2021-09-17 17:24:19 +0300

    replace all '!', need only in the end of word

--- 8 kyu Exclamation marks series #2 Remove all exclamation marks from the end of sentence.py
+++ 8 kyu Exclamation marks series #2 Remove all exclamation marks from the end of sentence.py
@@ -2,7 +2,7 @@
 # Exclamation marks series #2: Remove all exclamation marks from the end of sentence
 
 def remove(s):
-   print(s.index(s.endswith('!')))
+   return s.replace('!','')
 
 
 print(remove("Hi!"))

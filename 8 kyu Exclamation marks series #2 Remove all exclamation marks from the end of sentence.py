89a6223c48856bddb7bdb2cb30f9d1bfddd8abc4
Paul <gafgafgarion05@gmail.com>
2021-09-17 17:33:04 +0300

    2 best practices

--- 8 kyu Exclamation marks series #2 Remove all exclamation marks from the end of sentence.py
+++ 8 kyu Exclamation marks series #2 Remove all exclamation marks from the end of sentence.py
@@ -1,8 +1,21 @@
 # 8 kyu
 # Exclamation marks series #2: Remove all exclamation marks from the end of sentence
 
+
+# best practice 1:
 def remove(s):
-    return s.rstrip('!')
+    while s and s[-1] == "!":
+        s = s[:-1]
+    return s
+
+
+# best practice 2:
+def remove(s):
+    i = len(s) - 1
+
+    while s[i] == '!':
+        i = i - 1
+    return s[:i + 1]
 
 
 print(remove("Hi!"))

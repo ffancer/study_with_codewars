d5318699dcf48675dc514dae2d9da50c17741609
Paul <gafgafgarion05@gmail.com>
2021-09-17 17:30:54 +0300

    solution

--- 8 kyu Exclamation marks series #2 Remove all exclamation marks from the end of sentence.py
+++ 8 kyu Exclamation marks series #2 Remove all exclamation marks from the end of sentence.py
@@ -2,7 +2,7 @@
 # Exclamation marks series #2: Remove all exclamation marks from the end of sentence
 
 def remove(s):
-   return s.replace('!','')
+    return s.rstrip('!')
 
 
 print(remove("Hi!"))

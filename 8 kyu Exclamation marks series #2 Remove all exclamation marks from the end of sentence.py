3f25c0ca560a110d000b0d4fcaf5b262dd242789
Paul <gafgafgarion05@gmail.com>
2021-09-17 17:14:28 +0300

    too much troubles

--- 8 kyu Exclamation marks series #2 Remove all exclamation marks from the end of sentence.py
+++ 8 kyu Exclamation marks series #2 Remove all exclamation marks from the end of sentence.py
@@ -2,12 +2,12 @@
 # Exclamation marks series #2: Remove all exclamation marks from the end of sentence
 
 def remove(s):
-    pass
+   print(s.index(s.endswith('!')))
 
 
-print("Hi!"  "Hi")
-print("Hi!!!", "Hi")
-print("!Hi", "!Hi")
-print("!Hi!", "!Hi")
-print("Hi! Hi!", "Hi! Hi")
-print("Hi", "Hi")
+print(remove("Hi!"))
+print(remove("Hi!!!"))
+print(remove("!Hi"))
+print(remove("!Hi!"))
+print(remove("Hi! Hi!"))
+print(remove("Hi"))

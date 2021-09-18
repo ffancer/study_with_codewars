90e8c2d12a5f120422384605cc63af0f7e142c1d
Paul <gafgafgarion05@gmail.com>
2021-09-18 14:42:51 +0300

    init

---
+++ 8 kyu Closest elevator.py
@@ -0,0 +1,9 @@
+def elevator(left, right, call):
+    pass # Code on! :)
+
+
+print(elevator(0, 1, 0), "left")
+print(elevator(0, 1, 1), "right")
+print(elevator(0, 1, 2), "right")
+print(elevator(0, 0, 0), "right")
+print(elevator(0, 2, 1), "right")

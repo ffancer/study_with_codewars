40d2051b4ae40a7930f3974baedabb7df0046cdc
Paul <gafgafgarion05@gmail.com>
2021-09-18 14:55:54 +0300

    try to use 'abs'

--- 8 kyu Closest elevator.py
+++ 8 kyu Closest elevator.py
@@ -2,7 +2,7 @@ def elevator(left, right, call):
     a = call - left
     b = call - right
 
-    return 'left' if a - b == 0 else 'right'
+    return abs(a), abs(b)
 
 print(elevator(0, 1, 0), "left")
 print(elevator(0, 1, 1), "right")

8e8d4830dd5587b7a466813dfd6e3de6d5489cfd
Paul <gafgafgarion05@gmail.com>
2021-09-18 14:57:34 +0300

    solution

--- 8 kyu Closest elevator.py
+++ 8 kyu Closest elevator.py
@@ -2,7 +2,7 @@ def elevator(left, right, call):
     a = call - left
     b = call - right
 
-    return abs(a), abs(b)
+    return 'left' if abs(a) < abs(b) else 'right'
 
 print(elevator(0, 1, 0), "left")
 print(elevator(0, 1, 1), "right")

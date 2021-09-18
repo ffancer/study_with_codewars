d230028b0dbecef9e6e96d82df67d0d2b678162f
Paul <gafgafgarion05@gmail.com>
2021-09-18 09:48:24 +0300

    init

---
+++ 8 kyu validate code with simple regex.py
@@ -0,0 +1,8 @@
+def validate_code(code):
+    #your code here
+
+print(validate_code(123), True)
+print(validate_code(248), True)
+print(validate_code(8), False)
+print(validate_code(321), True)
+print(validate_code(9453), False)

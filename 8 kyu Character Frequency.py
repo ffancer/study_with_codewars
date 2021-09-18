23f1ce8b79f0000cb74a7fe95efa15b77248bbce
Paul <gafgafgarion05@gmail.com>
2021-09-18 09:19:41 +0300

    solution

--- 8 kyu Character Frequency.py
+++ 8 kyu Character Frequency.py
@@ -1,5 +1,5 @@
 def char_freq(message):
-    pass
+    return {i: message.count(i) for i in message}
 
 
 print(char_freq("I like cats"), {'a': 1, ' ': 2, 'c': 1, 'e': 1, 'I': 1, 'k': 1, 'l': 1, 'i': 1, 's': 1, 't': 1})

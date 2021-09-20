8f702baf827129730c2a91724a178b8c3c5a38d3
Paul <gafgafgarion05@gmail.com>
2021-09-20 10:02:13 +0300

    best practice no generator, and using str method 'count'

--- 8 kyu Evil or Odious.py
+++ 8 kyu Evil or Odious.py
@@ -1,7 +1,7 @@
 def evil(n):
-    return "It's Evil!" if sum([int(i) for i in bin(n)[2:] if i == '1']) % 2 == 0 else "It's Odious!"
+    return "It's Evil!" if bin(n).count('1') % 2 == 0 else "It's Odious!"
 
 
-print(evil(1),"It's Odious!")
-print(evil(2),"It's Odious!")
-print(evil(3),"It's Evil!")
+print(evil(1), "It's Odious!")
+print(evil(2), "It's Odious!")
+print(evil(3), "It's Evil!")

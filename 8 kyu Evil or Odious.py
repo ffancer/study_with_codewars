cc6cf04575d78975c08640d592cc5001ca1cddc5
Paul <gafgafgarion05@gmail.com>
2021-09-20 10:00:50 +0300

    1 line solution

--- 8 kyu Evil or Odious.py
+++ 8 kyu Evil or Odious.py
@@ -1,5 +1,5 @@
 def evil(n):
-    return sum([int(i) for i in bin(n)[2:] if i == '1'])
+    return "It's Evil!" if sum([int(i) for i in bin(n)[2:] if i == '1']) % 2 == 0 else "It's Odious!"
 
 
 print(evil(1),"It's Odious!")

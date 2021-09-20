dfe4b10aa5977fcb096c64b4d35a3b110f47e4de
Paul <gafgafgarion05@gmail.com>
2021-09-20 09:28:13 +0300

    init

---
+++ 8 kyu Who ate the cookie.py
@@ -0,0 +1,11 @@
+# 8 kyu
+# Who ate the cookie?
+
+def cookie(x):
+    pass
+
+
+print(cookie("Ryan"), "Who ate the last cookie? It was Zach!")
+print(cookie(2.3), "Who ate the last cookie? It was Monica!")
+print(cookie(26), "Who ate the last cookie? It was Monica!")
+print(cookie(True), "Who ate the last cookie? It was the dog!")

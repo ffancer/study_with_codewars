0efc67a7b09ce1a8c8abae8fbfaa0a9189a7ba69
Paul <gafgafgarion05@gmail.com>
2021-09-21 13:28:41 +0300

    solution

--- 8 kyu Collatz Conjecture (3n+1).py
+++ 8 kyu Collatz Conjecture (3n+1).py
@@ -1,5 +1,17 @@
 def hotpo(n):
-    return
+    cnt = 0
+
+    while n > 1:
+        if n % 2 == 0:
+            n = n / 2
+            cnt += 1
+        else:
+            n = 3 * n + 1
+            cnt += 1
+        if n == 1:
+            break
+
+    return cnt
 
 
 print(hotpo(1), 0)

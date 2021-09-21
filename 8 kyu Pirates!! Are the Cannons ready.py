3e0cfd158b542a25b62ef1677886ce97a806a6a9
Paul <gafgafgarion05@gmail.com>
2021-09-21 13:49:55 +0300

    init

---
+++ 8 kyu Pirates!! Are the Cannons ready.py
@@ -0,0 +1,8 @@
+def cannons_ready(gunners):
+  # Fire! or Shiver me timbers!
+
+
+a = {'Mike': 'aye', 'Joe': 'aye', 'Johnson': 'aye', 'Peter': 'aye'}
+b = {'Mike': 'aye', 'Joe': 'nay', 'Johnson': 'aye', 'Peter': 'aye'}
+print(cannons_ready(a), 'Fire!')
+print(cannons_ready(b), 'Shiver me timbers!')

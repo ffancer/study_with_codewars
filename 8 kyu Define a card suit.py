660bc6c4dc6e298f09314b60c66fb250021a9f55
Paul <gafgafgarion05@gmail.com>
2021-09-22 13:48:08 +0300

    init

---
+++ 8 kyu Define a card suit.py
@@ -0,0 +1,7 @@
+def define_suit(card):
+    # Good luck
+
+print(define_suit('3C'), 'clubs')
+print(define_suit('QS'), 'spades')
+print(define_suit('9D'), 'diamonds')
+print(define_suit('JH'), 'hearts')

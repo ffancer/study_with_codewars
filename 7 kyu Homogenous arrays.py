525c59c38a91e15c2c11108e3c5b61db070b67b7
Paul <gafgafgarion05@gmail.com>
2021-09-23 10:56:20 +0300

    init

---
+++ 7 kyu Homogenous arrays.py
@@ -0,0 +1,7 @@
+def filter_homogenous(arrays):
+    # your code
+
+
+print(filter_homogenous([[1, 5, 4], ['a', 3, 5], ['b'], [], ['1', 2, 3]]), [[1, 5, 4], ['b']])
+print(filter_homogenous([[123, 234, 432], ['', 'abc'], [''], ['', 1], ['', '1'], []]),
+                       [[123, 234, 432], ['', 'abc'], [''], ['', '1']])

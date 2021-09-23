a7aa6f2807d739c2077f78b7fd5578009be2a62f
Paul <gafgafgarion05@gmail.com>
2021-09-23 11:17:38 +0300

    init

---
+++ 8 kyu Contamination #1 -String-.py
@@ -0,0 +1,9 @@
+def contamination(text, char):
+  #Code here ;)
+
+
+print(contamination("abc", "z"), "zzz")
+print(contamination("", "z"), "")
+print(contamination("abc", ""), "")
+print(contamination("_3ebzgh4", "&"), "&&&&&&&&")
+print(contamination("//case", " "), "      ")

4c64095cbb9c1fc1d7bfe3f29280265892bbaf9c
Paul <gafgafgarion05@gmail.com>
2021-09-23 10:58:42 +0300

    solution

--- 7 kyu Homogenous arrays.py
+++ 7 kyu Homogenous arrays.py
@@ -1,5 +1,11 @@
 def filter_homogenous(arrays):
-    # your code
+    lst = []
+
+    for i in arrays:
+        if len(list(set(map(type, i)))) == 1:
+            lst.append(i)
+
+    return lst
 
 
 print(filter_homogenous([[1, 5, 4], ['a', 3, 5], ['b'], [], ['1', 2, 3]]), [[1, 5, 4], ['b']])

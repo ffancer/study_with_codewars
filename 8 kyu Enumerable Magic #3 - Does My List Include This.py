75d9dd1711f97af55c1a98b649e657b85f0af884
Paul <gafgafgarion05@gmail.com>
2021-09-24 17:00:24 +0300

    solution

--- 8 kyu Enumerable Magic #3 - Does My List Include This.py
+++ 8 kyu Enumerable Magic #3 - Does My List Include This.py
@@ -1,8 +1,8 @@
-#8 kyu
+# 8 kyu
 # Enumerable Magic #3 - Does My List Include This?
-def include(arr,item):
-    pass
+def include(arr, item):
+    return item in arr
 
 
-print(include([1,2,3,4], 3), True )
-print(include([1,2,4,5], 3), False)
+print(include([1, 2, 3, 4], 3), True)
+print(include([1, 2, 4, 5], 3), False)

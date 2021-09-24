9a6b3d4a983529f5bed34b25599da9288eccfa7c
Paul <gafgafgarion05@gmail.com>
2021-09-24 16:59:05 +0300

    init

---
+++ 8 kyu Enumerable Magic #3 - Does My List Include This.py
@@ -0,0 +1,8 @@
+#8 kyu
+# Enumerable Magic #3 - Does My List Include This?
+def include(arr,item):
+    pass
+
+
+test.assert_equals(include([1,2,3,4], 3), True )
+test.assert_equals(include([1,2,4,5], 3), False)

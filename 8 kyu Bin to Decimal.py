64605a262b9e7f1473d890f7b1c35c0576499de1
Paul <gafgafgarion05@gmail.com>
2021-09-25 08:31:56 +0300

    init

---
+++ 8 kyu Bin to Decimal.py
@@ -0,0 +1,9 @@
+def bin_to_decimal(inp):
+    pass
+
+print(bin_to_decimal("0"), 0)
+print(bin_to_decimal("1"), 1)
+print(bin_to_decimal("10"), 2)
+print(bin_to_decimal("11"), 3)
+print(bin_to_decimal("101010"), 42)
+print(bin_to_decimal("1001001"), 73)

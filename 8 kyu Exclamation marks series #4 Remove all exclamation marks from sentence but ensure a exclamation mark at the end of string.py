e713a99c882300077766bccee7b020703066e190
Paul <gafgafgarion05@gmail.com>
2021-09-30 17:28:21 +0300

    init

---
+++ 8 kyu Exclamation marks series #4 Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string.py
@@ -0,0 +1,14 @@
+# 8 kyu
+# Exclamation marks series #4: Remove all exclamation marks
+# from sentence but ensure a exclamation mark at the end of string
+
+def remove(s):
+    pass
+
+
+print(remove("Hi!"))
+print(remove("Hi!!!"))
+print(remove("!Hi"))
+print(remove("!Hi!"))
+print(remove("Hi! Hi!"))
+print(remove("Hi"))

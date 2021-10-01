8cb37624dfa08a5a1a686e97cea378536cfa7683
Paul <gafgafgarion05@gmail.com>
2021-10-01 14:43:08 +0300

    init

---
+++ 7 kyu Simple Fun #13 Magical Well.py
@@ -0,0 +1,8 @@
+# 7 kyu
+# Simple Fun #13: Magical Well
+def magical_well(a, b, n):
+    #coding and coding..
+
+print(magical_well(1, 2, 2), 8)
+print(magical_well(1, 1, 1), 1)
+print(magical_well(6, 5, 3), 128)

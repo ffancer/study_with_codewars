37bbf034f32b706e1b85beabf02019645a00e98c
Paul <gafgafgarion05@gmail.com>
2021-10-01 14:32:25 +0300

    init

---
+++ 8 kyu Man in the west.py
@@ -0,0 +1,6 @@
+def check_the_bucket(bucket):
+    #code here
+
+print(check_the_bucket(['stone', 'stone', 'gold', 'stone', 'stone', ]), True, 'I will bye a teeth')
+print(check_the_bucket(['stone', 'stone', 'stone', 'stone', 'stone', ]), False, 'Not today')
+print(check_the_bucket(['gold', 'gold', 'gold', 'gold', 'gold', ]), True, 'I will bye a teeth')

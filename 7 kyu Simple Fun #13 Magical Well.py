9bd7e8ee6e87e43da60039c1d84bbd842cb09b0a
Paul <gafgafgarion05@gmail.com>
2021-10-01 14:50:57 +0300

    solution

--- 7 kyu Simple Fun #13 Magical Well.py
+++ 7 kyu Simple Fun #13 Magical Well.py
@@ -1,7 +1,16 @@
 # 7 kyu
 # Simple Fun #13: Magical Well
 def magical_well(a, b, n):
-    #coding and coding..
+    total = 0
+
+    for i in range(1, n + 1):
+        total += a * b
+        a += 1
+        b += 1
+
+    return total
+
+
 
 print(magical_well(1, 2, 2), 8)
 print(magical_well(1, 1, 1), 1)

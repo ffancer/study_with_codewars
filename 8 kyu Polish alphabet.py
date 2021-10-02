abee63c4fcf11c3d3f7ec2ea8df6a21ecf1804aa
Paul <gafgafgarion05@gmail.com>
2021-10-02 10:35:33 +0300

    init

---
+++ 8 kyu Polish alphabet.py
@@ -0,0 +1,8 @@
+def correct_polish_letters(st):
+    # your code here
+    pass
+
+
+print(correct_polish_letters("Jędrzej Błądziński"),"Jedrzej Bladzinski");
+print(correct_polish_letters("Lech Wałęsa"),"Lech Walesa");
+print(correct_polish_letters("Maria Skłodowska-Curie"),"Maria Sklodowska-Curie")

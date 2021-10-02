587ab1f20c7ffc799b8558e678b2e9f99f4d6f72
Paul <gafgafgarion05@gmail.com>
2021-10-02 10:46:47 +0300

    solution

--- 8 kyu Polish alphabet.py
+++ 8 kyu Polish alphabet.py
@@ -1,8 +1,26 @@
 def correct_polish_letters(st):
-    # your code here
-    pass
+    dct = {
+        'ą': 'a',
+        'ć': 'c',
+        'ę': 'e',
+        'ł': 'l',
+        'ń': 'n',
+        'ó': 'o',
+        'ś': 's',
+        'ź': 'z',
+        'ż': 'z'
+    }
+    s = ''
+
+    for i in st:
+        if i in dct:
+            s += dct[i]
+        else:
+            s += i
+
+    return s
 
 
-print(correct_polish_letters("Jędrzej Błądziński"),"Jedrzej Bladzinski");
-print(correct_polish_letters("Lech Wałęsa"),"Lech Walesa");
+print(correct_polish_letters("Jędrzej Błądziński"),"Jedrzej Bladzinski")
+print(correct_polish_letters("Lech Wałęsa"),"Lech Walesa")
 print(correct_polish_letters("Maria Skłodowska-Curie"),"Maria Sklodowska-Curie")

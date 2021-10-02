caf9e32a65a40477aba7d6ec273ecb25828b8bd6
Paul <gafgafgarion05@gmail.com>
2021-10-02 12:54:45 +0300

    solution

--- 8 kyu Training JS #18 Methods of String object--concat() split() and its good friend join().py
+++ 8 kyu Training JS #18 Methods of String object--concat() split() and its good friend join().py
@@ -1,8 +1,14 @@
 # 8 kyu
 # Training JS #18: Methods of String object--concat() split() and its good friend join()
 
+# def split_and_merge(string_, separator):
+#     # return separator.join(list(string_))
+#     return '-'.join(list(string_))
+
+
 def split_and_merge(string_, separator):
-    return  # your code here
+    return ' '.join(separator.join(j for j in i) for i in string_.split())
+
 
 
 print(split_and_merge("My name is John", " "), "M y n a m e i s J o h n")

17c9be829d7df7c27d5c9afb5f6dad2b27cc6e28
Paul <gafgafgarion05@gmail.com>
2021-10-02 11:38:39 +0300

    init

---
+++ 8 kyu Thinkful - Dictionary drills Order filler.py
@@ -0,0 +1,16 @@
+# 8 kyu
+# Thinkful - Dictionary drills: Order filler
+
+def fillable(stock, merch, n):
+    pass
+
+
+stock = {
+    'football': 4,
+    'boardgame': 10,
+    'leggos': 1,
+    'doll': 5}
+
+print(fillable(stock, 'football', 3), True)
+print(fillable(stock, 'leggos', 2), False)
+print(fillable(stock, 'action figure', 1), False)

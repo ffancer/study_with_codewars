b0bf619a2c7125b6d86d0808e5d19254cb811720
Paul <gafgafgarion05@gmail.com>
2021-10-02 12:24:25 +0300

    init

---
+++ 8 kyu Training JS #18 Methods of String object--concat() split() and its good friend join().py
@@ -0,0 +1,11 @@
+# 8 kyu
+# Training JS #18: Methods of String object--concat() split() and its good friend join()
+
+def split_and_merge(string_, separator):
+    return  # your code here
+
+
+print(split_and_merge("My name is John", " "), "M y n a m e i s J o h n")
+print(split_and_merge("My name is John", "-"), "M-y n-a-m-e i-s J-o-h-n")
+print(split_and_merge("Hello World!", "."), "H.e.l.l.o W.o.r.l.d.!")
+print(split_and_merge("Hello World!", ","), "H,e,l,l,o W,o,r,l,d,!")

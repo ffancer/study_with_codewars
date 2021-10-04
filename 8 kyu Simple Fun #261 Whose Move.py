49671fcd9e1e6e435693dcea24aef871990ac32e
Paul <gafgafgarion05@gmail.com>
2021-10-04 13:10:43 +0300

    solution

--- 8 kyu Simple Fun #261 Whose Move.py
+++ 8 kyu Simple Fun #261 Whose Move.py
@@ -1,7 +1,18 @@
 # 8 kyu
 # Simple Fun #261: Whose Move
 def whoseMove(lastPlayer, win):
-# Your Move...
+    answer = ''
+
+    if lastPlayer == 'black' and win is False:
+        answer = 'white'
+    elif lastPlayer == 'white' and win is False:
+        answer = 'black'
+    elif lastPlayer == 'black' and win is True:
+        answer = 'black'
+    elif lastPlayer == 'white' and win is True:
+        answer = 'white'
+
+    return answer
 
 
 print(whoseMove('black', False), 'white')

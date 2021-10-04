cf77f5330db2aee51d4f720012d5660fc92d18fa
Paul <gafgafgarion05@gmail.com>
2021-10-04 13:05:25 +0300

    init

---
+++ 8 kyu Simple Fun #261 Whose Move.py
@@ -0,0 +1,10 @@
+# 8 kyu
+# Simple Fun #261: Whose Move
+def whoseMove(lastPlayer, win):
+# Your Move...
+
+
+print(whoseMove('black', False), 'white')
+print(whoseMove('white', False), 'black')
+print(whoseMove('black', True), 'black')
+print(whoseMove('white', True), 'white')

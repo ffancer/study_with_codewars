948bde3333afdef75e2eaa32fc6fa71f858a5562
Paul <gafgafgarion05@gmail.com>
2021-10-04 13:49:39 +0300

    solution

--- 8 kyu Palindrome Strings.py
+++ 8 kyu Palindrome Strings.py
@@ -1,5 +1,17 @@
 def is_palindrome(string):
-    pass
+    if type(string) == int:
+        string = str(string)
+    i = 0
+    j = len(string) - 1
+    flag = True
+
+    while i < j:
+        if string[i] != string[j]:
+            flag = False
+        i += 1
+        j -= 1
+
+    return flag
 
 
 print(is_palindrome("anna"), True)

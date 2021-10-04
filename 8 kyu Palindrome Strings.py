1cfc97137325907b387930a2f1dcde080577ca6e
Paul <gafgafgarion05@gmail.com>
2021-10-04 13:49:20 +0300

    init

---
+++ 8 kyu Palindrome Strings.py
@@ -0,0 +1,8 @@
+def is_palindrome(string):
+    pass
+
+
+print(is_palindrome("anna"), True)
+print(is_palindrome("walter"), False)
+print(is_palindrome(12321), True)
+print(is_palindrome(123456), False)

381252882529a525af5827d8bb5afcc4fd7a1092
Paul <gafgafgarion05@gmail.com>
2021-10-05 15:09:47 +0300

    init

---
+++ 8 kyu No Loops 2 - You only need one.py
@@ -0,0 +1,9 @@
+def check(a, x):
+    # your code here
+    pass
+
+
+print(check([66, 101], 66), True)
+print(check([80, 117, 115, 104, 45, 85, 112, 115], 45), True)
+print(check(['t', 'e', 's', 't'], 'e'), True)
+print(check(['what', 'a', 'great', 'kata'], 'kat'), False)

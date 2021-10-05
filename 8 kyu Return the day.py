e3de758bef48753977929938d0c19e021ca205ab
Paul <gafgafgarion05@gmail.com>
2021-10-05 14:34:20 +0300

    init

---
+++ 8 kyu Return the day.py
@@ -0,0 +1,10 @@
+def whatday(num):
+    pass
+
+
+
+print(whatday(1), 'Sunday')
+print(whatday(2), 'Monday')
+print(whatday(3), 'Tuesday')
+print(whatday(8), 'Wrong, please enter a number between 1 and 7')
+print(whatday(20), 'Wrong, please enter a number between 1 and 7')

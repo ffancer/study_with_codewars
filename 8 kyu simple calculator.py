1cab5bc32a2ef5386f87ea56372cd15ca143e030
Paul <gafgafgarion05@gmail.com>
2021-10-05 14:54:49 +0300

    init

---
+++ 8 kyu simple calculator.py
@@ -0,0 +1,9 @@
+def calculator(x, y, op):
+    pass
+
+
+print(calculator(6, 2, '+'), 8)
+print(calculator(4, 3, '-'), 1)
+print(calculator(5, 5, '*'), 25)
+print(calculator(5, 4, '/'), 1.25)
+print(calculator(6, 2, '&'), "unknown value");

3617729abd60b53c9b3a9070541f33c18440137e
Paul <gafgafgarion05@gmail.com>
2021-10-05 15:32:44 +0300

    init

---
+++ 8 kyu NBA full 48 minutes average.py
@@ -0,0 +1,10 @@
+def nba_extrap(ppg, mpg):
+    return ppg
+
+
+print(nba_extrap(12, 20), 28.8)
+print(nba_extrap(10, 10), 48.0)
+print(nba_extrap(5, 17), 14.1)
+print(nba_extrap(0, 0), 0)
+print(nba_extrap(30.8, 34.7), 42.6)  # Russell Westbrook 1/15/17
+print(nba_extrap(22.9, 33.8), 32.5)  # Kemba Walker 1/15/17

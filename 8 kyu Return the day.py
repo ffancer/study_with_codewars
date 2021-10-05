0d231b55e8edee5e76666f949e09404d981bb588
Paul <gafgafgarion05@gmail.com>
2021-10-05 14:45:05 +0300

    3 best practices

--- 8 kyu Return the day.py
+++ 8 kyu Return the day.py
@@ -1,19 +1,39 @@
+# best practice 1:
+WEEK = ' Sunday Monday Tuesday Wednesday Thursday Friday Saturday'.split(" ")
+
+
+def whatday(n):
+    return WEEK[n] if 0 < n < 8 else 'Wrong, please enter a number between 1 and 7'
+
+
+# best practice 2:
+WEEKDAY = {
+    1: 'Sunday',
+    2: 'Monday',
+    3: 'Tuesday',
+    4: 'Wednesday',
+    5: 'Thursday',
+    6: 'Friday',
+    7: 'Saturday'}
+ERROR = 'Wrong, please enter a number between 1 and 7'
+
+
+def whatday(n):
+    return WEEKDAY.get(n, ERROR)
+
+
+# best practice 3:
 def whatday(num):
-    try:
-        days = {
-            1: 'Sunday',
-            2: 'Monday',
-            3: 'Tuesday',
-            4: 'Wednesday',
-            5: 'Thursday',
-            6: 'Friday',
-            7: 'Saturday',
-        }
-
-        return days[num]
-    except:
-        return 'Wrong, please enter a number between 1 and 7'
-
+    switcher = {
+        1: 'Sunday',
+        2: 'Monday',
+        3: 'Tuesday',
+        4: 'Wednesday',
+        5: 'Thursday',
+        6: 'Friday',
+        7: 'Saturday'
+    }
+    return switcher.get(num, 'Wrong, please enter a number between 1 and 7')
 
 
 print(whatday(1), 'Sunday')

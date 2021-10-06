d3b5ee8111265ba7caec52ffa7cca6b3b0925ede
Paul <gafgafgarion05@gmail.com>
2021-10-06 13:31:51 +0300

    init

---
+++ 8 kyu Freudian translator.py
@@ -0,0 +1,11 @@
+def to_freud(sentence):
+
+
+# your code here
+
+
+print(to_freud("test"), "sex")
+print(to_freud("sexy sex"), "sex sex")
+print(to_freud("This is a test"), "sex sex sex sex")
+print(to_freud("This is a longer test"), "sex sex sex sex sex")
+print(to_freud("You're becoming a true freudian expert"), "sex sex sex sex sex sex")

4199340f9fc180c79e41733b4dafc894d79ef4b0
Paul <gafgafgarion05@gmail.com>
2021-10-06 13:06:00 +0300

    init

---
+++ 8 kyu Merging sorted integer arrays (without duplicates).py
@@ -0,0 +1,7 @@
+def merge_arrays(first, second):
+    # your code here
+    pass
+
+
+print(merge_arrays([1, 3, 5], [2, 4, 6]), [1, 2, 3, 4, 5, 6])
+print(merge_arrays([2, 4, 8], [2, 4, 6]), [2, 4, 6, 8])

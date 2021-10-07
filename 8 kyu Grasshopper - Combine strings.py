9f59bdff894e0236dd17dd0ffecb279d1164bf00
Paul <gafgafgarion05@gmail.com>
2021-10-07 14:12:25 +0300

    init

---
+++ 8 kyu Grasshopper - Combine strings.py
@@ -0,0 +1,7 @@
+def combine_names(x, y):
+    pass
+
+
+print(combine_names("James", "Stevens"), "James Stevens")
+print(combine_names("Davy", "Back"), "Davy Back")
+print(combine_names("Arthur", "Dent"), "Arthur Dent")

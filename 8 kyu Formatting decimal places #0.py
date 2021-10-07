0caa7e15b5b8f84a00f0b7d25ef47ab42b792283
Paul <gafgafgarion05@gmail.com>
2021-10-07 13:42:49 +0300

    init

---
+++ 8 kyu Formatting decimal places #0.py
@@ -0,0 +1,7 @@
+def two_decimal_places(n):
+    raise NotImplementedError("TODO: two_decimal_places")
+
+
+print(two_decimal_places(4.659725356), 4.66, "didn't work for 4.659725356")
+print(two_decimal_places(173735326.3783732637948948), 173735326.38, "didn't work for 173735326.3783732637948948")
+print(two_decimal_places(4.653725356), 4.65, "didn't work for 4.653725356")

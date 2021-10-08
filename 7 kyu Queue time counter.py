ca314169b86292cc3d05bc9ce93ee03302135cbb
Paul <gafgafgarion05@gmail.com>
2021-10-08 10:56:06 +0300

    init

---
+++ 7 kyu Queue time counter.py
@@ -0,0 +1,9 @@
+def queue(queuers, pos):
+    pass
+
+
+print(queue([2, 5, 3, 6, 4], 0), 6)
+print(queue([2, 5, 3, 6, 4], 1), 18)
+print(queue([2, 5, 3, 6, 4], 2), 12)
+print(queue([2, 5, 3, 6, 4], 3), 20)
+print(queue([2, 5, 3, 6, 4], 4), 17)

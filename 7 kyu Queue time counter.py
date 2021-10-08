56e88d3649889e8cb9d663751531132737a89a39
Paul <gafgafgarion05@gmail.com>
2021-10-08 11:11:55 +0300

    solution

--- 7 kyu Queue time counter.py
+++ 7 kyu Queue time counter.py
@@ -1,5 +1,17 @@
-def queue(queuers, pos):
-    pass
+def queue(queuers,pos):
+    cnt = 0
+
+    while queuers[pos] > 0:
+        if pos == 0:
+            pos = len(queuers) - 1
+        else:
+            pos -= 1
+        queuers[0] -= 1
+        queuers.append(queuers[0])
+        queuers.pop(0)
+        cnt += 1
+
+    return cnt + sum([i for i in queuers if i < 0])
 
 
 print(queue([2, 5, 3, 6, 4], 0), 6)

8af5eacafc4d1101dfd11d8ba092270eb4c42dd2
Paul <gafgafgarion05@gmail.com>
2021-10-08 10:11:46 +0300

    init

---
+++ 8 kyu Area of a Square.py
@@ -0,0 +1,10 @@
+def square_area(A):
+
+
+# your code here
+
+print(square_area(2), 1.62)
+print(square_area(0), 0)
+print(square_area(14.05), 80)
+print(square_area(1), 0.41)
+print(square_area(100), 4052.85)

bc06a856468ca6664a173986aeae4a9944eb1fe8
Paul <gafgafgarion05@gmail.com>
2021-10-09 10:19:34 +0300

    init

---
+++ 8 kyu Name on billboard.py
@@ -0,0 +1,17 @@
+def billboard(name, price=30):
+
+
+# your code here - note that in Python we cannot prevent the use of the
+# multiplication, but try not to be lame and solve the kata in another way!
+
+print(billboard("Jeong-Ho Aristotelis"), 600)
+print(billboard("Abishai Charalampos"), 570)
+print(billboard("Idwal Augustin"), 420)
+print(billboard("Hadufuns John", 20), 260)
+print(billboard("Zoroaster Donnchadh"), 570)
+print(billboard("Claude Miljenko"), 450)
+print(billboard("Werner Vigi", 15), 165)
+print(billboard("Anani Fridumar"), 420)
+print(billboard("Paolo Oli"), 270)
+print(billboard("Hjalmar Liupold", 40), 600)
+print(billboard("Simon Eadwulf"), 390)

af93a01ba541baba942a6094de2059f528dbc23e
Paul <gafgafgarion05@gmail.com>
2021-10-09 10:07:19 +0300

    solution

--- 8 kyu Fix the Bugs (Syntax) - My First Kata.py
+++ 8 kyu Fix the Bugs (Syntax) - My First Kata.py
@@ -1,8 +1,7 @@
 def my_first_kata(a, b):
-    if type(a) or type(b) == "number":
+    if type(a) != int or type(b) != int:
         return False
-    else:
-        return a % b + + b % a
+    return a % b + b % a
 
 
 print(my_first_kata(3, 5), (3 % 5 + 5 % 3))

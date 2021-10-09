7e726feac63d2183cdb013a8442bd4bc3041d2df
Paul <gafgafgarion05@gmail.com>
2021-10-09 10:23:01 +0300

    first solution

--- 8 kyu Name on billboard.py
+++ 8 kyu Name on billboard.py
@@ -1,8 +1,10 @@
 def billboard(name, price=30):
+    cnt = 0
 
+    for i in range(len(name)):
+        cnt += 1
 
-# your code here - note that in Python we cannot prevent the use of the
-# multiplication, but try not to be lame and solve the kata in another way!
+    return cnt * price
 
 print(billboard("Jeong-Ho Aristotelis"), 600)
 print(billboard("Abishai Charalampos"), 570)

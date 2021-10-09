f23421cd31c66ac1ea8873782a8733d01929e644
Paul <gafgafgarion05@gmail.com>
2021-10-09 14:18:12 +0300

    init

---
+++ 8 kyu They say that only the name is long enough to attract attention. They also said that only a simple Kata will have someone to solve it..py
@@ -0,0 +1,12 @@
+# 8 kyu
+# They say that only the name is long enough to attract attention. They also said that only a simple Kata will have someone to solve it. This is a sadly story #1: Are they opposite?
+
+
+def is_opposite(s1, s2):
+    pass
+
+print(is_opposite("ab", "AB"), True)
+print(is_opposite("aB", "Ab"), True)
+print(is_opposite("aBcd", "AbCD"), True)
+print(is_opposite("AB", "Ab"), False)
+print(is_opposite("", ""), False)

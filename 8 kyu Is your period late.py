9edfcfd054d325a8692e730560e78e336846cc9d
Paul <gafgafgarion05@gmail.com>
2021-10-11 14:37:45 +0300

    2 solution

--- 8 kyu Is your period late.py
+++ 8 kyu Is your period late.py
@@ -1,19 +1,16 @@
 # 8 kyu
 # Is your period late?
-import datetime
-
-
 def period_is_late(last, today, cycle_length):
-    return last + datetime.timedelta(days=cycle_length) < today
+    return (today - last).days > cycle_length
 
 
-print(period_is_late(2016, 6, 13), (2016, 7, 16), 35)
-print(period_is_late(2016, 6, 13), (2016, 7, 16), 28)
-print(period_is_late(2016, 6, 13), (2016, 7, 16), 35)
-print(period_is_late(2016, 6, 13), (2016, 6, 29), 28)
-print(period_is_late(2016, 7, 12), (2016, 8, 9), 28)
-print(period_is_late(2016, 7, 12), (2016, 8, 10), 28)
-print(period_is_late(2016, 7, 1), (2016, 8, 1), 30)
-print(period_is_late(2016, 6, 1), (2016, 6, 30), 30)
-print(period_is_late(2016, 1, 1), (2016, 1, 31), 30)
-print(period_is_late(2016, 1, 1), (2016, 2, 1), 30)
+print(period_is_late(2016, 6, 13))
+print(period_is_late(2016, 6, 13))
+print(period_is_late(2016, 6, 13))
+print(period_is_late(2016, 6, 13))
+print(period_is_late(2016, 7, 12))
+print(period_is_late(2016, 7, 12))
+print(period_is_late(2016, 7, 1))
+print(period_is_late(2016, 6, 1))
+print(period_is_late(2016, 1, 1))
+print(period_is_late(2016, 1, 1))

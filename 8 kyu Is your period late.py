9392a253065734dccdbda069635e43078741c9d6
Paul <gafgafgarion05@gmail.com>
2021-10-11 14:36:15 +0300

    solution

--- 8 kyu Is your period late.py
+++ 8 kyu Is your period late.py
@@ -1,10 +1,11 @@
 # 8 kyu
 # Is your period late?
+import datetime
+
 
 def period_is_late(last, today, cycle_length):
+    return last + datetime.timedelta(days=cycle_length) < today
 
-
-# your code here
 
 print(period_is_late(2016, 6, 13), (2016, 7, 16), 35)
 print(period_is_late(2016, 6, 13), (2016, 7, 16), 28)

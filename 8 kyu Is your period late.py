e2a5f3da55b8c01a7dc42cd4854712708403833d
Paul <gafgafgarion05@gmail.com>
2021-10-11 14:21:46 +0300

    init

---
+++ 8 kyu Is your period late.py
@@ -0,0 +1,18 @@
+# 8 kyu
+# Is your period late?
+
+def period_is_late(last, today, cycle_length):
+
+
+# your code here
+
+print(period_is_late(2016, 6, 13), (2016, 7, 16), 35)
+print(period_is_late(2016, 6, 13), (2016, 7, 16), 28)
+print(period_is_late(2016, 6, 13), (2016, 7, 16), 35)
+print(period_is_late(2016, 6, 13), (2016, 6, 29), 28)
+print(period_is_late(2016, 7, 12), (2016, 8, 9), 28)
+print(period_is_late(2016, 7, 12), (2016, 8, 10), 28)
+print(period_is_late(2016, 7, 1), (2016, 8, 1), 30)
+print(period_is_late(2016, 6, 1), (2016, 6, 30), 30)
+print(period_is_late(2016, 1, 1), (2016, 1, 31), 30)
+print(period_is_late(2016, 1, 1), (2016, 2, 1), 30)

e1562892ad424eb90fcee2c1ac11f2628f0bc320
Paul <gafgafgarion05@gmail.com>
2021-10-11 10:13:15 +0300

    init

---
+++ 7 kyu Calculator Coin Combination.py
@@ -0,0 +1,10 @@
+# 7 kyu
+# Calculator: Coin Combination
+
+def coin_combo(cents):
+    return
+
+
+print(coin_combo(1), [1, 0, 0, 0])
+print(coin_combo(6), [1, 1, 0, 0])
+print(coin_combo(11), [1, 0, 1, 0])

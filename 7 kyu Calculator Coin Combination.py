96aef11c20d0586abf29a8c9ea13a6a02ad1d90f
Paul <gafgafgarion05@gmail.com>
2021-10-11 11:10:20 +0300

    solution

--- 7 kyu Calculator Coin Combination.py
+++ 7 kyu Calculator Coin Combination.py
@@ -2,7 +2,25 @@
 # Calculator: Coin Combination
 
 def coin_combo(cents):
-    return
+    coins = [0, 0, 0, 0]
+
+    while cents >= 25:
+        coins[3] += 1
+        cents -= 25
+
+    while cents >= 10:
+        coins[2] += 1
+        cents -= 10
+
+    while cents >= 5:
+        coins[1] += 1
+        cents -= 5
+
+    while cents >= 1:
+        coins[0] += 1
+        cents -= 1
+
+    return coins
 
 
 print(coin_combo(1), [1, 0, 0, 0])

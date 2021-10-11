b00e28c17600e1b777690843abc476fac1e08ecd
Paul <gafgafgarion05@gmail.com>
2021-10-11 11:16:43 +0300

    best practices

--- 7 kyu Calculator Coin Combination.py
+++ 7 kyu Calculator Coin Combination.py
@@ -1,14 +1,26 @@
 # 7 kyu
 # Calculator: Coin Combination
 
+# best practice 1
 def coin_combo(cents):
-    lst = []
+    return [cents % 5, ((cents % 25) % 10) // 5, (cents % 25) // 10, cents // 25]
 
-    for i in (25, 10, 5, 1):
-        lst.append(cents // i)
-        cents = cents % i
 
-    return lst[::-1]
+# best practice 2
+def coin_combo(cents):
+    d = cents // 25
+    c = (cents % 25) // 10
+    b = ((cents % 25) % 10) // 5
+    a = (((cents % 25) % 10) % 5) // 1
+    return [a, b, c, d]
+
+
+# best practice 3
+def coin_combo(cents):
+    coins = [1, 5, 10, 25]
+    for i in range(3, -1, -1):
+        coins[i], cents = divmod(cents, coins[i])
+    return coins
 
 
 print(coin_combo(1), [1, 0, 0, 0])

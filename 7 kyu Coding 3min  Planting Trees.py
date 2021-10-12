bc0a4a5f9eec77d534056af368fa13f897372363
Paul <gafgafgarion05@gmail.com>
2021-10-12 09:11:04 +0300

    init

---
+++ 7 kyu Coding 3min  Planting Trees.py
@@ -0,0 +1,19 @@
+# 7 kyu
+# Coding 3min : Planting Trees
+
+
+def sc(width, length, gaps):
+
+
+# your code here
+
+print(sc(3, 3, 1), 4)
+print(sc(3, 3, 3), 2)
+print(sc(3, 3, 2), 0)
+print(sc(7, 7, 3), 6)
+print(sc(3, 3, 0), 8)
+print(sc(3, 3, 10), 0)
+print(sc(2, 2, 1), 2)
+print(sc(2, 2, 0), 4)
+print(sc(200, 2, 1), 200)
+print(sc(2, 200, 1), 200)

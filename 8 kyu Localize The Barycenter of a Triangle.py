431714aad895ad118a4aa0f352b80fc1db1adc31
Paul <gafgafgarion05@gmail.com>
2021-10-12 15:01:02 +0300

    best solution

--- 8 kyu Localize The Barycenter of a Triangle.py
+++ 8 kyu Localize The Barycenter of a Triangle.py
@@ -1,5 +1,5 @@
-def bar_triang(pointA, pointB, pointC):
-    return [round((pointA[0] + pointB[0] + pointC[0]) / 3, 4), round((pointA[1] + pointB[1] + pointC[1]) / 3, 4)]
+def bar_triang(a, b, c):
+    return [round(sum(i) / 3.0, 4) for i in zip(a, b, c)]
 
 
 print(bar_triang([4, 6], [12, 4], [10, 10]), [8.6667, 6.6667])

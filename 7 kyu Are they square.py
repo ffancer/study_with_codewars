16d3e02e3419534a13383bd9e960081c5e30f2d4
Paul <gafgafgarion05@gmail.com>
2021-10-12 09:35:36 +0300

    init

---
+++ 7 kyu Are they square.py
@@ -0,0 +1,12 @@
+# 7 kyu
+# Are they square?
+
+def is_square(arr):
+    pass
+
+
+print(is_square([1, 4, 9, 16, 25, 36]), True)
+print(is_square([1, 2, 3, 4, 5, 6]), False)
+print(is_square([]), None)
+print(is_square([1, 4, 9, 16, 25]), True)
+print(is_square([1, 2, 4, 15]), False)

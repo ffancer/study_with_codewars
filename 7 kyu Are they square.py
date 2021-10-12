c1d548945e839aaf8c0572ca4fe78debc03f8107
Paul <gafgafgarion05@gmail.com>
2021-10-12 09:45:28 +0300

    1 line solution

--- 7 kyu Are they square.py
+++ 7 kyu Are they square.py
@@ -2,16 +2,7 @@
 # Are they square?
 
 def is_square(arr):
-    if not arr:
-        return None
-
-    lst = []
-
-    for i in arr:
-        if i**0.5 == int(i**0.5):
-            lst.append(i)
-
-    return arr == lst
+    return None if not arr else arr == [i for i in arr if i**0.5 == int(i**0.5)]
 
 
 print(is_square([1, 4, 9, 16, 25, 36]), True)

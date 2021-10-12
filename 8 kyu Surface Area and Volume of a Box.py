4cf8fc2e9a475facc63c23835de33dfa196b5783
Paul <gafgafgarion05@gmail.com>
2021-10-12 10:38:40 +0300

    init

---
+++ 8 kyu Surface Area and Volume of a Box.py
@@ -0,0 +1,10 @@
+def get_size(w, h, d):
+
+
+# your code here
+
+print(get_size(4, 2, 6), [88, 48])
+print(get_size(1, 1, 1), [6, 1])
+print(get_size(1, 2, 1), [10, 2])
+print(get_size(1, 2, 2), [16, 4])
+print(get_size(10, 10, 10), [600, 1000])

b095c90a99a7e4da821174555feef9fcb909388e
Paul <gafgafgarion05@gmail.com>
2021-10-13 10:44:15 +0300

    best practice + 1 line solution

--- 7 kyu Zero Terminated Sum.py
+++ 7 kyu Zero Terminated Sum.py
@@ -1,15 +1,5 @@
 def largest_sum(s):
-    if s == '0':
-        return 0
-
-    s = s.split('0')
-    [s.remove(i) for i in s if i == '']
-    lst = []
-
-    for i in s:
-        lst.append(sum([int(j) for j in list(i)]))
-
-    return max(lst)
+    return max(sum(map(int,x)) for x in s.split('0'))
 
 
 print(largest_sum("72102450111111090"), 11)

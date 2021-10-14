11975a8a608001d29120234bd610f72c1f633cec
Paul <gafgafgarion05@gmail.com>
2021-10-14 08:27:29 +0300

    init

---
+++ 7 kyu Simple Fun #238 Tennis Game Points.py
@@ -0,0 +1,15 @@
+# 7 kyu
+# Simple Fun #238: Tennis Game Points
+
+def tennis_game_points(score):
+    # your code here
+    pass
+
+
+print(tennis_game_points("15-40"), 4)
+print(tennis_game_points("30-all"), 4)
+print(tennis_game_points("love-15"), 1)
+print(tennis_game_points("love-30"), 2)
+print(tennis_game_points("love-40"), 3)
+print(tennis_game_points("15-love"), 1)
+print(tennis_game_points("15-30"), 3)

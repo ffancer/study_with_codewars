fd1643829c613ab25e9f845eabfa517fd7b05c7d
Paul <gafgafgarion05@gmail.com>
2021-10-14 14:16:39 +0300

    init

---
+++ 8 kyu Heads and Legs.py
@@ -0,0 +1,21 @@
+def animals(heads, legs):
+
+
+# return (Chickens, Cows)
+
+print(animals(72, 200), (44, 28))
+print(animals(116, 282), (91, 25))
+print(animals(12, 24), (12, 0))
+print(animals(6, 24), (0, 6))
+print(animals(344, 872), (252, 92))
+print(animals(158, 616), (8, 150))
+print(animals(25, 555), "No solutions")
+print(animals(12, 25), "No solutions")
+print(animals(54, 956), "No solutions")
+print(animals(5455, 54956), "No solutions")
+print(animals(0, 0), (0, 0))
+print(animals(-1, -1), "No solutions")
+print(animals(500, 0), "No solutions")
+print(animals(0, 500), "No solutions")
+print(animals(-45, 5), "No solutions")
+print(animals(5, -55), "No solutions")

b79321d7664ecd71da0ebfef772a4442ba17b594
Paul <gafgafgarion05@gmail.com>
2021-10-15 13:23:46 +0300

    solution

--- 8 kyu Calculate Price Excluding VAT.py
+++ 8 kyu Calculate Price Excluding VAT.py
@@ -1,5 +1,7 @@
 def excluding_vat_price(price):
-    return -1
+    if price is None or price <= 0:
+        return -1
+    return round(price / 1.15, 2)
 
 
 print(excluding_vat_price(230.00), 200.00)

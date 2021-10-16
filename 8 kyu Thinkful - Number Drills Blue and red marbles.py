4689a1ce0eb6f3d64ef3fed4eb4ddbc2a8c61037
Paul <gafgafgarion05@gmail.com>
2021-10-16 10:13:16 +0300

    init

---
+++ 8 kyu Thinkful - Number Drills Blue and red marbles.py
@@ -0,0 +1,11 @@
+# 8 kyu
+# Thinkful - Number Drills: Blue and red marbles
+
+def guess_blue(blue_start, red_start, blue_pulled, red_pulled):
+    # Your code here.
+    pass
+
+
+print(guess_blue(5, 5, 2, 3), 0.6)
+print(guess_blue(5, 7, 4, 3), 0.2)
+print(guess_blue(12, 18, 4, 6), 0.4)

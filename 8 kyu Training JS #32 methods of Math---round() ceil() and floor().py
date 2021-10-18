c631da563dce8bed443a195a95a3b449c7c38211
Paul <gafgafgarion05@gmail.com>
2021-10-18 10:42:27 +0300

    init

---
+++ 8 kyu Training JS #32 methods of Math---round() ceil() and floor().py
@@ -0,0 +1,10 @@
+# 8 kyu
+# Training JS #32: methods of Math---round() ceil() and floor()
+
+def round_it(n):
+    pass
+
+
+print(round_it(3.45), 4)
+print(round_it(34.5), 34)
+print(round_it(34.56), 35)

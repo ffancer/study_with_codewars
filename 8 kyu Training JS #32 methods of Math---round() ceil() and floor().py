42aaf36c7257ee14850512adc5efcbbb4c0c2f76
Paul <gafgafgarion05@gmail.com>
2021-10-18 10:58:43 +0300

    solution

--- 8 kyu Training JS #32 methods of Math---round() ceil() and floor().py
+++ 8 kyu Training JS #32 methods of Math---round() ceil() and floor().py
@@ -1,9 +1,16 @@
 # 8 kyu
 # Training JS #32: methods of Math---round() ceil() and floor()
+from math import ceil, floor
+
 
 def round_it(n):
-    pass
+    lst = str(n).split('.')
 
+    if len(lst[1]) > len(lst[0]):
+        return ceil(n)
+    elif len(lst[0]) > len(lst[1]):
+        return floor(n)
+    return round(n)
 
 print(round_it(3.45), 4)
 print(round_it(34.5), 34)

7e6541017cd764aeac07aab6fd5b738702bbebee
Paul <gafgafgarion05@gmail.com>
2021-10-18 11:53:21 +0300

    1 line solution

--- 8 kyu Filtering even numbers (Bug Fixes).py
+++ 8 kyu Filtering even numbers (Bug Fixes).py
@@ -1,13 +1,5 @@
 def kata_13_december(lst):
-    i = 0
-
-    while i < len(lst):
-        if lst[i] % 2 == 0:
-            lst.remove(lst[i])
-        else:
-            i += 1
-
-    return lst
+    return [i for i in lst if i % 2]
 
 
 print(kata_13_december([1, 2, 2, 2, 4, 3, 4, 5, 6, 7]), [1, 3, 5, 7])

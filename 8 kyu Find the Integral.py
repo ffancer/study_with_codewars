bcc848409f89742a3590196ab41e4eb2f8663b22
Paul <gafgafgarion05@gmail.com>
2021-10-19 18:48:08 +0300

    init

---
+++ 8 kyu Find the Integral.py
@@ -0,0 +1,10 @@
+def integrate(coefficient, exponent):
+
+
+# write your code here
+
+print(integrate(3, 2), "1x^3")
+print(integrate(12, 5), "2x^6")
+print(integrate(20, 1), "10x^2")
+print(integrate(40, 3), "10x^4")
+print(integrate(90, 2), "30x^3")

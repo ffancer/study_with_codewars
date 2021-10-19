530c3d867bfac0a1112aac5eaf1ec27d0af2c760
Paul <gafgafgarion05@gmail.com>
2021-10-19 18:36:59 +0300

    init

---
+++ 8 kyu Who is going to pay for the wall.py
@@ -0,0 +1,15 @@
+# 8 kyu
+# Who is going to pay for the wall?
+
+
+def who_is_paying(name):
+
+
+# your code here
+
+print(who_is_paying("Mexico"), ["Mexico", "Me"])
+print(who_is_paying("Melania"), ["Melania", "Me"])
+print(who_is_paying("Melissa"), ["Melissa", "Me"])
+print(who_is_paying("Me"), ["Me"])
+print(who_is_paying(""), [""])
+print(who_is_paying("I"), ["I"])

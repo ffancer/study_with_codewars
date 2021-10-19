26a669c29ecee70bbbbbbec2cfdfff529bd8e48a
Paul <gafgafgarion05@gmail.com>
2021-10-19 13:28:41 +0300

    init

---
+++ 7 kyu Simple Fun #87 Shuffled Array.py
@@ -0,0 +1,12 @@
+# 7 kyu
+# Simple Fun #87: Shuffled Array
+
+
+def shuffled_array(s):
+    pass
+
+
+print(shuffled_array([1, 12, 3, 6, 2]), [1, 2, 3, 6])
+print(shuffled_array([1, -3, -5, 7, 2]), [-5, -3, 2, 7])
+print(shuffled_array([2, -1, 2, 2, -1]), [-1, -1, 2, 2])
+print(shuffled_array([-3, -3]), [-3])

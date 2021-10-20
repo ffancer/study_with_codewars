6c24d73ea3ec15d8465e41c365a7e48eee8245fe
Paul <gafgafgarion05@gmail.com>
2021-10-20 11:13:56 +0300

    init

---
+++ 7 kyu Simple Fun #189 Plant Doubling.py
@@ -0,0 +1,11 @@
+# 7 kyu
+# Simple Fun #189: Plant Doubling
+
+def plant_doubling(n):
+    pass
+
+
+print(plant_doubling(5), 2)
+print(plant_doubling(8), 1)
+print(plant_doubling(536870911), 29)
+print(plant_doubling(1), 1)

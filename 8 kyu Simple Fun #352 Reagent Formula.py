a1c914ea34ab9ce9c24146d2d3661883d54c391b
Paul <gafgafgarion05@gmail.com>
2021-10-21 14:12:45 +0300

    init

---
+++ 8 kyu Simple Fun #352 Reagent Formula.py
@@ -0,0 +1,15 @@
+# 8 kyu
+# Simple Fun #352: Reagent Formula
+
+def isValid(formula):
+   pass
+
+
+print(isValid([1, 3, 7]), True)
+print(isValid([7, 1, 2, 3]), False)
+print(isValid([1, 3, 5, 7]), False)
+print(isValid([1, 5, 6, 7, 3]), True)
+print(isValid([5, 6, 7]), True)
+print(isValid([5, 6, 7, 8]), True)
+print(isValid([6, 7, 8]), False)
+print(isValid([7, 8]), True)

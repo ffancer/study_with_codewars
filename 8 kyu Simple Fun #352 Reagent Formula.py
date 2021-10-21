401488790982b041100c7c3e2a52025d0822907e
Paul <gafgafgarion05@gmail.com>
2021-10-21 14:45:18 +0300

    solution

--- 8 kyu Simple Fun #352 Reagent Formula.py
+++ 8 kyu Simple Fun #352 Reagent Formula.py
@@ -2,14 +2,9 @@
 # Simple Fun #352: Reagent Formula
 
 def isValid(formula):
-    if 1 and 2 in formula:
-        return False
-    if 3 and 4 in formula:
-        return False
-    if (5 in formula and 6 not in formula) or (6 in formula and 5 not in formula):
-        return False
-
-    return True
+    return (7 in formula or 8 in formula) and not (1 in formula and 2 in formula) and not (
+                3 in formula and 4 in formula) and (
+                       (5 in formula and 6 in formula) or (5 not in formula and 6 not in formula))
 
 
 print(isValid([1, 3, 7]), True)

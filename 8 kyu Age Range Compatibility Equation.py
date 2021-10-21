7ceb5b45aefeda7f6127bf0f0cbef163fb877eb0
Paul <gafgafgarion05@gmail.com>
2021-10-21 10:56:54 +0300

    init

---
+++ 8 kyu Age Range Compatibility Equation.py
@@ -0,0 +1,10 @@
+def dating_range(age):
+
+
+# return min-max
+
+print(dating_range(17), "15-20")
+print(dating_range(40), "27-66")
+print(dating_range(15), "14-16")
+print(dating_range(35), "24-56")
+print(dating_range(10), "9-11")

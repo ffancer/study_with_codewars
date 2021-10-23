b2907c9d7f2df074f628b1c8d376aebe936086a1
Paul <gafgafgarion05@gmail.com>
2021-10-23 11:01:16 +0300

    init

---
+++ 8 kyu Simple Fun #1 Seats in Theater.py
@@ -0,0 +1,14 @@
+# 8 kyu
+# Simple Fun #1: Seats in Theater
+
+
+def seats_in_theater(tot_cols, tot_rows, col, row):
+
+
+# your code here
+
+print(seats_in_theater(16, 11, 5, 3), 96)
+print(seats_in_theater(1, 1, 1, 1), 0)
+print(seats_in_theater(13, 6, 8, 3), 18)
+print(seats_in_theater(60, 100, 60, 1), 99)
+print(seats_in_theater(1000, 1000, 1000, 1000), 0)

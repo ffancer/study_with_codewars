a2f933a5385f573ab96909d1c97b6eae104ffeaa
Paul <gafgafgarion05@gmail.com>
2021-10-25 10:12:33 +0300

    init

---
+++ 8 kyu Thinkful - Number Drills Pixelart planning.py
@@ -0,0 +1,11 @@
+# 8 kyu
+# Thinkful - Number Drills: Pixelart planning
+
+def is_divisible(wall_length, pixel_size):
+    pass
+
+
+print(is_divisible(4050, 27), True)
+print(is_divisible(4066, 27), False)
+print(is_divisible(10000, 20), True)
+print(is_divisible(10005, 20), False)

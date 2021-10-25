d25a11b99beac7c2925d6b1d25d2daf1c38b5e14
Paul <gafgafgarion05@gmail.com>
2021-10-25 11:07:08 +0300

    init

---
+++ 7 kyu Divide and Conquer.py
@@ -0,0 +1,10 @@
+def div_con(x):
+    # your code here
+    return num
+
+
+print(div_con([9, 3, '7', '3']), 2)
+print(div_con(['5', '0', 9, 3, 2, 1, '9', 6, 7]), 14)
+print(div_con(['3', 6, 6, 0, '5', 8, 5, '6', 2, '0']), 13)
+print(div_con(['1', '5', '8', 8, 9, 9, 2, '3']), 11)
+print(div_con([8, 0, 0, 8, 5, 7, 2, 3, 7, 8, 6, 7]), 61)

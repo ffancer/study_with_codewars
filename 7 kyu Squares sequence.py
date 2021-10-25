0fcc4c470eff90e7f32ec6ffccaa855ba39a266e
Paul <gafgafgarion05@gmail.com>
2021-10-25 14:30:25 +0300

    init

---
+++ 7 kyu Squares sequence.py
@@ -0,0 +1,11 @@
+def squares(x, n):
+    # ...
+    return []
+
+
+print(squares(2, 5), [2, 4, 16, 256, 65536])
+print(squares(3, 3), [3, 9, 81])
+print(squares(5, 3), [5, 25, 625])
+print(squares(10, 4), [10, 100, 10000, 100000000])
+print(squares(2, 0), [])
+print(squares(2, -4), [])

c6ebd31992e49dbb007215f94450e7ac700721b8
Paul <gafgafgarion05@gmail.com>
2021-10-26 10:29:11 +0300

    init

---
+++ 7 kyu Split In Parts.py
@@ -0,0 +1,8 @@
+def split_in_parts(s, part_length):
+    # your code here
+    pass
+
+
+print(split_in_parts("supercalifragilisticexpialidocious", 3), "sup erc ali fra gil ist ice xpi ali doc iou s")
+print(split_in_parts("HelloKata", 1), "H e l l o K a t a")
+print(split_in_parts("HelloKata", 9), "HelloKata")

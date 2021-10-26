44716c6d621a7df473b1f9a32058002e412ff452
Paul <gafgafgarion05@gmail.com>
2021-10-26 10:50:42 +0300

    init

---
+++ 7 kyu Coding Meetup #6 - Higher-Order Functions Series - Can they code in the same language.py
@@ -0,0 +1,27 @@
+# 7 kyu
+# Coding Meetup #6 - Higher-Order Functions Series - Can they code in the same language?
+
+
+def is_same_language(lst):
+    # your code here
+    pass
+
+
+list1 = [
+    {'firstName': 'Daniel', 'lastName': 'J.', 'country': 'Aruba', 'continent': 'Americas', 'age': 42,
+     'language': 'JavaScript'},
+    {'firstName': 'Kseniya', 'lastName': 'T.', 'country': 'Belarus', 'continent': 'Europe', 'age': 22,
+     'language': 'JavaScript'},
+    {'firstName': 'Hanna', 'lastName': 'L.', 'country': 'Hungary', 'continent': 'Europe', 'age': 65,
+     'language': 'JavaScript'},
+]
+
+list2 = [
+    {'firstName': 'Mariami', 'lastName': 'G.', 'country': 'Georgia', 'continent': 'Europe', 'age': 29,
+     'language': 'Python'},
+    {'firstName': 'Mia', 'lastName': 'H.', 'country': 'Germany', 'continent': 'Europe', 'age': 39, 'language': 'Ruby'},
+    {'firstName': 'Maria', 'lastName': 'I.', 'country': 'Greece', 'continent': 'Europe', 'age': 32, 'language': 'C'},
+]
+
+print(is_same_language(list1), True)
+print(is_same_language(list2), False)

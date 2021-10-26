225fe54544128a93d8cae3a2df5c92d8abd77b3f
Paul <gafgafgarion05@gmail.com>
2021-10-26 14:10:26 +0300

    init

---
+++ 7 kyu Array Leaders (Array Series #3).py
@@ -0,0 +1,12 @@
+def array_leaders(numbers):
+    pass
+
+
+print(array_leaders([1, 2, 3, 4, 0]), [4])
+print(array_leaders([16, 17, 4, 3, 5, 2]), [17, 5, 2])
+
+print(array_leaders([-1, -29, -26, -2]), [-1])
+print(array_leaders([-36, -12, -27]), [-36, -12])
+
+print(array_leaders([5, 2]), [5, 2])
+print(array_leaders([0, -1, -29, 3, 2]), [0, -1, 3, 2])

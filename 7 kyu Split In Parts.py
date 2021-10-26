f1123b927ea28f65d564e38f16fbe3cb1638a8b5
Paul <gafgafgarion05@gmail.com>
2021-10-26 10:35:30 +0300

    1 line solution

--- 7 kyu Split In Parts.py
+++ 7 kyu Split In Parts.py
@@ -1,6 +1,5 @@
 def split_in_parts(s, part_length):
-    # your code here
-    pass
+    return ' '.join(s[i:i+part_length] for i in range(0, len(s), part_length))
 
 
 print(split_in_parts("supercalifragilisticexpialidocious", 3), "sup erc ali fra gil ist ice xpi ali doc iou s")

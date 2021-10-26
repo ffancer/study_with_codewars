1d019317d760427c3def6fedb086e43aedd58e74
Paul <gafgafgarion05@gmail.com>
2021-10-26 13:10:42 +0300

    init

---
+++ 7 kyu Pandemia 🌡️.py
@@ -0,0 +1,11 @@
+def infected(s):
+    pass
+
+
+print(infected("01000000X000X011X0X"))
+print(infected("01X000X010X011XX"))
+print(infected("XXXXX"))
+print(infected("00000000X00X0000"))
+print(infected("0000000010"))
+print(infected("000001XXXX0010X1X00010"))
+print(infected("X00X000000X10X0100"))

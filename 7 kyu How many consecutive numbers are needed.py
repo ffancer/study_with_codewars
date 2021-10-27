c38530a1503b3b0aa6c47f4705c2a11bee798e7d
Paul <gafgafgarion05@gmail.com>
2021-10-27 14:13:04 +0300

    init

---
+++ 7 kyu How many consecutive numbers are needed.py
@@ -0,0 +1,13 @@
+# 7 kyu
+# How many consecutive numbers are needed?
+
+
+def consecutive(arr):
+    pass
+
+
+print(consecutive([4, 8, 6]), 2)
+print(consecutive([1, 2, 3, 4]), 0)
+print(consecutive([]), 0)
+print(consecutive([1]), 0)
+print(consecutive([-10]), 0)

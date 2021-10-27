599c31872084277d3c60808ebc0cf5bd49b19a06
Paul <gafgafgarion05@gmail.com>
2021-10-27 13:36:24 +0300

    init

---
+++ 7 kyu L2 Triple X.py
@@ -0,0 +1,14 @@
+# 7 kyu
+# L2: Triple X
+
+
+def triple_x(s):
+    pass
+
+
+print(triple_x(""), False)
+print(triple_x("there's no XXX here"), False)
+print(triple_x("abraxxxas"), True)
+print(triple_x("xoxotrololololololoxxx"), False)
+print(triple_x("soft kitty, warm kitty, xxxxx"), True)
+print(triple_x("softx kitty, warm kitty, xxxxx"), False)

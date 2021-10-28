9b89183268ff450d97bdbbf235525c1e5ecaf818
Paul <gafgafgarion05@gmail.com>
2021-10-28 11:45:42 +0300

    3 best practices

--- 7 kyu Reversing Fun.py
+++ 7 kyu Reversing Fun.py
@@ -1,12 +1,22 @@
+# best practice 1
 def reverse_fun(n):
-    answer = ''
-
     for i in range(len(n)):
-        n = n[::-1]
-        answer += n[0]
-        n = n[1:]
+        n = n[:i] + n[i:][::-1]
+    return n
 
-    return answer
+
+# best practice 2
+def reverse_fun(n):
+    l = len(n)
+    return ''.join(b + a for a, b in zip(list(n[:l // 2]) + [''], n[l // 2:][::-1]))
+
+
+# best practice 3
+def reverse_fun(n):
+    n = n[::-1]
+    for i in range(1, len(n)):
+        n = n[:i] + n[i:][::-1]
+    return n
 
 
 print(reverse_fun('012345'), '504132')

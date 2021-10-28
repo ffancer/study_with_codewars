8f7d45ceba56d215a468be067da52a7402259ae4
Paul <gafgafgarion05@gmail.com>
2021-10-28 11:43:51 +0300

    solution

--- 7 kyu Reversing Fun.py
+++ 7 kyu Reversing Fun.py
@@ -1,6 +1,12 @@
 def reverse_fun(n):
-    # start flippin!
-    pass
+    answer = ''
+
+    for i in range(len(n)):
+        n = n[::-1]
+        answer += n[0]
+        n = n[1:]
+
+    return answer
 
 
 print(reverse_fun('012345'), '504132')

14e4b52df41c91b581f145133c13ee2f3d0a65c0
Paul <gafgafgarion05@gmail.com>
2021-10-28 14:16:05 +0300

    2 errors from 50 tests

--- 7 kyu Initialize my name.py
+++ 7 kyu Initialize my name.py
@@ -1,8 +1,11 @@
 def initialize_names(name):
-    pass
+    if len(name) > 2:
+        return name.split()[0] + ' ' + '. '.join([i[0] for i in name.split()[1:-1]]) + '. ' + name.split()[-1]
+    return name
 
 
 print(initialize_names('Jack Ryan'), 'Jack Ryan')
 print(initialize_names('Lois Mary Lane'), 'Lois M. Lane')
 print(initialize_names('Dimitri'), 'Dimitri')
 print(initialize_names('Alice Betty Catherine Davis'), 'Alice B. C. Davis')
+print(initialize_names('Noel Marshall'))

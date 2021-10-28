79a6bd4e94ebbdea212c14ed78b121626e346364
Paul <gafgafgarion05@gmail.com>
2021-10-28 14:08:28 +0300

    init

---
+++ 7 kyu Initialize my name.py
@@ -0,0 +1,8 @@
+def initialize_names(name):
+    pass
+
+
+print(initialize_names('Jack Ryan'), 'Jack Ryan')
+print(initialize_names('Lois Mary Lane'), 'Lois M. Lane')
+print(initialize_names('Dimitri'), 'Dimitri')
+print(initialize_names('Alice Betty Catherine Davis'), 'Alice B. C. Davis')

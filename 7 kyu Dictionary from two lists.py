6fd9c8c271dbc4752399666dab985d84268d0ab2
Paul <gafgafgarion05@gmail.com>
2021-10-28 15:00:30 +0300

    init

---
+++ 7 kyu Dictionary from two lists.py
@@ -0,0 +1,8 @@
+def createDict(keys, values):
+    """
+    Write your code here.
+    """
+
+
+print(createDict(['a', 'b', 'c', 'd'], [1, 2, 3]), {'a': 1, 'b': 2, 'c': 3, 'd': None})
+print(createDict(['a', 'b', 'c'], [1, 2, 3, 4]), {'a': 1, 'b': 2, 'c': 3})

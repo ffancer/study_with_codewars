54b08d480811a4fd1e51c09c9c19f9696693b333
Paul <gafgafgarion05@gmail.com>
2021-10-28 10:26:05 +0300

    init

---
+++ 7 kyu Coding Meetup #11 - Higher-Order Functions Series - Find the average age.py
@@ -0,0 +1,17 @@
+def get_average(lst):
+    # your code here
+    pass
+
+
+list1 = [
+    {'firstName': 'Maria', 'lastName': 'Y.', 'country': 'Cyprus', 'continent': 'Europe', 'age': 30, 'language': 'Java'},
+    {'firstName': 'Victoria', 'lastName': 'T.', 'country': 'Puerto Rico', 'continent': 'Americas', 'age': 70,
+     'language': 'Python'},
+]
+print(get_average(list1), 50)
+
+list2 = [
+    {'firstName': 'Noa', 'lastName': 'A.', 'country': 'Israel', 'continent': 'Asia', 'age': 20, 'language': 'Ruby'},
+    {'firstName': 'Andrei', 'lastName': 'E.', 'country': 'Romania', 'continent': 'Europe', 'age': 21, 'language': 'C'},
+]
+print(get_average(list2), 20)

b98d0924bbed0751ba45fc1226e91b2efd175e0b
Paul <gafgafgarion05@gmail.com>
2021-10-28 16:05:32 +0300

    4 best practices

--- 7 kyu Return the closest number multiple of 10.py
+++ 7 kyu Return the closest number multiple of 10.py
@@ -1,6 +1,21 @@
+# best practice 1:
 def closest_multiple_10(i):
-    return int(round(i / 10.0) * 10.0)
+    return round(i, -1)
 
+
+# best practice 2:
+def closest_multiple_10(i):
+    return round(i / 10) * 10
+
+
+# best practice 3:
+def closest_multiple_10(i):
+    r = i % 10
+    return i - r if r < 5 else i - r + 10
+
+
+# best practice 4:
+closest_multiple_10 = lambda x: round(x, -1)
 
 print(closest_multiple_10(54), 50)
 print(closest_multiple_10(55), 60)

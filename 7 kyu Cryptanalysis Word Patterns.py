89f5cc5d42e2b2121f6594e8093d7a3b28f56ecf
Paul <gafgafgarion05@gmail.com>
2021-10-29 13:08:18 +0300

    init

---
+++ 7 kyu Cryptanalysis Word Patterns.py
@@ -0,0 +1,9 @@
+def word_pattern(word):
+    pass
+
+
+print(word_pattern("hello"), "0.1.2.2.3"),
+print(word_pattern("heLlo"), "0.1.2.2.3"),
+print(word_pattern("helLo"), "0.1.2.2.3"),
+print(word_pattern("Hippopotomonstrosesquippedaliophobia"),
+      "0.1.2.2.3.2.3.4.3.5.3.6.7.4.8.3.7.9.7.10.11.1.2.2.9.12.13.14.1.3.2.0.3.15.1.13")

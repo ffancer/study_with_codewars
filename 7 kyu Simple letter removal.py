506df52c48de1094ce14f764f219ae99ca6ed5cf
Paul <gafgafgarion05@gmail.com>
2021-10-29 11:38:05 +0300

    solution

--- 7 kyu Simple letter removal.py
+++ 7 kyu Simple letter removal.py
@@ -1,5 +1,7 @@
 def solve(st,k):
-    pass
+    for i in sorted(st)[:k]:
+        st = st.replace(i, '', 1)
+    return st
 
 print(solve('abracadabra', 1),'bracadabra')
 print(solve('abracadabra', 2),'brcadabra')

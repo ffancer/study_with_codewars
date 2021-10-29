141484ec64b8a940cdc4539da2e389260058d369
Paul <gafgafgarion05@gmail.com>
2021-10-29 11:37:47 +0300

    init

---
+++ 7 kyu Simple letter removal.py
@@ -0,0 +1,8 @@
+def solve(st,k):
+    pass
+
+print(solve('abracadabra', 1),'bracadabra')
+print(solve('abracadabra', 2),'brcadabra')
+print(solve('abracadabra', 6),'rcdbr')
+print(solve('abracadabra', 8),'rdr')
+print(solve('abracadabra', 50),'')

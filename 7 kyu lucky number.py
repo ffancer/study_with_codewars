0084b2f76e038a72bc807fdde42918a4864858d6
Paul <gafgafgarion05@gmail.com>
2021-10-30 09:36:22 +0300

    init

---
+++ 7 kyu lucky number.py
@@ -0,0 +1,10 @@
+def is_lucky(n):
+    raise Exception("TODO: is_lucky")
+
+
+print(is_lucky(1892376), True, "Wrong result for 1892376")
+print(is_lucky(189237), False, "Wrong result for 189237")
+print(is_lucky(18922314324324234423437), False, "Wrong result for 18922314324324234423437")
+print(is_lucky(189223141324324234423437), True, "Wrong result for 189223141324324234423437")
+print(is_lucky(1892231413243242344321432142343423437), True, "Wrong result for 1892231413243242344321432142343423437")
+print(is_lucky(0), True, "Wrong result for 0")

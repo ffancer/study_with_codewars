1bbfabf9770abae32a613583f7b9e64e37def09a
Paul <gafgafgarion05@gmail.com>
2021-10-30 11:32:10 +0300

    init

---
+++ 7 kyu Excessively Abundant Numbers.py
@@ -0,0 +1,13 @@
+def abundant_number(num):
+    # Your code here
+
+print(abundant_number(12), True)
+print(abundant_number(18), True)
+print(abundant_number(37), False)
+print(abundant_number(120), True)
+print(abundant_number(77), False)
+print(abundant_number(118), False)
+print(abundant_number(5830), True)
+print(abundant_number(11410), True)
+print(abundant_number(14771), False)
+print(abundant_number(11690), True)

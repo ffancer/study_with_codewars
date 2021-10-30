6104fa9dee2a5a1ba25579e7643a046c3ae9f20f
Paul <gafgafgarion05@gmail.com>
2021-10-30 11:15:07 +0300

    init

---
+++ 7 kyu Simple Fun #63 Shape Area.py
@@ -0,0 +1,12 @@
+# 7 kyu
+# Simple Fun #63: Shape Area
+
+def shape_area(n):
+
+
+# coding and coding..
+
+print(shape_area(2), 5)
+print(shape_area(3), 13)
+print(shape_area(1), 1)
+print(shape_area(5), 41)

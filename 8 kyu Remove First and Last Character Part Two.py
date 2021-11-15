b2ba488e3be5edab0b8222279156d437d8af0caf
Paul <gafgafgarion05@gmail.com>
2021-11-15 14:37:37 +0300

    init

---
+++ 8 kyu Remove First and Last Character Part Two.py
@@ -0,0 +1,10 @@
+def array(string):
+    pass
+
+
+print(array('1,2,3'), '2')
+print(array('1,2,3,4'), '2 3')
+print(array('1,2,3,4,5'), '2 3 4')
+print(array(''), None)
+print(array('1'), None)
+print(array('1,2'), None)

61697c83b0135bcf03b53536bb51b84333ea6d01
Paul <gafgafgarion05@gmail.com>
2021-11-15 13:20:29 +0300

    solution

--- 8 kyu Points of Reflection.py
+++ 8 kyu Points of Reflection.py
@@ -1,6 +1,5 @@
 def symmetric_point(p, q):
-    # your code here
-    return []
+    return [q[0] * 2 - p[0], q[1] * 2 - p[1]]
 
 
 print(symmetric_point([0, 0], [1, 1]), [2, 2])
@@ -9,3 +8,4 @@ print(symmetric_point([10, -10], [-10, 10]), [-30, 30])
 print(symmetric_point([1, -35], [-12, 1]), [-25, 37])
 print(symmetric_point([1000, 15], [-7, -214]), [-1014, -443])
 print(symmetric_point([0, 0], [0, 0]), [0, 0])
+print(symmetric_point([0, 1], [2, 3]), [4, 5])  # my test

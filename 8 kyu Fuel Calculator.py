9281bb635b5b4c193da0e829167942de9ce6923b
Paul <gafgafgarion05@gmail.com>
2021-11-15 11:23:06 +0300

    solution

--- 8 kyu Fuel Calculator.py
+++ 8 kyu Fuel Calculator.py
@@ -1,14 +1,17 @@
 def fuel_price(litres, price_per_litre):
     discount = 0
+
     if 4 > litres >= 2:
-        discount = price_per_litre * 0.05
+        discount = 0.05
     elif 6 > litres >= 4:
-        discount = price_per_litre * 0.1
+        discount = 0.1
     elif 8 > litres >= 6:
-        discount = price_per_litre * 0.2
-    elif 10 >= litres >= 8:
-        discount = price_per_litre * 0.25
-    return litres * price_per_litre - litres * discount
+        discount = 0.2
+    elif litres >= 8:
+        discount = 0.25
+
+    return round(litres * price_per_litre - litres * discount, 2)
+
 
 
 print(fuel_price(10, 21.5), 212.5)

2e1e762902f9884cd24fc4ac291b5d36c8eeef24
Paul <gafgafgarion05@gmail.com>
2021-11-15 13:14:21 +0300

    init

---
+++ 8 kyu Points of Reflection.py
@@ -0,0 +1,11 @@
+def symmetric_point(p, q):
+    # your code here
+    return []
+
+
+print(symmetric_point([0, 0], [1, 1]), [2, 2])
+print(symmetric_point([2, 6], [-2, -6]), [-6, -18])
+print(symmetric_point([10, -10], [-10, 10]), [-30, 30])
+print(symmetric_point([1, -35], [-12, 1]), [-25, 37])
+print(symmetric_point([1000, 15], [-7, -214]), [-1014, -443])
+print(symmetric_point([0, 0], [0, 0]), [0, 0])

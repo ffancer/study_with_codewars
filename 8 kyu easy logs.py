a4ab34427634203bc8214b73d697956d56ed328b
Paul <gafgafgarion05@gmail.com>
2021-11-15 13:08:07 +0300

    init

---
+++ 8 kyu easy logs.py
@@ -0,0 +1,8 @@
+def logs(x, a, b):
+    # Your code here
+    pass
+
+
+print(logs(10, 2, 3), 0.7781512503836435)
+print(logs(5, 2, 3), 1.1132827525593785)
+print(logs(1000, 2, 3), 0.25938375012788123)

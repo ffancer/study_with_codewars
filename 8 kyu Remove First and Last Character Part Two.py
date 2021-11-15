1a878dd4a6d0e73a6fcc4dcb46192559b36ef139
Paul <gafgafgarion05@gmail.com>
2021-11-15 14:55:04 +0300

    solution just type 'or None' and it's work perfectly

--- 8 kyu Remove First and Last Character Part Two.py
+++ 8 kyu Remove First and Last Character Part Two.py
@@ -1,7 +1,5 @@
 def array(string):
-    if string == '' or len(string) <= 3:
-        return None
-    return ' '.join(string.split(",")[1:-1])
+    return ' '.join(string.split(",")[1:-1]) or None
 
 
 print(array('1,2,3'), '2')

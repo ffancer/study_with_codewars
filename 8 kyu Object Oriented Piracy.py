59c4c57b86bbe61b7f8b6eddd1dd69fc68f624f1
Paul <gafgafgarion05@gmail.com>
2021-11-16 14:54:41 +0300

    solution

--- 8 kyu Object Oriented Piracy.py
+++ 8 kyu Object Oriented Piracy.py
@@ -2,7 +2,9 @@ class Ship:
     def __init__(self, draft, crew):
         self.draft = draft
         self.crew = crew
-    # Your code here
+
+    def is_worth_it(self):
+        return True if self.draft - self.crew * 1.5 > 20 else False
 
 
 EmptyShip = Ship(0, 0)

4aed1f929af4d60a1af9fa4a986a71d6c7396313
Paul <gafgafgarion05@gmail.com>
2021-11-16 15:18:32 +0300

    solution

--- 8 kyu Pillars.py
+++ 8 kyu Pillars.py
@@ -1,5 +1,5 @@
 def pillars(num_pill, dist, width):
-    return (num_pill - 1) * dist * 100 + (num_pill - 2) * width
+    return (num_pill - 1) * dist * 100 + (num_pill - 2) * width if num_pill > 1 else 0
 
 
 print(pillars(1, 10, 10), 0)

9a4ff3006fba6f7fdc70c8159501b1520b37ac07
Paul <gafgafgarion05@gmail.com>
2021-11-16 13:36:03 +0300

    init

---
+++ 8 kyu Holiday VI - Shark Pontoon.py
@@ -0,0 +1,7 @@
+def shark(pontoon_distance, shark_distance, you_speed, shark_speed, dolphin):
+    pass # Don't get eaten!
+
+
+print(shark(12, 50, 4, 8, True), "Alive!")
+print(shark(7, 55, 4, 16, True), "Alive!")
+print(shark(24, 0, 4, 8, True), "Shark Bait!")

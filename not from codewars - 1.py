80a9da720e8328f460d69d0c67fcc64f0f889f4b
Paul <gafgafgarion05@gmail.com>
2021-11-17 11:04:43 +0300

    init

---
+++ not from codewars - 1.py
@@ -0,0 +1,3 @@
+# Заполнить массив нулями, кроме первого и последнего элементов, которые должны быть равны единице.
+# Fill the array with zeros, except for the first and last elements, which must be equal to one.
+

9dcc790b9677a7573696a328b1c3951d72aaf255
Paul <gafgafgarion05@gmail.com>
2021-11-17 11:16:01 +0300

    init

---
+++ not from codewars - 2.py
@@ -0,0 +1,12 @@
+# Заполнить массив нулями и единицами, при этом данные значения чередуются, начиная с нуля.
+# Fill the array with zeros and ones, with these values ​​alternating starting from zero.
+
+
+def fill_list(n):
+    pass
+
+
+print(fill_list(0))
+print(fill_list(2))
+print(fill_list(10))
+print(fill_list(5))

403c1b16f1e8703f08983318d586351c8ca59ff2
Paul <gafgafgarion05@gmail.com>
2021-11-17 13:48:07 +0300

    init

---
+++ not from codewars - 5.py
@@ -0,0 +1,4 @@
+# Компьютер загадывает число от 1 до n. У пользователя k попыток отгадать. После каждой неудачной попытки компьютер сообщает меньше или больше загаданное число. В конце игры текст с результатом (или “Вы угадали”, или “Попытки закончились”).
+# The computer thinks out a number from 1 to n. The user has k attempts to guess. After each unsuccessful attempt, the computer reports less or more of the hidden number. At the end of the game, the text with the result (either “You guessed”, or “Attempts ended”).
+
+

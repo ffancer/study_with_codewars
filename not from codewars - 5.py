7036c62f18c759668f2343ca3a3bf42f7f213578
Paul <gafgafgarion05@gmail.com>
2021-11-17 14:48:27 +0300

    -

--- not from codewars - 5.py
+++ not from codewars - 5.py
@@ -3,34 +3,34 @@
 
 
 from random import randrange
+
 print('Угадайте число от 0 до 10/Guess a number from 0 to 10')
 num_of_attempts = 1
 num = randrange(0, 11, 1)
 print(num)
 print('Число попыток/Number of attempts - 3')
-user_input = int(input('Напишите число/Write the number: '))
 
-# while type(user_input) != int or 0 > int(user_input) > 10:
-#     print('Вы ввели не число, введите число от 0 до 10/You have not entered a number, please enter a number from 0 to 10')
-#     user_input = input('Напишите число/Write the number: ')
-
-if user_input == num:
-    print(f'Вы угадали число!/You guess a number! - {num}')
-    print(f'Вам понадобилось {num_of_attempts} попыток/You need {num_of_attempts} tries')
-
-while user_input != num:
-
-    print(f'Число попыток/Number of attempts - {num_of_attempts}')
+try:
     user_input = int(input('Напишите число/Write the number: '))
-    num_of_attempts += 1
 
-    if num_of_attempts == 3:
-        print('Попытки кончились и вы не угадали число/Attempts ended and you did not guess the number')
-        break
+    # while type(user_input) != int or 0 > int(user_input) > 10:
+    #
+    #     user_input = input('Напишите число/Write the number: ')
 
+    if user_input == num:
+        print(f'Вы угадали число!/You guess a number! - {num}')
+        print(f'Вам понадобилось {num_of_attempts} попыток/You need {num_of_attempts} tries')
 
+    while user_input != num:
 
+        print(f'Число попыток/Number of attempts - {num_of_attempts}')
+        user_input = int(input('Напишите число/Write the number: '))
+        num_of_attempts += 1
 
-
-
-
+        if num_of_attempts == 3:
+            print('Попытки кончились и вы не угадали число/Attempts ended and you did not guess the number')
+            break
+except:
+    print(
+        'Вы ввели не число, введите число от 0 до 10/You have not entered a number, please enter a number from 0 to 10')
+    user_input = input('Напишите число/Write the number: ')

e2b69edc4dbf548e8623bc48c15d29a507a5987a
Paul <gafgafgarion05@gmail.com>
2021-11-17 11:30:10 +0300

    init

---
+++ not from codewars - 3.py
@@ -0,0 +1,12 @@
+# Заполнить массив последовательными нечетными числами, начиная с единицы.
+# Fill the array with consecutive odd numbers, starting at one.
+
+def fill_list(n):
+    pass
+
+
+print(fill_list(0))
+print(fill_list(1))
+print(fill_list(5))
+print(fill_list(11))
+print(fill_list(15))

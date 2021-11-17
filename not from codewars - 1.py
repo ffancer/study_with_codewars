75233c72c4e6231f46635d194b82b0f80c3a3dec
Paul <gafgafgarion05@gmail.com>
2021-11-17 11:05:02 +0300

    simple solution

--- not from codewars - 1.py
+++ not from codewars - 1.py
@@ -1,3 +1,7 @@
 # Заполнить массив нулями, кроме первого и последнего элементов, которые должны быть равны единице.
 # Fill the array with zeros, except for the first and last elements, which must be equal to one.
 
+n = 5
+lst = [str(i).replace(str(i), '0') for i in range(n)]
+lst = [int(i) for i in lst]
+print([1] + lst[1:-1] + [1])

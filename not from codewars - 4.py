07451159588f3608c00d7293c2dc4f8b5d743aea
Paul <gafgafgarion05@gmail.com>
2021-11-17 13:03:45 +0300

    init

---
+++ not from codewars - 4.py
@@ -0,0 +1,13 @@
+# Дан текст. Найти сумму имеющихся в нем цифр.
+# Given text. Find the sum of the digits in it.
+
+def sum_digit(s):
+    pass
+
+
+print(sum_digit('1ght57676g'))
+print(sum_digit('1n'))
+print(sum_digit('0vc0vg0mg5'))
+print(sum_digit('123ghmnbd123'))
+print(sum_digit('123456'))
+print(sum_digit('mnbiu'))

fc4e322e2ca04abf2970247585e9167e86ce5af0
Paul <gafgafgarion05@gmail.com>
2021-11-19 10:50:36 +0300

    3 errors in tests but tests are hidden (sad)

--- 8 kyu Regexp Basics - is it a digit.py
+++ 8 kyu Regexp Basics - is it a digit.py
@@ -2,8 +2,10 @@
 # Regexp Basics - is it a digit?
 
 def is_digit(n):
-    pass
-
+    try:
+        return True if 9 >= int(n) >= 0 else False
+    except ValueError:
+        return False
 
 print(is_digit(""), False)
 print(is_digit("7"), True)

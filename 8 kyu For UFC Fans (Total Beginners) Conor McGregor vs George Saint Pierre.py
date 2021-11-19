d2fb074713eee82953bac8dfc80be8a97fc88c35
Paul <gafgafgarion05@gmail.com>
2021-11-19 13:29:40 +0300

    solution

--- 8 kyu For UFC Fans (Total Beginners) Conor McGregor vs George Saint Pierre.py
+++ 8 kyu For UFC Fans (Total Beginners) Conor McGregor vs George Saint Pierre.py
@@ -3,7 +3,7 @@
 
 
 def quote(fighter):
-    pass
+    return "I am not impressed by your performance." if fighter.lower() == 'george saint pierre' else "I'd like to take this chance to apologize.. To absolutely NOBODY!"
 
 
 print(quote('george saint pierre'), "I am not impressed by your performance.")

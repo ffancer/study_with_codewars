032659b766dff422b6dac1da0dcb69a43a1fdc29
Paul <gafgafgarion05@gmail.com>
2021-11-19 13:44:09 +0300

    init

---
+++ 7 kyu Breaking chocolate problem.py
@@ -0,0 +1,9 @@
+def break_chocolate(n, m):
+    return 0
+
+
+print(break_chocolate(5, 5), 24)
+print(break_chocolate(7, 4), 27)
+print(break_chocolate(1, 1), 0)
+print(break_chocolate(0, 0), 0)
+print(break_chocolate(6, 1), 5)

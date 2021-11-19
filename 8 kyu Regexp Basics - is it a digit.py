5859f47da442420c9e1147de6b2a8e06ef180d7a
Paul <gafgafgarion05@gmail.com>
2021-11-19 10:41:41 +0300

    init

---
+++ 8 kyu Regexp Basics - is it a digit.py
@@ -0,0 +1,12 @@
+# 8 kyu
+# Regexp Basics - is it a digit?
+
+def is_digit(n):
+    pass
+
+
+print(is_digit(""), False)
+print(is_digit("7"), True)
+print(is_digit(" "), False)
+print(is_digit("a"), False)
+print(is_digit("a5"), False)

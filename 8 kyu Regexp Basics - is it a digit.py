c53c5401f882f91c09e60a57466960958ee7d070
Paul <gafgafgarion05@gmail.com>
2021-11-19 10:56:34 +0300

    solution haha it was fun, so stupid solution

--- 8 kyu Regexp Basics - is it a digit.py
+++ 8 kyu Regexp Basics - is it a digit.py
@@ -2,13 +2,11 @@
 # Regexp Basics - is it a digit?
 
 def is_digit(n):
-    try:
-        return True if 9 >= int(n) >= 0 else False
-    except ValueError:
-        return False
+    return True if n in ['0','1','2','3','4','5','6','7','8','9'] else False
 
 print(is_digit(""), False)
 print(is_digit("7"), True)
 print(is_digit(" "), False)
 print(is_digit("a"), False)
 print(is_digit("a5"), False)
+print(is_digit("1\n0"))

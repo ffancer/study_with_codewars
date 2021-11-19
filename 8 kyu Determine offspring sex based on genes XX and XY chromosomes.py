d9400747db259d98402c2302978633f027e919af
Paul <gafgafgarion05@gmail.com>
2021-11-19 10:26:26 +0300

    init

---
+++ 8 kyu Determine offspring sex based on genes XX and XY chromosomes.py
@@ -0,0 +1,6 @@
+def chromosome_check(sperm):
+    pass
+
+
+print(chromosome_check('XY'), 'Congratulations! You\'re going to have a son.')
+print(chromosome_check('XX'), 'Congratulations! You\'re going to have a daughter.')

e983c16d81fb7ff7bda901c46305f2aa0ee79429
Paul <gafgafgarion05@gmail.com>
2021-11-20 11:09:36 +0300

    init

---
+++ 8 kyu Duck Duck Goose.py
@@ -0,0 +1,15 @@
+def duck_duck_goose(players, goose):
+    pass
+
+
+players = ['a', 'b', 'c']
+print(duck_duck_goose(players, 1),  "a")
+print(duck_duck_goose(players, 3),  "c")
+print(duck_duck_goose(players, 10), "z")
+print(duck_duck_goose(players, 20), "z")
+print(duck_duck_goose(players, 30), "z")
+print(duck_duck_goose(players, 18), "g")
+print(duck_duck_goose(players, 28), "g")
+print(duck_duck_goose(players, 12), "b")
+print(duck_duck_goose(players, 2),  "b")
+print(duck_duck_goose(players, 7),  "f")

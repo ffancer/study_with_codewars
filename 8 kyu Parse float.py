e0a75f7ee666fd87cd8a0bd24e6b5477c4cfa5ea
Paul <gafgafgarion05@gmail.com>
2021-11-22 10:16:59 +0300

    1

--- 8 kyu Parse float.py
+++ 8 kyu Parse float.py
@@ -1,7 +1,12 @@
 def parse_float(string):
-    pass
+    string_duplicate = string.replace('.', '')
+
+    if string_duplicate.isdigit():
+        return string
+    return string
 
 
 print(parse_float("1.0"))
 print(parse_float("a"))
 print(parse_float("234.0234"))
+print(parse_float('111'))

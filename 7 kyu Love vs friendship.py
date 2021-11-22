fd1a7b9e672c9517b0fe00677f3ac8afd60eef54
Paul <gafgafgarion05@gmail.com>
2021-11-22 13:05:07 +0300

    init

---
+++ 7 kyu Love vs friendship.py
@@ -0,0 +1,9 @@
+def words_to_marks(s):
+    pass
+
+
+print(words_to_marks('attitude'), 100)
+print(words_to_marks('friends'), 75)
+print(words_to_marks('family'), 66)
+print(words_to_marks('selfness'), 99)
+print(words_to_marks('knowledge'), 96)

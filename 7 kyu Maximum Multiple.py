fc72428bf85cfe20459e525299e9801f68cc351a
Paul <gafgafgarion05@gmail.com>
2021-11-22 09:53:59 +0300

    init

---
+++ 7 kyu Maximum Multiple.py
@@ -0,0 +1,9 @@
+def max_multiple(divisor, bound):
+    pass
+
+print(max_multiple(2, 7), 6)
+print(max_multiple(3, 10), 9)
+print(max_multiple(7, 17), 14)
+print(max_multiple(10, 50), 50)
+print(max_multiple(37, 200), 185)
+print(max_multiple(7, 100), 98)

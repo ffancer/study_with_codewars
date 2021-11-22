4034176241666fca3257daa41b9221065d608289
Paul <gafgafgarion05@gmail.com>
2021-11-22 09:56:37 +0300

    solution

--- 7 kyu Maximum Multiple.py
+++ 7 kyu Maximum Multiple.py
@@ -1,5 +1,11 @@
 def max_multiple(divisor, bound):
-    pass
+    a = 0
+
+    for i in range(divisor, bound+1):
+        if i % divisor == 0:
+            a = i
+
+    return a
 
 print(max_multiple(2, 7), 6)
 print(max_multiple(3, 10), 9)

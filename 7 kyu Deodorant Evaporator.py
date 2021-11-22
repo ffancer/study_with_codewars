346495a0a8a39e36141d5f7ad2dfaac4f4ae2dc0
Paul <gafgafgarion05@gmail.com>
2021-11-22 10:58:26 +0300

    solution

--- 7 kyu Deodorant Evaporator.py
+++ 7 kyu Deodorant Evaporator.py
@@ -1,5 +1,11 @@
 def evaporator(content, evap_per_day, threshold):
-    return None
+    percentage = 100
+    result = 0
+    while percentage > threshold:
+        percentage = percentage - percentage * (evap_per_day / 100)
+        result += 1
 
+    return result
 
 print(evaporator(10, 10, 10), 22)
+

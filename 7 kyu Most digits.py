fc5373ca33e763e2e820edcef13dd6f98ae0c39b
Paul <gafgafgarion05@gmail.com>
2021-11-23 10:28:06 +0300

    init

---
+++ 7 kyu Most digits.py
@@ -0,0 +1,9 @@
+def find_longest(arr):
+    pass
+
+
+print(find_longest([1, 10, 100]), 100)
+print(find_longest([9000, 8, 800]), 9000)
+print(find_longest([8, 900, 500]), 900)
+print(find_longest([3, 40000, 100]), 40000)
+print(find_longest([1, 200, 100000]), 100000)

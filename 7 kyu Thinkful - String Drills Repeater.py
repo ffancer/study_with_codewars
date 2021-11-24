8c9b8e4ef14766b537442743ed014b18d4f2a05e
Paul <gafgafgarion05@gmail.com>
2021-11-24 13:26:20 +0300

    init

---
+++ 7 kyu Thinkful - String Drills Repeater.py
@@ -0,0 +1,13 @@
+# 7 kyu
+# Thinkful - String Drills: Repeater
+
+
+def repeater(string, n):
+
+
+# Your code goes here.
+
+
+print(repeater('a', 5), 'aaaaa')
+print(repeater('Na', 16), 'NaNaNaNaNaNaNaNaNaNaNaNaNaNaNaNa')
+print(repeater('Wub ', 6), 'Wub Wub Wub Wub Wub Wub ')

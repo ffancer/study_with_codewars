685dfd9bf4afde5bffde8713a9748bda1b5aed22
Paul <gafgafgarion05@gmail.com>
2021-11-24 10:56:29 +0300

    init

---
+++ 7 kyu Moves in squared strings (I).py
@@ -0,0 +1,21 @@
+def vert_mirror(strng):
+
+
+# your code
+def hor_mirror(strng):
+
+
+# your code
+def oper(fct, s):
+
+
+# your code
+
+
+print(oper(vert_mirror, "hSgdHQ\nHnDMao\nClNNxX\niRvxxH\nbqTVvA\nwvSyRu"),
+      "QHdgSh\noaMDnH\nXxNNlC\nHxxvRi\nAvVTqb\nuRySvw")
+print(oper(vert_mirror, "IzOTWE\nkkbeCM\nWuzZxM\nvDddJw\njiJyHF\nPVHfSx"),
+      "EWTOzI\nMCebkk\nMxZzuW\nwJddDv\nFHyJij\nxSfHVP")
+
+print(oper(hor_mirror, "lVHt\nJVhv\nCSbg\nyeCt"), "yeCt\nCSbg\nJVhv\nlVHt")
+print(oper(hor_mirror, "njMK\ndbrZ\nLPKo\ncEYz"), "cEYz\nLPKo\ndbrZ\nnjMK")

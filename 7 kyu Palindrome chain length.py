3c34c7c6d3bb98f6510f42ad9f4cf26e72fd31bd
Paul <gafgafgarion05@gmail.com>
2021-11-24 15:18:58 +0300

    1/5 of tests are fail

--- 7 kyu Palindrome chain length.py
+++ 7 kyu Palindrome chain length.py
@@ -1,8 +1,10 @@
 def palindrome_chain_length(n):
-    # parameter n is a positive integer
-    # your function should return the number of steps
-    pass
-
+    cnt = 0
+    n = n + int(str(n)[::-1])
+    while str(n) != str(n)[::-1]:
+        cnt += 1
+        n = int(n) + int(str(n)[::-1])
+    return cnt + 1
 
 print(palindrome_chain_length(1), 0)
 print(palindrome_chain_length(88), 0)

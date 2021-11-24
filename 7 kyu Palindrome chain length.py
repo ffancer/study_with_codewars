849a735d4879a44a26ac0af2d8f2224530677687
Paul <gafgafgarion05@gmail.com>
2021-11-24 14:13:40 +0300

    init

---
+++ 7 kyu Palindrome chain length.py
@@ -0,0 +1,11 @@
+def palindrome_chain_length(n):
+    # parameter n is a positive integer
+    # your function should return the number of steps
+    pass
+
+
+print(palindrome_chain_length(1), 0)
+print(palindrome_chain_length(88), 0)
+print(palindrome_chain_length(87), 4)
+print(palindrome_chain_length(89), 24)
+print(palindrome_chain_length(10), 1)

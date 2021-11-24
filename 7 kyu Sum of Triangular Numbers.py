d7523f18dc9381241c90ca451414792e574271e2
Paul <gafgafgarion05@gmail.com>
2021-11-24 13:10:53 +0300

    init

---
+++ 7 kyu Sum of Triangular Numbers.py
@@ -0,0 +1,10 @@
+def sum_triangular_numbers(n):
+    # your code here
+    return sum_
+
+
+print(sum_triangular_numbers(6), 56)
+print(sum_triangular_numbers(34), 7140)
+print(sum_triangular_numbers(-291), 0)
+print(sum_triangular_numbers(943), 140205240)
+print(sum_triangular_numbers(-971), 0)

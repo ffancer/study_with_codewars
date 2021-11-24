b8182b8971198f56f22fa048a04ff985aacb887c
Paul <gafgafgarion05@gmail.com>
2021-11-24 10:46:21 +0300

    1 line solution replace many arguments with *ages

--- 7 kyu Predict your age!.py
+++ 7 kyu Predict your age!.py
@@ -1,8 +1,5 @@
-def predict_age(age_1, age_2, age_3, age_4, age_5, age_6, age_7, age_8):
-    multiply_and_sum = age_1 ** 2 + age_2 ** 2 + age_3 ** 2 + age_4 ** 2 + age_5 ** 2 + age_6 ** 2 + age_7 ** 2 + age_8 ** 2
-    squre_root = multiply_and_sum ** 0.5
-    divide_by_two = squre_root // 2
+def predict_age(*ages):
+    return int(sum(i * i for i in ages) ** 0.5 // 2)
 
-    return int(divide_by_two)
 
 print(predict_age(65, 60, 75, 55, 60, 63, 64, 45))

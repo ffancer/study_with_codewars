10d441919fd42ece621acf3b85ca790cff3dd8d0
Paul <gafgafgarion05@gmail.com>
2021-11-24 15:19:53 +0300

    solution

--- 7 kyu Palindrome chain length.py
+++ 7 kyu Palindrome chain length.py
@@ -1,10 +1,11 @@
 def palindrome_chain_length(n):
     cnt = 0
-    n = n + int(str(n)[::-1])
+
     while str(n) != str(n)[::-1]:
         cnt += 1
         n = int(n) + int(str(n)[::-1])
-    return cnt + 1
+
+    return cnt
 
 print(palindrome_chain_length(1), 0)
 print(palindrome_chain_length(88), 0)

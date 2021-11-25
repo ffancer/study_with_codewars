f9529092543f56caeb4c45d505f774ab8846e2a4
Paul <gafgafgarion05@gmail.com>
2021-11-25 11:07:43 +0300

    init

---
+++ 7 kyu Sum of numbers from 0 to N.py
@@ -0,0 +1,9 @@
+def show_sequence(n):
+    pass
+
+
+print((show_sequence(6)))  # "0+1+2+3+4+5+6 = 21"
+print((show_sequence(7)))  # "0+1+2+3+4+5+6+7 = 28"
+print((show_sequence(0)))
+print((show_sequence(-1)))
+print((show_sequence(-10)))

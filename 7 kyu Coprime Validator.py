b4c5a87b5b0dca2692c68b67155c58e6c71c46a1
Paul <gafgafgarion05@gmail.com>
2021-11-27 09:44:51 +0300

    best practice second with 'gcd' doesn't work, so I don't add it

--- 7 kyu Coprime Validator.py
+++ 7 kyu Coprime Validator.py
@@ -1,18 +1,9 @@
+# best practice 1:
+# Euclid algorithm
 def are_coprime(n, m):
-    list_factors_n, list_factors_m, list_factors_n_and_m = [], [], []
-
-    for i in range(1, n + 1):
-        if n % i == 0:
-            list_factors_n.append(i)
-    for i in range(1, m + 1):
-        if m % i == 0:
-            list_factors_m.append(i)
-
-    for i in list_factors_n:
-        if i in list_factors_m:
-            list_factors_n_and_m.append(i)
-
-    return max(list_factors_n_and_m) == 1
+    while m > 0:
+        n, m = m, n % m
+    return n == 1
 
 
 print(are_coprime(20, 27), True, 'The numbers were 20 and 27')

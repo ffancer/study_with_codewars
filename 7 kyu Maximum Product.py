1898ae870c9b0dc362a1bc090f56b77c5ec27731
Paul <gafgafgarion05@gmail.com>
2021-11-27 09:17:24 +0300

    init

---
+++ 7 kyu Maximum Product.py
@@ -0,0 +1,16 @@
+def adjacent_element_product(array):
+    return  # max product
+
+
+print(adjacent_element_product([5, 8]), 40)
+print(adjacent_element_product([1, 2, 3]), 6)
+print(adjacent_element_product([1, 5, 10, 9]), 90)
+print(adjacent_element_product([4, 12, 3, 1, 5]), 48)
+print(adjacent_element_product([5, 1, 2, 3, 1, 4]), 6)
+print(adjacent_element_product([3, 6, -2, -5, 7, 3]), 21)
+print(adjacent_element_product([9, 5, 10, 2, 24, -1, -48]), 50)
+print(adjacent_element_product([5, 6, -4, 2, 3, 2, -23]), 30)
+print(adjacent_element_product([-23, 4, -5, 99, -27, 329, -2, 7, -921]), -14)
+print(adjacent_element_product([5, 1, 2, 3, 1, 4]), 6)
+print(adjacent_element_product([1, 0, 1, 0, 1000]), 0)
+print(adjacent_element_product([1, 2, 3, 0]), 6)

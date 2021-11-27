1965b8f7093aa42dce849efa93bcefcc76b7c318
Paul <gafgafgarion05@gmail.com>
2021-11-27 09:41:38 +0300

    my solution

--- 7 kyu Coprime Validator.py
+++ 7 kyu Coprime Validator.py
@@ -1,5 +1,18 @@
 def are_coprime(n, m):
-    pass
+    list_factors_n, list_factors_m, list_factors_n_and_m = [], [], []
+
+    for i in range(1, n + 1):
+        if n % i == 0:
+            list_factors_n.append(i)
+    for i in range(1, m + 1):
+        if m % i == 0:
+            list_factors_m.append(i)
+
+    for i in list_factors_n:
+        if i in list_factors_m:
+            list_factors_n_and_m.append(i)
+
+    return max(list_factors_n_and_m) == 1
 
 
 print(are_coprime(20, 27), True, 'The numbers were 20 and 27')

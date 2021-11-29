c426849245f67d73c3a4dfd38f5c387bc121e748
Paul <gafgafgarion05@gmail.com>
2021-11-29 10:22:21 +0300

    init

---
+++ 7 kyu Form The Largest.py
@@ -0,0 +1,8 @@
+def max_number(n):
+    pass
+
+print(max_number(213), 321)
+print(max_number(7389), 9873)
+print(max_number(63792), 97632)
+print(max_number(566797), 977665)
+print(max_number(1000000), 1000000)

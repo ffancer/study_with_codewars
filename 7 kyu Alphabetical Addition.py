07e9a9f9b361ef7a54cc0bd6210f8f7ed5e9cff9
Paul <gafgafgarion05@gmail.com>
2021-11-29 16:47:50 +0300

    init

---
+++ 7 kyu Alphabetical Addition.py
@@ -0,0 +1,11 @@
+def add_letters(*letters):
+    pass # your code here
+
+
+print(add_letters(['a', 'b', 'c'], 'f'))
+print(add_letters(['z'], 'z'))
+print(add_letters(['a', 'b'], 'c'))
+print(add_letters(['c'], 'c'))
+print(add_letters(['z', 'a'], 'a'))
+print(add_letters(['y', 'c', 'b'], 'd'))
+print(add_letters([], 'z'))

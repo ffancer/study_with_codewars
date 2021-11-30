fb4546127e9e53fe38ce63d2eb3b255d6a67b054
Paul <gafgafgarion05@gmail.com>
2021-11-30 09:39:08 +0300

    solution troubles with tests, but work in site

--- 7 kyu Alphabetical Addition.py
+++ 7 kyu Alphabetical Addition.py
@@ -1,11 +1,11 @@
 def add_letters(*letters):
-    pass # your code here
+    return chr((sum(ord(i) - 96 for i in letters) - 1) % 26 + 97)
 
 
-print(add_letters(['a', 'b', 'c'], 'f'))
-print(add_letters(['z'], 'z'))
-print(add_letters(['a', 'b'], 'c'))
-print(add_letters(['c'], 'c'))
-print(add_letters(['z', 'a'], 'a'))
-print(add_letters(['y', 'c', 'b'], 'd'))
-print(add_letters([], 'z'))
+print(add_letters(['a', 'b', 'c']))
+print(add_letters(['z']))
+print(add_letters(['a', 'b']))
+print(add_letters(['c']))
+print(add_letters(['z', 'a']))
+print(add_letters(['y', 'c', 'b']))
+print(add_letters([]))  # z

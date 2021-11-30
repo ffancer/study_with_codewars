4a576e573c93a8a21f1ac79f39d36d0675c7b2c4
Paul <gafgafgarion05@gmail.com>
2021-11-30 13:09:00 +0300

    init

---
+++ 7 kyu Find the nth Digit of a Number.py
@@ -0,0 +1,20 @@
+def find_digit(num, nth):
+
+
+# your code here
+
+
+print(find_digit(5673, 4), 5)
+print(find_digit(129, 2), 2)
+print(find_digit(-2825, 3), 8)
+print(find_digit(0, 20), 0)
+print(find_digit(65, 0), -1)
+print(find_digit(24, -8), -1)
+
+print(find_digit(-456, 5), 0)
+print(find_digit(-1234, 2), 3)
+print(find_digit(-5540, 1), 0)
+
+print(find_digit(678998, 0), -1)
+print(find_digit(-67854, -57), -1)
+print(find_digit(0, -3), -1)

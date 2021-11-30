f4e32b12e2c8822d43ecff5be7c7ce73d7f1a562
Paul <gafgafgarion05@gmail.com>
2021-11-30 10:16:33 +0300

    my solution all looks fine, but I want to delete 'j'

--- 7 kyu Averages of numbers.py
+++ 7 kyu Averages of numbers.py
@@ -1,8 +1,16 @@
 def averages(arr):
-    pass
+    i, j = 0, 1
+    lst = []
+
+    while j < len(arr):
+        lst.append(arr[i] + arr[j])
+        i += 1
+        j += 1
+
+    return lst
 
 
-print(averages([2, 2, 2, 2]))  # [2, 2, 2, 2, 2]
-print(averages([0, 0, 0, 0]))  # [2, -2, 2, -2, 2]
-print(averages([2, 4, 3, -4.5]))  # [1, 3, 5, 1, -10]
-print(averages([]))  # []
+print(averages([2, 2, 2, 2]))
+print(averages([0, 0, 0, 0]))
+print(averages([2, 4, 3, -4.5]))
+print(averages([]))

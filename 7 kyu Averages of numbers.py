1a70f22a87d59f8b9cc6708287614eb8941bd385
Paul <gafgafgarion05@gmail.com>
2021-11-30 10:33:27 +0300

    my solution for

--- 7 kyu Averages of numbers.py
+++ 7 kyu Averages of numbers.py
@@ -1,12 +1,8 @@
 def averages(arr):
     try:
-        i = 0
         lst = []
-
-        while i < len(arr) - 1:
+        for i in range(len(arr) - 1):
             lst.append((arr[i] + arr[i + 1]) / 2)
-            i += 1
-
         return lst
     except:
         return []

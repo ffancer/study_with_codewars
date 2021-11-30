a55305e3c6c1530366b572c8851b2667b6822524
Paul <gafgafgarion05@gmail.com>
2021-11-30 10:12:31 +0300

    init

---
+++ 7 kyu Averages of numbers.py
@@ -0,0 +1,8 @@
+def averages(arr):
+    pass
+
+
+print(averages([2, 2, 2, 2]))  # [2, 2, 2, 2, 2]
+print(averages([0, 0, 0, 0]))  # [2, -2, 2, -2, 2]
+print(averages([2, 4, 3, -4.5]))  # [1, 3, 5, 1, -10]
+print(averages([]))  # []

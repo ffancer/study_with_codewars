101fd23dd2d5266bf12b76d31b348ffe4902721a
Paul <gafgafgarion05@gmail.com>
2021-12-01 10:43:50 +0300

    init

---
+++ 7 kyu Simple Fun #152 Invite More Women.py
@@ -0,0 +1,12 @@
+# 7 kyu
+# Simple Fun #152: Invite More Women?
+
+
+def invite_more_women(arr):
+    pass  # your code here
+
+
+print(invite_more_women([1, -1, 1]), True)
+print(invite_more_women([-1, -1, -1]), False)
+print(invite_more_women([1, -1]), False)
+print(invite_more_women([1, 1, 1]), True)

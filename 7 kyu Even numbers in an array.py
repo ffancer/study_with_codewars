2ebf09d49c1306dc33d5488d4ca3ac3d880b8570
Paul <gafgafgarion05@gmail.com>
2021-12-01 10:53:08 +0300

    my solution

--- 7 kyu Even numbers in an array.py
+++ 7 kyu Even numbers in an array.py
@@ -1,5 +1,11 @@
 def even_numbers(arr, n):
-    pass
+    lst = []
+
+    for i in arr:
+        if i % 2 == 0:
+            lst.append(i)
+
+    return lst[-n:]
 
 
 print(even_numbers([1, 2, 3, 4, 5, 6, 7, 8, 9], 3), [4, 6, 8])

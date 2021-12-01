bc75680ff2e86e632faa1dab5c363769d55095ef
Paul <gafgafgarion05@gmail.com>
2021-12-01 14:24:10 +0300

    1

--- 7 kyu Coloured Triangles.py
+++ 7 kyu Coloured Triangles.py
@@ -1,22 +1,18 @@
 def triangle(row):
     i, j = 0, 1
     while len(row) != 1:
-        if 'GG' in row[i:j+1]:
-            row = 'G' + row[j+1:]
-        elif 'BG' or 'GB' in row[i:j+1]:
-            row = 'R' + row[j+1:]
-        elif 'RG' or 'GR' in row[i:j + 1]:
-            row = 'B' + row[j + 1:]
-        elif 'BR' or 'RB' in row[i:j + 1]:
-            row = 'G' + row[j + 1:]
-        i += 1
-        j += 1
+        if row[i:j+1] in ['GG', 'BR', 'RB']:
+            row = row.replace(row[i:j + 1], 'G')
+        if row[i:j+1] in ['BB', 'RG', 'GR']:
+            row = row.replace(row[i:j + 1], 'B')
+        if row[i:j+1] in ['RR', 'BG', 'GB']:
+            row = row.replace(row[i:j + 1], 'R')
     return row
-# print(triangle('GGBR'), 'R')
 
-print(triangle('GB'), 'R')
-print(triangle('RRR'), 'R')
+
+# print(triangle('GB'), 'R')
+# print(triangle('RRR'), 'R')
 print(triangle('RGBG'), 'B')
 print(triangle('RBRGBRB'), 'G')
-print(triangle('RBRGBRBGGRRRBGBBBGG'), 'G')
-print(triangle('B'), 'B')
+# print(triangle('RBRGBRBGGRRRBGBBBGG'), 'G')
+# print(triangle('B'), 'B')

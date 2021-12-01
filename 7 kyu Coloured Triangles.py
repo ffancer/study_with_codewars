b08fcec5887f616b87226f958f71c5b707aab1d9
Paul <gafgafgarion05@gmail.com>
2021-12-01 13:18:40 +0300

    init

---
+++ 7 kyu Coloured Triangles.py
@@ -0,0 +1,11 @@
+def triangle(row):
+    # Your code here:
+    return None
+
+
+print(triangle('GB'), 'R')
+print(triangle('RRR'), 'R')
+print(triangle('RGBG'), 'B')
+print(triangle('RBRGBRB'), 'G')
+print(triangle('RBRGBRBGGRRRBGBBBGG'), 'G')
+print(triangle('B'), 'B')

c8d7d892d3dac781589b85b908a4bb986419ebe7
Paul <gafgafgarion05@gmail.com>
2021-12-01 13:34:37 +0300

    90 tests are ok, 116 tests - errors

--- 7 kyu Coloured Triangles.py
+++ 7 kyu Coloured Triangles.py
@@ -1,7 +1,18 @@
 def triangle(row):
-    # Your code here:
-    return None
-
+    i, j = 0, 1
+    while len(row) != 1:
+        if 'GG' in row[i:j+1]:
+            row = 'G' + row[j+1:]
+        elif 'BG' or 'GB' in row[i:j+1]:
+            row = 'R' + row[j+1:]
+        elif 'RG' or 'GR' in row[i:j + 1]:
+            row = 'B' + row[j + 1:]
+        elif 'BR' or 'RB' in row[i:j + 1]:
+            row = 'G' + row[j + 1:]
+        i += 1
+        j += 1
+    return row
+# print(triangle('GGBR'), 'R')
 
 print(triangle('GB'), 'R')
 print(triangle('RRR'), 'R')

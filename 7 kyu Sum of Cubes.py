976fa54914f2dfce568102ad51ccd5c85074fe8a
Paul <gafgafgarion05@gmail.com>
2021-12-01 11:04:44 +0300

    init

---
+++ 7 kyu Sum of Cubes.py
@@ -0,0 +1,10 @@
+def sum_cubes(n):
+    pass
+
+
+print(sum_cubes(1), 1)
+print(sum_cubes(2), 9)
+print(sum_cubes(3), 36)
+print(sum_cubes(4), 100)
+print(sum_cubes(10), 3025)
+print(sum_cubes(123), 58155876)

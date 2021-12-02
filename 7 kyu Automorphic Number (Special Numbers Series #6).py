19cf52ff93edee548bf9f42eb66a5100208fa8ec
Paul <gafgafgarion05@gmail.com>
2021-12-02 14:15:50 +0300

    init

---
+++ 7 kyu Automorphic Number (Special Numbers Series #6).py
@@ -0,0 +1,14 @@
+def automorphic(n):
+    pass
+
+
+print(automorphic(1), "Automorphic")
+print(automorphic(3), "Not!!")
+print(automorphic(6), "Automorphic")
+print(automorphic(9), "Not!!")
+print(automorphic(25), "Automorphic")
+print(automorphic(53), "Not!!")
+print(automorphic(76), "Automorphic")
+print(automorphic(95), "Not!!")
+print(automorphic(625), "Automorphic")
+print(automorphic(225), "Not!!")

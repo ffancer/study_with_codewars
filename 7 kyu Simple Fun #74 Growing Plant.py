259278b62a2fce671f43b5dff13e0d0cb63dd3fc
Paul <gafgafgarion05@gmail.com>
2021-12-03 10:52:58 +0300

    init

---
+++ 7 kyu Simple Fun #74 Growing Plant.py
@@ -0,0 +1,13 @@
+# 7 kyu
+# Simple Fun #74: Growing Plant
+
+
+def growing_plant(upSpeed, downSpeed, desiredHeight):
+    pass
+
+
+print(growing_plant(100, 10, 910), 10)
+print(growing_plant(10, 9, 4), 1)
+print(growing_plant(5, 2, 0), 1)
+print(growing_plant(5, 2, 5), 1)
+print(growing_plant(5, 2, 6), 2)

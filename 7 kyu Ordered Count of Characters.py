d8b89603a1d41e8d50a6bf582b43bb1a163cb2ff
Paul <gafgafgarion05@gmail.com>
2021-12-04 10:13:21 +0300

    init

---
+++ 7 kyu Ordered Count of Characters.py
@@ -0,0 +1,6 @@
+def ordered_count(inp):
+    pass
+
+
+print('abracadabra', [('a', 5), ('b', 2), ('r', 2), ('c', 1), ('d', 1)])
+print('Code Wars', [('C', 1), ('o', 1), ('d', 1), ('e', 1), (' ', 1), ('W', 1), ('a', 1), ('r', 1), ('s', 1)])

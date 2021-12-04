abe69e386923ec5970ad9402be1656bf31fb547f
Paul <gafgafgarion05@gmail.com>
2021-12-04 10:35:41 +0300

    1

--- 7 kyu Ordered Count of Characters.py
+++ 7 kyu Ordered Count of Characters.py
@@ -1,6 +1,10 @@
 def ordered_count(inp):
-    pass
+    dct = {i: inp.count(i) for i in inp}
+    lst = []
 
+    # lst = dct.items()
 
-print('abracadabra', [('a', 5), ('b', 2), ('r', 2), ('c', 1), ('d', 1)])
-print('Code Wars', [('C', 1), ('o', 1), ('d', 1), ('e', 1), (' ', 1), ('W', 1), ('a', 1), ('r', 1), ('s', 1)])
+    return lst
+
+print(ordered_count('abracadabra'))
+print(ordered_count('Code Wars'))

a74538c71dc22e11ddd240a584fb1a2ef931c669
Paul <gafgafgarion05@gmail.com>
2021-12-04 11:33:42 +0300

    init

---
+++ 7 kyu Balanced Number (Special Numbers Series #1 ).py
@@ -0,0 +1,15 @@
+def balanced_num(number):
+    pass
+
+
+print(balanced_num(7), "Balanced")
+print(balanced_num(959), "Balanced")
+print(balanced_num(13), "Balanced")
+print(balanced_num(432), "Not Balanced")
+print(balanced_num(424), "Balanced")
+
+print(balanced_num(1024), "Not Balanced")
+print(balanced_num(66545), "Not Balanced")
+print(balanced_num(295591), "Not Balanced")
+print(balanced_num(1230987), "Not Balanced")
+print(balanced_num(56239814), "Balanced")

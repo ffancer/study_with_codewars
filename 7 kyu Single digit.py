40dcb7258d214c3829c016b6d631efbf23b6ce33
Paul <gafgafgarion05@gmail.com>
2021-12-04 11:19:01 +0300

    my solution

--- 7 kyu Single digit.py
+++ 7 kyu Single digit.py
@@ -1,7 +1,10 @@
 def single_digit(n):
+    if len(str(n)) == 1:
+        return n
     cnt = bin(n)[2::].count('1')
     return cnt if cnt <= 9 else bin(cnt)[2::].count('1')
 
 
+print(single_digit(5), 5)
 print(single_digit(5665), 5)
 print(single_digit(123456789), 1)

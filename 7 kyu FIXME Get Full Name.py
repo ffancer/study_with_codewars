84b43070e5c5642c2fdfacbcbc75ee66e3f39313
Paul <gafgafgarion05@gmail.com>
2021-12-06 11:24:24 +0300

    my solution add self. add strip()

--- 7 kyu FIXME Get Full Name.py
+++ 7 kyu FIXME Get Full Name.py
@@ -7,7 +7,7 @@ class Dinglemouse(object):
         self.first_name = first_name
         self.last_name = last_name
     def get_full_name(self):
-        return self.first_name + ' ' + self.last_name
+        return (self.first_name + ' ' + self.last_name).strip()
 
 
 print(Dinglemouse('Clint', 'Eastwood').get_full_name(), 'Clint Eastwood')

5f80adc75902b39759f4726f735a5dcb486a4140
Paul <gafgafgarion05@gmail.com>
2021-12-06 10:27:54 +0300

    Tests good, but other not. Many errors

--- 7 kyu All unique.py
+++ 7 kyu All unique.py
@@ -1,5 +1,8 @@
 def has_unique_chars(string):
-    pass
+    for i in range(len(string)):
+        if string[i] == string[i + 1]:
+            return False
+        return True
 
 
 print(has_unique_chars("abcdef"), True)

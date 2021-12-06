98144a97345232dfde03571074231405d87ec820
Paul <gafgafgarion05@gmail.com>
2021-12-06 11:05:46 +0300

    my first solution

--- 7 kyu Sum even numbers.py
+++ 7 kyu Sum even numbers.py
@@ -1,6 +1,11 @@
 def sum_even_numbers(seq):
-    # your code here
-    pass
+    total = 0
+
+    for i in seq:
+        if i % 2 == 0:
+            total += i
+
+    return total
 
 
 print(sum_even_numbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), 30)

aa39ddd223dea09b4b1b48314153dfcc02407d0d
Paul <gafgafgarion05@gmail.com>
2021-12-06 11:23:19 +0300

    problems with space in the beggining and in the end of sentences

--- 7 kyu FIXME Get Full Name.py
+++ 7 kyu FIXME Get Full Name.py
@@ -4,10 +4,10 @@
 
 class Dinglemouse(object):
     def __init__(self, first_name, last_name):
-        first_name = first_name
-        last_name = last_name
+        self.first_name = first_name
+        self.last_name = last_name
     def get_full_name(self):
-        return first_name + ' ' + last_name
+        return self.first_name + ' ' + self.last_name
 
 
 print(Dinglemouse('Clint', 'Eastwood').get_full_name(), 'Clint Eastwood')

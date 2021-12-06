95880101f1abf9c1a3b8dba268a7619c90fa4811
Paul <gafgafgarion05@gmail.com>
2021-12-06 10:01:26 +0300

    init

---
+++ 7 kyu Lost number in number sequence.py
@@ -0,0 +1,7 @@
+def find_deleted_number(arr, mixed_arr):
+    pass
+
+
+print(find_deleted_number([1, 2, 3, 4, 5], [3, 4, 1, 5]), 2, 'Deletion')
+print(find_deleted_number([1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 9, 7, 4, 6, 2, 3, 8]), 5, 'Deletion')
+print(find_deleted_number([1, 2, 3, 4, 5, 6, 7, 8, 9], [5, 7, 6, 9, 4, 8, 1, 2, 3]), 0, 'No deletion')

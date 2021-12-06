c09afc4847500661a4fe0e2a8aeebca4ec7d8d71
Paul <gafgafgarion05@gmail.com>
2021-12-06 13:35:34 +0300

    my 1 line solution

--- 7 kyu Difference Of Squares.py
+++ 7 kyu Difference Of Squares.py
@@ -1,12 +1,5 @@
 def difference_of_squares(n):
-    total = 0
-    total_square = 0
-
-    for i in range(1, n + 1):
-        total += i
-        total_square += i ** 2
-
-    return total ** 2 - total_square
+    return sum(i for i in range(1, n + 1)) ** 2 - sum(i ** 2 for i in range(1, n + 1))
 
 
 print(difference_of_squares(5), 170)

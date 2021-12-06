ac3f9741dc88e83ce81a1312869bc9dc10bc9492
Paul <gafgafgarion05@gmail.com>
2021-12-06 10:37:38 +0300

    my 1 line solution

--- 7 kyu All unique.py
+++ 7 kyu All unique.py
@@ -1,12 +1,6 @@
 def has_unique_chars(string):
-    i = 0
+    return len(set(string)) == len(string)
 
-    while i != len(string)-1:
-        if string[i] == string[i+1]:
-            return False
-        i += 1
-
-    return True
 
 print(has_unique_chars("abcdef"), True)
 print(has_unique_chars("++-"), False)

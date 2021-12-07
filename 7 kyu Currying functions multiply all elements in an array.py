11e9f7499005736f16d4dce4117580cace74cc04
Paul <gafgafgarion05@gmail.com>
2021-12-07 11:32:58 +0300

    solution is right but tests here are not

--- 7 kyu Currying functions multiply all elements in an array.py
+++ 7 kyu Currying functions multiply all elements in an array.py
@@ -2,9 +2,9 @@ def multiply_all(lst):
     return lambda n: [i * n for i in lst]
 
 
-print(multiply_all([1], 1))
-print(multiply_all([1, 2], 1))
-print(multiply_all([1, 2, 3], 1))
-print(multiply_all([1, 2, 3], 2))
-print(multiply_all([1, 2, 3], 0))
-print(multiply_all([], 10))
+print(multiply_all([1]))
+print(multiply_all([1, 2]))
+print(multiply_all([1, 2, 3]))
+print(multiply_all([1, 2, 3]))
+print(multiply_all([1, 2, 3]))
+print(multiply_all([]))

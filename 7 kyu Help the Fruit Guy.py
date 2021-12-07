3c5f5c0465e63a28800f7ad471682f1cf33c689b
Paul <gafgafgarion05@gmail.com>
2021-12-07 14:41:45 +0300

    my solution 2

--- 7 kyu Help the Fruit Guy.py
+++ 7 kyu Help the Fruit Guy.py
@@ -1,8 +1,8 @@
 def remove_rotten(bag_of_fruits):
-    if type(bag_of_fruits) is None or len(bag_of_fruits) == 0:
+    try:
+        return [i.replace('rotten', '').lower() for i in bag_of_fruits]
+    except:
         return []
-    return [i.replace('rotten', '').lower() for i in bag_of_fruits]
-
 
 print(remove_rotten(["rottenApple","rottenBanana","rottenApple","rottenPineapple","rottenKiwi"]), ["apple","banana","apple","pineapple","kiwi"])
 print(remove_rotten([]), [])

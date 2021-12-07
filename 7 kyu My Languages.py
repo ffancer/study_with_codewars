d7a6059ebeb37bfb50b1653d3bf4822c9a35df03
Paul <gafgafgarion05@gmail.com>
2021-12-07 08:40:57 +0300

    init

---
+++ 7 kyu My Languages.py
@@ -0,0 +1,11 @@
+def my_languages(results):
+    # your fantastic code here
+    return []
+
+
+print(my_languages({"Java": 10, "Ruby": 80, "Python": 65}),
+      ["Ruby", "Python"])
+print(my_languages({"Hindi": 60, "Dutch": 93, "Greek": 71}),
+      ["Dutch", "Greek", "Hindi"])
+print(my_languages({"C++": 50, "ASM": 10, "Haskell": 20}),
+      [])

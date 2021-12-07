1e297517a03f697b672abc83c219fecb63885ca2
Paul <gafgafgarion05@gmail.com>
2021-12-07 11:25:48 +0300

    init

---
+++ 7 kyu Currying functions multiply all elements in an array.py
@@ -0,0 +1,12 @@
+# left blank for unlimited creativity :)
+
+def multiply_all(arr, n):
+    pass
+
+
+print(multiply_all([1], 1))
+print(multiply_all([1, 2], 1))
+print(multiply_all([1, 2, 3], 1))
+print(multiply_all([1, 2, 3], 2))
+print(multiply_all([1, 2, 3], 0))
+print(multiply_all([], 10))

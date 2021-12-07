92ae0d549606c53044f1c41788ed267dde86e74c
Paul <gafgafgarion05@gmail.com>
2021-12-07 09:29:22 +0300

    my solution

--- 7 kyu Going to the cinema.py
+++ 7 kyu Going to the cinema.py
@@ -1,5 +1,15 @@
-def movie(card, ticket, perc):
-    pass
+from math import ceil
+
+
+def movie(card, ticket, rate):
+    cnt, total_a, total_b = 0, 0, 0
+
+    while ceil(card + total_b) >= total_a:
+        total_a += ticket
+        total_b = (total_b + ticket) * rate
+        cnt += 1
+
+    return cnt
 
 
 print(movie(500, 15, 0.9), 43)

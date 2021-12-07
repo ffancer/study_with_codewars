f5c0b4e430446a3855c28bdb5f0e05c355295381
Paul <gafgafgarion05@gmail.com>
2021-12-07 14:20:17 +0300

    init

---
+++ 7 kyu Help the Fruit Guy.py
@@ -0,0 +1,6 @@
+def remove_rotten(bag_of_fruits):
+    pass
+
+
+print(remove_rotten(["rottenApple","rottenBanana","rottenApple","rottenPineapple","rottenKiwi"]), ["apple","banana","apple","pineapple","kiwi"])
+print(remove_rotten([]), [])

83bbbace48d9ef56426edc7552820c54d845b156
Paul <gafgafgarion05@gmail.com>
2021-12-08 10:04:47 +0300

    init

---
+++ 7 kyu Strong Number (Special Numbers Series #2).py
@@ -0,0 +1,11 @@
+def strong_num(number):
+    pass
+
+
+print(strong_num(1), "STRONG!!!!")
+print(strong_num(2), "STRONG!!!!")
+print(strong_num(145), "STRONG!!!!")
+
+print(strong_num(7), "Not Strong !!")
+print(strong_num(93), "Not Strong !!")
+print(strong_num(185), "Not Strong !!")

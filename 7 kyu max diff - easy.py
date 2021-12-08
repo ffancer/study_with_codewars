417fcae187cbddd1545af942191c1ec0cde7a5c9
Paul <gafgafgarion05@gmail.com>
2021-12-08 10:49:14 +0300

    init

---
+++ 7 kyu max diff - easy.py
@@ -0,0 +1,9 @@
+def max_diff(lst):
+    return 0
+
+
+print(max_diff([0, 1, 2, 3, 4, 5, 6]), 6)
+print(max_diff([-0, 1, 2, -3, 4, 5, -6]), 11)
+print(max_diff([0, 1, 2, 3, 4, 5, 16]), 16)
+print(max_diff([16]), 0)
+print(max_diff([]), 0)

007886f49f034e111e88d22139a1ee2292fd5d60
Paul <gafgafgarion05@gmail.com>
2021-12-08 13:09:10 +0300

    my solution

--- 7 kyu Coding Meetup #2 - Higher-Order Functions Series - Greet developers.py
+++ 7 kyu Coding Meetup #2 - Higher-Order Functions Series - Greet developers.py
@@ -1,6 +1,12 @@
 def greet_developers(lst):
-    # your code here
-    pass
+    arr = []
+
+    for i in lst:
+        temp = i
+        temp['greeting'] = f'Hi {i["firstName"]}, what do you like the most about {i["language"]}?'
+        arr.append(temp)
+
+    return arr
 
 
 print(greet_developers([

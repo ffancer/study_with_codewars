15b091534635b03c8e07dc3953e9017dbe22e601
Paul <gafgafgarion05@gmail.com>
2021-12-08 14:41:57 +0300

    init

---
+++ 7 kyu Indexed capitalization.py
@@ -0,0 +1,9 @@
+def capitalize(s, ind):
+    pass
+
+
+print(capitalize("abcdef", [1, 2, 5]), 'aBCdeF')
+print(capitalize("abcdef", [1, 2, 5, 100]), 'aBCdeF', )
+print(capitalize("codewars", [1, 3, 5, 50]), 'cOdEwArs')
+print(capitalize("abracadabra", [2, 6, 9, 10]), 'abRacaDabRA')
+print(capitalize("codewarriors", [5]), 'codewArriors')

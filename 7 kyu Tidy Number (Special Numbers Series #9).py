5e0aef45ddbe8e8b043c1670baffc2de10b95b56
Paul <gafgafgarion05@gmail.com>
2021-12-08 10:32:39 +0300

    my solution

--- 7 kyu Tidy Number (Special Numbers Series #9).py
+++ 7 kyu Tidy Number (Special Numbers Series #9).py
@@ -1,7 +1,8 @@
 def tidyNumber(n):
     n = str(n)
+
     for i in range(len(n)-1):
-        if int(n[i]) < int(n[i+1]):
+        if int(n[i]) <= int(n[i+1]):
             continue
         return False
     return True

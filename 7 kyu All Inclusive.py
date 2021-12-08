1c703f002bdf10e99436d54f937579da6e295338
Paul <gafgafgarion05@gmail.com>
2021-12-08 13:10:32 +0300

    init

---
+++ 7 kyu All Inclusive.py
@@ -0,0 +1,13 @@
+# 7 kyu
+# All Inclusive?
+
+def contain_all_rots(strng, arr):
+    pass
+
+
+print(contain_all_rots("", []), True)
+print(contain_all_rots("", ["bsjq", "qbsj"]), True)
+print(contain_all_rots("bsjq", ["bsjq", "qbsj", "sjqb", "twZNsslC", "jqbs"]), True)
+print(contain_all_rots("XjYABhR",
+                       ["TzYxlgfnhf", "yqVAuoLjMLy", "BhRXjYA", "YABhRXj", "hRXjYAB", "jYABhRX", "XjYABhR",
+                        "ABhRXjY"]), False)

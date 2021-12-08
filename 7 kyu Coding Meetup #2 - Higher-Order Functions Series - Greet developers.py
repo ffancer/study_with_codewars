cf9f24f4d5418b83eb521879ebf6df84a2faa067
Paul <gafgafgarion05@gmail.com>
2021-12-08 11:04:37 +0300

    init

---
+++ 7 kyu Coding Meetup #2 - Higher-Order Functions Series - Greet developers.py
@@ -0,0 +1,13 @@
+def greet_developers(lst):
+    # your code here
+    pass
+
+
+print(greet_developers([
+    {'firstName': 'Sofia', 'lastName': 'I.', 'country': 'Argentina', 'continent': 'Americas', 'age': 35,
+     'language': 'Java'},
+    {'firstName': 'Lukas', 'lastName': 'X.', 'country': 'Croatia', 'continent': 'Europe', 'age': 35,
+     'language': 'Python'},
+    {'firstName': 'Madison', 'lastName': 'U.', 'country': 'United States', 'continent': 'Americas', 'age': 32,
+     'language': 'Ruby'}
+]))

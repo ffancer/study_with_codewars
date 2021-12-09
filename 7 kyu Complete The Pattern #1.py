9052ee32383423c91fc9b197ce436ddbfc65e638
Paul <gafgafgarion05@gmail.com>
2021-12-09 11:13:34 +0300

    my solution

--- 7 kyu Complete The Pattern #1.py
+++ 7 kyu Complete The Pattern #1.py
@@ -1,9 +1,13 @@
 def pattern(n):
-    pass
+    s = ''
+    for i in range(1, n + 1):
+        s += i * f'{i}' + '\n'
+
+    return s[:-1]
 
 
 print(pattern(1), "1")
-print(pattern(2), "1\n22")
-print(pattern(5), "1\n22\n333\n4444\n55555")
+print(pattern(2))  # "1\n22"
+print(pattern(5))  # "1\n22\n333\n4444\n55555"
 print(pattern(0), "")
 print(pattern(-25), "")

5e0f91b19a985b1571eadb92dd0ae2464ea57f26
Paul <gafgafgarion05@gmail.com>
2021-12-09 10:03:28 +0300

    init

---
+++ 7 kyu Alternate case.py
@@ -0,0 +1,11 @@
+def alternate_case(s):
+    # your code here
+    return s
+
+
+print(alternate_case("ABC"), "abc")
+print(alternate_case(""), "")
+print(alternate_case(" "), " ")
+print(alternate_case("Hello World"), "hELLO wORLD")
+print(alternate_case("cODEwARS"), "CodeWars")
+print(alternate_case("i LIKE MAKING KATAS VERY MUCH"), "I like making katas very much")

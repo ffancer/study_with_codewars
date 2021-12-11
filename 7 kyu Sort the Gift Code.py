183e17fa324b35abb6ffff67608a9ef58da9bf65
Paul <gafgafgarion05@gmail.com>
2021-12-11 11:45:59 +0300

    init

---
+++ 7 kyu Sort the Gift Code.py
@@ -0,0 +1,7 @@
+def sort_gift_code(code):
+    pass
+
+
+print(sort_gift_code('abcdef'), 'abcdef')
+print(sort_gift_code('pqksuvy'), 'kpqsuvy')
+print(sort_gift_code('zyxwvutsrqponmlkjihgfedcba'), 'abcdefghijklmnopqrstuvwxyz')

33541692ffdb902589d54a336a24725de494d46b
Paul <gafgafgarion05@gmail.com>
2021-12-13 14:33:06 +0300

    init

---
+++ 7 kyu Cat and Mouse - Easy Version.py
@@ -0,0 +1,9 @@
+def cat_mouse(x):
+    pass
+
+
+print(cat_mouse('C....m'), "Escaped!")
+print(cat_mouse('C..m'), "Caught!")
+print(cat_mouse('C.....m'), "Escaped!")
+print(cat_mouse('C.m'), "Caught!")
+print(cat_mouse('m...C'), "Caught!")

38a79b686094641cb96e99ded474ae923e83f101
Paul <gafgafgarion05@gmail.com>
2021-12-13 15:13:15 +0300

    init

---
+++ 7 kyu ToLeetSpeak.py
@@ -0,0 +1,9 @@
+def to_leet_speak(str):
+    pass
+
+
+print(to_leet_speak("LEET"), "1337")
+print(to_leet_speak("CODEWARS"), "(0D3W@R$")
+print(to_leet_speak("HELLO WORLD"), "#3110 W0R1D")
+print(to_leet_speak("LOREM IPSUM DOLOR SIT AMET"), "10R3M !P$UM D010R $!7 @M37")
+print(to_leet_speak("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG"), "7#3 QU!(K 8R0WN F0X JUMP$ 0V3R 7#3 1@2Y D06")

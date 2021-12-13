3a4e2597a08b894e071709f33f107596440f0fa5
Paul <gafgafgarion05@gmail.com>
2021-12-13 13:44:52 +0300

    init

---
+++ 7 kyu Sort the Bytes.py
@@ -0,0 +1,15 @@
+def sort_bytes(number):
+    pass
+
+print(sort_bytes(0), 0)
+print(sort_bytes(1), 16777216)
+print(sort_bytes(2), 33554432)
+print(sort_bytes(4294967295), 4294967295)
+print(sort_bytes(3735928559), 4024352429)
+print(sort_bytes(255), 4278190080)
+print(sort_bytes(305419896), 2018915346)
+print(sort_bytes(19088743), 1732584193)
+print(sort_bytes(555885348), 606282273)
+print(sort_bytes(43839), 2873032704)
+print(sort_bytes(723893), 3037399808)
+print(sort_bytes(3567), 4010606592)

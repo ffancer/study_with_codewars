7cb646d6d97aeb7efb9dd310e14a4e5d47127c39
Paul <gafgafgarion05@gmail.com>
2021-12-13 13:18:07 +0300

    init

---
+++ 7 kyu Jumping Number (Special Numbers Series #4).py
@@ -0,0 +1,14 @@
+def jumping_number(number):
+    pass
+
+
+print(jumping_number(1), "Jumping!!")
+print(jumping_number(7), "Jumping!!")
+print(jumping_number(9), "Jumping!!")
+print(jumping_number(23), "Jumping!!")
+print(jumping_number(32), "Jumping!!")
+print(jumping_number(79), "Not!!")
+print(jumping_number(98), "Jumping!!")
+print(jumping_number(8987), "Jumping!!")
+print(jumping_number(4343456), "Jumping!!")
+print(jumping_number(98789876), "Jumping!!")

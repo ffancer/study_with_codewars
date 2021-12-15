90a4b8ce78f19998a8b5f9d1b34747b7f5e4226a
Paul <gafgafgarion05@gmail.com>
2021-12-15 15:43:06 +0300

    1 error

--- 7 kyu Simple Fun #37 House Numbers Sum.py
+++ 7 kyu Simple Fun #37 House Numbers Sum.py
@@ -3,7 +3,15 @@
 
 
 def house_numbers_sum(inp):
-    pass
+    total, lst = 0, []
+
+    for i in inp:
+        if i != 0:
+            total += i
+        else:
+            lst.append(total)
+
+    return max(lst)
 
 
 print(house_numbers_sum([5, 1, 2, 3, 0, 1, 5, 0, 2]))

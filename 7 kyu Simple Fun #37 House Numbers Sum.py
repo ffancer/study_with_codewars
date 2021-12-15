7d81ba9118637ff6e9ca1479655ab74c05c2e204
Paul <gafgafgarion05@gmail.com>
2021-12-15 15:34:11 +0300

    init

---
+++ 7 kyu Simple Fun #37 House Numbers Sum.py
@@ -0,0 +1,12 @@
+# 7 kyu
+# Simple Fun #37: House Numbers Sum
+
+
+def house_numbers_sum(inp):
+    pass
+
+
+print(house_numbers_sum([5, 1, 2, 3, 0, 1, 5, 0, 2]))
+print(house_numbers_sum([4, 2, 1, 6, 0]))
+print(house_numbers_sum([4, 1, 2, 3, 0, 10, 2]))
+print(house_numbers_sum([0, 1, 2, 3, 4, 5]))

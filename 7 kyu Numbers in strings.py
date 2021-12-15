95cfacb679b42b738bc28b5dba005a93dd452c79
Paul <gafgafgarion05@gmail.com>
2021-12-15 13:01:38 +0300

    init

---
+++ 7 kyu Numbers in strings.py
@@ -0,0 +1,9 @@
+def solve(s):
+    pass
+
+
+print(solve('gh12cdy695m1'), 695)
+print(solve('2ti9iei7qhr5'), 9)
+print(solve('vih61w8oohj5'), 61)
+print(solve('f7g42g16hcu5'), 42)
+print(solve('lu1j8qbbb85'), 85)

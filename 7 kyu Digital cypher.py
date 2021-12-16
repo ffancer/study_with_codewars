dfafa36f5a08e750f98d3176d3a3b6852da7989d
Paul <gafgafgarion05@gmail.com>
2021-12-16 10:10:14 +0300

    1

--- 7 kyu Digital cypher.py
+++ 7 kyu Digital cypher.py
@@ -1,6 +1,12 @@
 def encode(message, key):
-    pass
+    lst = []
+
+    for i in message:
+        lst.append((ord(i)-96)% 26)
+
+    return lst
 
 
+print(encode("abcde", 1939), [1,2,3,4,5])
 print(encode("scout", 1939), [20, 12, 18, 30, 21])
 print(encode("masterpiece", 1939), [14, 10, 22, 29, 6, 27, 19, 18, 6, 12, 8])

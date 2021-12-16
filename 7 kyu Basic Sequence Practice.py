436b03b3c6f2b38e1a02629fa3ec7f49799d5962
Paul <gafgafgarion05@gmail.com>
2021-12-16 11:13:46 +0300

    init

---
+++ 7 kyu Basic Sequence Practice.py
@@ -0,0 +1,10 @@
+def sum_of_n(n):
+    # your code goes here
+    return []
+
+
+print(sum_of_n(3), [0, 1, 3, 6])
+print(sum_of_n(-4), [0, -1, -3, -6, -10])
+print(sum_of_n(1), [0, 1])
+print(sum_of_n(0), [0])
+print(sum_of_n(10), [0, 1, 3, 6, 10, 15, 21, 28, 36, 45, 55])

f7e9b8fcb940e9e8dbe3fbd0af2317cf552aa54e
Paul <gafgafgarion05@gmail.com>
2021-12-16 11:21:02 +0300

    my solution

--- 7 kyu Basic Sequence Practice.py
+++ 7 kyu Basic Sequence Practice.py
@@ -1,6 +1,8 @@
 def sum_of_n(n):
-    # your code goes here
-    return []
+    if n < 0:
+        return sorted([sum(x for x in range(i, 1)) for i in range(n, 1)], reverse=True)
+    else:
+        return [sum([x for x in range(i + 1)]) for i in range(n + 1)]
 
 
 print(sum_of_n(3), [0, 1, 3, 6])

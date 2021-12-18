59248ac4cd9337797e21117e2abcabdda4ba13c2
Paul <gafgafgarion05@gmail.com>
2021-12-18 10:56:05 +0300

    my solution

--- 7 kyu Series of integers from m to n.py
+++ 7 kyu Series of integers from m to n.py
@@ -1,6 +1,10 @@
 def generate_integers(m, n):
-    # your code here
-    pass
+    lst = []
+
+    for i in range(m, n+1):
+        lst.append(i)
+
+    return lst
 
 
 print(generate_integers(2, 5), [2, 3, 4, 5])

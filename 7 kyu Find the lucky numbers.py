ee424d5eea3cec127a0dc1f19e9790b289a5c629
Paul <gafgafgarion05@gmail.com>
2021-12-18 11:17:00 +0300

    my solution

--- 7 kyu Find the lucky numbers.py
+++ 7 kyu Find the lucky numbers.py
@@ -1,9 +1,18 @@
 def filter_lucky(lst):
-    pass
+    lst = [str(i) for i in lst]
+    arr = []
+
+    for i in lst:
+        if '7' in i:
+            arr.append(i)
+
+    return [int(i) for i in arr]
 
 
-print(filter_lucky([[7], [7]]))
-print(filter_lucky([[1, 2, 3], []]))
-print(filter_lucky([[77, 8], [77]]))
-print(filter_lucky([[71], [71]]))
-print(filter_lucky([[71, 9907, 69], [71, 9907]]))
+
+print(filter_lucky([1,2,3,4,5,6,7,68,69,70,15,17]))
+print(filter_lucky([7]))
+print(filter_lucky([1, 2, 3]))
+print(filter_lucky([77, 8]))
+print(filter_lucky([71]))
+print(filter_lucky([71, 9907, 69]))

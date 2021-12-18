290552157ca69ef2964582d9e27d240e08f97d72
Paul <gafgafgarion05@gmail.com>
2021-12-18 11:12:41 +0300

    init

---
+++ 7 kyu Find the lucky numbers.py
@@ -0,0 +1,9 @@
+def filter_lucky(lst):
+    pass
+
+
+print(filter_lucky([[7], [7]]))
+print(filter_lucky([[1, 2, 3], []]))
+print(filter_lucky([[77, 8], [77]]))
+print(filter_lucky([[71], [71]]))
+print(filter_lucky([[71, 9907, 69], [71, 9907]]))

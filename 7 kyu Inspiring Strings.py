8e05924bd8dbb351ad03b91ce3ccbc38f2949236
Paul <gafgafgarion05@gmail.com>
2021-12-18 10:36:13 +0300

    init

---
+++ 7 kyu Inspiring Strings.py
@@ -0,0 +1,8 @@
+def longest_word(string_of_words):
+    # Give me back the longest word!
+    pass
+
+
+print(longest_word('a b c d e fgh'), "fgh")
+print(longest_word('one two three'), "three")
+print(longest_word('red blue grey'), "grey")

2bb9a3fbb88058f203cb813cf0c931ff2f43d312
Paul <gafgafgarion05@gmail.com>
2021-12-20 10:32:23 +0300

    init

---
+++ 7 kyu Char Code Calculation.py
@@ -0,0 +1,10 @@
+def calc(x):
+    # your code here
+    return n
+
+
+print(calc('abcdef'), 6)
+print(calc('ifkhchlhfd'), 6)
+print(calc('aaaaaddddr'), 30)
+print(calc('jfmgklf8hglbe'), 6)
+print(calc('jaam'), 12)

6d6aaf1c9c40eee3eaf62cf6174eca9bd50418bf
Paul <gafgafgarion05@gmail.com>
2021-12-20 10:42:20 +0300

    more compact solution

--- 7 kyu Char Code Calculation.py
+++ 7 kyu Char Code Calculation.py
@@ -2,10 +2,7 @@ def calc(x):
     total1 = ''.join(str(ord(i)) for i in x)
     total2 = total1.replace('7', '1')
 
-    total1_sum = sum(int(i) for i in total1)
-    total2_sum = sum(int(i) for i in total2)
-
-    return total1_sum - total2_sum
+    return sum(int(i) for i in total1) - sum(int(i) for i in total2)
 
 
 print(calc('abcdef'), 6)

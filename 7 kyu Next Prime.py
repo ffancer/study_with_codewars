2f2f881e4d92da21e1043857c8d8224952bf7775
Paul <gafgafgarion05@gmail.com>
2021-12-20 11:04:15 +0300

    init

---
+++ 7 kyu Next Prime.py
@@ -0,0 +1,9 @@
+def next_prime(n):
+    # have fun ^.^
+
+print(next_prime(0), 2)
+print(next_prime(2), 3)
+print(next_prime(3), 5)
+print(next_prime(13), 17)
+print(next_prime(181), 191)
+print(next_prime(911), 919)

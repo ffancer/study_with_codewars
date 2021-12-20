775230a8989e359a8fb8e88a17c407264d9c3769
Paul <gafgafgarion05@gmail.com>
2021-12-20 13:40:52 +0300

    4

--- 7 kyu Next Prime.py
+++ 7 kyu Next Prime.py
@@ -1,15 +1,8 @@
-import math
-
-
 def next_prime(n):
-    n += 1
-    while True:
-        if n % 1 == 0 and n % n ==0:
-            return n
-        else:
-            n += 1
+    lst = all(x % i for i in range(2, x))
 
-print(next_prime(0), 2)
+
+# print(next_prime(0), 2)
 print(next_prime(2), 3)
 print(next_prime(3), 5)
 print(next_prime(13), 17)

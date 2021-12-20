5a5f5138fe256b8cfee6a31495fdce737b6f22d6
Paul <gafgafgarion05@gmail.com>
2021-12-20 11:06:10 +0300

    1

--- 7 kyu Next Prime.py
+++ 7 kyu Next Prime.py
@@ -1,5 +1,9 @@
 def next_prime(n):
-    # have fun ^.^
+    answer = n
+
+    if answer + 1 % 1 == 0 and answer + 1 % answer == 0:
+        return answer
+
 
 print(next_prime(0), 2)
 print(next_prime(2), 3)

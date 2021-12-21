aeea9a6de0261ef853c1fce0a66a721d2ca22814
Paul <gafgafgarion05@gmail.com>
2021-12-21 09:09:17 +0300

    1

--- 7 kyu 16+18=214.py
+++ 7 kyu 16+18=214.py
@@ -1,5 +1,14 @@
 def add(num1, num2):
-    pass
+    answer = ''
+
+    last_digit1 = num1 % 10
+    last_digit2 = num2 % 10
+
+    while len(str(num1)) != len(str(num2)):
+        answer += str(last_digit1 + last_digit2)
+        last_digit1 = str(num1)[:-1] % 10
+        last_digit2 = str(num2)[:-1] % 10
+    return answer
 
 
 print(add(2, 11), 13)

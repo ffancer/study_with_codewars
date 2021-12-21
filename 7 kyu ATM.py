581be773d8a384cd63d6bdec9e5c7a482680ab50
Paul <gafgafgarion05@gmail.com>
2021-12-21 10:03:23 +0300

    init

---
+++ 7 kyu ATM.py
@@ -0,0 +1,12 @@
+def solve(n):
+    pass
+
+
+print(solve(770), 4, "Wrong result for 770")
+print(solve(550), 2, "Wrong result for 550")
+print(solve(10), 1, "Wrong result for 10")
+print(solve(1250), 4, "Wrong result for 1250")
+print(solve(1500), 3, "Wrong result for 1500")
+print(solve(125), -1, "Wrong result for 125")
+print(solve(666), -1, "Wrong result for 666")
+print(solve(42), -1, "Wrong result for 42")

cfb1c8770f2b9a6b71f60c75b78062b1bd832020
Paul <gafgafgarion05@gmail.com>
2021-12-21 13:40:32 +0300

    init

---
+++ 7 kyu Spacify.py
@@ -0,0 +1,9 @@
+def spacify(string):
+    pass
+
+
+print(spacify("hello world"), "h e l l o   w o r l d");
+print(spacify("12345"), "1 2 3 4 5")
+print(spacify(""), "")
+print(spacify("a"), "a")
+print(spacify("Pippi"), "P i p p i")

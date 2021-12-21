1c4d653e7c3ba4ffc901badf3af3907f50aa4a6f
Paul <gafgafgarion05@gmail.com>
2021-12-21 13:43:47 +0300

    my solution

--- 7 kyu Spacify.py
+++ 7 kyu Spacify.py
@@ -1,8 +1,17 @@
 def spacify(string):
-    pass
+    if len(string) == 1 or string == '':
+        return string
+
+    s = ''
+
+    for i in string:
+        s += i + ' '
+
+    return s[:-1]
 
 
-print(spacify("hello world"), "h e l l o   w o r l d");
+
+print(spacify("hello world"), "h e l l o   w o r l d")
 print(spacify("12345"), "1 2 3 4 5")
 print(spacify(""), "")
 print(spacify("a"), "a")

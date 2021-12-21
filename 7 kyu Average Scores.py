769d3d785296b007eec90419eaa4dfdc3481899b
Paul <gafgafgarion05@gmail.com>
2021-12-21 14:10:18 +0300

    init

---
+++ 7 kyu Average Scores.py
@@ -0,0 +1,9 @@
+def average(array):
+    pass
+
+
+print(average([5, 78, 52, 900, 1]), 207)
+print(average([5, 25, 50, 75]), 39)
+print(average([2]), 2)
+print(average([1, 1, 1, 1, 9999]), 2001)
+print(average([0]), 0)

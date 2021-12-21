00509573e63542ca565442f19f07dda7ead8f1ba
Paul <gafgafgarion05@gmail.com>
2021-12-21 13:12:10 +0300

    init

---
+++ 7 kyu Unique string characters.py
@@ -0,0 +1,8 @@
+def solve(a, b):
+    pass
+
+
+print(solve("xyab", "xzca"), "ybzc")
+print(solve("xyabb", "xzca"), "ybbzc")
+print(solve("abcd", "xyz"), "abcdxyz")
+print(solve("xxx", "xzca"), "zca")

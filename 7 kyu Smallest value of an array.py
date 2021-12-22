3afba65fcc02e0d7aa984d6d1d98a7d6546af637
Paul <gafgafgarion05@gmail.com>
2021-12-22 11:15:18 +0300

    init

---
+++ 7 kyu Smallest value of an array.py
@@ -0,0 +1,11 @@
+def find_smallest(numbers, to_return):
+    # Enter your code here!
+    return 0
+
+
+print(find_smallest([5, 4, 3, 2, 1], "value"), 1)
+print(find_smallest([5, 4, 3, 2, 1], "index"), 4)
+print(find_smallest([8, 0, 9], "index"), 1)
+print(find_smallest([8, 0, 9], "value"), 0)
+print(find_smallest([1, 1, 0, 0, 1, 1], "value"), 0)
+print(find_smallest([1, 1, 0, 0, 1, 1], "index"), 2)

698d7b6bf7e0a0971d9217237f52dfc730e2e36b
Paul <gafgafgarion05@gmail.com>
2021-12-22 11:18:57 +0300

    my 1 line solution

--- 7 kyu Smallest value of an array.py
+++ 7 kyu Smallest value of an array.py
@@ -1,8 +1,5 @@
 def find_smallest(numbers, to_return):
-    if to_return == 'value':
-        return min(numbers)
-    return numbers.index(min(numbers))
-
+    return min(numbers) if to_return == 'value' else numbers.index(min(numbers))
 
 print(find_smallest([5, 4, 3, 2, 1], "value"), 1)
 print(find_smallest([5, 4, 3, 2, 1], "index"), 4)

71f042be3805d1cea3835982f93beec320b2534f
Paul <gafgafgarion05@gmail.com>
2021-12-22 13:39:42 +0300

    init

---
+++ 7 kyu Sort by Last Char.py
@@ -0,0 +1,10 @@
+def last(s):
+    pass
+
+
+print(last("man i need a taxi up to ubud"), ["a", "need", "ubud", "i", "taxi", "man", "to", "up"])
+print(last("what time are we climbing up the volcano"),
+      ["time", "are", "we", "the", "climbing", "volcano", "up", "what"])
+print(last("take me to semynak"), ["take", "me", "semynak", "to"])
+print(last("massage yes massage yes massage"), ["massage", "massage", "massage", "yes", "yes"])
+print(last("take bintang and a dance please"), ["a", "and", "take", "dance", "please", "bintang"])

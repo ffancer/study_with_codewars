6dcc96f106f5ee794ad00ddb8100f1d332a1c080
Paul <gafgafgarion05@gmail.com>
2021-12-22 09:46:54 +0300

    3 don't understand why s2.isdigit in 2 line is working, but in line 6 not

--- 7 kyu Compare Strings by Sum of Chars.py
+++ 7 kyu Compare Strings by Sum of Chars.py
@@ -1,7 +1,9 @@
 def compare(s1,s2):
+    if s2.isdigit():
+        s2 = ''
     if s1 in [None, ''] or not s1.isalpha():
         s1 = ''
-    elif s2 in [None, ''] or not s2.isalpha():
+    elif s2.isdigit() or s2 in [None, ''] or not s2.isalpha():
         s2 = ''
 
     sum_of_s1 = sum(ord(i) for i in s1.upper())
@@ -9,13 +11,13 @@ def compare(s1,s2):
     return sum_of_s1 == sum_of_s2
 
 
-print(compare("AD", "BC"), True, "\'AD\' vs \'BC\'")
-print(compare("AD", "DD"), False, "\'AD\' vs \'DD\'")
-print(compare("gf", "FG"), True, "\'gf\' vs \'FG\'")
-print(compare("Ad", "DD"), False, "\'Ad\' vs \'DD\'")
-print(compare("zz1", ""), True, "\'zz1\' vs \'\'")
-print(compare("ZzZz", "ffPFF"), True, "\'ZzZz\' vs \'ffPFF\'")
-print(compare("kl", "lz"), False, "\'kl\' vs \'lz\'")
-print(compare(None, ""), True, "\'<null>\' vs \'\'")
+# print(compare("AD", "BC"), True, "\'AD\' vs \'BC\'")
+# print(compare("AD", "DD"), False, "\'AD\' vs \'DD\'")
+# print(compare("gf", "FG"), True, "\'gf\' vs \'FG\'")
+# print(compare("Ad", "DD"), False, "\'Ad\' vs \'DD\'")
+# print(compare("zz1", ""), True, "\'zz1\' vs \'\'")
+# print(compare("ZzZz", "ffPFF"), True, "\'ZzZz\' vs \'ffPFF\'")
+# print(compare("kl", "lz"), False, "\'kl\' vs \'lz\'")
+# print(compare(None, ""), True, "\'<null>\' vs \'\'")
 print(compare("!!", "7476"), True, "\'!!\' vs \'7476\'")
 print(compare("##", "1176"), True, "\'##\' vs \'1176\'")

24b50f3951e15ac5ba47da6fdcac4ab71b6e2dc1
Paul <gafgafgarion05@gmail.com>
2021-12-22 10:35:21 +0300

    init

---
+++ 7 kyu Say hello!.py
@@ -0,0 +1,9 @@
+def greet(name):
+    # your code here
+    pass
+
+
+print(greet("Niks"), "hello Niks!", "didn't work for 'Niks'")
+print(greet("Nick"), "hello Nick!", "didn't work for 'Nick'")
+print(greet(""), None, "didn't work for empty string")
+print(greet(None), None, "didn't work for None")

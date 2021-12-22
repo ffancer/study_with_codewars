73d76da4a65b8c72063befac58aad4eb09324906
Paul <gafgafgarion05@gmail.com>
2021-12-22 10:45:05 +0300

    init

---
+++ 7 kyu Responsible Drinking.py
@@ -0,0 +1,7 @@
+def hydrate(drink_string):
+    # your code here
+    pass
+
+
+print(hydrate("1 beer"), "1 glass of water")
+print(hydrate("1 shot, 5 beers, 2 shots, 1 glass of wine, 1 beer"), "10 glasses of water")

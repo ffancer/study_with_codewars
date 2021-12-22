b52b49dba5c0fa2c892b4008452939611c7dedb3
Paul <gafgafgarion05@gmail.com>
2021-12-22 13:28:57 +0300

    init

---
+++ 7 kyu Count all the sheep on farm in the heights of New Zealand.py
@@ -0,0 +1,8 @@
+def lost_sheep(friday,saturday,total):
+    #your awesome code here
+    return 0
+
+
+print(lost_sheep([1, 2], [3, 4], 15), 5)
+print(lost_sheep([3, 1, 2], [4, 5], 21), 6)
+print(lost_sheep([5, 1, 4], [5, 4], 29), 10)

5ca90214475bc4c244b4c9bd30bc5a9fb0749002
Paul <gafgafgarion05@gmail.com>
2021-12-23 10:02:29 +0300

    init

---
+++ 7 kyu Will you survive the zombie onslaught.py
@@ -0,0 +1,12 @@
+# 7 kyu
+# Will you survive the zombie onslaught?
+
+
+def zombie_shootout(zombies, distance, ammo):
+    # your code goes here
+    pass
+
+
+print(zombie_shootout(3, 10, 10), "You shot all 3 zombies.")
+print(zombie_shootout(100, 8, 200), "You shot 16 zombies before being eaten: overwhelmed.")
+print(zombie_shootout(50, 10, 8), "You shot 8 zombies before being eaten: ran out of ammo.")

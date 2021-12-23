3c6a673830ab393fcbf7f926781e1e28dfeb8f87
Paul <gafgafgarion05@gmail.com>
2021-12-23 11:45:10 +0300

    my solution

--- 7 kyu The wheat-rice and chessboard problem.py
+++ 7 kyu The wheat-rice and chessboard problem.py
@@ -1,10 +1,11 @@
 from math import log2
 
+
 def squares_needed(grains):
-    return int(log2(grains))
+    return int(log2(grains) + 1) if grains > 0 else 0
 
 
-# print(squares_needed(0), 0)
+print(squares_needed(0), 0)
 print(squares_needed(1), 1)
 print(squares_needed(2), 2)
 print(squares_needed(3), 2)

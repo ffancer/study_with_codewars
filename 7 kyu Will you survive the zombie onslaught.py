e6f121c0b8a87524d45d3e546cc531c7488c0384
Paul <gafgafgarion05@gmail.com>
2021-12-23 10:07:12 +0300

    1

--- 7 kyu Will you survive the zombie onslaught.py
+++ 7 kyu Will you survive the zombie onslaught.py
@@ -3,8 +3,17 @@
 
 
 def zombie_shootout(zombies, distance, ammo):
-    # your code goes here
-    pass
+    shot_zomb = 0
+
+    while True:
+        ammo -= 1
+        distance -= 0.5
+        zombies -= 1
+        shot_zomb += 1
+        if zombies == 0 or distance == 0 or ammo == 0:
+            break
+
+    return zombies, distance, ammo, shot_zomb
 
 
 print(zombie_shootout(3, 10, 10), "You shot all 3 zombies.")

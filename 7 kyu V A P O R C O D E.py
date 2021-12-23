a3fce9a6939eada77d36bc45afc69f64ab1f8c72
Paul <gafgafgarion05@gmail.com>
2021-12-23 08:52:41 +0300

    init

---
+++ 7 kyu V A P O R C O D E.py
@@ -0,0 +1,6 @@
+def vaporcode(s):
+    pass
+
+
+print(vaporcode("Lets go to the movies"), "L  E  T  S  G  O  T  O  T  H  E  M  O  V  I  E  S")
+print(vaporcode("Why isn't my code working?"), "W  H  Y  I  S  N  '  T  M  Y  C  O  D  E  W  O  R  K  I  N  G  ?")

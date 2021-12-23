6bd6b3f4ca249e4093dd5f49db1b9087564c865e
Paul <gafgafgarion05@gmail.com>
2021-12-23 11:42:16 +0300

    init

---
+++ 7 kyu The wheat-rice and chessboard problem.py
@@ -0,0 +1,9 @@
+def squares_needed(grains):
+    pass
+
+
+print(squares_needed(0), 0)
+print(squares_needed(1), 1)
+print(squares_needed(2), 2)
+print(squares_needed(3), 2)
+print(squares_needed(4), 3)

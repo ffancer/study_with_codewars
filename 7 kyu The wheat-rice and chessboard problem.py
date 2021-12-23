c9611eabab7c928f024b1a0de3f8e3135f95ad22
Paul <gafgafgarion05@gmail.com>
2021-12-23 11:43:43 +0300

    1

--- 7 kyu The wheat-rice and chessboard problem.py
+++ 7 kyu The wheat-rice and chessboard problem.py
@@ -1,8 +1,10 @@
+from math import log2
+
 def squares_needed(grains):
-    pass
+    return int(log2(grains))
 
 
-print(squares_needed(0), 0)
+# print(squares_needed(0), 0)
 print(squares_needed(1), 1)
 print(squares_needed(2), 2)
 print(squares_needed(3), 2)

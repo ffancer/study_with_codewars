0cec4de356cedcc23a22d657a11b68728f2e1847
Paul <gafgafgarion05@gmail.com>
2021-12-24 09:47:02 +0300

    init

---
+++ 7 kyu Counter of neighbor ones.py
@@ -0,0 +1,11 @@
+def ones_counter(inp):
+    pass
+
+
+print(ones_counter([0]), [])
+print(ones_counter([1, 0, 1, 1]), [1, 2])
+print(ones_counter([0, 0, 0, 0, 0, 0, 0, 0]), [])
+print(ones_counter([1, 1, 1, 1, 1]), [5])
+print(ones_counter([1, 1, 1, 0, 0, 1, 0, 1, 1, 0]), [3, 1, 2])
+print(ones_counter([0, 0, 0, 1, 0, 0, 1, 1]), [1, 2])
+print(ones_counter([1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1]), [1, 2, 4, 1])

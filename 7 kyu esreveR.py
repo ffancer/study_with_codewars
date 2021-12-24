bdac537659e4750948bdecc29e1577d5c546ef50
Paul <gafgafgarion05@gmail.com>
2021-12-24 10:23:10 +0300

    my solution all works but 1 test fail

--- 7 kyu esreveR.py
+++ 7 kyu esreveR.py
@@ -1,6 +1,12 @@
 def reverse(lst):
     empty_list = list()
+    i = len(lst) - 1
 
+    while i != -1:
+        empty_list.append(lst[i])
+        i -= 1
+
+    return empty_list
 
 print(reverse(list([1, 2, 3])), [3, 2, 1])
 print(reverse(list([1, None, 14, "two"])), ["two", 14, None, 1])

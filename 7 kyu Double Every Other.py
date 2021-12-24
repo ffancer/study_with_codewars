b94c5670bb4bdaef34c250bc8727d63bdc7123d5
Paul <gafgafgarion05@gmail.com>
2021-12-24 10:37:59 +0300

    init

---
+++ 7 kyu Double Every Other.py
@@ -0,0 +1,7 @@
+def double_every_other(lst):
+    pass
+
+
+print(double_every_other([1, 2, 3, 4, 5]), [1, 4, 3, 8, 5])
+print(double_every_other([1, 19, 6, 2, 12, -3]), [1, 38, 6, 4, 12, -6])
+print(double_every_other([-1000, 1653, 210, 0, 1]), [-1000, 3306, 210, 0, 1])

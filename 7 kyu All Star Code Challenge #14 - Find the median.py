b2f09475233a1238e902a89506657819f40c7cc8
Paul <gafgafgarion05@gmail.com>
2021-12-24 09:24:38 +0300

    init

---
+++ 7 kyu All Star Code Challenge #14 - Find the median.py
@@ -0,0 +1,9 @@
+def median(array):
+    pass
+
+
+print(median([3, 2, 1]), 2)
+print(median([1]), 1)
+print(median([1234, 345, 78]), 345)
+print(median([33, 99, 100, 30, 29, 50]), 41.5)
+print(median([3, 50]), 26.5)

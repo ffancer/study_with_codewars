1748911a9faa6c29372b30bdd4fae137f5ae50f8
Paul <gafgafgarion05@gmail.com>
2021-12-24 13:09:25 +0300

    82 tests are ok, 22 - errors

--- 7 kyu Double Trouble.py
+++ 7 kyu Double Trouble.py
@@ -1,10 +1,16 @@
 def trouble(x, t):
-    for i in range(len(x)-1):
-        if x[i] + x[i+1] == t:
-            x[i + 1] = 0
+    i, j = 0, 1
+
+    while i < len(x)-1:
+        if x[i] + x[j] != t:
+            i += 1
+            j += 1
+        else:
+            x.remove(x[j])
     return x
 
-print(trouble([1, 3, 5, 6, 7, 4, 3], 7)) # [1, 3, 5, 6, 7, 4]
-print(trouble([4, 1, 1, 1, 4], 2)) # [4, 1, 4]
-print(trouble([2, 6, 2], 8)) # [2, 2]
-print(trouble([2, 2, 2, 2, 2, 2], 4)) # [2]
+
+print(trouble([1, 3, 5, 6, 7, 4, 3], 7))  # [1, 3, 5, 6, 7, 4]
+print(trouble([4, 1, 1, 1, 4], 2))  # [4, 1, 4]
+print(trouble([2, 6, 2], 8))  # [2, 2]
+print(trouble([2, 2, 2, 2, 2, 2], 4))  # [2]

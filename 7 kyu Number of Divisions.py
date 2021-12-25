7cd2f7afb97edaeb27ca039d62608b603d99968d
Paul <gafgafgarion05@gmail.com>
2021-12-25 08:55:01 +0300

    init

---
+++ 7 kyu Number of Divisions.py
@@ -0,0 +1,10 @@
+def divisions(n, divisor):
+    pass
+
+
+print(divisions(6, 2), 2)
+print(divisions(100, 2), 6)
+print(divisions(2450, 5), 4)
+print(divisions(9999, 3), 8)
+print(divisions(2, 3), 0)
+print(divisions(5, 5), 1)

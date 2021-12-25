a7b6797eb020913e4b1e9d0a33c8ea911a1ae71a
Paul <gafgafgarion05@gmail.com>
2021-12-25 08:32:58 +0300

    init

---
+++ 7 kyu The old switcheroo 2.py
@@ -0,0 +1,10 @@
+def encode(string):
+    pass
+
+
+print(encode('abc'), '123')
+print(encode('ABCD'), '1234')
+print(encode('ZzzzZ'), '2626262626')
+print(encode('abc-#@5'), '123-#@5')
+print(encode('this is a long string!! Please [encode] @C0RrEctly'),
+      '208919 919 1 1215147 1920189147!! 161251195 [51431545] @30181853201225')

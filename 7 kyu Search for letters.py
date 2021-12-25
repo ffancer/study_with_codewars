dd09180ae91438ab40a37c60f68e48909232e16c
Paul <gafgafgarion05@gmail.com>
2021-12-25 09:45:04 +0300

    my solution

--- 7 kyu Search for letters.py
+++ 7 kyu Search for letters.py
@@ -1,10 +1,7 @@
 def change(st):
-    s = '00000000000000000000000000'
+    st = st.lower()
+    return ''.join(str(int(chr(i) in st)) for i in range(97, 123))
 
-    for i in st:
-        if i.isalpha():
-            s.replace(str(st.find(ord(i)-96)), '1')
-    return s
 print(change("a **&  bZ"), "11000000000000000000000001")
 print(change('Abc e  $$  z'), "11101000000000000000000001")
 print(change("!!a$%&RgTT"), "10000010000000000101000000")

e557a5d10c23f740217a75933ed33e50bbc729dc
Paul <gafgafgarion05@gmail.com>
2021-12-25 10:52:25 +0300

    my 1 line solution

--- 7 kyu Get the integers between two numbers.py
+++ 7 kyu Get the integers between two numbers.py
@@ -1,10 +1,8 @@
 def function(start_num, end_num):
-    lst = []
+    return [i for i in range(start_num+1, end_num)]
 
-    for i in range(start_num+1, end_num):
-        lst.append(i)
 
-    return lst
+
 
 
 print(function(2, 9), [3, 4, 5, 6, 7, 8])

09a87ae8c550719b5fbdb7d546219145af29a5e7
Paul <gafgafgarion05@gmail.com>
2021-12-25 09:11:17 +0300

    init

---
+++ 7 kyu Search for letters.py
@@ -0,0 +1,12 @@
+def change(st):
+    # your code here
+    pass
+
+
+print(change("a **&  bZ"), "11000000000000000000000001")
+print(change('Abc e  $$  z'), "11101000000000000000000001")
+print(change("!!a$%&RgTT"), "10000010000000000101000000")
+print(change(""), "00000000000000000000000000", "empty string should return 26 '0'")
+print(change("abcdefghijklmnopqrstuvwxyz"), "11111111111111111111111111")
+print(change("aaaaaaaaaaa"), "10000000000000000000000000")
+print(change("&%&%/$%$%$%$%GYtf67fg34678hgfdyd"), "00010111000000000001000010")

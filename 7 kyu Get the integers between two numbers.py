0d0980bb0a920be42100301bd0b1ff49c4956f54
Paul <gafgafgarion05@gmail.com>
2021-12-25 10:50:04 +0300

    init

---
+++ 7 kyu Get the integers between two numbers.py
@@ -0,0 +1,8 @@
+def function(start_num, end_num):
+    # your code here
+    pass
+
+
+print(function(2, 9), [3, 4, 5, 6, 7, 8])
+print(function(6, 8), [7])
+print(function(2, 9), [3, 4, 5, 6, 7, 8])

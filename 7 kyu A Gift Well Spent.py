617a73a32fb9b0361d6451c5cfa61eda572d3451
Paul <gafgafgarion05@gmail.com>
2021-12-27 09:54:57 +0300

    3

--- 7 kyu A Gift Well Spent.py
+++ 7 kyu A Gift Well Spent.py
@@ -1,19 +1,17 @@
 def buy(x, arr):
-    i = 0
-    j = 1
     lst = []
-    while True:
-        if arr[i] + arr[j] != x:
-            j += 1
-            if j == len(arr)-1:
-                i += 1
-        lst.append(i)
-        lst.append(j)
+    i, j = 0, 1
+    while j < len(arr):
+        if arr[i] + arr[j] == x:
+            lst.append(i)
+            lst.append(j)
+        j += 1
+        if j == len(arr):
+            i += 1
     return lst
-
 
 print(buy(2, [1, 1]), [0, 1])
 # print(buy(3, [1, 1]), None)
-# print(buy(5, [5, 2, 3, 4, 5]), [1, 2])
+print(buy(5, [5, 2, 3, 4, 5]), [1, 2])
 # print(buy(5, [1, 2, 3, 4, 5]), [0, 3])
 # print(buy(5, [0, 0, 0, 2, 3]), [3, 4])

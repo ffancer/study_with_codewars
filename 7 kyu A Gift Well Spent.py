b4e3af499825d9714bd3242bd2241cc5d0814e22
Paul <gafgafgarion05@gmail.com>
2021-12-27 10:58:04 +0300

    my solution

--- 7 kyu A Gift Well Spent.py
+++ 7 kyu A Gift Well Spent.py
@@ -1,9 +1,9 @@
 def buy(x, arr):
     if len(arr) < 2 or sum(arr) < x:
         return None
+
     for i in range(len(arr)):
         for j in range(len(arr)):
-            # print(i, j)
             if i != j and arr[i] + arr[j] == x:
                 return [i, j]
 

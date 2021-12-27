9a92acfb842ef31da48cfccd2720870680514d8c
Paul <gafgafgarion05@gmail.com>
2021-12-27 13:19:14 +0300

    my solution with counter cnt

--- 7 kyu Stones on the Table.py
+++ 7 kyu Stones on the Table.py
@@ -1,6 +1,11 @@
 def solution(stones):
-    pass
+    cnt = 0
 
+    for i in range(len(stones) - 1):
+        if stones[i] == stones[i + 1]:
+            cnt += 1
+
+    return cnt
 
 print(solution("RGBRGBRGGB"), 1)
 print(solution("RGGRGBBRGRR"), 3)

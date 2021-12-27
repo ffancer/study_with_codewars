a4fd62191317b0558015052b4b47b555ebdec020
Paul <gafgafgarion05@gmail.com>
2021-12-27 10:43:21 +0300

    6

--- 7 kyu A Gift Well Spent.py
+++ 7 kyu A Gift Well Spent.py
@@ -1,4 +1,6 @@
 def buy(x, arr):
+    if len(arr) < 2 or sum(arr) < x:
+        return None
     i, j = 0,1
 
     while i < len(arr):
@@ -11,6 +13,7 @@ def buy(x, arr):
 
 
 
+print(buy(0, [2, -2]), [0, 1])
 print(buy(2, [1, 1]), [0, 1])
 print(buy(3, [1, 1]), None)
 print(buy(5, [5, 2, 3, 4, 5]), [1, 2])

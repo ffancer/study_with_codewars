b480f41fb6d85f521ed95388c4ad3cff153f4fc0
Paul <gafgafgarion05@gmail.com>
2021-12-27 14:57:32 +0300

    2

--- 7 kyu Check three and two.py
+++ 7 kyu Check three and two.py
@@ -1,5 +1,13 @@
 def check_three_and_two(array):
-    return array.count('a') in [3, 2] or array.count('b') in [3, 2] or array.count('c') in [3, 2]
+    cnt = 0
+    if array.count('a') in [3, 2]:
+        cnt += 1
+    if array.count('b') in [3, 2]:
+        cnt += 1
+    if array.count('c') in [3, 2]:
+        cnt += 1
+
+    return cnt >= 2
 
 
 print(check_three_and_two(["a", "a", "a", "b", "b"]), True, str(["a", "a", "a", "b", "b"]))

de6411ad479b908abbd2b55d2c9dc5d64b274f20
Paul <gafgafgarion05@gmail.com>
2021-12-27 10:00:51 +0300

    6 errors from 24 tests

--- 7 kyu A Gift Well Spent.py
+++ 7 kyu A Gift Well Spent.py
@@ -10,10 +10,10 @@ def buy(x, arr):
             j = i
             i += 1
 
-    return lst
+    return None if lst == [] else lst[:2]
 
 print(buy(2, [1, 1]), [0, 1])
-# print(buy(3, [1, 1]), None)
+print(buy(3, [1, 1]), None)
 print(buy(5, [5, 2, 3, 4, 5]), [1, 2])
 print(buy(5, [1, 2, 3, 4, 5]), [0, 3])
 print(buy(5, [0, 0, 0, 2, 3]), [3, 4])

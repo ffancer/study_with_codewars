58e260cd7d457b99a0ce1c4fbd1df8cb8c9ecb53
Paul <gafgafgarion05@gmail.com>
2021-12-27 13:20:06 +0300

    my 1 line solution sum()

--- 7 kyu Stones on the Table.py
+++ 7 kyu Stones on the Table.py
@@ -1,11 +1,5 @@
 def solution(stones):
-    cnt = 0
-
-    for i in range(len(stones) - 1):
-        if stones[i] == stones[i + 1]:
-            cnt += 1
-
-    return cnt
+    return sum(1 for i in range(len(stones) - 1) if stones[i] == stones[i + 1])
 
 print(solution("RGBRGBRGGB"), 1)
 print(solution("RGGRGBBRGRR"), 3)

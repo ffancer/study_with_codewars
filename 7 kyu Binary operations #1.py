dcdf32749487f63a9fd0199188fd319123a9f474
Paul <gafgafgarion05@gmail.com>
2021-12-28 13:18:31 +0300

    solution

--- 7 kyu Binary operations #1.py
+++ 7 kyu Binary operations #1.py
@@ -1,8 +1,9 @@
 def flip_bit(value, bit_index):
-    n = bin(value)[2:]
-    return n
+    return value ^ (1 << (bit_index - 1))
 
 
+print(flip_bit(15, 4), 7)
+print(flip_bit(15, 5), 31)
 print(flip_bit(0, 16), 1 << 15)
 print(flip_bit((1 << 31) - 1, 31), (1 << 30) - 1)
 print(flip_bit(127, 8), 0xFF)

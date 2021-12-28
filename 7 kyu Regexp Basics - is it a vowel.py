f97527a0192ec9d1f49e51edf102b300bfa1cc8c
Paul <gafgafgarion05@gmail.com>
2021-12-28 09:38:01 +0300

    init

---
+++ 7 kyu Regexp Basics - is it a vowel.py
@@ -0,0 +1,14 @@
+# 7 kyu
+# Regexp Basics - is it a vowel?
+
+def is_vowel(s):
+    # your code here
+    pass
+
+
+print(is_vowel(""), False)
+print(is_vowel("a"), True)
+print(is_vowel("E"), True)
+print(is_vowel("ou"), False)
+print(is_vowel("z"), False)
+print(is_vowel("lol"), False)

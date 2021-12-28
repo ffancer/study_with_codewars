1e31fd7a610c9e7d9883203f323439ae1c6901df
Paul <gafgafgarion05@gmail.com>
2021-12-28 10:40:46 +0300

    init

---
+++ 7 kyu Minimum Steps (Array Series #6).py
@@ -0,0 +1,9 @@
+def minimum_steps(numbers, value):
+    pass
+
+
+print(minimum_steps([4, 6, 3], 7), 1)
+print(minimum_steps([10, 9, 9, 8], 17), 1)
+print(minimum_steps([8, 9, 10, 4, 2], 23), 3)
+print(minimum_steps([19, 98, 69, 28, 75, 45, 17, 98, 67], 464), 8)
+print(minimum_steps([4, 6, 3], 2), 0)

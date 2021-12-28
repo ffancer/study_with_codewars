e722bb53a0e416d400ac7f7ad3b36ce58c33aa71
Paul <gafgafgarion05@gmail.com>
2021-12-28 08:02:11 +0300

    4

--- 7 kyu Rearrange Number to Get its Maximum.py
+++ 7 kyu Rearrange Number to Get its Maximum.py
@@ -1,9 +1,12 @@
 def max_redigit(num):
-    if len(str(num)) != 3:
+    if 99 < num < 1000:
         return None
     lst = [i for i in str(num) if i.isdigit()]
     return int(''.join(sorted(lst, reverse=True)))
 
+print(max_redigit(82), None)
+print(max_redigit(33), None)
+print(max_redigit(29), None)
 print(max_redigit(123), 321)
 print(max_redigit(555), 555)
 print(max_redigit(-1), None)

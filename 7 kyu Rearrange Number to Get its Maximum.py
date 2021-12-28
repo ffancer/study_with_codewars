079566cd8293195901a1163188cbc0bf48911ec2
Paul <gafgafgarion05@gmail.com>
2021-12-28 07:39:48 +0300

    3 1 error

--- 7 kyu Rearrange Number to Get its Maximum.py
+++ 7 kyu Rearrange Number to Get its Maximum.py
@@ -1,5 +1,7 @@
 def max_redigit(num):
-    lst = [i for i in str(num)]
+    if len(str(num)) != 3:
+        return None
+    lst = [i for i in str(num) if i.isdigit()]
     return int(''.join(sorted(lst, reverse=True)))
 
 print(max_redigit(123), 321)

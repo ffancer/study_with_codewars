20cec7152862f48dfa2c2663e88f33e3ec871cda
Paul <gafgafgarion05@gmail.com>
2021-12-28 07:33:27 +0300

    init

---
+++ 7 kyu Rearrange Number to Get its Maximum.py
@@ -0,0 +1,11 @@
+def max_redigit(num):
+    # your code here
+    pass
+
+
+print(max_redigit(123), 321)
+print(max_redigit(555), 555)
+print(max_redigit(-1), None)
+print(max_redigit(0), None)
+print(max_redigit(99), None)
+print(max_redigit(1000), None)

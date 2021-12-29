00c3973bfae3439becd40b0aba73bd9f1571576a
Paul <gafgafgarion05@gmail.com>
2021-12-29 09:41:06 +0300

    2 errors

--- 7 kyu Looking for a benefactor.py
+++ 7 kyu Looking for a benefactor.py
@@ -1,7 +1,6 @@
 def new_avg(arr, newavg):
-    return (len(arr) + 1) * newavg
+    return (len(arr) + 1) * newavg - sum(arr)
 
 
 print(new_avg([14, 30, 5, 7, 9, 11, 16], 90), 628)
 print(new_avg([14, 30, 5, 7, 9, 11, 15], 92), 645)
-# ("Error expected", lambda: new_avg([14, 30, 5, 7, 9, 11, 15], 2))

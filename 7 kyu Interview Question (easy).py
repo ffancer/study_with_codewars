4effa0feb7151782ea67f5f752504f85a925d7de
Paul <gafgafgarion05@gmail.com>
2021-12-29 14:38:28 +0300

    my solution

--- 7 kyu Interview Question (easy).py
+++ 7 kyu Interview Question (easy).py
@@ -5,7 +5,9 @@ def get_strings(city):
     city = [i for i in city.lower() if i.isalpha()]
     city = Counter(city)
 
-    return city
+    return ','.join(f"{i}:{j * '*'}" for i, j in city.items())
+
+
 print(get_strings("Chicago"), "c:**,h:*,i:*,a:*,g:*,o:*")
 print(get_strings("Bangkok"), "b:*,a:*,n:*,g:*,k:**,o:*")
 print(get_strings("Las Vegas"), "l:*,a:**,s:**,v:*,e:*,g:*")

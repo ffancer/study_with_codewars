b3caaa97207d43d1f10287bfc86996cae1b0c3d8
Paul <gafgafgarion05@gmail.com>
2021-12-29 13:30:28 +0300

    init

---
+++ 7 kyu Interview Question (easy).py
@@ -0,0 +1,7 @@
+def get_strings(city):
+    pass
+
+
+print(get_strings("Chicago"), "c:**,h:*,i:*,a:*,g:*,o:*")
+print(get_strings("Bangkok"), "b:*,a:*,n:*,g:*,k:**,o:*")
+print(get_strings("Las Vegas"), "l:*,a:**,s:**,v:*,e:*,g:*")

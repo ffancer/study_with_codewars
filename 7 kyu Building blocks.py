71c2295bacbc4853982238e8b6aa251e41de801c
Paul <gafgafgarion05@gmail.com>
2021-12-29 10:46:34 +0300

    init

---
+++ 7 kyu Building blocks.py
@@ -0,0 +1,10 @@
+class Block:
+    pass
+
+
+block1 = Block([2, 2, 2])
+print(block1.get_width(), 2)
+print(block1.get_length(), 2)
+print(block1.get_height(), 2)
+print(block1.get_volume(), 8)
+print(block1.get_surface_area(), 24)

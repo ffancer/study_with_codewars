398dcdb625cc908a4b68decc7a7b3be4d980d931
Paul <gafgafgarion05@gmail.com>
2021-12-29 09:17:40 +0300

    init

---
+++ 7 kyu Simple Fun #2 Circle of Numbers.py
@@ -0,0 +1,12 @@
+# 7 kyu
+# Simple Fun #2: Circle of Numbers
+
+
+def circle_of_numbers(n, fst):
+    pass
+
+
+print(circle_of_numbers(10, 2), 7)
+print(circle_of_numbers(10, 7), 2)
+print(circle_of_numbers(4, 1), 3)
+print(circle_of_numbers(6, 3), 0)

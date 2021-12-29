8c5aaa98271c3cb75ec56fb50d171fe477aba909
Paul <gafgafgarion05@gmail.com>
2021-12-29 09:36:30 +0300

    init

---
+++ 7 kyu Looking for a benefactor.py
@@ -0,0 +1,7 @@
+def new_avg(arr, newavg):
+    pass
+
+
+print(new_avg([14, 30, 5, 7, 9, 11, 16], 90), 628)
+print(new_avg([14, 30, 5, 7, 9, 11, 15], 92), 645)
+# ("Error expected", lambda: new_avg([14, 30, 5, 7, 9, 11, 15], 2))

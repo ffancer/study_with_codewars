9f7ecfd5fd91ae968a9a56b822962867a78aab7e
Paul <gafgafgarion05@gmail.com>
2021-12-29 09:44:40 +0300

    my solution

--- 7 kyu Looking for a benefactor.py
+++ 7 kyu Looking for a benefactor.py
@@ -1,5 +1,9 @@
 def new_avg(arr, newavg):
-    return (len(arr) + 1) * newavg - sum(arr)
+    answer = newavg*float(len(arr) + 1) - sum(arr)
+    if answer < 0:
+        raise ValueError
+    return int(answer + 0.999)
+
 
 
 print(new_avg([14, 30, 5, 7, 9, 11, 16], 90), 628)

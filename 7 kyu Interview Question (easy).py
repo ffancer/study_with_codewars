ffccf29838c113e9f42443e5ead25e3a3a147b31
Paul <gafgafgarion05@gmail.com>
2021-12-29 14:17:28 +0300

    4

--- 7 kyu Interview Question (easy).py
+++ 7 kyu Interview Question (easy).py
@@ -1,12 +1,10 @@
+from collections import Counter
+
+
 def get_strings(city):
-    s = ''
+    city = Counter(city.lower())
 
-    for i in city.lower():
-        if i.isalpha():
-            if i not in s:
-                s += f'{i}:{"*" * city.count(i)},'
-
-    return s
+    return city
 print(get_strings("Chicago"), "c:**,h:*,i:*,a:*,g:*,o:*")
 print(get_strings("Bangkok"), "b:*,a:*,n:*,g:*,k:**,o:*")
 print(get_strings("Las Vegas"), "l:*,a:**,s:**,v:*,e:*,g:*")

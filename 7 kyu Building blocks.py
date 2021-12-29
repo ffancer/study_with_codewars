78e4b69e1bb2ba01ac7700f13542be84ac58f0a4
Paul <gafgafgarion05@gmail.com>
2021-12-29 10:48:56 +0300

    1

--- 7 kyu Building blocks.py
+++ 7 kyu Building blocks.py
@@ -1,5 +1,18 @@
 class Block:
-    pass
+    def get_width(self):
+        pass
+
+    def get_length(self):
+        pass
+
+    def get_height(self):
+        pass
+
+    def get_volume(self):
+        pass
+
+    def get_surface_area(self):
+        pass
 
 
 block1 = Block([2, 2, 2])

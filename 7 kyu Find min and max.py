781a337d41dc84588e75ec1088656a4289a6a7fe
Paul <gafgafgarion05@gmail.com>
2021-12-30 13:43:45 +0300

    my solution more complex without min() max()

--- 7 kyu Find min and max.py
+++ 7 kyu Find min and max.py
@@ -1,5 +1,14 @@
 def get_min_max(seq):
-    return min(seq), max(seq)
+    minimum = 0
+    maximum = 100
+
+    for i in seq:
+        if i > minimum:
+            minimum = i
+        if i < maximum:
+            maximum = i
+
+    return maximum, minimum
 
 
 print(get_min_max([1]), (1, 1))

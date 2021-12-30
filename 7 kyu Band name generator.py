f4b407d2f968ff2c13c0568110a91dae641959e8
Paul <gafgafgarion05@gmail.com>
2021-12-30 14:21:16 +0300

    init

---
+++ 7 kyu Band name generator.py
@@ -0,0 +1,9 @@
+def band_name_generator(name):
+    pass
+
+
+print(band_name_generator("knife"), "The Knife")
+print(band_name_generator("tart"), "Tartart")
+print(band_name_generator("sandles"), "Sandlesandles")
+print(band_name_generator("bed"), "The Bed")
+print(band_name_generator("qq"), "Qqq")

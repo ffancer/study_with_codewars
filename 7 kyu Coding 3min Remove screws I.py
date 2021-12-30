ac91a67f3f65e7dd7d23c52b05c856bd7e17adfb
Paul <gafgafgarion05@gmail.com>
2021-12-30 10:59:31 +0300

    init

---
+++ 7 kyu Coding 3min Remove screws I.py
@@ -0,0 +1,16 @@
+# 7 kyu
+# Coding 3min : Remove screws I
+
+
+def sc(s):
+    return
+
+
+print(sc("+"), 1)
+print(sc("-"), 1)
+print(sc("+-"), 8)
+print(sc("-+"), 8)
+print(sc("---+++"), 16)
+print(sc("-+-+-+"), 36)
+print(sc("-+-+-----------"), 49)
+print(sc("-+-+-++++++++++"), 54)

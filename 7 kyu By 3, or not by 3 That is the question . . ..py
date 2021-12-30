a596e8865929ff0133056a7427d86c0ca9d13afe
Paul <gafgafgarion05@gmail.com>
2021-12-30 08:37:35 +0300

    init

---
+++ 7 kyu By 3, or not by 3 That is the question . . ..py
@@ -0,0 +1,17 @@
+# 7 kyu
+# By 3, or not by 3? That is the question . . .
+
+
+def divisible_by_three(st):
+    # your code here
+    pass
+
+
+print(divisible_by_three('123'), True,
+      "Should return true if the sum of the given digits is divisible by 3.")
+print(divisible_by_three('19254'), True,
+      "Should return true if the sum of the given digits is divisible by 3.")
+print(divisible_by_three('88'), False,
+      "Should return false if the sum of the given digits is not divisible by 3.")
+print(divisible_by_three('1'), False,
+      "Should return false if the sum of the given digits is not divisible by 3")

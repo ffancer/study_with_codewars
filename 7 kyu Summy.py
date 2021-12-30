60581767719ffba9fdd155cad2577d8b31c143b7
Paul <gafgafgarion05@gmail.com>
2021-12-30 10:10:37 +0300

    init

---
+++ 7 kyu Summy.py
@@ -0,0 +1,10 @@
+def summy(string_of_ints):
+    # do your magic !
+    return sum_of_ints
+
+
+print(summy("1 2 3"), 6)
+print(summy("1 2 3 4"), 10)
+print(summy("1 2 3 4 5"), 15)
+print(summy("10 10"), 20)
+print(summy("0 0"), 0)

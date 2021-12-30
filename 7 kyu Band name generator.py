2919ca80a7c55bbce1f80b049020004fba3b3714
Paul <gafgafgarion05@gmail.com>
2021-12-30 14:24:37 +0300

    my 1 line solution

--- 7 kyu Band name generator.py
+++ 7 kyu Band name generator.py
@@ -1,5 +1,5 @@
 def band_name_generator(name):
-    return f'The {name.title()}'
+    return f'{name.title() + name[1::]}' if name[0] == name[-1] else f'The {name.title()}'
 
 
 print(band_name_generator("knife"), "The Knife")

c5b9f27eaa100ac9b19e273f16a9d3e0710bad36
Paul <gafgafgarion05@gmail.com>
2021-12-31 11:19:06 +0300

    init

---
+++ 7 kyu String matchup.py
@@ -0,0 +1,7 @@
+def solve(a, b):
+    pass
+
+
+print(solve(['abc', 'abc', 'xyz', 'abcd', 'cde'], ['abc', 'cde', 'uap']), [2, 1, 0])
+print(solve(['abc', 'xyz', 'abc', 'xyz', 'cde'], ['abc', 'cde', 'xyz']), [2, 1, 2])
+print(solve(['quick', 'brown', 'fox', 'is', 'quick'], ['quick', 'abc', 'fox']), [2, 0, 1])

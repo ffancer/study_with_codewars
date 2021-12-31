21b325e6be961f257c6e8c33f5df7c1561736150
Paul <gafgafgarion05@gmail.com>
2021-12-31 14:03:27 +0300

    init

---
+++ 7 kyu Arithmetic progression.py
@@ -0,0 +1,7 @@
+def arithmetic_sequence_elements(a, r, n):
+    pass
+
+
+print(arithmetic_sequence_elements(1, 2, 5), '1, 3, 5, 7, 9')
+print(arithmetic_sequence_elements(1, 0, 5), '1, 1, 1, 1, 1')
+print(arithmetic_sequence_elements(1, -3, 10), '1, -2, -5, -8, -11, -14, -17, -20, -23, -26')

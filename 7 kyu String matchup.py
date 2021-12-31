6b789eac23800642665e926b514b58aeeb9e8703
Paul <gafgafgarion05@gmail.com>
2021-12-31 11:22:12 +0300

    1

--- 7 kyu String matchup.py
+++ 7 kyu String matchup.py
@@ -1,6 +1,19 @@
 def solve(a, b):
-    pass
+    cnt_1, cnt_2, cnt_3 = 0,0,0
 
+    for i in b:
+        if i in a:
+            cnt_1 += 1
+
+    for j in b:
+        if j in a:
+            cnt_2 += 1
+
+    for k in b:
+        if k in a:
+            cnt_3 += 1
+
+    return [cnt_1, cnt_2, cnt_3]
 
 print(solve(['abc', 'abc', 'xyz', 'abcd', 'cde'], ['abc', 'cde', 'uap']), [2, 1, 0])
 print(solve(['abc', 'xyz', 'abc', 'xyz', 'cde'], ['abc', 'cde', 'xyz']), [2, 1, 2])

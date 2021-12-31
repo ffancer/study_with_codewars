87ebce47ae07c2fa94746278efe10f840595ec2c
Paul <gafgafgarion05@gmail.com>
2021-12-31 11:42:15 +0300

    init

---
+++ 7 kyu Covfefe.py
@@ -0,0 +1,9 @@
+def covfefe(s):
+    pass
+
+
+print(covfefe("coverage"), "covfefe")
+print(covfefe("coverage coverage"), "covfefe covfefe")
+print(covfefe("nothing"), "nothing covfefe")
+print(covfefe("double space "), "double space  covfefe")
+print(covfefe("covfefe"), "covfefe covfefe")

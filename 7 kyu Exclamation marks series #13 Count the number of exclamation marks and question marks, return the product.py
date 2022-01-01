315c587c0c21d221988bb79e54102eb22dba592c
Paul <gafgafgarion05@gmail.com>
2022-01-01 08:07:48 +0300

    init

---
+++ 7 kyu Exclamation marks series #13 Count the number of exclamation marks and question marks, return the product.py
@@ -0,0 +1,11 @@
+# 7 kyu
+# Exclamation marks series #13: Count the number of exclamation marks and question marks, return the product
+
+
+def product(s):
+    pass
+
+
+print(product(''), 0)
+print(product('!'), 0)
+print(product('!!??!!'), 8)

c167a0143260afa58f7a379b6cbd09dd5248c33d
Paul <gafgafgarion05@gmail.com>
2022-01-01 10:07:19 +0300

    init

---
+++ 7 kyu Spoonerize Me.py
@@ -0,0 +1,9 @@
+def spoonerize(words):
+    # ...aaaaand SPOONERIZE!
+    return ""
+
+
+print(spoonerize("nit picking"), "pit nicking")
+print(spoonerize("wedding bells"), "bedding wells")
+print(spoonerize("jelly beans"), "belly jeans")
+print(spoonerize("pop corn"), "cop porn")

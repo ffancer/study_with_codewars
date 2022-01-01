4cd92542205a5140fe2926a5862d56e3af2fbf61
Paul <gafgafgarion05@gmail.com>
2022-01-01 10:11:51 +0300

    my solution

--- 7 kyu Spoonerize Me.py
+++ 7 kyu Spoonerize Me.py
@@ -1,6 +1,6 @@
 def spoonerize(words):
     a, b = words.split()[0][0], words.split()[-1][0]
-    return
+    return b + words.split()[0][1:] + ' ' + a + words.split()[-1][1:]
 
 print(spoonerize("nit picking"), "pit nicking")
 print(spoonerize("wedding bells"), "bedding wells")

340370419f98b4d1ef14b20c3739a89a64865012
Paul <gafgafgarion05@gmail.com>
2022-01-01 13:36:53 +0300

    init

---
+++ 7 kyu Help Bob count letters and digits..py
@@ -0,0 +1,9 @@
+def count_letters_and_digits(s):
+    return
+
+
+print(count_letters_and_digits('n!!ice!!123'), 7)
+print(count_letters_and_digits('de?=?=tttes!!t'), 8)
+print(count_letters_and_digits(''), 0)
+print(count_letters_and_digits('!@#$%^&`~.'), 0)
+print(count_letters_and_digits('u_n_d_e_r__S_C_O_R_E'), 10)

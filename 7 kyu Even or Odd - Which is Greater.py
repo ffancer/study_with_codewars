ce33cdd0a3de2ea54cffc684748eaabae3e0d7e0
Paul <gafgafgarion05@gmail.com>
2022-01-01 08:24:03 +0300

    init

---
+++ 7 kyu Even or Odd - Which is Greater.py
@@ -0,0 +1,12 @@
+# 7 kyu
+# Even or Odd - Which is Greater?
+
+
+def even_or_odd(s):
+    # your code here
+    return ''
+
+
+print(even_or_odd('12'), 'Even is greater than Odd')
+print(even_or_odd('123'), 'Odd is greater than Even')
+print(even_or_odd('112'), 'Even and Odd are the same')

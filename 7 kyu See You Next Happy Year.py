b23ac6fac2b2f27fc1803011a5b63bc77430fc6c
Paul <gafgafgarion05@gmail.com>
2022-01-03 11:58:18 +0300

    init

---
+++ 7 kyu See You Next Happy Year.py
@@ -0,0 +1,13 @@
+def next_happy_year(year):
+    pass
+
+print(next_happy_year(1001), 1023)
+print(next_happy_year(1123), 1203)
+print(next_happy_year(2001), 2013)
+print(next_happy_year(2334), 2340)
+print(next_happy_year(3331), 3401)
+print(next_happy_year(1987), 2013)
+print(next_happy_year(5555), 5601)
+print(next_happy_year(7712), 7801)
+print(next_happy_year(8088), 8091)
+print(next_happy_year(8999), 9012)

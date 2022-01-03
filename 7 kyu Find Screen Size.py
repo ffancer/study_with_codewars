c7b98570dcde79358b5701747daf104a4eae3874
Paul <gafgafgarion05@gmail.com>
2022-01-03 11:06:50 +0300

    init

---
+++ 7 kyu Find Screen Size.py
@@ -0,0 +1,8 @@
+def find_screen_height(width, ratio):
+    # your code here
+    pass
+
+
+print(find_screen_height(1024, "4:3"), "1024x768")
+print(find_screen_height(1280, "16:9"), "1280x720")
+print(find_screen_height(3840, "32:9"), "3840x1080")

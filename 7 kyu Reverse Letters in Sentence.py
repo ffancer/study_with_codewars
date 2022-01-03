de62a9c21a447f3e6efd0af800236d6c3068ac01
Paul <gafgafgarion05@gmail.com>
2022-01-03 13:57:34 +0300

    1

--- 7 kyu Reverse Letters in Sentence.py
+++ 7 kyu Reverse Letters in Sentence.py
@@ -1,5 +1,15 @@
 def reverser(sentence):
-    pass
+    s = ''
+    splt = sentence.split()
+    for i in splt:
+        s += str(i)[::-1] + ' '
+
+    if sentence[0] == ' ':
+        return ' ' + s
+    if sentence[-1] == ' ':
+        return s + ' '
+    if sentence[0] == ' ' and sentence[-1] == ' ':
+        return s.strip()
 
 
 print(reverser("Hi mom"), "iH mom")

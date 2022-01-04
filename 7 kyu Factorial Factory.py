51cfd7892a20d63b9c133e8378959bc66cb0895a
Paul <gafgafgarion05@gmail.com>
2022-01-04 13:25:07 +0300

    init

---
+++ 7 kyu Factorial Factory.py
@@ -0,0 +1,8 @@
+# This function should return n!
+def factorial(n):
+    pass
+
+
+print(factorial(2), 2, 'Your math may be incorrect')
+print(factorial(5), 120, 'Your math may be incorrect')
+print(factorial(-1), None, "Don't forget to check for negatives!")

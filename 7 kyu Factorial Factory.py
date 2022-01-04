7ac2a2329eb963737f5b4ea0943ff9ffa6b55dc7
Paul <gafgafgarion05@gmail.com>
2022-01-04 13:28:13 +0300

    my solution

--- 7 kyu Factorial Factory.py
+++ 7 kyu Factorial Factory.py
@@ -2,9 +2,13 @@ from math import factorial as fctrl
 
 
 def factorial(n):
-    return fctrl(n)
+    if n in [0, 1]:
+        return 1
+    return fctrl(n) if n > 1 else None
 
 
+print(factorial(0), 1, 'Your math may be incorrect')
+print(factorial(1), 1, 'Your math may be incorrect')
 print(factorial(2), 2, 'Your math may be incorrect')
 print(factorial(5), 120, 'Your math may be incorrect')
 print(factorial(-1), None, "Don't forget to check for negatives!")

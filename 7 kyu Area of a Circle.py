7dac0bf1208f9b8c5e1867d6b0aebfbb51a11b83
Paul <gafgafgarion05@gmail.com>
2022-01-04 10:08:36 +0300

    init

---
+++ 7 kyu Area of a Circle.py
@@ -0,0 +1,8 @@
+def circle_area(r):
+    pass
+
+
+print(circle_area(2), 12.57, "Incorrect radius")
+print(circle_area(-3), False, "Negative radii don't have circle areas")
+print(circle_area('An Integer'), False, "Strings can't generate areas")
+print(circle_area(0), False, "Radius of 0 can't generate an area")

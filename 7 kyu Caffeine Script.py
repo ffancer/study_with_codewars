0a177334e513d137bafa7524f7747ace2b20984f
Paul <gafgafgarion05@gmail.com>
2022-01-04 09:53:00 +0300

    init

---
+++ 7 kyu Caffeine Script.py
@@ -0,0 +1,16 @@
+def caffeine_buzz(n):
+    pass
+
+
+print(caffeine_buzz(1), "mocha_missing!")
+print(caffeine_buzz(2), "mocha_missing!")
+print(caffeine_buzz(3), "Java")
+print(caffeine_buzz(4), "mocha_missing!")
+print(caffeine_buzz(5), "mocha_missing!")
+print(caffeine_buzz(6), "JavaScript")
+print(caffeine_buzz(7), "mocha_missing!")
+print(caffeine_buzz(8), "mocha_missing!")
+print(caffeine_buzz(9), "Java")
+print(caffeine_buzz(10), "mocha_missing!")
+print(caffeine_buzz(11), "mocha_missing!")
+print(caffeine_buzz(12), "CoffeeScript")

ff20bfbfbf776870126833ac2c93c6ebeda7dd93
Paul <gafgafgarion05@gmail.com>
2022-01-05 07:24:29 +0300

    init

---
+++ 7 kyu Head, Tail, Init and Last.py
@@ -0,0 +1,7 @@
+# TODO: implement the four functions specified.
+
+
+test.assert_equals(head([5, 1]), 5)
+test.assert_equals(tail([1]), [])
+test.assert_equals(init([1, 5, 7, 9]), [1, 5, 7])
+test.assert_equals(last([7, 2]), 2)

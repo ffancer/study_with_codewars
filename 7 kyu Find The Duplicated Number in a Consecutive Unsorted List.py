c21b1ea21630e708e0f5573df410d052c2760b76
Paul <gafgafgarion05@gmail.com>
2022-01-05 13:20:37 +0300

    my second solution with count()

--- 7 kyu Find The Duplicated Number in a Consecutive Unsorted List.py
+++ 7 kyu Find The Duplicated Number in a Consecutive Unsorted List.py
@@ -1,15 +1,7 @@
 def find_dup(arr):
-    lst = []
-    duplicate = ''
-
     for i in arr:
-        if i not in lst:
-            lst.append(i)
-        else:
-            duplicate += str(i)
-
-    return int(duplicate)
-
+        if arr.count(i) == 2:
+            return i
 
 
 print(find_dup([5, 4, 3, 2, 1, 1]), 1)

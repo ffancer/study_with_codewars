e64b9f0b73ac3b5f6bf7f7483694f06c9e9a285c
Paul <gafgafgarion05@gmail.com>
2022-01-05 08:38:19 +0300

    solution

--- 7 kyu Make them bark!.py
+++ 7 kyu Make them bark!.py
@@ -1,16 +1,11 @@
-class Dog:
-    def __init__(self, name, breed, sex, age):
-        self.name = name
-        self.breed = breed
-        self.sex = sex
-        self.age = age
+def bark(self):
+    return "Woof!"
 
-
-
+Dog.bark = bark
 
 
 apollo = Dog('Apollo', 'Dobermann', 'male', '4')
 zeus = Dog('Zeus', 'Dobermann', 'male', '4')
 
-test.assert_equals(apollo.bark(), 'Woof!')
-test.assert_equals(zeus.bark(), 'Woof!')
+print(apollo.bark(), 'Woof!')
+print(zeus.bark(), 'Woof!')

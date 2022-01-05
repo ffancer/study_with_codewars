445a10e8836204a8b7974a02946ff617733647e9
Paul <gafgafgarion05@gmail.com>
2022-01-05 07:46:26 +0300

    init

---
+++ 7 kyu Making Copies.py
@@ -0,0 +1,10 @@
+def copy_list(l):
+    pass
+
+
+t = [1, 2, 3, 4]
+t_copy = copy_list(t)
+print(t, t_copy)  # copy should be the same
+
+t[1] += 5
+print(t, t_copy)  # copy should NOT be the same anymore

d6e19c81311bc765d393d0ae29ae484e13a25602
Paul <gafgafgarion05@gmail.com>
2022-01-05 08:18:13 +0300

    init

---
+++ 7 kyu Make them bark!.py
@@ -0,0 +1,8 @@
+# TODO: solve the barking problem!
+
+
+apollo = Dog('Apollo', 'Dobermann', 'male', '4')
+zeus = Dog('Zeus', 'Dobermann', 'male', '4')
+
+test.assert_equals(apollo.bark(), 'Woof!')
+test.assert_equals(zeus.bark(), 'Woof!')

8f37b44f9b86c8741f374082c53b541bb147f925
Paul <gafgafgarion05@gmail.com>
2022-01-05 11:58:29 +0300

    2

--- 7 kyu Last.py
+++ 7 kyu Last.py
@@ -1,7 +1,9 @@
 def last(*x):
-    return len(x)
+    return type(x)
 
 
 print(last([1, 2, 3, 4, 5]), 5)
 print(last("abcde"), "e")
 print(last(1, "b", 3, "d", 5), 5)
+print(last([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), 10)
+print(last(['a', 'b', 'c', 'k', 'x', 'y', 'z']), 'z')

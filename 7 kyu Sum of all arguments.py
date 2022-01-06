db0a85432a00c69b4a102290069eda5d35930ed3
Paul <gafgafgarion05@gmail.com>
2022-01-06 13:17:45 +0300

    my solution

--- 7 kyu Sum of all arguments.py
+++ 7 kyu Sum of all arguments.py
@@ -1,9 +1,14 @@
-def sum_args():
-    # ...
+def sum_args(*args):
+    total = 0
+
+    for i in args:
+        total += i
+
+    return total
 
 
-(sum_args(1), 1)
-(sum_args(1, 2), 3)
-(sum_args(5, 7, 9), 21)
-(sum_args(12, 1, 1, 1, 1), 16)
-(sum_args(12, 1, 1, 1, 1, 1, 1), 18)
+print(sum_args(1), 1)
+print(sum_args(1, 2), 3)
+print(sum_args(5, 7, 9), 21)
+print(sum_args(12, 1, 1, 1, 1), 16)
+print(sum_args(12, 1, 1, 1, 1, 1, 1), 18)

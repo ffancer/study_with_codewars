f1cfd8a22f255d001b4dcdb89550ce93225e3c74
Paul <gafgafgarion05@gmail.com>
2022-01-06 08:02:49 +0300

    init

---
+++ 7 kyu Javascript filter - 1.py
@@ -0,0 +1,18 @@
+def search_names(logins):
+    pass
+
+
+a = [["foo", "foo@foo.com"], ["bar_", "bar@bar.com"]]
+b = [["bar_", "bar@bar.com"]]
+print(search_names(a), b)
+
+a = [["foobar_", "foo@foo.com"], ["bar_", "bar@bar.com"]]
+b = [["foobar_", "foo@foo.com"], ["bar_", "bar@bar.com"]]
+print(search_names(a), b)
+
+a = [["foo", "foo@foo.com"], ["bar", "bar@bar.com"]]
+b = []
+print(search_names(a), b)
+
+# with open("/workspace/solution.txt", "r") as file:
+#     test.expect("filter" in file.read(), 'Use filter function' )

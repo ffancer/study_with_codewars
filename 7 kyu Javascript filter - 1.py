be7c019944a56af6e7d310ac9f7c25ace3a69f6f
Paul <gafgafgarion05@gmail.com>
2022-01-06 08:30:58 +0300

    my solution tests here didn't wok, but at codewars all is good

--- 7 kyu Javascript filter - 1.py
+++ 7 kyu Javascript filter - 1.py
@@ -1,5 +1,5 @@
 def search_names(logins):
-    return filter('_', logins)
+    return filter(lambda x: x[0][-1] == '_', logins)
 
 
 a = [["foo", "foo@foo.com"], ["bar_", "bar@bar.com"]]
@@ -13,6 +13,3 @@ print(search_names(a), b)
 a = [["foo", "foo@foo.com"], ["bar", "bar@bar.com"]]
 b = []
 print(search_names(a), b)
-
-# with open("/workspace/solution.txt", "r") as file:
-#     test.expect("filter" in file.read(), 'Use filter function' )

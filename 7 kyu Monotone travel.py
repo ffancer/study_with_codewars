4185d216583f614c2966197d8f3f674a821aa391
Paul <gafgafgarion05@gmail.com>
2022-01-07 13:21:31 +0300

    3

--- 7 kyu Monotone travel.py
+++ 7 kyu Monotone travel.py
@@ -1,6 +1,11 @@
 def is_monotone(heights):
+    lst_1, lst_2 = [], []
+
     for i,j in zip(heights, heights[1:]):
-        print(i, j)
+        lst_1.append(i)
+        lst_1.append(j)
+
+    return len(lst_1) == len(lst_2)
 
 
 print(is_monotone(list(range(1, 11))), 'Should work on increasing lists')

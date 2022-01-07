605eaf41c38ed353bd9a22d696d5ce34a8deab99
Paul <gafgafgarion05@gmail.com>
2022-01-07 13:07:21 +0300

    init

---
+++ 7 kyu Monotone travel.py
@@ -0,0 +1,11 @@
+def is_monotone(heights):
+    pass
+
+
+print(is_monotone(list(range(1, 11))), 'Should work on increasing lists')
+print(is_monotone([5, 5, 5, 5, 5, 5, 5]), 'Should work on constant lists')
+print(is_monotone([]), 'Should work on empty list')
+print(is_monotone([1]), 'Should work on size 1 list')
+
+print(not is_monotone(list(range(5, 0, -1))), 'Should return false on a decreasing list')
+print(not is_monotone(list(range(5, -40, -1))), 'Should return false on a decreasing list')

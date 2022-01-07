f969dbe6b755e68d580609d8d996d49593fed442
Paul <gafgafgarion05@gmail.com>
2022-01-07 12:10:09 +0300

    my solution

--- 7 kyu Collatz Conjecture Length.py
+++ 7 kyu Collatz Conjecture Length.py
@@ -1,5 +1,14 @@
 def collatz(n):
-    pass
+    cnt = 1
+
+    while n != 1:
+        cnt += 1
+        if n % 2 == 0:
+            n //= 2
+        else:
+            n = n * 3 + 1
+
+    return cnt
 
 
 print(collatz(20), 8)

c84449feae1da0a24231a74e78c2fbea68348161
Paul <gafgafgarion05@gmail.com>
2022-01-07 11:23:50 +0300

    init

---
+++ 7 kyu Birthday I - Cake.py
@@ -0,0 +1,9 @@
+def cake(candles, debris):
+    pass
+
+
+print(cake(900, 'abcdef'), 'That was close!')
+print(cake(56, 'ifkhchlhfd'), 'Fire!')
+print(cake(256, 'aaaaaddddr'), 'Fire!')
+print(cake(333, 'jfmgklfhglbe'), 'Fire!')
+print(cake(12, 'jaam'), 'Fire!')

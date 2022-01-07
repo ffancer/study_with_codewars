e4827e2a041d7b58bc2962f6c68927a765c1a292
Paul <gafgafgarion05@gmail.com>
2022-01-07 11:29:53 +0300

    4

--- 7 kyu Birthday I - Cake.py
+++ 7 kyu Birthday I - Cake.py
@@ -1,10 +1,12 @@
 def cake(candles, debris):
     total = 0
 
-    for i in debris:
-        total += ord(i)-96
+    for i,j in enumerate(debris):
+        # total += ord(i)-96
+        print(i, j)
 
-    return 'Fire!' if candles * 0.7 < total else 'That was close!'
+    # return 'Fire!' if candles * 0.7 < total else 'That was close!'
+    # return total
 
 
 print(cake(900, 'abcdef'), 'That was close!')

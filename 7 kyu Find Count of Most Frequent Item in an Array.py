0430413f37cafafa80e3eba002e56ac19c88852e
Paul <gafgafgarion05@gmail.com>
2022-01-08 14:51:29 +0300

    init

---
+++ 7 kyu Find Count of Most Frequent Item in an Array.py
@@ -0,0 +1,8 @@
+def most_frequent_item_count(collection):
+    pass
+
+
+print(most_frequent_item_count([3, -1, -1]), 2, "didn't work for [3, -1, -1]")
+print(most_frequent_item_count([3, -1, -1, -1, 2, 3, -1, 3, -1, 2, 4, 9, 3]), 5, "didn't work for [3, -1, -1, -1, 2, 3, -1, 3, -1, 2, 4, 9, 3]")
+print(most_frequent_item_count([]), 0, "didn't work for []")
+print(most_frequent_item_count([9]), 1, "didn't work for [9]")

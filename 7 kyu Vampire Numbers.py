f735861261f3303d5b9094bc26df3f065793d340
Paul <gafgafgarion05@gmail.com>
2022-01-08 12:44:16 +0300

    1

--- 7 kyu Vampire Numbers.py
+++ 7 kyu Vampire Numbers.py
@@ -1,10 +1,16 @@
 def vampire_test(x, y):
-    pass
+    z = x * y
+    a = str(x) + str(y)
+    cnt = 0
+    for i in a:
+        if i in str(z):
+            cnt += 1
+    return cnt
 
 
-print(vampire_test(21, 6) == True, "Basic: 21 * 6 = 126 should return True")
-print(vampire_test(204, 615) == True, "Basic: 204 * 615 = 125460 should return True")
-print(vampire_test(30, -51) == True, "One Negative: 30 * -51 = -1530 should return True")
-print(vampire_test(-246, -510) == False,
+print(vampire_test(21, 6), "Basic: 21 * 6 = 126 should return True")
+print(vampire_test(204, 615), "Basic: 204 * 615 = 125460 should return True")
+print(vampire_test(30, -51), "One Negative: 30 * -51 = -1530 should return True")
+print(vampire_test(-246, -510),
       "Double Negatives: -246 * -510 = 125460 should return False (The negative signs aren't present on the product)")
-print(vampire_test(210, 600) == True, "Trailing Zeroes: 210 * 600 = 126000 should return True")
+print(vampire_test(210, 600), "Trailing Zeroes: 210 * 600 = 126000 should return True")

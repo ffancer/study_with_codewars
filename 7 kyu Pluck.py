61eacc6ea091e215f3f7e2e347ae61da75361615
Paul <gafgafgarion05@gmail.com>
2022-01-10 10:20:01 +0300

    1

--- 7 kyu Pluck.py
+++ 7 kyu Pluck.py
@@ -1,6 +1,6 @@
 def pluck(objs, name):
-    # your code here
-    pass
+    for i in objs:
+        print(i)
 
 
 objs = [{'a': 1, 'b': 2, 'c': 3}, {'a': 4, 'b': 5, 'c': 6}, {'a': 7, 'b': 8, 'c': 9}, {'a': 10, 'b': 11}]

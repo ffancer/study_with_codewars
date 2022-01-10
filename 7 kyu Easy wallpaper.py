eb593abc5de5213338690dfabf0d2e96439f5a03
Paul <gafgafgarion05@gmail.com>
2022-01-10 09:39:36 +0300

    4

--- 7 kyu Easy wallpaper.py
+++ 7 kyu Easy wallpaper.py
@@ -2,7 +2,7 @@ def wallpaper(l, w, h):
     numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven",
                "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty"]
 
-    return ((l*h)*2 + (w*h)*2) / 5.2
+    return int(((l*h)*2 + (w*h)*2) / 5.2 * 1.15)
 
 print(wallpaper(6.3, 4.5, 3.29), "sixteen")
 print(wallpaper(7.8, 2.9, 3.29), "sixteen")

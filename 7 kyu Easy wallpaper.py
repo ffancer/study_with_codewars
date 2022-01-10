c3960f9efeba570e179646c0e16e94e1513b9eeb
Paul <gafgafgarion05@gmail.com>
2022-01-10 09:56:22 +0300

    my solution was problem with 'zero' when l * w * h == 0 I think will be better use numbers[0] than 'zero' in return

--- 7 kyu Easy wallpaper.py
+++ 7 kyu Easy wallpaper.py
@@ -1,10 +1,12 @@
 from math import ceil
 
+
 def wallpaper(l, w, h):
     numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven",
                "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty"]
 
-    return numbers[ceil(((l*h)*2 + (w*h)*2) / 5.2 * 1.15)]
+    return 'zero' if l * h * w == 0 else numbers[ceil(((l * h) * 2 + (w * h) * 2) / 5.2 * 1.15)]
+
 
 print(wallpaper(6.3, 4.5, 3.29), "sixteen")
 print(wallpaper(7.8, 2.9, 3.29), "sixteen")

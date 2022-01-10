7706f5b5990f05bcac16602978e65af6926b08df
Paul <gafgafgarion05@gmail.com>
2022-01-10 09:36:36 +0300

    init

---
+++ 7 kyu Easy wallpaper.py
@@ -0,0 +1,8 @@
+def wallpaper(l, w, h):
+    pass
+
+
+print(wallpaper(6.3, 4.5, 3.29), "sixteen")
+print(wallpaper(7.8, 2.9, 3.29), "sixteen")
+print(wallpaper(6.3, 5.8, 3.13), "seventeen")
+print(wallpaper(6.1, 6.7, 2.81), "sixteen")

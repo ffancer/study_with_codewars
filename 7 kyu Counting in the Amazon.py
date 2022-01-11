3b9a9813178ef2912d6d80026f15fe723ac40302
Paul <gafgafgarion05@gmail.com>
2022-01-11 09:09:19 +0300

    init

---
+++ 7 kyu Counting in the Amazon.py
@@ -0,0 +1,9 @@
+def count_arara(n):
+    # ...
+    pass
+
+
+print(count_arara(1), "anane")
+print(count_arara(2), "adak")
+print(count_arara(3), "adak anane")
+print(count_arara(9), "adak adak adak adak anane")

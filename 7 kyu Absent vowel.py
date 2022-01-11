ddf94e877880643069e0f38506beb545b70fd311
Paul <gafgafgarion05@gmail.com>
2022-01-11 10:55:02 +0300

    2

--- 7 kyu Absent vowel.py
+++ 7 kyu Absent vowel.py
@@ -1,6 +1,9 @@
 def absent_vowel(x):
     dct = {'a': 0, 'e': 1, 'i': 2, 'o': 3, 'u': 4}
 
+    for i, j in dct.items():
+        print(i, j)
+
 
 print(absent_vowel("John Doe hs seven red pples under his bsket"), 0)
 print(absent_vowel("Bb Smith sent us six neatly arranged range bicycles"), 3)

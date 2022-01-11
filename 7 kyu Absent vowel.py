14883403417413d2b8bfdb70de9e533af404904f
Paul <gafgafgarion05@gmail.com>
2022-01-11 10:53:28 +0300

    init

---
+++ 7 kyu Absent vowel.py
@@ -0,0 +1,7 @@
+def absent_vowel(x):
+    # your code here
+    pass
+
+
+print(absent_vowel("John Doe hs seven red pples under his bsket"), 0)
+print(absent_vowel("Bb Smith sent us six neatly arranged range bicycles"), 3)

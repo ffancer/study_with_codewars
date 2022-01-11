95ba5c237faeff59fa70c05535eac2c9ff822abf
Paul <gafgafgarion05@gmail.com>
2022-01-11 13:07:18 +0300

    init

---
+++ 7 kyu Insert dashes.py
@@ -0,0 +1,9 @@
+def insert_dash(num):
+    pass
+
+
+print(insert_dash(454793), '4547-9-3')
+print(insert_dash(123456), '123456')
+print(insert_dash(1003567), '1003-567')
+print(insert_dash(24680), '24680')
+print(insert_dash(13579), '1-3-5-7-9')

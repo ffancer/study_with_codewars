8e60bd56d06726e02aa1300c98d1ee379ed06e7f
Paul <gafgafgarion05@gmail.com>
2022-01-11 09:52:21 +0300

    init

---
+++ 7 kyu How many arguments.py
@@ -0,0 +1,7 @@
+# Create a function args_count, that returns count of passed arguments
+
+
+print(args_count(100), 1)
+print(args_count(100, 2, 3), 3)
+print(args_count(32, a1=12), 2)
+print(args_count(), 0)

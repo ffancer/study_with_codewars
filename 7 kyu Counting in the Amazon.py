1d90bc5e4bf184ec1cd82ee7e5171640501b7466
Paul <gafgafgarion05@gmail.com>
2022-01-11 09:13:17 +0300

    2

--- 7 kyu Counting in the Amazon.py
+++ 7 kyu Counting in the Amazon.py
@@ -1,7 +1,10 @@
 def count_arara(n):
-    lst = ['anane', 'adak', 'adak anane', 'adak adak', 'adak adak anane', 'adak adak adak', 'adak adak adak anane', 'adak adak adak adak']
+    # lst = ['anane', 'adak', 'adak anane', 'adak adak', 'adak adak anane', 'adak adak adak', 'adak adak adak anane', 'adak adak adak adak']
+    #
+    # return lst[n-1]
+    anane = 'anane'  # it's 1
+    adak = 'adak'  # it's 2
 
-    return lst[n-1]
 
 print(count_arara(1), "anane")
 print(count_arara(2), "adak")

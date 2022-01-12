42e62a87c6ab38ea0ace953f1d9e089341eb51a2
Paul <gafgafgarion05@gmail.com>
2022-01-12 09:33:49 +0300

    1

---
+++ 7 kyu List to Array.py
@@ -0,0 +1,19 @@
+class LinkedList:
+    def __init__(self, value=0, next=None):
+        self.value = value
+        self.next = next
+
+
+def list_to_array(node):
+    # ???
+    return []
+
+
+u = LinkedList(1)
+print(list_to_array(u), [1])
+
+u = LinkedList(4, LinkedList(25, LinkedList(30)))
+print(list_to_array(u), [4, 25, 30])
+
+u = LinkedList(2, LinkedList(40, LinkedList(43, LinkedList(25, LinkedList(125)))))
+print(list_to_array(u), [2, 40, 43, 25, 125])

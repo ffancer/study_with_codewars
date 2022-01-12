802a5a7bcb86b56c111c356f541e7a865cfb1484
Paul <gafgafgarion05@gmail.com>
2022-01-12 13:48:22 +0300

    3

--- 7 kyu Formatting decimal places #1.py
+++ 7 kyu Formatting decimal places #1.py
@@ -1,5 +1,5 @@
 def two_decimal_places(number):
-    return str(number).split('.')[1][:2]
+    return ''.join(str(number).split('.')[0] + '.' + str(number).split('.')[1][:2])
 
 
 print(two_decimal_places(10.1289767789), 10.12, "didn't work for 10.1289767789")

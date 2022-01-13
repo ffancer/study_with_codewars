2b2faef7eb46c8f7827d5653493d0182f6cf77ec
Paul <gafgafgarion05@gmail.com>
2022-01-13 10:30:57 +0300

    3

--- 7 kyu Split The Bill.py
+++ 7 kyu Split The Bill.py
@@ -1,8 +1,10 @@
 def split_the_bill(x):
-    # total = sum(x.values())
+    total = sum(x.values())
     how_many_guests = len(x)
+    dct = {}
     for i, j in x.items():
-        print(i, j)
+        dct.update(i: round(j - (total / how_many_guests))
+    return dct
 
 print(split_the_bill({'A': 20, 'B': 15, 'C': 10}), {'A': 5, 'B': 0, 'C': -5})
 print(split_the_bill({'A': 40, 'B': 25, 'X': 10}), {'A': 15, 'B': 0, 'X': -15})

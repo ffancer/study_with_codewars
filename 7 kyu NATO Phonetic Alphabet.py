edb86b5c6c88b23e118a3095d7666ce00fcd17f5
Paul <gafgafgarion05@gmail.com>
2022-01-13 09:36:53 +0300

    my solution

--- 7 kyu NATO Phonetic Alphabet.py
+++ 7 kyu NATO Phonetic Alphabet.py
@@ -15,9 +15,9 @@ def nato(word):
     s = ''
 
     for i in word:
-        # if i in letters.keys():
-        #     s += letters[i]
-        print(letters.get(i.upper()))
-    return s
+        s += letters.get(i.upper()) + ' '
+
+    return s[:-1]
+
 
 print(nato("babble"), "Bravo Alpha Bravo Bravo Lima Echo")

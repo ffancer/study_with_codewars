33200b3b7af8b60573f15d6ea546b67013d66ea3
Paul <gafgafgarion05@gmail.com>
2022-01-13 07:42:09 +0300

    init

---
+++ 7 kyu Digits explosion.py
@@ -0,0 +1,9 @@
+def explode(s):
+    pass
+
+
+print(explode("312"), "333122")
+print(explode("102269"), "12222666666999999999")
+print(explode("0"), "")
+print(explode("000"), "")
+print(explode("123"), "122333")

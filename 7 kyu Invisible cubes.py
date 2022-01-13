56a9096009188585b09524a0e36192132b9c9d4b
Paul <gafgafgarion05@gmail.com>
2022-01-13 08:32:24 +0300

    init

---
+++ 7 kyu Invisible cubes.py
@@ -0,0 +1,9 @@
+def not_visible_cubes(n):
+    pass
+
+
+print(not_visible_cubes(0), 0)
+print(not_visible_cubes(1), 0)
+print(not_visible_cubes(2), 0)
+print(not_visible_cubes(3), 1)
+print(not_visible_cubes(4), 8)

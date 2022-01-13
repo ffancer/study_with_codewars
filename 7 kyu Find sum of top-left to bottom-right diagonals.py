254ad2728e3ef1f842e2f8553d6ae15765ef7533
Paul <gafgafgarion05@gmail.com>
2022-01-13 09:08:45 +0300

    init

---
+++ 7 kyu Find sum of top-left to bottom-right diagonals.py
@@ -0,0 +1,19 @@
+def diagonal_sum(array):
+    # your code here
+    return
+
+
+print(diagonal_sum([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]]), 15)
+
+print(diagonal_sum([
+    [1, 2],
+    [3, 4]]), 5)
+
+print(diagonal_sum([
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16]]), 34)

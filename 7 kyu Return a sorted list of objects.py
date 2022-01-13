344177440be4f5374647456e1644efb9e04bba59
Paul <gafgafgarion05@gmail.com>
2022-01-13 11:08:39 +0300

    init

---
+++ 7 kyu Return a sorted list of objects.py
@@ -0,0 +1,17 @@
+def sort_list(sort_by, lst):
+    return  # sorted list
+
+
+print(sort_list("a",
+                [
+                    {"a": 1, "b": 3},
+                    {"a": 3, "b": 2},
+                    {"a": 2, "b": 40},
+                    {"a": 4, "b": 12}
+                ]),
+      [
+          {"a": 4, "b": 12},
+          {"a": 3, "b": 2},
+          {"a": 2, "b": 40},
+          {"a": 1, "b": 3}
+      ])

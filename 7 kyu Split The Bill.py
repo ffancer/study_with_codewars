5c32437cf244ab140c05193a996e2076a496a3b4
Paul <gafgafgarion05@gmail.com>
2022-01-13 10:47:10 +0300

    my solution

--- 7 kyu Split The Bill.py
+++ 7 kyu Split The Bill.py
@@ -1,10 +1,8 @@
 def split_the_bill(x):
     total = sum(x.values())
-    how_many_guests = len(x)
-    dct = {}
-    for i, j in x.items():
-        dct.update(i: round(j - (total / how_many_guests))
-    return dct
+    how_many_guests = len(x.values())
+
+    return {i: round(j - (total / how_many_guests), 2) for i, j in x.items()}
 
 print(split_the_bill({'A': 20, 'B': 15, 'C': 10}), {'A': 5, 'B': 0, 'C': -5})
 print(split_the_bill({'A': 40, 'B': 25, 'X': 10}), {'A': 15, 'B': 0, 'X': -15})

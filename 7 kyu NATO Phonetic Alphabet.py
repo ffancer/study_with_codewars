8a03f652917a9ca74c646a15c335b0b6e6a053d9
Paul <gafgafgarion05@gmail.com>
2022-01-13 09:35:39 +0300

    3

--- 7 kyu NATO Phonetic Alphabet.py
+++ 7 kyu NATO Phonetic Alphabet.py
@@ -17,7 +17,7 @@ def nato(word):
     for i in word:
         # if i in letters.keys():
         #     s += letters[i]
-        print(letters.get(i))
+        print(letters.get(i.upper()))
     return s
 
 print(nato("babble"), "Bravo Alpha Bravo Bravo Lima Echo")

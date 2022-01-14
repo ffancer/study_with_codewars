c317e659cef5baa98de1bf440474f357c69f83c4
Paul <gafgafgarion05@gmail.com>
2022-01-14 11:47:28 +0300

    solution dunno why, but this is solution for cata

--- 7 kyu Arithmetic List!.py
+++ 7 kyu Arithmetic List!.py
@@ -1,10 +1,5 @@
 def seqlist(first, c, l):
-    lst = []
-
-    for i in range(first, l, c):
-        lst.append(i)
-
-    return lst
+    return range(first, first + l * c, c)
 
 
 print(seqlist(0, 1, 20), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19])

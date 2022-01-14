6840a24ba50b2a46826037a62f93d9296fe5bae3
Paul <gafgafgarion05@gmail.com>
2022-01-14 09:15:34 +0300

    init

---
+++ 7 kyu Drone Fly-By.py
@@ -0,0 +1,7 @@
+def fly_by(lamps, drone):
+    pass
+
+
+print(fly_by('xxxxxx', '====T'), 'ooooox')
+print(fly_by('xxxxxxxxx', '==T'), 'oooxxxxxx')
+print(fly_by('xxxxxxxxxxxxxxx', '=========T'), 'ooooooooooxxxxx')

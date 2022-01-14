443e68b0dbe8e3d088487480c89ca6035b0b7af4
Paul <gafgafgarion05@gmail.com>
2022-01-14 10:34:06 +0300

    2

--- 7 kyu Candy problem.py
+++ 7 kyu Candy problem.py
@@ -2,6 +2,16 @@ def candies(s):
     if len(s) < 2:
         return -1
 
+    max_candys = max(s)
+    total= 0
+
+    while s:
+        total += max_candys - s[0]
+        s = s[1:]
+
+    return total
+
+
 
 print(candies([5, 8, 6, 4]), 9)
 print(candies([1, 2, 4, 6]), 11)

46f9eb7077222771d94ef9b53f343ba585aa10aa
Paul <gafgafgarion05@gmail.com>
2022-01-14 10:32:21 +0300

    1

--- 7 kyu Candy problem.py
+++ 7 kyu Candy problem.py
@@ -1,8 +1,11 @@
 def candies(s):
-    return -1
+    if len(s) < 2:
+        return -1
 
 
 print(candies([5, 8, 6, 4]), 9)
 print(candies([1, 2, 4, 6]), 11)
 print(candies([1, 2]), 1)
 print(candies([4, 2]), 2)
+print(candies([]), -1)
+print(candies([6]), -1)

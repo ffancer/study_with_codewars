31787088b7f289ce577168896c3fab0e83be0485
Paul <gafgafgarion05@gmail.com>
2022-01-14 09:45:38 +0300

    init

---
+++ 7 kyu Simple string reversal.py
@@ -0,0 +1,8 @@
+def solve(s):
+    pass
+
+
+print(solve("codewars"), "srawedoc")
+print(solve("your code"), "edoc ruoy")
+print(solve("your code rocks"), "skco redo cruoy")
+print(solve("i love codewars"), "s rawe docevoli")

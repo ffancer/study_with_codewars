e3755c7713a63340144f30ef44f06cd6982a8527
Paul <gafgafgarion05@gmail.com>
2022-01-14 09:20:14 +0300

    3

--- 7 kyu Drone Fly-By.py
+++ 7 kyu Drone Fly-By.py
@@ -1,7 +1,8 @@
 def fly_by(lamps, drone):
     a = lamps[:len(drone)]
     b = lamps[len(drone):]
-    return lamps[:len(drone)], b
+    a = a.replace('x', 'o')
+    return a
 
 
 print(fly_by('xxxxxx', '====T'), 'ooooox')

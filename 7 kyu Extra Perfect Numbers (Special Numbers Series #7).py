b35ea129d089aeccbcb176d8291e40d0474812ad
Paul <gafgafgarion05@gmail.com>
2022-01-14 13:04:17 +0300

    init

---
+++ 7 kyu Extra Perfect Numbers (Special Numbers Series #7).py
@@ -0,0 +1,9 @@
+def extra_perfect(n):
+    pass
+
+
+print(extra_perfect(3), [1, 3])
+print(extra_perfect(5), [1, 3, 5])
+print(extra_perfect(7), [1, 3, 5, 7])
+print(extra_perfect(28), [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27])
+print(extra_perfect(39), [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39])

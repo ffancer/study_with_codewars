472f29e7081c5e44eb13f009188fe43837536078
Paul <gafgafgarion05@gmail.com>
2022-01-14 11:46:23 +0300

    my solution don't know why this shit cata didn't work, in IDE it's work perfectly

--- 7 kyu Arithmetic List!.py
+++ 7 kyu Arithmetic List!.py
@@ -1,11 +1,12 @@
 def seqlist(first, c, l):
-    lst = [first]
+    lst = []
 
-    while first < l:
-        lst.append(first + c)
-        first = first + c
+    for i in range(first, l, c):
+        lst.append(i)
 
     return lst
 
 
 print(seqlist(0, 1, 20), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19])
+print(seqlist(0, 3, 58))
+print(seqlist(0, -5, -96))

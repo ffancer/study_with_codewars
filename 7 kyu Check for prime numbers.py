377b8d276b6ed6fcbe94df90f182c9f9e784c89a
Paul <gafgafgarion05@gmail.com>
2022-01-14 14:16:28 +0300

    init

---
+++ 7 kyu Check for prime numbers.py
@@ -0,0 +1,12 @@
+def is_prime(n):
+    'Return True if n is a prime number otherwise return False'
+
+
+print(is_prime(0), False)
+print(is_prime(1), False)
+print(is_prime(2), True)
+print(is_prime(3), True)
+print(is_prime(11), True)
+print(is_prime(12), False)
+print(is_prime(571), True)
+print(is_prime(25), False)

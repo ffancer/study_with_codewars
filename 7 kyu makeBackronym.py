5ab2ae81b95d97751280966f8ac342e69adeecce
Paul <gafgafgarion05@gmail.com>
2022-01-15 09:34:19 +0300

    init

---
+++ 7 kyu makeBackronym.py
@@ -0,0 +1,13 @@
+# preloaded variable: "dictionary"
+
+def make_backronym(acronym):
+    pass
+
+
+print(make_backronym("dgm"), "disturbing gregarious mustache")
+print(make_backronym("lkj"), "literal klingon joke")
+print(make_backronym('interesting'),
+      'ingestable newtonian turn eager rant eager stylish turn ingestable newtonian gregarious',
+      'Output not as expected')
+print(make_backronym('codewars'), 'confident oscillating disturbing eager weird awesome rant stylish',
+      'Output not as expected')

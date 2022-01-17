a04ef889b143cce110a47c86cb8079c50fe765c7
Paul <gafgafgarion05@gmail.com>
2022-01-17 10:08:32 +0300

    init

---
+++ 7 kyu Remove All The Marked Elements of a List.py
@@ -0,0 +1,19 @@
+class List:
+    def remove_(self, integer_list, values_list):
+        # your code here
+        return []
+
+
+l = List()
+
+integer_list = [1, 1, 2, 3, 1, 2, 3, 4]
+values_list = [1, 3]
+print(l.remove_(integer_list, values_list), [2, 2, 4])
+
+integer_list = [1, 1, 2, 3, 1, 2, 3, 4, 4, 3, 5, 6, 7, 2, 8]
+values_list = [1, 3, 4, 2]
+print(l.remove_(integer_list, values_list), [5, 6, 7, 8])
+
+integer_list = [8, 2, 7, 2, 3, 4, 6, 5, 4, 4, 1, 2, 3]
+values_list = [2, 4, 3]
+print(l.remove_(integer_list, values_list), [8, 7, 6, 5, 1])

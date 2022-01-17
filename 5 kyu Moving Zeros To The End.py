701fdc65fe506a30e2238400a394a052d8fe8dd9
Paul <gafgafgarion05@gmail.com>
2022-01-17 13:13:37 +0300

    init

---
+++ 5 kyu Moving Zeros To The End.py
@@ -0,0 +1,11 @@
+def move_zeros(array):
+    return array
+
+
+print(move_zeros([1, 2, 0, 1, 0, 1, 0, 3, 0, 1]))
+print(move_zeros([1, 2, 1, 1, 3, 1, 0, 0, 0, 0]))
+print(move_zeros([9, 0, 0, 9, 1, 2, 0, 1, 0, 1, 0, 3, 0, 1, 9, 0, 0, 0, 0, 9]))
+print(move_zeros([9, 9, 1, 2, 1, 1, 3, 1, 9, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]))
+print(move_zeros([0, 0]), [0, 0])
+print(move_zeros([0]), [0])
+print(move_zeros([]), [])

80f45fd8ab245f0320bd28986a106b139f3d463f
Paul <gafgafgarion05@gmail.com>
2022-01-18 13:28:14 +0300

    init

---
+++ 7 kyu Return a string's even characters..py
@@ -0,0 +1,8 @@
+def even_chars(st):
+    # your code here
+    pass
+
+
+print(even_chars("a"), "invalid string")
+print(even_chars("abcdefghijklm"), ["b", "d", "f", "h", "j", "l"])
+print(even_chars("aBc_e9g*i-k$m"), ["B", "_", "9", "*", "-", "$"])

2b16dfd48feadcc3056a9f7543261b09861213a3
Paul <gafgafgarion05@gmail.com>
2022-01-18 11:12:05 +0300

    my solution

--- 7 kyu The reject() function.py
+++ 7 kyu The reject() function.py
@@ -1,8 +1,7 @@
 def reject(seq, predicate):
-    # your code here
-    pass
+    return [i for i in seq if not predicate(i)]
 
 
 print(reject([1, 2, 3, 4, 5, 6], lambda x: x % 2 == 0), [1, 3, 5])
-print(reject(['a', 'b', 3, 'd'], lambda x: type(x) == int), ['a', 'b', 'd']);
-print(reject(['a', 'b', 3, 'd'], lambda x: type(x) == str), [3]);
+print(reject(['a', 'b', 3, 'd'], lambda x: type(x) == int), ['a', 'b', 'd'])
+print(reject(['a', 'b', 3, 'd'], lambda x: type(x) == str), [3])

40d50cd84a35878f60133925f2f4cddba5637a83
Paul <gafgafgarion05@gmail.com>
2022-01-18 13:45:50 +0300

    6 just 2 errors

--- 7 kyu Return a string's even characters..py
+++ 7 kyu Return a string's even characters..py
@@ -1,4 +1,7 @@
 def even_chars(st):
+    if len(st) > 100 or len(st) < 3:
+        return "invalid string"
+
     lst = []
 
     for i,j in enumerate(st):
@@ -11,3 +14,4 @@ def even_chars(st):
 print(even_chars("a"), "invalid string")
 print(even_chars("abcdefghijklm"), ["b", "d", "f", "h", "j", "l"])
 print(even_chars("aBc_e9g*i-k$m"), ["B", "_", "9", "*", "-", "$"])
+print(even_chars("1234567890" * 11), "invalid string")

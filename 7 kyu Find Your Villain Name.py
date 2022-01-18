c85e7d7440a675f3e047c00f926e8632947bab68
Paul <gafgafgarion05@gmail.com>
2022-01-18 09:33:56 +0300

    init

---
+++ 7 kyu Find Your Villain Name.py
@@ -0,0 +1,16 @@
+import datetime
+
+
+def get_villain_name(birthdate):
+    first = ["The Evil", "The Vile", "The Cruel", "The Trashy", "The Despicable", "The Embarrassing",
+             "The Disreputable", "The Atrocious", "The Twirling", "The Orange", "The Terrifying", "The Awkward"]
+    last = ["Mustache", "Pickle", "Hood Ornament", "Raisin", "Recycling Bin", "Potato", "Tomato", "House Cat",
+            "Teaspoon", "Laundry Basket"]
+    # your code here
+    pass
+
+
+format_str = '%d/%m/%Y'  # The format
+print(get_villain_name(datetime.datetime.strptime("1/1/2000", format_str)), "The Evil Pickle")
+print(get_villain_name(datetime.datetime.strptime("2/2/2000", format_str)), "The Vile Hood Ornament")
+print(get_villain_name(datetime.datetime.strptime("2/12/2000", format_str)), "The Awkward Hood Ornament")

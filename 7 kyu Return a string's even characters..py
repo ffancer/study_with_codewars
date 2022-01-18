d0d6c2533e173672df4b99b15d2956cdf1a6a151
Paul <gafgafgarion05@gmail.com>
2022-01-18 13:33:33 +0300

    2

--- 7 kyu Return a string's even characters..py
+++ 7 kyu Return a string's even characters..py
@@ -1,6 +1,10 @@
 def even_chars(st):
+    lst = []
     for i, j in enumerate(st, 1):
-        print(i, j)
+        # print(i, j)
+        if i % 2 == 0:
+            lst.append(j)
+    return lst
 
 
 print(even_chars("a"), "invalid string")

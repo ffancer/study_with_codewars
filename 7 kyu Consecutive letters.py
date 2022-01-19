854ff4c596d6b84b25f03f08067ffb143125a0ba
Paul <gafgafgarion05@gmail.com>
2022-01-19 13:47:37 +0300

    my solution

--- 7 kyu Consecutive letters.py
+++ 7 kyu Consecutive letters.py
@@ -1,10 +1,15 @@
 def solve(st):
-    for i in st:
-        if st.count(i) > 1:
+    s = ''.join(sorted(st))
+
+    for i in range(1, len(st)):
+        if ord(s[i]) - ord(s[i - 1]) != 1:
             return False
+    else:
+        return True
 
 
 print(solve("abc"), True)
 print(solve("abd"), False)
 print(solve("dabc"), True)
 print(solve("abbc"), False)
+# 3

069c5af743b6f1ba309b5f5092efaa2b8b233789
Paul <gafgafgarion05@gmail.com>
2022-01-19 09:11:33 +0300

    init

---
+++ 7 kyu Product Array (Array Series #5).py
@@ -0,0 +1,9 @@
+def product_array(numbers):
+    pass
+
+
+print(product_array([12, 20]), [20, 12])
+print(product_array([12, 20]), [20, 12])
+print(product_array([3, 27, 4, 2]), [216, 24, 162, 324])
+print(product_array([13, 10, 5, 2, 9]), [900, 1170, 2340, 5850, 1300])
+print(product_array([16, 17, 4, 3, 5, 2]), [2040, 1920, 8160, 10880, 6528, 16320])

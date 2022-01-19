7e5bd5b26ba86c4db6a99687b35ed1def44ce52e
Paul <gafgafgarion05@gmail.com>
2022-01-19 09:58:25 +0300

    init

---
+++ 7 kyu Who is the killer.py
@@ -0,0 +1,12 @@
+# 7 kyu
+# Who is the killer?
+
+
+def killer(suspect_info, dead):
+    pass
+
+
+print(
+    killer({'James': ['Jacob', 'Bill', 'Lucas'], 'Johnny': ['David', 'Kyle', 'Lucas'], 'Peter': ['Lucy', 'Kyle']},
+           ['Lucas', 'Bill']), 'James')
+print(killer({'Brad': [], 'Megan': ['Ben', 'Kevin'], 'Finn': []}, ['Ben']), 'Megan')

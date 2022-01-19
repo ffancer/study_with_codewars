576b89b26d930f66f6c10fc87629d1e016ef9d81
Paul <gafgafgarion05@gmail.com>
2022-01-19 10:53:29 +0300

    init

---
+++ 7 kyu Nickname Generator.py
@@ -0,0 +1,10 @@
+def nickname_generator(name):
+    pass
+
+
+print(nickname_generator("Jimmy"), "Jim")
+print(nickname_generator("Samantha"), "Sam")
+print(nickname_generator("Sam"), "Error: Name too short")
+print(nickname_generator("Kayne"), "Kay", "'y' is not a vowel")
+print(nickname_generator("Melissa"), "Mel")
+print(nickname_generator("James"), "Jam")

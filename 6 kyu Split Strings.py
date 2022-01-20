d6280bb92cf7910d9f27a808a25969d5a1378296
Paul <gafgafgarion05@gmail.com>
2022-01-20 09:35:17 +0300

    2

--- 6 kyu Split Strings.py
+++ 6 kyu Split Strings.py
@@ -1,7 +1,8 @@
 def solution(s):
     if len(s) % 2 != 0:
         return '____'
-
+    lst = [s[i:i+2] for i in range(0, len(s), 2)]
+    return lst
 
 print(solution("asdfadsf"), ['as', 'df', 'ad', 'sf'])
 print(solution("asdfads"), ['as', 'df', 'ad', 's_'])

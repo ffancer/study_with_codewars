60d2c4e68a95634f43e60301e13c5e57fa007e78
Paul <gafgafgarion05@gmail.com>
2022-01-20 08:32:58 +0300

    my solution

--- 6 kyu Does my number look big in this.py
+++ 6 kyu Does my number look big in this.py
@@ -4,9 +4,11 @@ def narcissistic(value):
     for i in str(value):
         num += int(i) ** int(len(str(value)))
 
-    return num
+    return num == value
 
 
+print(narcissistic(153), True)
+print(narcissistic(1652), False)
 print(narcissistic(7), True, '7 is narcissistic')
 print(narcissistic(371), True, '371 is narcissistic')
 print(narcissistic(122), False, '122 is not narcissistic')

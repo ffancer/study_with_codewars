b73eeb6415ddeb8a9f424a6a46c84379e8c14707
Paul <gafgafgarion05@gmail.com>
2022-01-20 09:39:09 +0300

    my solution

--- 6 kyu Split Strings.py
+++ 6 kyu Split Strings.py
@@ -1,8 +1,8 @@
 def solution(s):
     if len(s) % 2 != 0:
-        return s[:-1] + s[-1]
+        s += '_'
+    return [s[i:i + 2] for i in range(0, len(s), 2)]
 
-    return [s[i:i+2] for i in range(0, len(s), 2)]
 
 print(solution("asdfadsf"), ['as', 'df', 'ad', 'sf'])
 print(solution("asdfads"), ['as', 'df', 'ad', 's_'])

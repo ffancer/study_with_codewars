99e77edecc0ececccfb408af551cf88a737dcc5c
Paul <gafgafgarion05@gmail.com>
2022-01-20 11:01:58 +0300

    init

---
+++ 6 kyu Find the unique number.py
@@ -0,0 +1,8 @@
+def find_uniq(arr):
+    # your code here
+    return n  # n: unique number in the array
+
+
+print(find_uniq([1, 1, 1, 2, 1, 1]), 2)
+print(find_uniq([0, 0, 0.55, 0, 0]), 0.55)
+print(find_uniq([3, 10, 3, 3, 3]), 10)

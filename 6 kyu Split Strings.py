57f4b7fa18e7c29ce8e4d850b914fb949defcb08
Paul <gafgafgarion05@gmail.com>
2022-01-20 09:26:15 +0300

    init

---
+++ 6 kyu Split Strings.py
@@ -0,0 +1,8 @@
+def solution(s):
+    pass
+
+
+print(solution("asdfadsf"), ['as', 'df', 'ad', 'sf'])
+print(solution("asdfads"), ['as', 'df', 'ad', 's_'])
+print(solution(""))
+print(solution("x"), ["x_"])

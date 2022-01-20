d63c5b2f41586782fd4b855199cb04ec95119b78
Paul <gafgafgarion05@gmail.com>
2022-01-20 11:15:06 +0300

    2 try to use dict

--- 6 kyu Find the unique number.py
+++ 6 kyu Find the unique number.py
@@ -1,10 +1,21 @@
 def find_uniq(arr):
-    n = arr[0]
-    for i in arr:
-        if i != n:
-            return i
+    # first = arr[0]
+    # last = arr[-1]
+    #
+    # if first != last:
+    #     return first
+    # return last
+    #
+    # for i in arr:
+    #     if i != first:
+    #         return i
+
+    dct = {i: arr.count(i) for i in arr}
+
+    return dct
 
 
 print(find_uniq([1, 1, 1, 2, 1, 1]), 2)
 print(find_uniq([0, 0, 0.55, 0, 0]), 0.55)
 print(find_uniq([3, 10, 3, 3, 3]), 10)
+print(find_uniq([10, 3, 3, 3]), 10)

e25c2bef9778b6bdde5d4820e9a0e476dc00a09f
Paul <gafgafgarion05@gmail.com>
2022-01-20 07:51:10 +0300

    3

--- 6 kyu Detect Pangram.py
+++ 6 kyu Detect Pangram.py
@@ -1,13 +1,8 @@
 import string
 
 def is_pangram(s):
-    lst = [1] * 26
-    lst_s = []
-    for i in s:
-        lst_s.append(s.count(i))
-
-
-    return lst_s
+    ascii_s = string.ascii_lowercase
+    return ascii_s
 
 
 pangram = "The quick, brown fox jumps over the lazy dog!"

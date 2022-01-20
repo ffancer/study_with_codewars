59b6febda94b7c9b472c922a9daf2261ef0dec8b
Paul <gafgafgarion05@gmail.com>
2022-01-20 08:28:35 +0300

    init

---
+++ 6 kyu Does my number look big in this.py
@@ -0,0 +1,8 @@
+def narcissistic(value):
+    pass
+
+
+print(narcissistic(7), True, '7 is narcissistic')
+print(narcissistic(371), True, '371 is narcissistic')
+print(narcissistic(122), False, '122 is not narcissistic')
+print(narcissistic(4887), False, '4887 is not narcissistic')

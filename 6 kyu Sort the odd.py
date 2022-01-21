a91a74561f8e4139f7ab79a98e66cff19a2c7a5b
Paul <gafgafgarion05@gmail.com>
2022-01-21 14:16:39 +0300

    init

---
+++ 6 kyu Sort the odd.py
@@ -0,0 +1,7 @@
+def sort_array(source_array):
+    pass
+
+
+print(sort_array([5, 3, 2, 8, 1, 4]), [1, 3, 2, 8, 5, 4])
+print(sort_array([5, 3, 1, 8, 0]), [1, 3, 5, 8, 0])
+print(sort_array([]), [])

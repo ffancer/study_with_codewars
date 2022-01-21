700b71d380524794f4634c5917d8351291078b4b
Paul <gafgafgarion05@gmail.com>
2022-01-21 10:47:12 +0300

    2

--- 6 kyu Is a number prime.py
+++ 6 kyu Is a number prime.py
@@ -4,7 +4,8 @@
 
 def is_prime(num):
     if num > 1:
-        for i in range(1, num):
+        temp = int(num ** .5)
+        for i in range(1, temp):
             if num % i == 0:
                 return True
     return False

87e7e5b0a1ae12895fe01706d87dd4b99a234001
Paul <gafgafgarion05@gmail.com>
2022-01-21 14:20:33 +0300

    2

--- 6 kyu Sort the odd.py
+++ 6 kyu Sort the odd.py
@@ -1,7 +1,9 @@
 def sort_array(source_array):
     dct = {i: source_array.index(i) for i in source_array if i % 2 != 0}
-    return dct
+    return sorted(dct.items())
 
+
+print(sort_array([5, 8, 6, 3, 4]), [3, 8, 6, 5, 4])
 print(sort_array([5, 3, 2, 8, 1, 4]), [1, 3, 2, 8, 5, 4])
 print(sort_array([5, 3, 1, 8, 0]), [1, 3, 5, 8, 0])
 print(sort_array([]), [])

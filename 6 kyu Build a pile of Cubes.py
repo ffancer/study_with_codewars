eeac430fc2fdbeba940efa1ceef8ff0fe13c9db6
Paul <gafgafgarion05@gmail.com>
2022-01-21 13:51:02 +0300

    init

---
+++ 6 kyu Build a pile of Cubes.py
@@ -0,0 +1,9 @@
+def find_nb(m):
+    pass
+
+
+print(find_nb(4183059834009), 2022)
+print(find_nb(24723578342962), -1)
+print(find_nb(135440716410000), 4824)
+print(find_nb(40539911473216), 3568)
+print(find_nb(26825883955641), 3218)

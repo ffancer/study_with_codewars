90b07415f620dfed30dbb9a25665be717a2a19d6
Paul <gafgafgarion05@gmail.com>
2022-01-22 10:05:26 +0300

    2

--- 5 kyu Where my anagrams at.py
+++ 5 kyu Where my anagrams at.py
@@ -1,7 +1,13 @@
 def anagrams(word, words):
-
-
-    return list(word)
+    # return list(word)
+    lst = []
+    for i in words:
+        for j in i:
+            if j not in list(word):
+                continue
+            else:
+                lst.append(i)
+    return lst
 
 print(anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']), ['aabb', 'bbaa'])
 print(anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']), ['carer', 'racer'])

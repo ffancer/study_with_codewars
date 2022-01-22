62d059a4b0532312d90bb98d314e1f388948fd0e
Paul <gafgafgarion05@gmail.com>
2022-01-22 10:30:14 +0300

    3

--- 5 kyu Where my anagrams at.py
+++ 5 kyu Where my anagrams at.py
@@ -5,9 +5,11 @@ def anagrams(word, words):
     for i in words:
         lst_sorted.append(sorted(i))
 
-
-    # return lst_sorted
-    return sorted(word)
+    for i in lst_sorted:
+        if i == sorted(word):
+            lst.append(i)
+    return lst_sorted
+    # return lst
 
 print(anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']), ['aabb', 'bbaa'])
 print(anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']), ['carer', 'racer'])

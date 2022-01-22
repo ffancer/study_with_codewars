ff0af3a3b49a3ad10807f90aaea6f8a36cff27a3
Paul <gafgafgarion05@gmail.com>
2022-01-22 10:21:07 +0300

    make definition to check anagrams with help sorted()

--- 5 kyu Where my anagrams at.py
+++ 5 kyu Where my anagrams at.py
@@ -1,13 +1,9 @@
 def anagrams(word, words):
-    # return list(word)
-    lst = []
-    for i in words:
-        for j in i:
-            if j not in list(word):
-                continue
-            else:
-                lst.append(i)
-    return lst
+
+    def anagram(word):
+        return sorted(word)
+
+    return anagram(word)
 
 print(anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']), ['aabb', 'bbaa'])
 print(anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']), ['carer', 'racer'])

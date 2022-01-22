b7d799a6c4030414922b45cee9272bb45e2e4d82
Paul <gafgafgarion05@gmail.com>
2022-01-22 10:32:57 +0300

    my solution

--- 5 kyu Where my anagrams at.py
+++ 5 kyu Where my anagrams at.py
@@ -1,8 +1,15 @@
 from collections import Counter
+
+
 def anagrams(word, words):
-    # return Counter(word)
+    lst = []
+
     for i in words:
-        print(Counter(i))
+        if Counter(i) == Counter(word):
+            lst.append(i)
+
+    return lst
+
 
 print(anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']), ['aabb', 'bbaa'])
 print(anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']), ['carer', 'racer'])

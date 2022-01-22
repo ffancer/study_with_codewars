8e73ee352e76126b1b95ee1561a03cf12100ffe9
Paul <gafgafgarion05@gmail.com>
2022-01-22 10:03:13 +0300

    init

---
+++ 5 kyu Where my anagrams at.py
@@ -0,0 +1,7 @@
+def anagrams(word, words):
+
+
+# your code here
+
+print(anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']), ['aabb', 'bbaa'])
+print(anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']), ['carer', 'racer'])

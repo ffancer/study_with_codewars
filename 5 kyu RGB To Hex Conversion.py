3310a274b9ca140c1ae67bd44849b9212f3ba90c
Paul <gafgafgarion05@gmail.com>
2022-01-22 08:05:46 +0300

    init

---
+++ 5 kyu RGB To Hex Conversion.py
@@ -0,0 +1,10 @@
+def rgb(r, g, b):
+    # your code here :)
+    pass
+
+
+print(rgb(0, 0, 0), "000000", "testing zero values")
+print(rgb(1, 2, 3), "010203", "testing near zero values")
+print(rgb(255, 255, 255), "FFFFFF", "testing max values")
+print(rgb(254, 253, 252), "FEFDFC", "testing near max values")
+print(rgb(-20, 275, 125), "00FF7D", "testing out of range values")

96a4bc3f610b9ac8140873bb75775b177153bc3e
Paul <gafgafgarion05@gmail.com>
2022-01-22 16:14:36 +0300

    init

---
+++ 5 kyu Directions Reduction.py
@@ -0,0 +1,8 @@
+def dirReduc(arr):
+    pass
+
+
+a = ["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]
+print(dirReduc(a), ['WEST'])
+u = ["NORTH", "WEST", "SOUTH", "EAST"]
+print(dirReduc(u), ["NORTH", "WEST", "SOUTH", "EAST"])

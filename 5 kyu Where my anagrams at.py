25374741491aad74c7e9df5b1e723b1262195d57
Paul <gafgafgarion05@gmail.com>
2022-01-22 10:31:36 +0300

    try to use Counter from collections

--- 5 kyu Where my anagrams at.py
+++ 5 kyu Where my anagrams at.py
@@ -1,15 +1,8 @@
+from collections import Counter
 def anagrams(word, words):
-    lst_sorted = []
-    lst = []
-
+    # return Counter(word)
     for i in words:
-        lst_sorted.append(sorted(i))
-
-    for i in lst_sorted:
-        if i == sorted(word):
-            lst.append(i)
-    return lst_sorted
-    # return lst
+        print(Counter(i))
 
 print(anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']), ['aabb', 'bbaa'])
 print(anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']), ['carer', 'racer'])

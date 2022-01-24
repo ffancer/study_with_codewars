6614deacae2b9a8882fdc2b105da71cd53946e7e
Paul <gafgafgarion05@gmail.com>
2022-01-24 09:44:55 +0300

    init

---
+++ 4 kyu Human readable duration format.py
@@ -0,0 +1,9 @@
+def format_duration(seconds):
+    pass
+
+
+print(format_duration(1), "1 second")
+print(format_duration(62), "1 minute and 2 seconds")
+print(format_duration(120), "2 minutes")
+print(format_duration(3600), "1 hour")
+print(format_duration(3662), "1 hour, 1 minute and 2 seconds")

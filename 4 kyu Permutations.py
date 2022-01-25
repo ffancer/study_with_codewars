cb414ce5e928e3fb3d179363c79b59900583d259
Paul <gafgafgarion05@gmail.com>
2022-01-25 09:39:48 +0300

    init

---
+++ 4 kyu Permutations.py
@@ -0,0 +1,7 @@
+def permutations(string):
+    pass
+
+
+print(sorted(permutations('a')), ['a'])
+print(sorted(permutations('ab')), ['ab', 'ba'])
+print(sorted(permutations('aabb')), ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa'])

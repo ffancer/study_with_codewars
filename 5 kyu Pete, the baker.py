8c8449f18712bd51ef258f92fb9b306831dff85b
Paul <gafgafgarion05@gmail.com>
2022-01-27 13:53:57 +0300

    4

--- 5 kyu Pete, the baker.py
+++ 5 kyu Pete, the baker.py
@@ -1,14 +1,18 @@
 def cakes(recipe, available):
-    minimum = 0
+    # minimum = -1
+    minimum = float('inf')
 
     for i in recipe:
         if i in available:
-            minimum = (minimum, available[i] / recipe[i])
+            minimum = min(minimum, available[i] // recipe[i])
+        return 0
 
     return minimum
-# recipe = {"flour": 500, "sugar": 200, "eggs": 1}
-# available = {"flour": 1200, "sugar": 1200, "eggs": 5, "milk": 200}
-# print(cakes(recipe, available), 2, 'example #1')
+
+
+recipe = {"flour": 500, "sugar": 200, "eggs": 1}
+available = {"flour": 1200, "sugar": 1200, "eggs": 5, "milk": 200}
+print(cakes(recipe, available), 2, 'example #1')
 
 recipe = {"apples": 3, "flour": 300, "sugar": 150, "milk": 100, "oil": 100}
 available = {"sugar": 500, "flour": 2000, "milk": 2000}

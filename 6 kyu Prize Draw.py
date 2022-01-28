dac2bf4092caa4d0545f4976f7af6058ff5281f8
Paul <gafgafgarion05@gmail.com>
2022-01-28 14:12:43 +0300

    2

--- 6 kyu Prize Draw.py
+++ 6 kyu Prize Draw.py
@@ -10,20 +10,16 @@ def rank(st, we, n):
     if n > len(participants):
         return "Not enough participants"
 
-
     for i, j in enumerate(participants):
         total = len(j)
         for k in j:
-            total += ord(k.lower())-96
+            total += ord(k.lower()) - 96
         participants[j] = total * we[i]
 
     sorted_by_name = sorted(participants.items(), key=itemgetter(0))
     sorted_by_value = sorted(sorted_by_name, key=itemgetter(1), reverse=True)
 
-    return list(sorted_by_value)[0]
-
-
-
+    return list(sorted_by_value)[n - 1]
 
 
 print(rank("Addison,Jayden,Sofia,Michael,Andrew,Lily,Benjamin", [4, 2, 1, 4, 3, 1, 2], 4), "Benjamin")

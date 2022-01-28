c8cb2a2add7cae5bf20e8095dcd927563be9313c
Paul <gafgafgarion05@gmail.com>
2022-01-28 09:27:59 +0300

    init

---
+++ 5 kyu First non-repeating character.py
@@ -0,0 +1,14 @@
+def first_non_repeating_letter(string):
+    pass
+
+
+print(first_non_repeating_letter('a'), 'a')
+print(first_non_repeating_letter('stress'), 't')
+print(first_non_repeating_letter('moonmen'), 'e')
+print(first_non_repeating_letter(''), '')
+print(first_non_repeating_letter('abba'), '')
+print(first_non_repeating_letter('aa'), '')
+print(first_non_repeating_letter('~><#~><'), '#')
+print(first_non_repeating_letter('hello world, eh?'), 'w')
+print(first_non_repeating_letter('sTreSS'), 'T')
+print(first_non_repeating_letter('Go hang a salami, I\'m a lasagna hog!'), ',')

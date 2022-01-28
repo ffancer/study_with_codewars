b904ffedb53fa3526bdfde9175ef38b039f19bfb
Paul <gafgafgarion05@gmail.com>
2022-01-28 11:28:25 +0300

    used ASCII to calculated sum of letters in names

--- 6 kyu Prize Draw.py
+++ 6 kyu Prize Draw.py
@@ -2,9 +2,17 @@ def rank(st, we, n):
     if st == '':
         return "No participants"
 
-    participants = {i : 0 for i in st.split(',')}
+    participants = {i: 0 for i in st.split(',')}
 
-    return participants
+    for i,j in enumerate(participants):
+        # print(i, j)
+        total = 0
+        for k in j:
+            total += ord(k)-96
+        return total
+
+
+
 print(rank("Addison,Jayden,Sofia,Michael,Andrew,Lily,Benjamin", [4, 2, 1, 4, 3, 1, 2], 4), "Benjamin")
 print(rank("Lagon,Lily", [1, 5], 2), "Lagon")
 print(rank("Addison,Jayden,Sofia,Michael,Andrew,Lily,Benjamin", [4, 2, 1, 4, 3, 1, 2], 8), "Not enough participants")

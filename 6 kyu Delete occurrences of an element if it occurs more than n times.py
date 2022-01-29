3afc039f9c60c087a15588d38c687ab33a35d16b
Paul <gafgafgarion05@gmail.com>
2022-01-29 09:34:02 +0300

    3

--- 6 kyu Delete occurrences of an element if it occurs more than n times.py
+++ 6 kyu Delete occurrences of an element if it occurs more than n times.py
@@ -3,7 +3,10 @@ def delete_nth(order, max_e):
     dct = {i: order.count(i) for i in order}
 
     for i, j in dct.items():
-        print(i, j)
+        if j <= max_e:
+            lst.append(i)
+
+    return lst
 
 
 print(delete_nth([20, 37, 20, 21], 1), [20, 37, 21])

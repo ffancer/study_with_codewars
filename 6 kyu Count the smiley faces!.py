546622b12fcc76406f80eeb13916e5ccf31d262b
Paul <gafgafgarion05@gmail.com>
2022-01-29 11:32:48 +0300

    my solution

--- 6 kyu Count the smiley faces!.py
+++ 6 kyu Count the smiley faces!.py
@@ -1,9 +1,12 @@
 def count_smileys(arr):
     lst = []
+
     for i in arr:
         if i[0] in [':', ';']:
-            if i[1] in['-', '~', ')', 'D']:
-                lst.append(i)
+            if i[1] in ['-', '~', ')', 'D']:
+                if i[-1] in ['-', '~', ')', 'D']:
+                    lst.append(i)
+
     return len(lst)
 
 
@@ -11,3 +14,4 @@ print(count_smileys([]), 0)
 print(count_smileys([':D', ':~)', ';~D', ':)']), 4)
 print(count_smileys([':)', ':(', ':D', ':O', ':;']), 2)
 print(count_smileys([';]', ':[', ';*', ':$', ';-D']), 1)
+print(count_smileys([';-]', ':-[', ';~*', ':~$', ';D']), 1)

67509c16c016f8ca5065270ec4d20e1ef854a65d
Paul <gafgafgarion05@gmail.com>
2022-01-29 08:48:48 +0300

    2 failed tests: TypeError: object of type 'NoneType' has no len()

--- 6 kyu Are they the same.py
+++ 6 kyu Are they the same.py
@@ -3,12 +3,14 @@
 
 
 def comp(array1, array2):
+    if len(array1) != len(array2):
+        return False
     for i in array1:
         sqrt_num = i ** 2
         if sqrt_num not in array2:
             return False
         array2.remove(sqrt_num)
-
+    return True
 
 a1 = [121, 144, 19, 161, 19, 144, 19, 11]
 a2 = [11 * 11, 121 * 121, 144 * 144, 19 * 19, 161 * 161, 19 * 19, 144 * 144, 19 * 19]

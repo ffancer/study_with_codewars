4abdda7e924607eb2ef067b24194bc1a92382878
Paul <gafgafgarion05@gmail.com>
2022-01-29 11:40:44 +0300

    best practice

--- 5 kyu Can you get the loop.py
+++ 5 kyu Can you get the loop.py
@@ -2,14 +2,30 @@
 # Can you get the loop ?
 
 
-def loop_size(n):
-    lst = []
+def loop_size(node):
+    turtle, rabbit = node.next, node.next.next
 
-    while not n in lst:
-        lst.append(n)
-        n = n.next
+    # Find a point in the loop.  Any point will do!
+    # Since the rabbit moves faster than the turtle
+    # and the kata guarantees a loop, the rabbit will
+    # eventually catch up with the turtle.
+    while turtle != rabbit:
+        turtle = turtle.next
+        rabbit = rabbit.next.next
 
-    return len(lst) - lst.index(n)
+    # The turtle and rabbit are now on the same node,
+    # but we know that node is in a loop.  So now we
+    # keep the turtle motionless and move the rabbit
+    # until it finds the turtle again, counting the
+    # nodes the rabbit visits in the mean time.
+    count = 1
+    rabbit = rabbit.next
+    while turtle != rabbit:
+        count += 1
+        rabbit = rabbit.next
+
+    # voila
+    return count
 
 
 

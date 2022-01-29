21b31302cbb60b2684d309afdf9255d13678773d
Paul <gafgafgarion05@gmail.com>
2022-01-29 08:51:40 +0300

    best practice

--- 6 kyu Are they the same.py
+++ 6 kyu Are they the same.py
@@ -3,14 +3,10 @@
 
 
 def comp(array1, array2):
-    if (array1 is None or array2 is None) or len(array1) != len(array2):
+    try:
+        return sorted([i ** 2 for i in array1]) == sorted(array2)
+    except:
         return False
-    for i in array1:
-        sqrt_num = i ** 2
-        if sqrt_num not in array2:
-            return False
-        array2.remove(sqrt_num)
-    return True
 
 a1 = [121, 144, 19, 161, 19, 144, 19, 11]
 a2 = [11 * 11, 121 * 121, 144 * 144, 19 * 19, 161 * 161, 19 * 19, 144 * 144, 19 * 19]

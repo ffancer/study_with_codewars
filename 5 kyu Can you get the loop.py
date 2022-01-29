6bd5c107e5147d2f1f05aec31d3759d780bb2f37
Paul <gafgafgarion05@gmail.com>
2022-01-29 11:40:12 +0300

    solution

--- 5 kyu Can you get the loop.py
+++ 5 kyu Can you get the loop.py
@@ -2,8 +2,14 @@
 # Can you get the loop ?
 
 
-def loop_size(node):
-    pass
+def loop_size(n):
+    lst = []
+
+    while not n in lst:
+        lst.append(n)
+        n = n.next
+
+    return len(lst) - lst.index(n)
 
 
 

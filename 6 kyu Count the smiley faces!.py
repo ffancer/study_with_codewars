8e02cfc52b0262c10f58e679525b80cbc3513281
Paul <gafgafgarion05@gmail.com>
2022-01-29 10:30:06 +0300

    init

---
+++ 6 kyu Count the smiley faces!.py
@@ -0,0 +1,8 @@
+def count_smileys(arr):
+    return  # the number of valid smiley faces in array/list
+
+
+print(count_smileys([]), 0)
+print(count_smileys([':D', ':~)', ';~D', ':)']), 4)
+print(count_smileys([':)', ':(', ':D', ':O', ':;']), 2)
+print(count_smileys([';]', ':[', ';*', ':$', ';-D']), 1)

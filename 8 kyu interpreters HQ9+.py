c9d1c97e9afdb7bf5ec54aaff90edb64c45761bf
Paul <gafgafgarion05@gmail.com>
2022-01-31 14:53:14 +0300

    2

--- 8 kyu interpreters HQ9+.py
+++ 8 kyu interpreters HQ9+.py
@@ -9,6 +9,11 @@ def HQ9(code):
             print(f'{cnt_bottle} bottles of beer on the wall, {cnt_bottle} bottles of beer.')
             cnt_bottle -= 1
             print(f'Take one down and pass it around, {cnt_bottle} bottles of beer on the wall.')
+
+        print('1 bottle of beer on the wall, 1 bottle of beer.')
+        print('Take one down and pass it around, no more bottles of beer on the wall.')
+        print('No more bottles of beer on the wall, no more bottles of beer.')
+        print('Go to the store and buy some more, 99 bottles of beer on the wall.')
 print(HQ9('H'))
 print(HQ9('Q'))
 print(HQ9('9'))

c32d5a8f4e71d397867ff98b12a8aab132b8f487
Paul <gafgafgarion05@gmail.com>
2022-01-31 11:59:03 +0300

    init

---
+++ 8 kyu Leonardo Dicaprio and Oscars.py
@@ -0,0 +1,8 @@
+def leo(oscar):
+    pass
+
+
+print(leo(89), "Leo got one already!")
+print(leo(88), "Leo finally won the oscar! Leo is happy");
+print(leo(87), "When will you give Leo an Oscar?")
+print(leo(86), "Not even for Wolf of wallstreet?!")

dda13326c807ea829eff113fdffaa0438c1c61b9
Paul <gafgafgarion05@gmail.com>
2022-01-31 11:23:18 +0300

    "NameError: name 'Human' is not defined"

--- 8 kyu Basic subclasses - Adam and Eve.py
+++ 8 kyu Basic subclasses - Adam and Eve.py
@@ -1,6 +1,18 @@
 def God():
-    pass
+    man = Man()
+    woman = Woman()
+    return [man, woman]
+
+
+class Man:
+    def __init__(self):
+        pass
+
+
+class Woman:
+    def __init__(self):
+        pass
 
 
 paradise = God()
-print(isinstance(paradise[0], Man) , True, "First object are a man")
+print(isinstance(paradise[0], Man), True, "First object are a man")

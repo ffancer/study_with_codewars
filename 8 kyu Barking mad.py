dfd003d500ce2d744e4bb91524579f8d6247900d
Paul <gafgafgarion05@gmail.com>
2022-01-31 14:00:44 +0300

    init

---
+++ 8 kyu Barking mad.py
@@ -0,0 +1,13 @@
+class Dog():
+    def __init__(self, breed):
+        self.breed = breed
+
+
+snoopy = Dog("Beagle")
+
+snoopy.bark = lambda: "Woof"
+
+scoobydoo = Dog("Great Dane")
+
+print(snoopy.bark(), "Woof")
+print(scoobydoo.bark(), "Woof")

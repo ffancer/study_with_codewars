2a550a935df7353b5b5e70f9fd9b3caace17941d
Paul <gafgafgarion05@gmail.com>
2022-01-31 14:03:47 +0300

    my solution

--- 8 kyu Barking mad.py
+++ 8 kyu Barking mad.py
@@ -2,14 +2,15 @@ class Dog():
     def __init__(self, breed):
         self.breed = breed
 
-
     def bark(self):
         return 'Woof'
 
 
-
 snoopy = Dog("Beagle")
+
 snoopy.bark = lambda: "Woof"
+
 scoobydoo = Dog("Great Dane")
+
 print(snoopy.bark(), "Woof")
 print(scoobydoo.bark(), "Woof")

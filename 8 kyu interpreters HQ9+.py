ec7cbd8ec357ff7dd77515fae750a425fbba5aea
Paul <gafgafgarion05@gmail.com>
2022-01-31 14:54:31 +0300

    my solution

--- 8 kyu interpreters HQ9+.py
+++ 8 kyu interpreters HQ9+.py
@@ -4,16 +4,18 @@ def HQ9(code):
     if code == 'Q':
         return code
     if code == '9':
-        cnt_bottle = 99
-        while cnt_bottle != 1:
-            print(f'{cnt_bottle} bottles of beer on the wall, {cnt_bottle} bottles of beer.')
-            cnt_bottle -= 1
-            print(f'Take one down and pass it around, {cnt_bottle} bottles of beer on the wall.')
+        lst = []
+        next_string = ''
+        for n in range(99, -1, -1):
+            if n != 0:
+                bottle_s = f'bottle{"s" if n > 1 else ""}'
+                next_string = f'{n} {bottle_s} of beer on the wall, {n} {bottle_s} of beer.\nTake one down and pass it around, {n - 1 if n > 1 else "no more"} bottle{"s" if n - 1 != 1 else ""} of beer on the wall.'
+            else:
+                next_string = 'No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.'
+            lst.append(next_string)
+        return '\n'.join(lst)
 
-        print('1 bottle of beer on the wall, 1 bottle of beer.')
-        print('Take one down and pass it around, no more bottles of beer on the wall.')
-        print('No more bottles of beer on the wall, no more bottles of beer.')
-        print('Go to the store and buy some more, 99 bottles of beer on the wall.')
+
 print(HQ9('H'))
 print(HQ9('Q'))
 print(HQ9('9'))

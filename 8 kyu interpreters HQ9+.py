b28cfa714af34bd8970b922c03bbcbc3a8999036
Paul <gafgafgarion05@gmail.com>
2022-01-31 14:43:12 +0300

    1

--- 8 kyu interpreters HQ9+.py
+++ 8 kyu interpreters HQ9+.py
@@ -1,7 +1,14 @@
 def HQ9(code):
-    pass
-
-
+    if code == 'H':
+        return 'Hello World!'
+    if code == 'Q':
+        return code
+    if code == '9':
+        cnt_bottle = 99
+        while cnt_bottle != 1:
+            print(f'{cnt_bottle} bottles of beer on the wall, {cnt_bottle} bottles of beer.')
+            cnt_bottle -= 1
+            print(f'Take one down and pass it around, {cnt_bottle} bottles of beer on the wall.')
 print(HQ9('H'))
 print(HQ9('Q'))
 print(HQ9('9'))

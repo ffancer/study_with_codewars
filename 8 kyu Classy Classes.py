4e0ea1572bd1aa578d517f5d1dcaae95d8b15dfa
Paul <gafgafgarion05@gmail.com>
2022-01-31 13:47:09 +0300

    my solution tests were at codewars.com

--- 8 kyu Classy Classes.py
+++ 8 kyu Classy Classes.py
@@ -2,14 +2,5 @@ class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
-        self.info = f"#{name}s age is #{age}"
+        self.info = f"{name}s age is {age}"
         print(self.info)
-
-
-
-names=["john","matt","alex","cam"]
-ages=[16,25,57,39]
-for i in range(4):
-    name,age=names[i],ages[i]
-person=Person(name,age)
-Person.info, name+"s age is "+str(age)

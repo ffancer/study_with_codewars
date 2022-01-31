6c1a16d6c727b094cf3bd99ae6c254a0c57fc3a2
Paul <gafgafgarion05@gmail.com>
2022-01-31 12:03:36 +0300

    my solution

--- 8 kyu Leonardo Dicaprio and Oscars.py
+++ 8 kyu Leonardo Dicaprio and Oscars.py
@@ -1,8 +1,15 @@
 def leo(oscar):
-    pass
+    if oscar > 88:
+        return "Leo got one already!"
+    if oscar == 86:
+        return "Not even for Wolf of wallstreet?!"
+    if oscar == 88:
+        return "Leo finally won the oscar! Leo is happy"
+    if oscar < 88:
+        return "When will you give Leo an Oscar?"
 
 
 print(leo(89), "Leo got one already!")
-print(leo(88), "Leo finally won the oscar! Leo is happy");
+print(leo(88), "Leo finally won the oscar! Leo is happy")
 print(leo(87), "When will you give Leo an Oscar?")
 print(leo(86), "Not even for Wolf of wallstreet?!")

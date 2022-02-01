24bd6022ee5e433c1efb1ad2fb85927573d72f1d
Paul <gafgafgarion05@gmail.com>
2022-02-01 11:15:45 +0300

    init

---
+++ 7 kyu Converting integer to currency format.py
@@ -0,0 +1,8 @@
+def to_currency(price):
+    pass
+
+
+print(to_currency(123456), "123,456")
+print(to_currency(1234), "1,234")
+print(to_currency(123), "123")
+print(to_currency(123456789012), "123,456,789,012")

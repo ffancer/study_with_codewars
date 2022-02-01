57270a4dcfd80c9b58cc62f6c60bf257e7982cfe
Paul <gafgafgarion05@gmail.com>
2022-02-01 08:12:55 +0300

    3

--- 7 kyu Sum of a Beach.py
+++ 7 kyu Sum of a Beach.py
@@ -1,6 +1,17 @@
-def sum_of_a_beach(beach):
+def sum_of_a_beach(beach):  # "Sand", "Water", "Fish", and "Sun"
     beach = beach.lower()
-    return beach[:3]
+    cnt = 0
+    i = 3
+    if "sun" in beach[:i]:
+        cnt += 1
+        i += 1
+        if "sand" or "fish" in beach[:i]:
+            cnt += 1
+            i += 1
+            if "water" in beach[:i]:
+                cnt += 1
+                i += 1
+    return cnt
 
 
 print(sum_of_a_beach("SanD"), 1)

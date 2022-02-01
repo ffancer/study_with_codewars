bfcbb744356feaa5182239dc35a58e9232191e44
Paul <gafgafgarion05@gmail.com>
2022-02-01 08:08:32 +0300

    init

---
+++ 7 kyu Sum of a Beach.py
@@ -0,0 +1,8 @@
+def sum_of_a_beach(beach):
+    pass
+
+
+print(sum_of_a_beach("SanD"), 1)
+print(sum_of_a_beach("sunshine"), 1)
+print(sum_of_a_beach("sunsunsunsun"), 4)
+print(sum_of_a_beach("123FISH321"), 1)

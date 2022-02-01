92b56190b46548f98f93834680b0d5734bc8e999
Paul <gafgafgarion05@gmail.com>
2022-02-01 09:50:35 +0300

    init

---
+++ 7 kyu Remove consecutive duplicate words.py
@@ -0,0 +1,6 @@
+def remove_consecutive_duplicates(s):
+    pass
+
+
+print(remove_consecutive_duplicates('alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'),
+      'alpha beta gamma delta alpha beta gamma delta')

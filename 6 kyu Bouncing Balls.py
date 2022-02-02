c77bb98660bee335ba524b56d1044207b444ff98
Paul <gafgafgarion05@gmail.com>
2022-02-02 09:21:59 +0300

    3

--- 6 kyu Bouncing Balls.py
+++ 6 kyu Bouncing Balls.py
@@ -2,11 +2,12 @@ from math import log
 
 
 def bouncing_ball(h, bounce, window):
-    try:
-        return 2 * int(log(window / float(h)) / log(bounce)) + 1 if h > window else -1
-    except ZeroDivisionError:
-        return -1
+    answer = -1
 
+    while h > window:
+        answer += 2
+        h *= bounce
+    return answer
 
 print(bouncing_ball(2, 0.5, 1))  # 1
 print(bouncing_ball(3, 0.66, 1.5))  # 3

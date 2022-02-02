432a28d5de2b945b218093b528ed98d589280bcd
Paul <gafgafgarion05@gmail.com>
2022-02-02 09:34:56 +0300

    init

---
+++ 6 kyu Which are in.py
@@ -0,0 +1,17 @@
+# 6 kyu
+# Which are in?
+
+
+def in_array(array1, array2):
+    # your code
+    return array1
+
+
+a1 = ["live", "arp", "strong"]
+a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+r = ['arp', 'live', 'strong']
+print(in_array(a1, a2))
+a1 = ["arp", "mice", "bull"]
+a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+r = ['arp']
+print(in_array(a1, a2))

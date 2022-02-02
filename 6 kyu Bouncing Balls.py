5fd44ea50ebda45557b2dd25b7db3ebeca1b69f5
Paul <gafgafgarion05@gmail.com>
2022-02-02 09:28:43 +0300

    solution

--- 6 kyu Bouncing Balls.py
+++ 6 kyu Bouncing Balls.py
@@ -1,13 +1,14 @@
-from math import log
+import math
 
 
 def bouncing_ball(h, bounce, window):
-    answer = -1
+    if not (h > 0 and 0 < bounce < 1 and window < h):
+        return -1
+    num_past_window = math.log(window / h, bounce)
+    floored_num = int(num_past_window)
 
-    while h > window:
-        answer += 2
-        h *= bounce
-    return answer
+    return num_past_window if num_past_window == floored_num else floored_num * 2 + 1
+
 
 print(bouncing_ball(2, 0.5, 1))  # 1
 print(bouncing_ball(3, 0.66, 1.5))  # 3

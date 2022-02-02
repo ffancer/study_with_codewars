504eea8055f8bce210242d3ee30608f1b6e9ef24
Paul <gafgafgarion05@gmail.com>
2022-02-02 09:16:17 +0300

    init

---
+++ 6 kyu Bouncing Balls.py
@@ -0,0 +1,9 @@
+def bouncing_ball(h, bounce, window):
+    # your code
+    return -1
+
+
+print(bouncing_ball(2, 0.5, 1))  # 1
+print(bouncing_ball(3, 0.66, 1.5))  # 3
+print(bouncing_ball(30, 0.66, 1.5))  # 15
+print(bouncing_ball(30, 0.75, 1.5))  # 21

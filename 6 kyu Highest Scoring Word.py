773f9ec2a79636b04f6519f0c0865a297a067b61
Paul <gafgafgarion05@gmail.com>
2022-02-02 09:53:45 +0300

    init

---
+++ 6 kyu Highest Scoring Word.py
@@ -0,0 +1,12 @@
+def high(x):
+    pass
+
+
+print(high('man i need a taxi up to ubud'), 'taxi')
+print(high('what time are we climbing up the volcano'), 'volcano')
+print(high('take me to semynak'), 'semynak')
+print(high('aa b'), 'aa')
+print(high('b aa'), 'b')
+print(high('bb d'), 'bb')
+print(high('d bb'), 'd')
+print(high("aaa b"), "aaa")

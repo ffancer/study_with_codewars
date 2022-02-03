9e7d1667db003a527acc22ab1d4a4982c624049f
Paul <gafgafgarion05@gmail.com>
2022-02-03 13:33:12 +0300

    my solution

--- 5 kyu Greed is Good.py
+++ 5 kyu Greed is Good.py
@@ -1,5 +1,13 @@
 def score(dice):
-    pass
+    three_dice = [1000, 200, 300, 400, 500, 600]
+    one_dice = [100, 0, 0, 0, 50, 0]
+    total = 0
+
+    for i in range(1, 7):
+        cnt = dice.count(i)
+        total += (cnt // 3) * three_dice[i - 1] + (cnt % 3) * one_dice[i - 1]
+
+    return total
 
 
 print(score([2, 3, 4, 6, 2]), 0)

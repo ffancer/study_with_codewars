be6011edc46ae0136187f9460489fa2d9e9b47db
Paul <gafgafgarion05@gmail.com>
2022-02-03 10:56:02 +0300

    try to use all()

--- 5 kyu Scramblies.py
+++ 5 kyu Scramblies.py
@@ -1,11 +1,5 @@
 def scramble(s1, s2):
-    cnt = 0
-
-    for i in s2:
-        if i in s1:
-            cnt += 1
-
-    return cnt == len(s2)
+    return list(s2)
 
 print(scramble('rkqodlw', 'world'), True)
 print(scramble('cedewaraaossoqqyt', 'codewars'), True)

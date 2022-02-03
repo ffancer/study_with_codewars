b1e832d636991c756381728b895771dc0b570a75
Paul <gafgafgarion05@gmail.com>
2022-02-03 10:52:15 +0300

    6 errors from 520 tests

--- 5 kyu Scramblies.py
+++ 5 kyu Scramblies.py
@@ -1,9 +1,11 @@
 def scramble(s1, s2):
     cnt = 0
+
     for i in s2:
         if i in s1:
             cnt += 1
-    return cnt
+
+    return cnt == len(s2)
 
 print(scramble('rkqodlw', 'world'), True)
 print(scramble('cedewaraaossoqqyt', 'codewars'), True)

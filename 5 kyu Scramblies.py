62ee378fef4182ac14c18f60792164b5804489f3
Paul <gafgafgarion05@gmail.com>
2022-02-03 11:02:53 +0300

    too long solution with any()

--- 5 kyu Scramblies.py
+++ 5 kyu Scramblies.py
@@ -1,5 +1,6 @@
 def scramble(s1, s2):
-    return list(s2)
+    return all(s1.count(i) >= s2.count(i) for i in s2)
+
 
 print(scramble('rkqodlw', 'world'), True)
 print(scramble('cedewaraaossoqqyt', 'codewars'), True)

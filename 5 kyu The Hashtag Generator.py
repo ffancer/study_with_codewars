e7bbf3a4d05771bee72e5919f11f67f750434ab3
Paul <gafgafgarion05@gmail.com>
2022-02-03 10:17:32 +0300

    my solution

--- 5 kyu The Hashtag Generator.py
+++ 5 kyu The Hashtag Generator.py
@@ -1,8 +1,12 @@
 def generate_hashtag(s):
+    if s == '':
+        return False
+
     s = s.split()
     s = [i.capitalize() for i in s]
     s = '#' + ''.join(s)
-    return s
+
+    return False if len(s) > 140 else s
 
 
 print(generate_hashtag(''), False, 'Expected an empty string to return False')
@@ -11,7 +15,10 @@ print(generate_hashtag('Codewars'), '#Codewars', 'Should handle a single word.')
 print(generate_hashtag('Codewars      '), '#Codewars', 'Should handle trailing whitespace.')
 print(generate_hashtag('Codewars Is Nice'), '#CodewarsIsNice', 'Should remove spaces.')
 print(generate_hashtag('codewars is nice'), '#CodewarsIsNice', 'Should capitalize first letters of words.')
-print(generate_hashtag('CodeWars is nice'), '#CodewarsIsNice', 'Should capitalize all letters of words - all lower case but the first.')
+print(generate_hashtag('CodeWars is nice'), '#CodewarsIsNice',
+      'Should capitalize all letters of words - all lower case but the first.')
 print(generate_hashtag('c i n'), '#CIN', 'Should capitalize first letters of words even when single letters.')
 print(generate_hashtag('codewars  is  nice'), '#CodewarsIsNice', 'Should deal with unnecessary middle spaces.')
-print(generate_hashtag('Looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong Cat'), False, 'Should return False if the final word is longer than 140 chars.')
+print(generate_hashtag(
+    'Looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong Cat'),
+      False, 'Should return False if the final word is longer than 140 chars.')

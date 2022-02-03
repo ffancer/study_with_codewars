8b9905b63c9147f0b8a3eac9ef9769e18c807831
Paul <gafgafgarion05@gmail.com>
2022-02-03 10:50:23 +0300

    init

---
+++ 5 kyu Scramblies.py
@@ -0,0 +1,9 @@
+def scramble(s1, s2):
+    pass
+
+
+print(scramble('rkqodlw', 'world'), True)
+print(scramble('cedewaraaossoqqyt', 'codewars'), True)
+print(scramble('katas', 'steak'), False)
+print(scramble('scriptjava', 'javascript'), True)
+print(scramble('scriptingjava', 'javascript'), True)

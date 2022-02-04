6fa9446db8b9090c65da42f064c16ae37217f872
Paul <gafgafgarion05@gmail.com>
2022-02-04 10:11:22 +0300

    init

---
+++ 4 kyu Strip Comments.py
@@ -0,0 +1,6 @@
+def solution(string, markers):
+    pass
+
+
+print(solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"]), "apples, pears\ngrapes\nbananas")
+print(solution("a #b\nc\nd $e f g", ["#", "$"]), "a\nc\nd")

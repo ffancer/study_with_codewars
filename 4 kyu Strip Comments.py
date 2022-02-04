5aede1e514b4d508c920fa82d6b1cfa3a05ad3df
Paul <gafgafgarion05@gmail.com>
2022-02-04 11:27:52 +0300

    my solution

--- 4 kyu Strip Comments.py
+++ 4 kyu Strip Comments.py
@@ -1,5 +1,16 @@
 def solution(string, markers):
-    return string.split('\n')
+    s = string.split('\n')
+    answer = ''
+
+    for i in s:
+        length = len(i)
+        for j, k in enumerate(i):
+            if k in markers:
+                length = j
+                break
+        answer += i[:length].strip() + '\n'
+
+    return answer[:-1]
 
 
 print(solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"]), "apples, pears\ngrapes\nbananas")

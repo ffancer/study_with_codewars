fa6a57d0b69e88839ce49e0d92225a9689796177
Paul <gafgafgarion05@gmail.com>
2022-02-05 11:37:39 +0300

    init

---
+++ 3 kyu Help the general decode secret enemy messages..py
@@ -0,0 +1,17 @@
+def decode(s):
+    print(encode("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"))
+    print(encode("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"))
+    print(encode("!@#$%^&*()_+-"))
+    a,b,c = "", "", ""
+    for w in "abcdefghijklmnopqrstuvwxyz":
+        a += encode(  "" + w)[0]
+        b += encode( "_" + w)[1]
+        c += encode("__" + w)[2]
+    print(a)
+    print(b)
+    print(c)
+    return s;
+
+
+print(decode('hello world'))
+print(decode('("!@#$%^&*()_+-")'))

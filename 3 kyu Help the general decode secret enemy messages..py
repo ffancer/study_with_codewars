13c5d3f55e7762461251f012107bbe4d5ab86e3a
Paul <gafgafgarion05@gmail.com>
2022-02-05 11:37:55 +0300

    my solution

--- 3 kyu Help the general decode secret enemy messages..py
+++ 3 kyu Help the general decode secret enemy messages..py
@@ -1,16 +1,13 @@
 def decode(s):
-    print(encode("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"))
-    print(encode("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"))
-    print(encode("!@#$%^&*()_+-"))
-    a,b,c = "", "", ""
-    for w in "abcdefghijklmnopqrstuvwxyz":
-        a += encode(  "" + w)[0]
-        b += encode( "_" + w)[1]
-        c += encode("__" + w)[2]
-    print(a)
-    print(b)
-    print(c)
-    return s;
+    alphabet = "!abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,? "
+    alph_len = len(alphabet)
+    answer = ''
+
+    for i, j in enumerate(s, 1):
+        k = [alphabet[2 ** i * v % alph_len] for v in range(len(alphabet))]
+        answer += alphabet[k.index(j)] if j in k else j
+
+    return answer
 
 
 print(decode('hello world'))

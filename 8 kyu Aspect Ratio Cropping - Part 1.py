2e3cee8137a88b33186ddc23906b8ce31c39441d
Paul <gafgafgarion05@gmail.com>
2022-02-07 10:13:40 +0300

    my 1 line solution

--- 8 kyu Aspect Ratio Cropping - Part 1.py
+++ 8 kyu Aspect Ratio Cropping - Part 1.py
@@ -3,25 +3,4 @@ from math import ceil
 
 
 def aspect_ratio(x: int, y: int) -> Tuple[int, int]:
-    return [ceil(y*16/9), y]
-
-
-
-def assertion_str(x, y, actual, expected):
-    return f"Testing aspect_ratio({x}, {y}) \nActual: {actual} \nExpected: {expected}\n\nError"
-
-
-def test_group():
-    def example_tests():
-        cases = [
-            # x, y, expected
-            (640, 480, (854, 480)),
-            (960, 720, (1280, 720)),
-            (1440, 1080, (1920, 1080)),
-            (1920, 1440, (2560, 1440)),
-        ]
-
-        for case in cases:
-            x, y, expected = case
-            actual = aspect_ratio(x, y)
-            print(actual, expected, assertion_str(x, y, actual, expected))
+    return (ceil(y * 16 / 9), y)

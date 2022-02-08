2fcf4a9ff6eae6eceea417ec90155e80eb38e48f
Paul <gafgafgarion05@gmail.com>
2022-02-08 12:55:38 +0300

    init

---
+++ 7 kyu Return String of First Characters.py
@@ -0,0 +1,8 @@
+def make_string(s):
+    pass
+
+
+print(make_string("sees eyes xray yoat"), "sexy", "Wrong result for 'sees eyes xray yoat'")
+print(make_string("brown eyes are nice"), "bean", "Wrong result for 'brown eyes are nice'")
+print(make_string("cars are very nice"), "cavn", "Wrong result for 'cars are very nice'")
+print(make_string("kaks de gan has a big head"), "kdghabh", "Wrong result for 'kaks de gan has a big head'")

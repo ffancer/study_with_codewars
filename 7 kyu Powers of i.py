f5cd2cff782b65f949b7f2e7d4b558edddf3c1ef
Paul <gafgafgarion05@gmail.com>
2022-02-08 11:38:21 +0300

    init

---
+++ 7 kyu Powers of i.py
@@ -0,0 +1,15 @@
+def pofi(n):
+    return 'i'
+
+
+print(pofi(0), '1')
+print(pofi(1), 'i')
+print(pofi(2), '-1')
+print(pofi(3), '-i')
+print(pofi(4), '1')
+print(pofi(5), 'i')
+print(pofi(6), '-1')
+print(pofi(7), '-i')
+print(pofi(8), '1')
+print(pofi(9), 'i')
+print(pofi(10), '-1')

b0ead0041ab7fb7f5b58c5c6e75303adb9185113
Paul <gafgafgarion05@gmail.com>
2022-02-09 10:41:34 +0300

    my solution

--- 7 kyu Previous multiple of three.py
+++ 7 kyu Previous multiple of three.py
@@ -1,12 +1,8 @@
 def prev_mult_of_three(n):
-    n = str(n)
-    i = -1
+    while n % 3:
+        n = n // 10
+    return n or None
 
-    while i < 0:
-        if int(n[i] + n[i-1]) % 3 != 0:
-            i -= 1
-        return int(n[i] + n[i-1])
-    return None
 
 print(prev_mult_of_three(1), None)
 print(prev_mult_of_three(25), None)

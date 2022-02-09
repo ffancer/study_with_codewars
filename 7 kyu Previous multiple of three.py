4e8f7af501f62dd6631a3a38418dada9b2457079
Paul <gafgafgarion05@gmail.com>
2022-02-09 10:30:33 +0300

    init

---
+++ 7 kyu Previous multiple of three.py
@@ -0,0 +1,9 @@
+def prev_mult_of_three(n):
+    pass
+
+
+print(prev_mult_of_three(1), None)
+print(prev_mult_of_three(25), None)
+print(prev_mult_of_three(36), 36)
+print(prev_mult_of_three(1244), 12)
+print(prev_mult_of_three(952406), 9)

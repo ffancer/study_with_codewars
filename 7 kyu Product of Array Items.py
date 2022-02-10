ce734963f64172acef5744c6bbc07f8fadb5fce3
Paul <gafgafgarion05@gmail.com>
2022-02-10 09:10:02 +0300

    init

---
+++ 7 kyu Product of Array Items.py
@@ -0,0 +1,10 @@
+def product(numbers):
+    pass
+
+
+print(product([5, 4, 1, 3, 9]), 540)
+print(product([-2, 6, 7, 8]), -672)
+print(product([10]), 10)
+print(product([0, 2, 9, 7]), 0)
+print(product(None), None)
+print(product([]), None)

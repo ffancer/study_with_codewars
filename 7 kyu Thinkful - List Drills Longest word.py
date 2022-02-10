6d3d3bcfadebb3ce2243dbb0f771e0c3c920716d
Paul <gafgafgarion05@gmail.com>
2022-02-10 07:45:14 +0300

    init

---
+++ 7 kyu Thinkful - List Drills Longest word.py
@@ -0,0 +1,7 @@
+def longest(words):
+    pass
+
+
+print(longest(['simple', 'is', 'better', 'than', 'complex']), 7)
+print(longest(['explicit', 'is', 'better', 'than', 'implicit']), 8)
+print(longest(['beautiful', 'is', 'better', 'than', 'ugly']), 9)

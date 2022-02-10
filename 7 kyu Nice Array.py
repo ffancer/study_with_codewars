b932f1db78f332f4eb7134e03e9403fb994729f4
Paul <gafgafgarion05@gmail.com>
2022-02-10 13:36:54 +0300

    my 1 line solution

--- 7 kyu Nice Array.py
+++ 7 kyu Nice Array.py
@@ -1,5 +1,5 @@
 def is_nice(arr):
-    return all(i+1 in arr or i-1 in arr for i in arr)
+    return all(i+1 in arr or i-1 in arr for i in arr) and len(arr) > 0
 
 
 print(is_nice([2, 10, 9, 3]), True)

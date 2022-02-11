1cc969dbc066d70afa1ec6e737bb91482ce925c1
Paul <gafgafgarion05@gmail.com>
2022-02-11 11:05:38 +0300

    init

---
+++ 7 kyu Fun with lists length.py
@@ -0,0 +1,20 @@
+# 7 kyu
+# Fun with lists: length
+
+
+def length(head):
+    # your code here
+    pass
+
+
+
+
+# def example_test_case():
+#     head = None
+#     test.assert_equals(length(head), 0)
+#
+#     n1 = Node(1)
+#     n2 = Node(2, n1)
+#     n3 = Node(3, n2)
+#     head = Node(4, n3)
+#     test.assert_equals(length(head), 4)

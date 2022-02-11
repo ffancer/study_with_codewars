881424cd8c15be834d696fce15ec5b9108a95389
Paul <gafgafgarion05@gmail.com>
2022-02-11 13:09:10 +0300

    3

--- 7 kyu Numbers with this digit inside.py
+++ 7 kyu Numbers with this digit inside.py
@@ -1,16 +1,16 @@
 from math import prod
 
-lst = []
-
-# for i in range(1, 12):
-#     if '1' in str(i):
-#         lst.append(i)
-# print(lst)
 
 def numbers_with_digit_inside(x, d):
-    lst = [i for i in range(d, x+1) if str(d) in str(i)]
-    return [len(lst), sum(lst), prod(lst)]
-    # return lst
+    lst = [i for i in range(d, x + 1) if str(d) in str(i) and str(i) != '0']
+    # lst = []
+    #
+    # for i in range(d, x + 1):
+    #     if str(d) in str(i) and str(i) != '0':
+    #         lst.append(i)
+    # return len(lst)
+    # return sum(lst)
+    return prod(lst)
 
 tests = (
     ((5, 6), [0, 0, 0]),

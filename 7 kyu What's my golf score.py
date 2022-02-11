214c7209f0da8b66cdf01b8dedc3396cba9af20f
Paul <gafgafgarion05@gmail.com>
2022-02-11 12:34:30 +0300

    1

--- 7 kyu What's my golf score.py
+++ 7 kyu What's my golf score.py
@@ -3,7 +3,12 @@
 
 
 def golf_score_calculator(par_string, score_string):
-    pass
+    total = 0
+    for i in par_string:
+        for j in score_string:
+            total += int(i) - int(j)
+
+    return total
 
 
 print(golf_score_calculator('443454444344544443', '353445334534445344'), -1)

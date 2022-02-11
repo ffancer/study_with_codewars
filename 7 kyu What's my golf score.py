8e29e3b2fa7ca17287355aba2e2f6c88adaf2061
Paul <gafgafgarion05@gmail.com>
2022-02-11 12:45:50 +0300

    my 1 line solution more compact

--- 7 kyu What's my golf score.py
+++ 7 kyu What's my golf score.py
@@ -3,10 +3,7 @@
 
 
 def golf_score_calculator(par_string, score_string):
-    lst_1 = [int(i) for i in par_string]
-    lst_2 = [int(i) for i in score_string]
-    ans = [y - x for x, y in zip(lst_1, lst_2)]
-    return sum(ans)
+    return sum(int(j) - int(i) for i, j in zip(par_string, score_string))
 
 
 print(golf_score_calculator('443454444344544443', '353445334534445344'), -1)

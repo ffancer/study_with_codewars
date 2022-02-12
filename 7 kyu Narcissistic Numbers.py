31564a1b9d42025bc36715de16478355de3f1cb6
Paul <gafgafgarion05@gmail.com>
2022-02-12 09:43:56 +0300

    init

---
+++ 7 kyu Narcissistic Numbers.py
@@ -0,0 +1,12 @@
+def is_narcissistic(i):
+    pass
+
+
+print(is_narcissistic(153), True)
+print(is_narcissistic(201), False)
+print(is_narcissistic(259), False)
+print(is_narcissistic(371), True)
+print(is_narcissistic(407), True)
+print(is_narcissistic(595), False)
+print(is_narcissistic(825), False)
+print(is_narcissistic(1634), True)

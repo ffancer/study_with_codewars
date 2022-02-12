39d4d29e715c9b59cf4981f3b32f6c118c017237
Paul <gafgafgarion05@gmail.com>
2022-02-12 09:51:30 +0300

    init

---
+++ 7 kyu Find all pairs.py
@@ -0,0 +1,10 @@
+def duplicates(arr):
+    pass
+
+
+print(duplicates([1, 2, 5, 6, 5, 2]), 2)
+print(duplicates([1, 2, 2, 20, 6, 20, 2, 6, 2]), 4)
+print(duplicates([0, 0, 0, 0, 0, 0, 0]), 3)
+print(duplicates([1000, 1000]), 1)
+print(duplicates([]), 0)
+print(duplicates([54]), 0)

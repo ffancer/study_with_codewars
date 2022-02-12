a7a4b763fcd4be84107bcee07a69eaf157d5a083
Paul <gafgafgarion05@gmail.com>
2022-02-12 09:05:16 +0300

    init

---
+++ 7 kyu Find the Missing Number.py
@@ -0,0 +1,13 @@
+def missing_no(nums):
+    pass
+
+
+def examples():
+    # Numbers from 0 to 100, but missing 5
+    nums = list(range(0, 101))
+    nums.remove(5)
+    print(missing_no(nums), 5)
+
+    nums = list(reversed(range(0, 101)))
+    nums.remove(10)
+    print(missing_no(nums), 10)

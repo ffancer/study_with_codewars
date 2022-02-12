3d725314ea002125f05923110f97e5ce091f174d
Paul <gafgafgarion05@gmail.com>
2022-02-12 12:03:12 +0300

    best solution + 1 line

--- 7 kyu Find all pairs.py
+++ 7 kyu Find all pairs.py
@@ -1,12 +1,5 @@
 def duplicates(arr):
-    total = 0
-    dct = {i: arr.count(i) for i in arr}
-
-    for i in dct.values():
-        if i > 1:
-            total += i // 2
-
-    return total
+    return sum(arr.count(i)//2 for i in set(arr))
 
 
 print(duplicates([1, 2, 5, 6, 5, 2]), 2)

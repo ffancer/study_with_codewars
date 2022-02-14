6d2b88b439729077f163d1a811622a54bf39bc6d
Paul <gafgafgarion05@gmail.com>
2022-02-14 11:32:10 +0300

    my solution

--- 7 kyu Happy Birthday.py
+++ 7 kyu Happy Birthday.py
@@ -1,6 +1,12 @@
 def wrap(height, width, length):
     min_value = min(height, width, length)
+    answer = (height * 4 + length * 2 + width * 2) + 20
 
+    if min_value == width:
+        answer = (width * 4 + height * 2 + length * 2) + 20
+    elif min_value == length:
+        answer = (length * 4 + height * 2 + width * 2) + 20
+    return answer
 
 
 print(wrap(17, 32, 11), 162)

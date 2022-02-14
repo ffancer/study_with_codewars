b76eda7017a07ad173ba8fae7ccc0eec24ebc072
Paul <gafgafgarion05@gmail.com>
2022-02-14 09:33:26 +0300

    init

---
+++ 7 kyu Enumerable Magic #5- True for Just One.py
@@ -0,0 +1,17 @@
+# 7 kyu
+# Enumerable Magic #5- True for Just One?
+
+
+def one(sq, fun):
+    # your code here
+    pass
+
+
+equals_9 = lambda x: x == 9
+less_than_9 = lambda x: x < 9
+greater_than_9 = lambda x: x > 9
+arr = (6, 7, 8, 9, 10, 11)
+
+print(one(arr, equals_9), True)
+print(one(arr, less_than_9), False)
+print(one(arr, greater_than_9), False)

18e306e9bb7b36df04196ca1b112b9fee984f3b6
Paul <gafgafgarion05@gmail.com>
2022-02-14 10:40:13 +0300

    solution was bad tests

--- 7 kyu Linked Lists - Get Nth Node.py
+++ 7 kyu Linked Lists - Get Nth Node.py
@@ -5,18 +5,12 @@ class Node(object):
 
 
 def get_nth(node, index):
-    pass
+    v = -1
+    n = node
+    while n:
+        v += 1
+        if v == index:
+            return n
+        n = n.next
 
-
-def v():
-    # list = build_one_two_three()
-    print(get_nth(list, 0).data, 1, "First node should be located at index 0.")
-    print(get_nth(list, 1).data, 2, "Second node should be located at index 1.")
-    print(get_nth(list, 2).data, 3, "Third node should be located at index 2.")
-    print("Invalid index value should throw error.", lambda: get_nth(list, 3))
-    print("Invalid index value should throw error.", lambda: get_nth(list, -1))
-    print("Invalid index value should throw error.", lambda: get_nth(list, 100))
-    print("None linked list should throw error.", lambda: get_nth(None, 0))
-
-
-print(v())
+    raise ValueError

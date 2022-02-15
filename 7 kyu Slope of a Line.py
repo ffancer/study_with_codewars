5fa1473a420963dd6ffe01f6d6f585ce5a5d33da
Paul <gafgafgarion05@gmail.com>
2022-02-15 08:24:04 +0300

    my solution this is work coz /

--- 7 kyu Slope of a Line.py
+++ 7 kyu Slope of a Line.py
@@ -1,7 +1,9 @@
 def getSlope(p1, p2):
-    if p2[0] == p1[0] or p2[1] == p1[1] or p2[0] == p1[0]:
-        return None
-    return (p2[1] - p1[1]) // (p2[0] - p1[0])
+    (x, y), (x2, y2) = p1, p2
+    try:
+        return (y - y2) / (x - x2)
+    except ZeroDivisionError:
+        pass
 
 
 # ("Should calculate the existing non-zero between 2 points")

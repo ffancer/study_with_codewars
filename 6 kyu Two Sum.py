1848459c7978fff575b2bfe331160e07222bfad3
Paul <gafgafgarion05@gmail.com>
2022-02-16 09:37:26 +0300

    my solution

--- 6 kyu Two Sum.py
+++ 6 kyu Two Sum.py
@@ -1,7 +1,7 @@
 def two_sum(numbers, target):
     for i in range(len(numbers)):
         for j in range(len(numbers)):
-            if numbers[i] + numbers[j] == target:
+            if numbers[i] + numbers[j] == target and i != j:
                 return [i, j]
 
 

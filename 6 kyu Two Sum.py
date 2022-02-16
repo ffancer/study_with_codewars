4ca0915e401e38cfe081835e1845ff5844e202d8
Paul <gafgafgarion05@gmail.com>
2022-02-16 09:20:54 +0300

    init

---
+++ 6 kyu Two Sum.py
@@ -0,0 +1,8 @@
+def two_sum(numbers, target):
+    # start coding!
+    pass
+
+
+print(sorted(two_sum([1, 2, 3], 4)), [0, 2])
+print(sorted(two_sum([1234, 5678, 9012], 14690)), [1, 2])
+print(sorted(two_sum([2, 2, 3], 4)), [0, 1])

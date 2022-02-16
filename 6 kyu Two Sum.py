00cd36fd9d44454fe36d3cce2359a8a9c0fdb960
Paul <gafgafgarion05@gmail.com>
2022-02-16 09:34:07 +0300

    1

--- 6 kyu Two Sum.py
+++ 6 kyu Two Sum.py
@@ -1,8 +1,17 @@
 def two_sum(numbers, target):
-    # start coding!
-    pass
+    i, j = 0, 1
+    lst = []
+    while i < j:
+        if numbers[i] + numbers[j] != target:
+            j += 1
+        else:
+            lst.append(i)
+            lst.append(j)
+
+    return lst
 
 
-print(sorted(two_sum([1, 2, 3], 4)), [0, 2])
-print(sorted(two_sum([1234, 5678, 9012], 14690)), [1, 2])
-print(sorted(two_sum([2, 2, 3], 4)), [0, 1])
+
+print(two_sum([1, 2, 3], 4), [0, 2])
+print(two_sum([1234, 5678, 9012], 14690), [1, 2])
+print(two_sum([2, 2, 3], 4), [0, 1])

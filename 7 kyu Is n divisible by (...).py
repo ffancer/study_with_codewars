f82992ded824f02c14a599c93ce8dd3ff51b2901
Paul <gafgafgarion05@gmail.com>
2022-02-16 14:14:27 +0300

    init

---
+++ 7 kyu Is n divisible by (...).py
@@ -0,0 +1,11 @@
+# 7 kyu
+# Is n divisible by (...)?
+
+
+def is_divisible():
+    pass
+
+
+print(is_divisible(3, 3, 4), False)
+print(is_divisible(12, 3, 4), True)
+print(is_divisible(8, 3, 4, 2, 5), False)

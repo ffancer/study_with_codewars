fe30835d3f5108f490c403b564690e9ef919ef16
Paul <gafgafgarion05@gmail.com>
2022-02-17 10:29:21 +0300

    init

---
+++ 8 kyu Wilson primes.py
@@ -0,0 +1,9 @@
+def am_i_wilson(n):
+    pass
+
+
+print(am_i_wilson(0), False)
+print(am_i_wilson(1), False)
+print(am_i_wilson(5), True)
+print(am_i_wilson(8), False)
+print(am_i_wilson(9), False)

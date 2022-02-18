192cfe93eaf034b9aabe93f3101760830273db96
Paul <gafgafgarion05@gmail.com>
2022-02-18 12:09:44 +0300

    init

---
+++ 8 kyu Generate user links.py
@@ -0,0 +1,9 @@
+def generate_link(user):
+    pass
+
+
+print('matt c', 'http://www.codewars.com/users/matt%20c')
+print('g964', 'http://www.codewars.com/users/g964')
+print('GiacomoSorbi', 'http://www.codewars.com/users/GiacomoSorbi')
+print('ZozoFouchtra', 'http://www.codewars.com/users/ZozoFouchtra')
+print('colbydauph', 'http://www.codewars.com/users/colbydauph')

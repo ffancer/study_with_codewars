2342de0f3d50c6cf8cbe4aca09dadd5ac4387973
Paul <gafgafgarion05@gmail.com>
2022-02-18 10:33:20 +0300

    init

---
+++ 8 kyu Finish Guess the Number Game.py
@@ -0,0 +1,27 @@
+class Guesser:
+    def __init__(self, number, lives):
+        self.number = number
+        self.lives = lives
+
+    def guess(self, n):
+        return False
+
+
+guesser = Guesser(10, 2)
+guesser.guess(10)
+guesser.guess(10)
+guesser.guess(10)
+guesser.guess(10)
+print(guesser.guess(10), True)
+
+# test.it('Wrong guess should return false')
+guesser = Guesser(10, 2)
+guesser.guess(1)
+print(guesser.guess(1), False)
+
+# test.it('Lives ran out should throw')
+guesser = Guesser(10, 2)
+guesser.guess(1)
+guesser.guess(2)
+
+print('Expect error: "Omae wa mo shindeiru"', lambda: guesser.guess(10))

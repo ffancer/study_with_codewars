1bbb4ebe621a1aeb207ee22067b9f85214c7d028
Paul <gafgafgarion05@gmail.com>
2022-02-18 09:55:36 +0300

    best practice here are True == 1 and False == 0

--- 8 kyu Check same case.py
+++ 8 kyu Check same case.py
@@ -1,9 +1,5 @@
 def same_case(a, b):
-    if not a.isalpha() or not b.isalpha():
-        return -1
-    if (a.islower() and b.islower()) or (a.isupper() and b.isupper()):
-        return 1
-    return 0
+    return a.isupper() == b.isupper() if a.isalpha() and b.isalpha() else -1
 
 
 print(same_case('C', 'B'), 1)

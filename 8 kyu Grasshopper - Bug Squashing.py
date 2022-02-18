0f3b212047745173e4e93bbad0c1dbc419b329ff
Paul <gafgafgarion05@gmail.com>
2022-02-18 15:14:24 +0300

    my solution

--- 8 kyu Grasshopper - Bug Squashing.py
+++ 8 kyu Grasshopper - Bug Squashing.py
@@ -1,14 +1,11 @@
-from preloaded import *
-
 health = 100
 position = 0
 coins = 0
 
-
 def main():
-    preloaded.combat()
-    preloaded.buy_health
-    preloaded.get_coins()
-    preloaded.print_status()
-    preloaded.roll_dice()
-    preloaded.move()
+    roll_dice()
+    move()
+    combat()
+    get_coins()
+    buy_health()
+    print_status()

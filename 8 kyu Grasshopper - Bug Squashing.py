f53ca22913168d1c9469914a73c73c938a03ad0e
Paul <gafgafgarion05@gmail.com>
2022-02-18 15:05:16 +0300

    7 errors from 21 tests

--- 8 kyu Grasshopper - Bug Squashing.py
+++ 8 kyu Grasshopper - Bug Squashing.py
@@ -5,10 +5,10 @@ position = 0
 coins = 0
 
 
-def(main):
-    combat()
-    buy_health
-    get_coins()
-    print_status()
-    roll_dice()
-    move()
+def main():
+    preloaded.combat()
+    preloaded.buy_health
+    preloaded.get_coins()
+    preloaded.print_status()
+    preloaded.roll_dice()
+    preloaded.move()

7ed39d6b0c3cf344d21aebf8a42f3361c8c7edc3
Paul <gafgafgarion05@gmail.com>
2022-02-19 12:38:36 +0300

    init

---
+++ 8 kyu For Twins 2. Math operations.py
@@ -0,0 +1,10 @@
+# 8 kyu
+# For Twins: 2. Math operations
+
+
+def ice_brick_volume(radius, bottle_length, rim_length):
+    pass
+
+
+print(ice_brick_volume(1, 10, 2), 16)
+print(ice_brick_volume(5, 30, 7), 1150)

1c8d91af69aa006996e0d06f280b451ff986d8f0
Paul <gafgafgarion05@gmail.com>
2022-02-19 09:57:24 +0300

    my solution

--- 8 kyu Pythagorean Triple.py
+++ 8 kyu Pythagorean Triple.py
@@ -1,6 +1,6 @@
 def pythagorean_triple(integers):
+    integers = sorted(integers)
     return integers[2] ** 2 == integers[0] ** 2 + integers[1] ** 2
-
 
 print(pythagorean_triple([3, 4, 5]), True)
 print(pythagorean_triple([3, 5, 9]), False)

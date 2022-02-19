dbff4992560a9c721d7447301aec9d2a693ef8d9
Paul <gafgafgarion05@gmail.com>
2022-02-19 10:46:54 +0300

    2

--- 8 kyu Subtract the Sum.py
+++ 8 kyu Subtract the Sum.py
@@ -1,11 +1,120 @@
-from math import prod
+lst_fruit = [
+    "kiwi",
+    "pear",
+    "kiwi",
+    "banana",
+    "melon",
+    "banana",
+    "melon",
+    "pineapple",
+    "apple",
+    "pineapple",
+    "cucumber",
+    "pineapple",
+    "cucumber",
+    "orange",
+    "grape",
+    "orange",
+    "grape",
+    "apple",
+    "grape",
+    "cherry",
+    "pear",
+    "cherry",
+    "pear",
+    "kiwi",
+    "banana",
+    "kiwi",
+    "apple",
+    "melon",
+    "banana",
+    "melon",
+    "pineapple",
+    "melon",
+    "pineapple",
+    "cucumber",
+    "orange",
+    "apple",
+    "orange",
+    "grape",
+    "orange",
+    "grape",
+    "cherry",
+    "pear",
+    "cherry",
+    "pear",
+    "apple",
+    "pear",
+    "kiwi",
+    "banana",
+    "kiwi",
+    "banana",
+    "melon",
+    "pineapple",
+    "melon",
+    "apple",
+    "cucumber",
+    "pineapple",
+    "cucumber",
+    "orange",
+    "cucumber",
+    "orange",
+    "grape",
+    "cherry",
+    "apple",
+    "cherry",
+    "pear",
+    "cherry",
+    "pear",
+    "kiwi",
+    "pear",
+    "kiwi",
+    "banana",
+    "apple",
+    "banana",
+    "melon",
+    "pineapple",
+    "melon",
+    "pineapple",
+    "cucumber",
+    "pineapple",
+    "cucumber",
+    "apple",
+    "grape",
+    "orange",
+    "grape",
+    "cherry",
+    "grape",
+    "cherry",
+    "pear",
+    "cherry",
+    "apple",
+    "kiwi",
+    "banana",
+    "kiwi",
+    "banana",
+    "melon",
+    "banana",
+    "melon",
+    "pineapple",
+    "apple",
+    "pineapple",
+]
 
 
-
-def subtract_sum(number):
-    number = list(str(number))
-    number = [int(i) for i in number]
-    return prod(number)
+# def subtract_sum(number):
+#     pass
 
 
-print(subtract_sum(10), "apple")
+def get_sum(number):
+    total = 0
+    while number > 0:
+        total += number % 10
+        number /= 10
+    return total
+
+
+# print(subtract_sum(10), "apple")
+print(get_sum(10))
+print(get_sum(321))
+print(get_sum(114567))

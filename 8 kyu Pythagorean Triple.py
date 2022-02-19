e974eab0f82b12d458978f47b28bf3cae2e3d55a
Paul <gafgafgarion05@gmail.com>
2022-02-19 10:00:35 +0300

    best practice more compact and readable

--- 8 kyu Pythagorean Triple.py
+++ 8 kyu Pythagorean Triple.py
@@ -1,6 +1,7 @@
 def pythagorean_triple(integers):
-    integers = sorted(integers)
-    return integers[2] ** 2 == integers[0] ** 2 + integers[1] ** 2
+    a, b, c = sorted(integers)
+    return c ** 2 == a ** 2 + b ** 2
+
 
 print(pythagorean_triple([3, 4, 5]), True)
 print(pythagorean_triple([3, 5, 9]), False)

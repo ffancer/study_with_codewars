8559821e5f6c006a4526b865aa9078410cd6ac3b
Paul <gafgafgarion05@gmail.com>
2022-02-19 16:07:09 +0300

    init

---
+++ 8 kyu Job Matching #1.py
@@ -0,0 +1,21 @@
+def match(candidate, job):
+    pass
+
+
+candidate1 = {'min_salary': 120000}
+candidate2 = {'min_salary': 190000}
+job1 = {'max_salary': 130000}
+job2 = {'max_salary': 80000}
+job3 = {'max_salary': 171000}
+
+print(match(candidate1, job1), True)
+print(match(candidate1, job3), True)
+print(match(candidate2, job3), True)
+
+print(match(candidate1, job2), False)
+print(match(candidate2, job1), False)
+print(match(candidate2, job2), False)
+
+print("Should throw error", lambda a: match({}, job2))
+
+print("Should throw error", lambda a: match(candidate1, {}))

4ebe4f47e82106a2620ce3b1e1532a33235d4d1b
Paul <gafgafgarion05@gmail.com>
2022-02-19 10:38:21 +0300

    1

--- 8 kyu Subtract the Sum.py
+++ 8 kyu Subtract the Sum.py
@@ -1,5 +1,11 @@
+from math import prod
+
+
+
 def subtract_sum(number):
-    pass
+    number = list(str(number))
+    number = [int(i) for i in number]
+    return prod(number)
 
 
 print(subtract_sum(10), "apple")

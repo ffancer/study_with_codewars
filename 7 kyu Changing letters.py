374ee8a3b662e8f9a4241fd45d2c5d6e513d6db4
Paul <gafgafgarion05@gmail.com>
2022-02-23 10:21:51 +0300

    my solution

--- 7 kyu Changing letters.py
+++ 7 kyu Changing letters.py
@@ -1,5 +1,14 @@
 def swap(st):
     vowels_lst = ['e', 'u', 'i', 'o', 'a']
+    ans = ''
+
+    for i in st:
+        if i in vowels_lst:
+            ans += i.upper()
+        else:
+            ans += i
+
+    return ans
 
 
 print(swap("HelloWorld!"), "HEllOWOrld!")

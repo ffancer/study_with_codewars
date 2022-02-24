7011de28704f65ad471439a95401efcf855c4448
Paul <gafgafgarion05@gmail.com>
2022-02-24 08:30:33 +0300

    make dictionary

--- 7 kyu The Office II - Boredom Score.py
+++ 7 kyu The Office II - Boredom Score.py
@@ -1,5 +1,14 @@
 def boredom(staff):
-    pass
+    dct = {'accounts': 1,
+           'finance': 2,
+           'canteen': 10,
+           'regulation': 3,
+           'trading': 6,
+           'change': 6,
+           'IS': 8,
+           'retail': 5,
+           'cleaning': 4,
+           'pissing about': 25}
 
 
 print(boredom({"tim": "change", "jim": "accounts",

7b041f8eeeef6d1c168ae7e1b934e9ea900b3b1b
Paul <gafgafgarion05@gmail.com>
2022-02-24 08:27:46 +0300

    init

---
+++ 7 kyu The Office II - Boredom Score.py
@@ -0,0 +1,16 @@
+def boredom(staff):
+    pass
+
+
+print(boredom({"tim": "change", "jim": "accounts",
+               "randy": "canteen", "sandy": "change", "andy": "change", "katie": "IS",
+               "laura": "change", "saajid": "IS", "alex": "trading", "john": "accounts",
+               "mr": "finance"}), "kill me now")
+print(boredom({"tim": "IS", "jim": "finance",
+               "randy": "pissing about", "sandy": "cleaning", "andy": "cleaning",
+               "katie": "cleaning", "laura": "pissing about", "saajid": "regulation",
+               "alex": "regulation", "john": "accounts", "mr": "canteen"}), "i can handle this")
+print(boredom({"tim": "accounts", "jim": "accounts",
+               "randy": "pissing about", "sandy": "finance", "andy": "change",
+               "katie": "IS", "laura": "IS", "saajid": "canteen", "alex": "pissing about",
+               "john": "retail", "mr": "pissing about"}), "party time!!")

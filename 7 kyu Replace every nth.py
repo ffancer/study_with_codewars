f9023a58aae5e068440f01ee036e33fded987d2b
Paul <gafgafgarion05@gmail.com>
2022-02-26 10:39:23 +0300

    init

---
+++ 7 kyu Replace every nth.py
@@ -0,0 +1,11 @@
+def replace_nth(text, n, old, new):
+    pass
+
+
+print(["Vader said: No, I am your father!", 2, 'a', 'o'])
+print(["Vader said: No, I am your father!", 4, 'a', 'o'])
+print(["Vader said: No, I am your father!", 6, 'a', 'o'])
+print(["Vader said: No, I am your father!", 0, 'a', 'o'])
+print(["Vader said: No, I am your father!", -2, 'a', 'o'])
+print(["Vader said: No, I am your father!", 1, 'i', 'y'])
+print(["Luke cries: Noooooooooooooooo!", 6, 'o', 'i'])

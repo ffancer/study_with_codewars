5d1bcfca1be19731597dde7d2048440a4e55b474
Paul <gafgafgarion05@gmail.com>
2022-02-26 08:34:04 +0300

    init

---
+++ 7 kyu Dot Calculator.py
@@ -0,0 +1,14 @@
+def calculator(txt):
+    # This is the place to code!
+    pass
+
+
+print(calculator("..... + ..............."), "....................")
+print(calculator("..... - ..."), "..")
+print(calculator("..... - ."), "....")
+print(calculator("..... * ..."), "...............")
+print(calculator("..... * .."), "..........")
+print(calculator("..... // .."), "..")
+print(calculator("..... // ."), ".....")
+print(calculator(". // .."), "")
+print(calculator(". - ."), "")

9abbb2c57c61be8614a84807b6a9110714db49d2
Paul <gafgafgarion05@gmail.com>
2022-02-26 10:42:39 +0300

    1

--- 7 kyu Replace every nth.py
+++ 7 kyu Replace every nth.py
@@ -1,11 +1,11 @@
 def replace_nth(text, n, old, new):
-    pass
+    return text.replace(old, new, n)
 
 
-print(["Vader said: No, I am your father!", 2, 'a', 'o'])
-print(["Vader said: No, I am your father!", 4, 'a', 'o'])
-print(["Vader said: No, I am your father!", 6, 'a', 'o'])
-print(["Vader said: No, I am your father!", 0, 'a', 'o'])
-print(["Vader said: No, I am your father!", -2, 'a', 'o'])
-print(["Vader said: No, I am your father!", 1, 'i', 'y'])
-print(["Luke cries: Noooooooooooooooo!", 6, 'o', 'i'])
+print(replace_nth("Vader said: No, I am your father!", 2, 'a', 'o'))
+print(replace_nth("Vader said: No, I am your father!", 4, 'a', 'o'))
+print(replace_nth("Vader said: No, I am your father!", 6, 'a', 'o'))
+print(replace_nth("Vader said: No, I am your father!", 0, 'a', 'o'))
+print(replace_nth("Vader said: No, I am your father!", -2, 'a', 'o'))
+print(replace_nth("Vader said: No, I am your father!", 1, 'i', 'y'))
+print(replace_nth("Luke cries: Noooooooooooooooo!", 6, 'o', 'i'))

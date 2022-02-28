4f8e109cbdc5d6a057fc0da85a8b182140d61ab2
Paul <gafgafgarion05@gmail.com>
2022-02-28 09:38:05 +0300

    1

--- 7 kyu Calculate Two People's Individual Ages.py
+++ 7 kyu Calculate Two People's Individual Ages.py
@@ -1,6 +1,7 @@
 def get_ages(sum_, difference):
-    return
-
+    old = sum_ // 2 + difference // 2
+    young = sum_ // 2 - difference // 2
+    return old, young
 
 print(get_ages(24, 4), (14, 10))
 print(get_ages(63, -14), None)

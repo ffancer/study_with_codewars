77823c2ca62d2eccb0edcf0a8dcd7b9c1c202bca
Paul <gafgafgarion05@gmail.com>
2022-03-01 08:52:50 +0300

    init

---
+++ 7 kyu Consecutive items.py
@@ -0,0 +1,7 @@
+def consecutive(arr, a, b):
+    pass
+
+
+print(consecutive([1, 3, 5, 7], 3, 7), False)
+print(consecutive([1, 3, 5, 7], 3, 1), True)
+print(consecutive([1, 6, 9, -3, 4, -78, 0], -3, 4), True)

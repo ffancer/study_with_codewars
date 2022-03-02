e6a4597da761ca7728d0316fc490fd4dcef945fc
Paul <gafgafgarion05@gmail.com>
2022-03-02 13:03:51 +0300

    init

---
+++ 7 kyu Count the Characters.py
@@ -0,0 +1,9 @@
+def count_char:
+    pass
+
+
+print(count_char("Hello there", "e"), 3)
+print(count_char("Hello there", "t"), 1)
+print(count_char("Hello there", "h"), 2)
+print(count_char("Hello there", "L"), 2)
+print(count_char("Hello there", " "), 1)

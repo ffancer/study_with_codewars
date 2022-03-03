b864b32f10b48c4a6c7e4f4b9eb6714cac900add
Paul <gafgafgarion05@gmail.com>
2022-03-03 09:17:35 +0300

    best practice bad kata, this is not 7 kyu

--- 7 kyu Correct the time-string.py
+++ 7 kyu Correct the time-string.py
@@ -1,10 +1,24 @@
 def time_correct(t):
-    if sum(i.isdigit() for i in t) == 6 and t.count(':') == 2:
-        return True
-    return False
+    try:
+        if t == "":
+            return ""
+        if len(t) != 8:
+            return None
+        h, m, s = [int(x) for x in t.split(":")]
+        if s >= 60:
+            s -= 60
+            m += 1
+        if m >= 60:
+            m -= 60
+            h += 1
+        if h >= 24:
+            h = h % 24
+        return '%02d:%02d:%02d' % (h, m, s)
+    except:
+        return None
 
 
-# print(time_correct(None), None)
+print(time_correct(None), None)
 print(time_correct(""), "")
 print(time_correct("001122"), None)
 print(time_correct("00;11;22"), None)

1ad50eb40912d7677f85d23030faf6f28dde57ff
Paul <gafgafgarion05@gmail.com>
2022-03-04 10:25:31 +0300

    my solution

--- 7 kyu filterEvenLengthWords.py
+++ 7 kyu filterEvenLengthWords.py
@@ -1,5 +1,11 @@
 def filter_even_length_words(words):
-    pass
+    lst = []
+
+    for i in words:
+        if len(i) % 2 == 0:
+            lst.append(i)
+
+    return lst
 
 
 print(filter_even_length_words(["Hello", "World"]), [])

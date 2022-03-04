335ec69fd4b1cc71914d873fea1d19016b3958b3
Paul <gafgafgarion05@gmail.com>
2022-03-04 10:20:43 +0300

    my solution

--- 7 kyu Arithmetic sequence - sum of n elements.py
+++ 7 kyu Arithmetic sequence - sum of n elements.py
@@ -1,5 +1,11 @@
 def arithmetic_sequence_sum(a, r, n):
-    pass
+    lst = []
+
+    for i in range(n):
+        lst.append(a + r * i)
+
+    return sum(lst)
+
 
 print(arithmetic_sequence_sum(3, 2, 20), 440)
 print(arithmetic_sequence_sum(2, 2, 10), 110)

f5c05b18b62b64d1bf8bd91a4c209ddab1ec2f52
Paul <gafgafgarion05@gmail.com>
2022-03-04 10:31:27 +0300

    init

---
+++ 7 kyu String Merge!.py
@@ -0,0 +1,8 @@
+def string_merge(string1, string2, letter):
+    pass
+
+
+print(string_merge("hello", "world", "l"), "held")
+print(string_merge("coding", "anywhere", "n"), "codinywhere")
+print(string_merge("jason", "samson", "s"), "jasamson")
+print(string_merge("wonderful", "people", "e"), "wondeople")

8b49d6f8b49ccef1dbaf7c863e0c24042e0b13de
Paul <gafgafgarion05@gmail.com>
2022-03-04 10:18:12 +0300

    init

---
+++ 7 kyu Arithmetic sequence - sum of n elements.py
@@ -0,0 +1,6 @@
+def arithmetic_sequence_sum(a, r, n):
+    pass
+
+print(arithmetic_sequence_sum(3, 2, 20), 440)
+print(arithmetic_sequence_sum(2, 2, 10), 110)
+print(arithmetic_sequence_sum(1, -2, 10), -80)

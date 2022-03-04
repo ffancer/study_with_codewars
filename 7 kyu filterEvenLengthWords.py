7bdf3f181502b1724595185442cfaa4550f25ed4
Paul <gafgafgarion05@gmail.com>
2022-03-04 10:25:20 +0300

    init

---
+++ 7 kyu filterEvenLengthWords.py
@@ -0,0 +1,6 @@
+def filter_even_length_words(words):
+    pass
+
+
+print(filter_even_length_words(["Hello", "World"]), [])
+print(filter_even_length_words(["One", "Two", "Three", "Four"]), ["Four"])

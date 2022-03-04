eb2500f9acff73c1cca8549132955624a5b9217a
Paul <gafgafgarion05@gmail.com>
2022-03-04 10:45:34 +0300

    init

---
+++ 7 kyu Plus - minus - plus - plus - ... - Count.py
@@ -0,0 +1,8 @@
+def catch_sign_change(lst):
+    pass
+
+
+print(catch_sign_change([1, 3, 4, 5]), 0)
+print(catch_sign_change([1, -3, -4, 0, 5]), 2)
+print(catch_sign_change([]), 0)
+print(catch_sign_change([-47, 84, -30, -11, -5, 74, 77]), 3)

77faf45ebcbc620cec685e0acb5cbfc11cccb0b6
Paul <gafgafgarion05@gmail.com>
2022-03-04 10:33:23 +0300

    2

--- 7 kyu String Merge!.py
+++ 7 kyu String Merge!.py
@@ -1,6 +1,6 @@
 def string_merge(string1, string2, letter):
-    return string1.find(letter), string2.find(letter)
-
+    # return string1.find(letter), string2.find(letter)
+    return string1[:string1.find(letter)], string2[string2.find(letter):]
 
 print(string_merge("hello", "world", "l"), "held")
 print(string_merge("coding", "anywhere", "n"), "codinywhere")

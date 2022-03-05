ec9e7baec9e18067d35625902c09d1e7977eb056
Paul <gafgafgarion05@gmail.com>
2022-03-05 10:23:55 +0300

    3

--- 7 kyu Fizz Buzz Cuckoo Clock.py
+++ 7 kyu Fizz Buzz Cuckoo Clock.py
@@ -3,7 +3,16 @@ def fizz_buzz_cuckoo_clock(time):
     time = [int(i) for i in time]
     hour = time[0]
     minute = time[1]
-    return hour, minute
+    # if minute == 0:
+    #     return 'Cuckoo' * hour
+    # if minute == 30:
+    #     return 'Cuckoo'
+    if minute % 15 == 0:
+        return "Fizz Buzz"
+    if minute % 3 == 0:
+        return "Fizz"
+    if minute % 5 == 0:
+        return "Buzz"
 
 print(fizz_buzz_cuckoo_clock("13:34"), "tick")
 print(fizz_buzz_cuckoo_clock("21:00"), "Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo")

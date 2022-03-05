e41baf65ffd6ff2a4b688d2150c7088f5108b784
Paul <gafgafgarion05@gmail.com>
2022-03-05 10:52:28 +0300

    init

---
+++ 7 kyu Very Even Numbers..py
@@ -0,0 +1,15 @@
+def is_very_even_number(n):
+    return True
+
+
+print(is_very_even_number(0), True)
+print(is_very_even_number(4), True)
+print(is_very_even_number(12), False)
+print(is_very_even_number(222), True)
+print(is_very_even_number(5), False)
+print(is_very_even_number(45), False)
+print(is_very_even_number(4554), False)
+print(is_very_even_number(1234), False)
+print(is_very_even_number(88), False)
+print(is_very_even_number(24), True)
+print(is_very_even_number(400000220), True)

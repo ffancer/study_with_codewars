cdd7bb50aec5a7a7478f6cbea66046283e283cef
Paul <gafgafgarion05@gmail.com>
2022-03-05 18:07:51 +0300

    init

---
+++ 7 kyu How many times should I go.py
@@ -0,0 +1,11 @@
+# 7 kyu
+# How many times should I go?
+
+
+def how_many_times(annual_price, individual_price):
+    pass
+
+
+print(how_many_times(40, 15), 3)
+print(how_many_times(30, 10), 3)
+print(how_many_times(80, 15), 6)

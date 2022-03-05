a1848594ca8c15445e3f266cd6642b8e2eb045bd
Paul <gafgafgarion05@gmail.com>
2022-03-05 10:37:25 +0300

    init

---
+++ 7 kyu Frequency sequence.py
@@ -0,0 +1,7 @@
+def freq_seq(s, sep):
+    pass
+
+
+print(freq_seq('hello world', '-'), '1-1-3-3-2-1-1-2-1-3-1')
+print(freq_seq('19999999', ':'), '1:7:7:7:7:7:7:7')
+print(freq_seq('^^^**$', 'x'), '3x3x3x2x2x1')

9855aead7555548ed8e2c327b5b79ba02975ecee
Paul <gafgafgarion05@gmail.com>
2022-03-05 10:09:51 +0300

    init

---
+++ 7 kyu Fizz Buzz Cuckoo Clock.py
@@ -0,0 +1,14 @@
+def fizz_buzz_cuckoo_clock(time):
+    pass
+
+
+print(fizz_buzz_cuckoo_clock("13:34"), "tick")
+print(fizz_buzz_cuckoo_clock("21:00"), "Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo")
+print(fizz_buzz_cuckoo_clock("11:15"), "Fizz Buzz")
+print(fizz_buzz_cuckoo_clock("03:03"), "Fizz")
+print(fizz_buzz_cuckoo_clock("14:30"), "Cuckoo")
+print(fizz_buzz_cuckoo_clock("08:55"), "Buzz")
+print(fizz_buzz_cuckoo_clock("00:00"),
+      "Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo")
+print(fizz_buzz_cuckoo_clock("12:00"),
+      "Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo")

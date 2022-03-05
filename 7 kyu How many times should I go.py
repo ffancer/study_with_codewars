9cc37beee683f854ce04d6365872a70481272b99
Paul <gafgafgarion05@gmail.com>
2022-03-05 18:10:38 +0300

    my solution

--- 7 kyu How many times should I go.py
+++ 7 kyu How many times should I go.py
@@ -1,9 +1,10 @@
 # 7 kyu
 # How many times should I go?
+from math import ceil
 
 
 def how_many_times(annual_price, individual_price):
-    pass
+    return ceil(annual_price / individual_price)
 
 
 print(how_many_times(40, 15), 3)

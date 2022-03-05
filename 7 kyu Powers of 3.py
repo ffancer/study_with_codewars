324e943fe9cc62959d4e636013698003b9f29733
Paul <gafgafgarion05@gmail.com>
2022-03-05 10:49:03 +0300

    best solution

--- 7 kyu Powers of 3.py
+++ 7 kyu Powers of 3.py
@@ -1,8 +1,10 @@
-from math import ceil, log
-
-
 def largest_power(N):
-    return ceil(log(N, 3)) - 1
+    i = 0
+
+    while 3 ** i < N:
+        i += 1
+
+    return i - 1
 
 
 print(largest_power(3), 0)

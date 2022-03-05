4279af7dbfa86bdc4db92a61469b89afbd94cb65
Paul <gafgafgarion05@gmail.com>
2022-03-05 18:16:42 +0300

    my solution

--- 7 kyu Scrolling Text.py
+++ 7 kyu Scrolling Text.py
@@ -1,5 +1,14 @@
 def scrolling_text(text):
-    return ['GOOD LUCK!', 'OOD LUCK!G', '...']
+    text = text.upper()
+    first_letter = text[0]
+    lst = []
+
+    while len(lst) != len(text):
+        lst.append(text)
+        text = text[1:] + first_letter
+        first_letter = text[0]
+
+    return lst
 
 
 print(scrolling_text("abc"), ["ABC", "BCA", "CAB"])

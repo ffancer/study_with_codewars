0bac46256826ba3467dc70dd4c2ac7602ce8ec1b
Paul <gafgafgarion05@gmail.com>
2022-03-07 10:35:39 +0300

    2

--- 7 kyu Calculate mean and concatenate string.py
+++ 7 kyu Calculate mean and concatenate string.py
@@ -2,7 +2,13 @@ def mean(lst):
     letters = ''
     digits = []
 
+    for i in lst:
+        if i.isdigit():
+            digits.append(int(i))
+        else:
+            letters += i
 
+    return letters, digits
 
 lst = ["u", "6", "d", "1", "i", "w", "6", "s", "t", "4", "a", "6", "g", "1", "2", "w", "8", "o", "2", "0"]
 print(mean(lst), [3.6, "udiwstagwo"])

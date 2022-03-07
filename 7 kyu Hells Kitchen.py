b1e60299fbad8f82c149e8acba25e4e1d25133de
Paul <gafgafgarion05@gmail.com>
2022-03-07 10:07:20 +0300

    my solution

--- 7 kyu Hells Kitchen.py
+++ 7 kyu Hells Kitchen.py
@@ -1,11 +1,11 @@
 def gordon(a):
     lst = []
     a = a.split()
-    # vowels = ['a', 'e', 'i', 'o', 'u']
 
     for i in a:
         lst.append(i.upper().replace('A', '@').replace('E', '*').replace('I', '*').replace('O', '*').replace('U', '*') + '!!!!')
-    return lst
+
+    return ' '.join(lst)
 
 print(gordon('What feck damn cake'), 'WH@T!!!! F*CK!!!! D@MN!!!! C@K*!!!!')
 print(gordon('are you stu pid'), '@R*!!!! Y**!!!! ST*!!!! P*D!!!!')

6b9b1674d3a7050123e05bcf1ff6e9e4559deb07
Paul <gafgafgarion05@gmail.com>
2022-03-07 09:58:15 +0300

    init

---
+++ 7 kyu Hells Kitchen.py
@@ -0,0 +1,10 @@
+def gordon(a):
+    # your code here
+    return ''
+
+
+print(gordon('What feck damn cake'), 'WH@T!!!! F*CK!!!! D@MN!!!! C@K*!!!!')
+print(gordon('are you stu pid'), '@R*!!!! Y**!!!! ST*!!!! P*D!!!!')
+print(gordon('i am a chef'), '*!!!! @M!!!! @!!!! CH*F!!!!')
+print(gordon('dont you talk tome'), 'D*NT!!!! Y**!!!! T@LK!!!! T*M*!!!!')
+print(gordon('how dare you feck'), 'H*W!!!! D@R*!!!! Y**!!!! F*CK!!!!')

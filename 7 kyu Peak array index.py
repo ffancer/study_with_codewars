5617020c76346d46cb663567505c41fe2085fd2e
Paul <gafgafgarion05@gmail.com>
2022-03-07 10:25:11 +0300

    my solution

--- 7 kyu Peak array index.py
+++ 7 kyu Peak array index.py
@@ -1,8 +1,11 @@
 def peak(arr):
     for i in range(len(arr)):
-        # left_sum = sum(arr[:i])
-        right_sum = sum(arr[:i+1])
-        print(right_sum)
+        left_sum = sum(arr[:i])
+        right_sum = sum(arr[i + 1:])
+        if left_sum == right_sum:
+            return i
+    return -1
+
 
 print(peak([1, 2, 3, 5, 3, 2, 1]), 3)
 print(peak([1, 12, 3, 3, 6, 3, 1]), 2)

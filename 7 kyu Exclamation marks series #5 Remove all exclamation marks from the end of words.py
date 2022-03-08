33049b8e4b118df3b16d3b11c176c159562634ac
Paul <gafgafgarion05@gmail.com>
2022-03-08 15:14:37 +0300

    init

---
+++ 7 kyu Exclamation marks series #5 Remove all exclamation marks from the end of words.py
@@ -0,0 +1,14 @@
+# 7 kyu
+# Exclamation marks series #5: Remove all exclamation marks from the end of words
+
+
+def remove(s):
+    pass
+
+
+print(remove('Hi!'), 'Hi')
+print(remove('Hi!!!'), 'Hi')
+print(remove('!Hi'), '!Hi')
+print(remove('!Hi!'), '!Hi')
+print(remove('Hi! Hi!'), 'Hi Hi')
+print(remove('!!!Hi !!hi!!! !hi'), '!!!Hi !!hi !hi')

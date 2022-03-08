39bf0721813ff0d8e4eb95ef998760a736d8d466
Paul <gafgafgarion05@gmail.com>
2022-03-08 15:31:49 +0300

    init

---
+++ 7 kyu TV Remote.py
@@ -0,0 +1,12 @@
+def tv_remote(word):
+    # Your code here!!
+    pass
+
+
+print(tv_remote("does"), 16)
+print(tv_remote("your"), 23)
+print(tv_remote("solution"), 33)
+print(tv_remote("work"), 20)
+print(tv_remote("for"), 12)
+print(tv_remote("these"), 27)
+print(tv_remote("words"), 25)

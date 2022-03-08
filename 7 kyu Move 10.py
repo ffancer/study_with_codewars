ef3991d84b36b211c2d3d5068d894d5d3223793b
Paul <gafgafgarion05@gmail.com>
2022-03-08 15:44:15 +0300

    init

---
+++ 7 kyu Move 10.py
@@ -0,0 +1,10 @@
+def move_ten(st):
+    pass
+
+
+print(move_ten("testcase"), "docdmkco")
+print(move_ten("codewars"), "mynogkbc")
+print(move_ten("exampletesthere"), "ohkwzvodocdrobo")
+print(move_ten("returnofthespacecamel"), "bodebxypdroczkmomkwov")
+print(move_ten("bringonthebootcamp"), "lbsxqyxdrolyydmkwz")
+print(move_ten("weneedanofficedog"), "goxoonkxyppsmonyq")

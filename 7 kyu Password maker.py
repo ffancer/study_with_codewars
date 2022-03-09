a3fbfa647db26dbbe097747389d05bfd84b8192c
Paul <gafgafgarion05@gmail.com>
2022-03-09 10:34:03 +0300

    init

---
+++ 7 kyu Password maker.py
@@ -0,0 +1,7 @@
+def make_password(phrase):
+    pass
+
+
+print(make_password("Give me liberty or give me death"), "Gml0gmd",
+      "Wrong output for 'Give me liberty or give me death'")
+print(make_password("Keep Calm and Carry On"), "KCaC0", "Wrong output for 'Keep Calm and Carry On'")

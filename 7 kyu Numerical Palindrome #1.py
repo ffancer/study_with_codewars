63d784133738526ffb2a429ec1e4868e4fb0f4a3
Paul <gafgafgarion05@gmail.com>
2022-03-09 10:44:34 +0300

    init

---
+++ 7 kyu Numerical Palindrome #1.py
@@ -0,0 +1,9 @@
+def palindrome(num):
+    pass
+
+
+print(palindrome(1221), True)
+print(palindrome(123322), False)
+print(palindrome("ACCDDCCA"), "Not valid")
+print(palindrome("1221"), "Not valid")
+print(palindrome(-450), "Not valid")

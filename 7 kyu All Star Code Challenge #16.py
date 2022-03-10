e36586a399f197acb230e7f22f7989758c40f3ca
Paul <gafgafgarion05@gmail.com>
2022-03-10 09:18:16 +0300

    init

---
+++ 7 kyu All Star Code Challenge #16.py
@@ -0,0 +1,9 @@
+def no_repeat(string):
+    pass
+
+
+print(no_repeat("aabbccdde"), "e")
+print(no_repeat("wxyz"), "w")
+print(no_repeat("testing"), "e")
+print(no_repeat("codewars"), "c")
+print(no_repeat("Testing"), "T")

ace36df1d6c027ae9bf39e5e7dec7e6b27127a21
Paul <gafgafgarion05@gmail.com>
2022-03-10 09:22:52 +0300

    init

---
+++ 7 kyu Simple string reversal II.py
@@ -0,0 +1,9 @@
+def solve(st, a, b):
+    pass
+
+
+print(solve("codewars", 1, 5), "cawedors")
+print(solve("codingIsFun", 2, 100), "conuFsIgnid")
+print(solve("FunctionalProgramming", 2, 15), "FuargorPlanoitcnmming")
+print(solve("abcefghijklmnopqrstuvwxyz", 0, 20), "vutsrqponmlkjihgfecbawxyz")
+print(solve("abcefghijklmnopqrstuvwxyz", 5, 20), "abcefvutsrqponmlkjihgwxyz")

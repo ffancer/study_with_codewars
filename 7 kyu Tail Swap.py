e2a792548e1ddf5d766bbad493a8b3c0ea0b7a8c
Paul <gafgafgarion05@gmail.com>
2022-03-10 09:38:57 +0300

    init

---
+++ 7 kyu Tail Swap.py
@@ -0,0 +1,8 @@
+def tail_swap(strings):
+    pass
+
+
+print(tail_swap(['abc:123', 'cde:456']),
+      ['abc:456', 'cde:123'])
+print(tail_swap(['a:12345', '777:xyz']),
+      ['a:xyz', '777:12345'])

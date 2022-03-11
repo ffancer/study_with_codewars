b23d8f9b4b362ebe543337c9d9e658ce83642cbc
Paul <gafgafgarion05@gmail.com>
2022-03-11 10:03:49 +0300

    init

---
+++ 7 kyu Debug the functions EASY.py
@@ -0,0 +1,16 @@
+def multi(l_st):
+    return l_st * l_st
+
+
+def add(l_st):
+    return l_st + l_st
+
+
+def reverse(string):
+    return string.reverse
+
+
+print(multi([8, 2, 5]), 80)
+print(add([1, 15, 3]), 19)
+print(add([7, 8, 6, 5, 4, 9]), 39)
+print(reverse("Hello World"), "dlroW olleH")

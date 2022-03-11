b85c759a81da4265310e10afb6224992bfdc0ed5
Paul <gafgafgarion05@gmail.com>
2022-03-11 10:49:13 +0300

    2

--- 7 kyu Sum of Array Averages.py
+++ 7 kyu Sum of Array Averages.py
@@ -2,7 +2,7 @@ def sum_average(arr):
     lst = []
     for i in arr:
         lst.append(sum(i) / len(i))
-    return lst
+    return sum(lst)
 
 print(sum_average([[1, 2, 2, 1], [2, 2, 2, 1]]), 3)
 print(sum_average([[52, 64, 84, 21, 54], [44, 87, 46, 90, 43]]), 117)

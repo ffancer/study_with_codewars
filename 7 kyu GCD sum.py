56b26529a918f5f70434e5493aeca85850168498
Paul <gafgafgarion05@gmail.com>
2022-03-11 10:21:59 +0300

    1

--- 7 kyu GCD sum.py
+++ 7 kyu GCD sum.py
@@ -1,7 +1,14 @@
 def solve(s, g):
-    pass
+    lst = []
+
+    for i in range(s):
+        if i % g == 0:
+            lst.append(i)
+
+    return -1 if s % g != 0 else lst[1], max(lst)
 
 
+print(solve(12, 4), (4, 8))
 print(solve(6, 3), (3, 3))
 print(solve(8, 2), (2, 6))
 print(solve(10, 3), -1)

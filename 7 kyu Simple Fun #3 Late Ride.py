deeb3c02d6862262f8b2a214d2ae079f09cfe732
Paul <gafgafgarion05@gmail.com>
2022-03-11 10:30:02 +0300

    init

---
+++ 7 kyu Simple Fun #3 Late Ride.py
@@ -0,0 +1,14 @@
+# 7 kyu
+# Simple Fun #3: Late Ride
+
+
+def late_ride(n):
+    pass
+
+
+print(late_ride(240), 4)
+print(late_ride(808), 14)
+print(late_ride(1439), 19)
+print(late_ride(0), 0)
+print(late_ride(23), 5)
+print(late_ride(8), 8)

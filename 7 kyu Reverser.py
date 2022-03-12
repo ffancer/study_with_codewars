e9eb96e97efe834766fd17968164db1817596f6c
Paul <gafgafgarion05@gmail.com>
2022-03-12 16:01:17 +0300

    my solution

--- 7 kyu Reverser.py
+++ 7 kyu Reverser.py
@@ -1,6 +1,13 @@
 def reverse(n):
-    return int(str(n)[::-1])
+    ans = 0
 
+    while n > 0:
+        digit = n % 10
+        n = n // 10
+        ans = ans * 10
+        ans = ans + digit
+
+    return ans
 
 print(reverse(1234), 4321)
 print(reverse(10987), 78901)

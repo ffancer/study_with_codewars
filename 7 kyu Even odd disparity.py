fd2b337192b1c807bc5616eeb34eae29f944dc45
Paul <gafgafgarion05@gmail.com>
2022-03-12 10:30:01 +0300

    init

---
+++ 7 kyu Even odd disparity.py
@@ -0,0 +1,9 @@
+def solve(a):
+    pass
+
+
+print(solve([0, 1, 2, 3]), 0)
+print(solve([0, 1, 2, 3, 'a', 'b']), 0)
+print(solve([0, 15, 'z', 16, 'm', 13, 14, 'c', 9, 10, 13, 'u', 4, 3]), 0)
+print(solve([13, 6, 8, 15, 4, 8, 13]), 1)
+print(solve([1, 'a', 17, 8, 'e', 3, 'i', 12, 1]), -2)

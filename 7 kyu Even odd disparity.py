2f0da866c4a0115a35ce5432ccdfd045a03228a8
Paul <gafgafgarion05@gmail.com>
2022-03-12 10:34:00 +0300

    1

--- 7 kyu Even odd disparity.py
+++ 7 kyu Even odd disparity.py
@@ -1,5 +1,14 @@
 def solve(a):
-    pass
+    odd_cnt, even_cnt = 0,0
+
+
+    for i, j in enumerate(a):
+        if i % 2 != 0:
+            odd_cnt += 1
+        else:
+            even_cnt += 1
+
+    return odd_cnt, even_cnt
 
 
 print(solve([0, 1, 2, 3]), 0)

6ce6bac8b198d51fa0f25086fcc691c820e84b85
Paul <gafgafgarion05@gmail.com>
2022-03-12 10:48:31 +0300

    init

---
+++ 7 kyu Borrower Speak.py
@@ -0,0 +1,7 @@
+def borrow(s):
+    return ""
+
+
+print(borrow('WhAt! FiCK! DaMn CAke?'), 'whatfickdamncake')
+print(borrow('THE big PeOpLE Here!!'), 'thebigpeoplehere')
+print(borrow('i AM a TINY BoY!!'), 'iamatinyboy')

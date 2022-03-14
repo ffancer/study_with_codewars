8c71ee92f20a45da1de717c54856fb6ad5bedd3a
Paul <gafgafgarion05@gmail.com>
2022-03-14 13:13:12 +0300

    my solution

--- 7 kyu Hit Count.py
+++ 7 kyu Hit Count.py
@@ -1,16 +1,10 @@
 def counter_effect(hit_count):
-    # lst = []
-    #
-    # for i in hit_count:
-    #     lst2 = []
-    #     for j in range(0, int(i)+1):
-    #         lst2.append(j)
-    #
-    # return lst2
     hit_count = [int(i) for i in hit_count]
     lst = []
+
     for i in hit_count:
         lst.append([j for j in range(0, i + 1)])
+
     return lst
 
 

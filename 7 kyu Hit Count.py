d3ffb06b8602e5f2285006f38170bb386ccead0f
Paul <gafgafgarion05@gmail.com>
2022-03-14 13:03:51 +0300

    init

---
+++ 7 kyu Hit Count.py
@@ -0,0 +1,7 @@
+def counter_effect(hit_count):
+    pass
+
+
+print(counter_effect("1250"), [[0, 1], [0, 1, 2], [0, 1, 2, 3, 4, 5], [0]])
+print(counter_effect("0050"), [[0], [0], [0, 1, 2, 3, 4, 5], [0]])
+print(counter_effect('0000'), [[0], [0], [0], [0]])

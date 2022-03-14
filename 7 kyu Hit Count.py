d1db83124fb550f0a1330946aea88fa981250b0c
Paul <gafgafgarion05@gmail.com>
2022-03-14 13:12:19 +0300

    4

--- 7 kyu Hit Count.py
+++ 7 kyu Hit Count.py
@@ -8,7 +8,12 @@ def counter_effect(hit_count):
     #
     # return lst2
     hit_count = [int(i) for i in hit_count]
-    return hit_count
+    lst = []
+    for i in hit_count:
+        lst.append([j for j in range(0, i + 1)])
+    return lst
+
+
 print(counter_effect("1250"), [[0, 1], [0, 1, 2], [0, 1, 2, 3, 4, 5], [0]])
 print(counter_effect("0050"), [[0], [0], [0, 1, 2, 3, 4, 5], [0]])
 print(counter_effect('0000'), [[0], [0], [0], [0]])

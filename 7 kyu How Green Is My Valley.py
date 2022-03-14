e5cc9dd1c18cf0b7ff67544fc1bc781d3ae66aab
Paul <gafgafgarion05@gmail.com>
2022-03-14 10:44:13 +0300

    my solution so spaghetti xD

--- 7 kyu How Green Is My Valley.py
+++ 7 kyu How Green Is My Valley.py
@@ -3,20 +3,28 @@
 
 
 def make_valley(arr):
-    left_wing_lst, right_wing_lst = [], []
-    middle_lst = [min(arr)]
-    arr.remove(min(arr))
+    left_wing_lst, right_wing_lst, middle_lst = [], [], []
 
-    while len(arr) != 0:
-        left_wing_lst.append(max(arr))
-        arr.remove(max(arr))
-        right_wing_lst.append(max(arr))
-        arr.remove(max(arr))
-    return left_wing_lst + middle_lst + right_wing_lst[::-1]
+    if len(arr) % 2 != 0:
+        middle_lst = [min(arr)]
+        arr.remove(min(arr))
+        while len(arr) > 1:
+            left_wing_lst.append(max(arr))
+            arr.remove(max(arr))
+            right_wing_lst.append(max(arr))
+            arr.remove(max(arr))
+        return left_wing_lst + middle_lst + right_wing_lst[::-1]
+    else:
+        while len(arr) > 1:
+            left_wing_lst.append(max(arr))
+            arr.remove(max(arr))
+            right_wing_lst.append(max(arr))
+            arr.remove(max(arr))
+        return left_wing_lst + right_wing_lst[::-1]
 
 
 print(make_valley([17, 17, 15, 14, 8, 7, 7, 5, 4, 4, 1]), [17, 15, 8, 7, 4, 1, 4, 5, 7, 14, 17])
-# print(make_valley([20, 7, 6, 2]), [20, 6, 2, 7])
+print(make_valley([20, 7, 6, 2]), [20, 6, 2, 7])
 print(make_valley([14, 10, 8]), [14, 8, 10])
 print(make_valley([20, 18, 17, 13, 12, 12, 10, 9, 4, 2, 2, 1, 1]), [20, 17, 12, 10, 4, 2, 1, 1, 2, 9, 12, 13, 18])
 print(make_valley([20, 16, 14, 10, 1]), [20, 14, 1, 10, 16])

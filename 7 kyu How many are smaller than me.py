9a288aaa5eddeaa82c6c73fc30f63cd265066e92
Paul <gafgafgarion05@gmail.com>
2022-03-14 13:32:49 +0300

    1

--- 7 kyu How many are smaller than me.py
+++ 7 kyu How many are smaller than me.py
@@ -3,7 +3,19 @@
 
 
 def smaller(arr):
-    pass
+    cnt = 0
+    i, j = 0, 1
+    lst = []
+    while j < len(arr):
+        if arr[i] > arr[j]:
+            cnt += 1
+            j += 1
+        lst.append(cnt)
+        cnt = 0
+        i += 1
+        j += 1
+        arr = arr[i:]
+    return cnt
 
 
 print(smaller([5, 4, 3, 2, 1]), [4, 3, 2, 1, 0])

216892199de6657002f8c549cdefe808e9f46a61
Paul <gafgafgarion05@gmail.com>
2022-03-14 13:18:52 +0300

    init

---
+++ 7 kyu How many are smaller than me.py
@@ -0,0 +1,14 @@
+# 7 kyu
+# How many are smaller than me?
+
+
+def smaller(arr):
+    pass
+
+
+print(smaller([5, 4, 3, 2, 1]), [4, 3, 2, 1, 0])
+print(smaller([1, 2, 3]), [0, 0, 0])
+print(smaller([1, 2, 0]), [1, 1, 0])
+print(smaller([1, 2, 1]), [0, 1, 0])
+print(smaller([1, 1, -1, 0, 0]), [3, 3, 0, 0, 0])
+print(smaller([5, 4, 7, 9, 2, 4, 4, 5, 6]), [4, 1, 5, 5, 0, 0, 0, 0, 0])

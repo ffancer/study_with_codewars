a6ab65466ef98c730b13bb5ac143547b2806f447
Paul <gafgafgarion05@gmail.com>
2022-03-15 10:20:32 +0300

    2

--- 7 kyu Driving Licence.py
+++ 7 kyu Driving Licence.py
@@ -1,6 +1,8 @@
 def driver(data):
-    first = data[2][:6].upper()
-    return first
+    first = data[2][:6].upper().ljust(5, '9')
+    second = data[3].split('-')[2][2]
+    return first, second
+
 
 data = ["John", "James", "Smith", "01-Jan-2000", "M"]
 print(driver(data), "SMITH001010JJ9AA")

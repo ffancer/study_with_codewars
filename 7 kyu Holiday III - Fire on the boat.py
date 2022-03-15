459290d3cc39aa154d5f190cbadb7401accf3a78
Paul <gafgafgarion05@gmail.com>
2022-03-15 09:46:09 +0300

    init

---
+++ 7 kyu Holiday III - Fire on the boat.py
@@ -0,0 +1,10 @@
+def fire_fight(s):
+    pass
+
+
+print(fire_fight("Boat Rudder Mast Boat Hull Water Fire Boat Deck Hull Fire Propeller Deck Fire Deck Boat Mast"),
+      "Boat Rudder Mast Boat Hull Water ~~ Boat Deck Hull ~~ Propeller Deck ~~ Deck Boat Mast")
+print(fire_fight("Mast Deck Engine Water Fire"),
+      "Mast Deck Engine Water ~~")
+print(fire_fight("Fire Deck Engine Sail Deck Fire Fire Fire Rudder Fire Boat Fire Fire Captain"),
+      "~~ Deck Engine Sail Deck ~~ ~~ ~~ Rudder ~~ Boat ~~ ~~ Captain")

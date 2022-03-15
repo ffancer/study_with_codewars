f999ca0424754dbb996bf3313e9126018bc78913
Paul <gafgafgarion05@gmail.com>
2022-03-15 14:55:35 +0300

    init

---
+++ 7 kyu Is It Negative Zero (-0).py
@@ -0,0 +1,18 @@
+def is_negative_zero(n):
+    pass
+
+
+print(is_negative_zero(-0.0), True)
+print(is_negative_zero(-(float("inf"))), False)
+print(is_negative_zero(-5.0), False)
+print(is_negative_zero(-4.0), False)
+print(is_negative_zero(-3.0), False)
+print(is_negative_zero(-2.0), False)
+print(is_negative_zero(-1.0), False)
+print(is_negative_zero(0.0), False)
+print(is_negative_zero(1.0), False)
+print(is_negative_zero(2.0), False)
+print(is_negative_zero(3.0), False)
+print(is_negative_zero(4.0), False)
+print(is_negative_zero(5.0), False)
+print(is_negative_zero(float("inf")), False)

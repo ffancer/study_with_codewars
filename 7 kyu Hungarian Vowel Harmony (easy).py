e370747c49833d6d5088d85055e98ec5a8c91150
Paul <gafgafgarion05@gmail.com>
2022-03-15 09:08:42 +0300

    init

---
+++ 7 kyu Hungarian Vowel Harmony (easy).py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+def dative(word):
+    pass
+
+
+print(dative(u"ablak"), u"ablaknak")
+print(dative(u"tükör"), u"tükörnek")
+print(dative(u"keret"), u"keretnek")
+print(dative(u"otthon"), u"otthonnak")
+print(dative(u"virág"), u"virágnak")
+print(dative(u"tett"), u"tettnek")
+print(dative(u"rokkant"), u"rokkantnak")
+print(dative(u"rossz"), u"rossznak")
+print(dative(u"gonosz"), u"gonosznak")
+print(dative(u"rög"), u"rögnek")
+print(dative(u"király"), u"királynak")

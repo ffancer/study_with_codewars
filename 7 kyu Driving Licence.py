837f6ba3cec8e73f4cc6967f4bfd1d1ea9559e66
Paul <gafgafgarion05@gmail.com>
2022-03-15 10:15:15 +0300

    init

---
+++ 7 kyu Driving Licence.py
@@ -0,0 +1,10 @@
+def driver(data):
+    pass
+
+
+data = ["John", "James", "Smith", "01-Jan-2000", "M"]
+print(driver(data), "SMITH001010JJ9AA")
+data = ["Johanna", "", "Gibbs", "13-Dec-1981", "F"]
+print(driver(data), "GIBBS862131J99AA")
+data = ["Andrew", "Robert", "Lee", "02-September-1981", "M"]
+print(driver(data), "LEE99809021AR9AA")

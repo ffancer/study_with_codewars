640488c71bb07d466d90da487a4dc926b1f1af7a
Paul <gafgafgarion05@gmail.com>
2022-03-15 09:57:42 +0300

    init

---
+++ 7 kyu Santa's Naughty List.py
@@ -0,0 +1,10 @@
+def find_children(santas_list, children):
+    pass
+
+
+print(find_children(["Jason", "Jackson", "Jordan", "Johnny"], ["Jason", "Jordan", "Jennifer"]),
+      ["Jason", "Jordan"], 'Does not pass basic test')
+print(find_children(["Jason", "Jackson", "Johnson", "JJ"], ["Jason", "James", "JJ"]), ["JJ", "Jason"],
+      'Does not pass sorting test')
+print(find_children(["jASon", "JAsoN", "JaSON", "jasON"], ["JasoN", "jASOn", "JAsoN", "jASon", "JASON"]),
+      ["JAsoN", "jASon"], 'Does not pass capitalization test')
